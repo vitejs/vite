bb9baa2f61136a7083c92ef67c92eb727eba3b40
Evan You <yyx990803@gmail.com>
2020-04-21 10:36:10 -0400

    fix: use dependency vue if user has no local installation

--- src/server/moduleResolver.ts
+++ src/server/moduleResolver.ts
@@ -10,39 +10,55 @@ export function resolveModule(id: string, cwd: string, res: ServerResponse) {
   let sourceMapPath: string | undefined = undefined
   // TODO support custom imports map e.g. for snowpack web_modules
 
-  if (id.endsWith('.map')) {
-    sourceMapPath = id
-    id = fileToIdMap.get(id.replace(/\.map$/, ''))
-    if (!id) {
-      res.statusCode = 404
-      res.end()
-      return
-    }
-  }
-
-  // fallback to node resolve
-  try {
-    modulePath = resolve(cwd, `${id}/package.json`)
-    if (id === 'vue') {
+  if (id === 'vue') {
+    // special handling for vue
+    try {
+      // see if user has local vue installation
       modulePath = path.join(
-        path.dirname(modulePath),
+        path.dirname(resolve(cwd, 'vue')),
         'dist/vue.runtime.esm-browser.js'
       )
-    } else {
-      // module resolved, try to locate its "module" entry
-      const pkg = require(modulePath)
-      modulePath = path.join(path.dirname(modulePath), pkg.module || pkg.main)
-      fileToIdMap.set(path.basename(modulePath), id)
-      // this is a source map request.
-      if (sourceMapPath) {
-        modulePath = path.join(path.dirname(modulePath), sourceMapPath)
+    } catch (e) {
+      // user has no local vue, use vite's dependency version
+      modulePath = require.resolve('vue/dist/vue.runtime.esm-browser.js')
+    }
+    sendJSStream(res, modulePath)
+  } else {
+    // handle source map requests
+    if (id.endsWith('.map')) {
+      sourceMapPath = id
+      id = fileToIdMap.get(id.replace(/\.map$/, ''))
+      if (!id) {
+        res.statusCode = 404
+        res.end()
+        return
       }
     }
 
-    sendJSStream(res, modulePath)
-  } catch (e) {
-    console.error(e)
-    res.statusCode = 404
-    res.end()
+    // fallback to node resolve
+    try {
+      modulePath = resolve(cwd, `${id}/package.json`)
+      if (id === 'vue') {
+        modulePath = path.join(
+          path.dirname(modulePath),
+          'dist/vue.runtime.esm-browser.js'
+        )
+      } else {
+        // module resolved, try to locate its "module" entry
+        const pkg = require(modulePath)
+        modulePath = path.join(path.dirname(modulePath), pkg.module || pkg.main)
+        fileToIdMap.set(path.basename(modulePath), id)
+        // this is a source map request.
+        if (sourceMapPath) {
+          modulePath = path.join(path.dirname(modulePath), sourceMapPath)
+        }
+      }
+
+      sendJSStream(res, modulePath)
+    } catch (e) {
+      console.error(e)
+      res.statusCode = 404
+      res.end()
+    }
   }
 }
