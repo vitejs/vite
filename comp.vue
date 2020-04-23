f490dd37c55d7ccc3bfe388a4e8bf7f9e906ae96
周良华 <zhoulianghua@zuoyebang.com>
2020-04-23 10:57:25 +0800

    add test file

---
+++ comp.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="count++">{{ count }}</button>
+</template>
+
+<script>
+console.log(12345);
+export default {
+  data: () => ({ count: 1 })
+}
+</script>
+
+<style scoped>
+button { color: red }
+</style>
