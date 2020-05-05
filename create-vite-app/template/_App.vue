d22bcce243a966c1c8c654358daa1a25ed8a2d2a
selvakumardhivakar <selvakumardhivakar@gmail.com>
2020-05-05 18:37:53 +0530

    :lipstick: Change _App.vue

--- create-vite-app/template/_App.vue
+++ create-vite-app/template/_App.vue
@@ -1,10 +1,9 @@
 <template>
-  <h1>Hello Vite + Vue 3!</h1>
-  <p>Edit ./App.vue to test hot module replacement (HMR).</p>
-  <p>
-    <span>Count is: {{ count }}</span>
-    <button @click="count++">increment</button>
-  </p>
+  <div>
+    <h1 class="title">Hello Vite + Vue 3!</h1>
+    <p>Count: {{ count }}</p>
+    <button @click="count++">Increment</button>
+  </div>
 </template>
 
 <script>
@@ -15,10 +14,18 @@ export default {
 
 <style scoped>
 h1 {
+  font-size: 2rem;
   color: #4fc08d;
 }
-
-h1, p {
+h1,
+p {
+  text-align: center;
   font-family: Arial, Helvetica, sans-serif;
+}
+
+button {
+  margin: 0 auto;
+  display: block;
+  padding: 0.5rem 0.75rem;
 }
 </style>
