decbfc2ee9e0c88c9e94a8f4f39032cdf5b5d6c5
Evan You <yyx990803@gmail.com>
2020-05-08 22:58:23 -0400

    feat(cva): support multiple templates

---
+++ create-vite-app/template-preact/main.jsx
@@ -0,0 +1,7 @@
+import { h, render } from 'preact'
+
+function MyComponent(props) {
+  return <div>{props.msg}</div>
+}
+
+render(<MyComponent msg="Hello Preact!" />, document.getElementById('app'))
