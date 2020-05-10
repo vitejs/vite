02491a4be84cce43a4c84598e4a51b9b247d0b71
Evan You <yyx990803@gmail.com>
2020-05-10 18:17:55 -0400

    feat(cva): support creating project in cwd (close #111, #112)

--- create-vite-app/index.js
+++ create-vite-app/index.js
@@ -9,25 +9,23 @@ const readdir = promisify(fs.readdir)
 const argv = require('minimist')(process.argv.slice(2))
 
 async function init() {
-  const targetDir = argv._[0]
-  if (!targetDir) {
-    console.error(
-      `Error: Please specify target directory. Example: create-vite-app my-project`
-    )
-    return
-  }
-  const root = path.join(process.cwd(), targetDir || '')
+  const targetDir = argv._[0] || '.'
+  const cwd = process.cwd()
+  const root = path.join(cwd, targetDir)
   console.log(`Scaffolding project in ${root}...`)
 
   try {
     await mkdir(root)
   } catch (e) {
     if (e.code === 'EEXIST') {
-      console.error(`Error: target directory already exists.`)
+      const files = await readdir(root)
+      if (files.length) {
+        console.error(`Error: target directory is not empty.`)
+        process.exit(1)
+      }
     } else {
       throw e
     }
-    return
   }
 
   const templateDir = path.join(__dirname, `template-${argv.template || 'vue'}`)
@@ -46,12 +44,13 @@ async function init() {
   }
 
   const pkg = require(path.join(templateDir, `package.json`))
-  pkg.name = targetDir
+  pkg.name = path.basename(root)
   await write('package.json', JSON.stringify(pkg, null, 2))
 
-  console.log(`Done. Now run: `)
-  console.log()
-  console.log(`  cd ${path.relative(process.cwd(), root)}`)
+  console.log(`\nDone. Now run:\n`)
+  if (root !== cwd) {
+    console.log(`  cd ${path.relative(cwd, root)}`)
+  }
   console.log(`  npm install (or \`yarn\`)`)
   console.log(`  npm run dev (or \`yarn dev\`)`)
   console.log()
