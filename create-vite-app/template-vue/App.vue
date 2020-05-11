8558a6d5994bf387080a7ded20f71047ab9f028f
Evan You <yyx990803@gmail.com>
2020-05-11 16:03:33 -0400

    cva: tweak vue template

--- create-vite-app/template-vue/App.vue
+++ create-vite-app/template-vue/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./logo.png">
-    <HelloWorld msg="Hello Vue 3.0 + Vite"/>
-  </div>
+  <img alt="Vue logo" src="./logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
@@ -15,14 +13,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
