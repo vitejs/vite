9a3164610ef35643e5dca523c2f002f33eecf235
Evan You <yyx990803@gmail.com>
2020-05-14 09:43:47 -0400

    workflow: enable source map for debugging

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -3,6 +3,10 @@ const argv = require('minimist')(process.argv.slice(2))
 // make sure to set debug flag before requiring anything
 if (argv.debug) {
   process.env.DEBUG = `vite:` + (argv.debug === true ? '*' : argv.debug)
+  try {
+    // this is only present during local development
+    require('source-map-support').install()
+  } catch (e) {}
 }
 
 import os from 'os'
