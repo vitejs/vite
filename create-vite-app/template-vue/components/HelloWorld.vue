0571e2249f649c00cae5068c58f91b85163b3338
Ramnath Shenoy <bekalshenoy@gmail.com>
2020-05-16 08:34:13 +0530

    Update HelloWorld.vue

--- create-vite-app/template-vue/components/HelloWorld.vue
+++ create-vite-app/template-vue/components/HelloWorld.vue
@@ -17,21 +17,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
