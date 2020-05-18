b8f6b5a36e612ecd84307706b9d273e1b14e984a
Ramnath Shenoy <bekalshenoy@gmail.com>
2020-05-19 03:08:06 +0530

    cva: update vue template (#151)

--- create-vite-app/template-vue/components/HelloWorld.vue
+++ create-vite-app/template-vue/components/HelloWorld.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button @click="count++">count is: {{ count }}</button>
-    <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
-  </div>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
 </template>
 
 <script>
@@ -19,21 +17,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
