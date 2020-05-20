318529f77be29bae63c3e4921453378f1389dee7
Evan You <yyx990803@gmail.com>
2020-05-20 13:34:07 -0400

    fix: only do path check if full reload is caused by html edit

--- src/client/client.ts
+++ src/client/client.ts
@@ -131,10 +131,18 @@ socket.addEventListener('message', async ({ data }) => {
       }
       break
     case 'full-reload':
-      if (
-        location.pathname === path ||
-        (location.pathname === '/' && path === '/index.html')
-      ) {
+      if (path.endsWith('.html')) {
+        // if html file is edited, only reload the page if the browser is
+        // currently on that page.
+        const pagePath = location.pathname
+        if (
+          pagePath === path ||
+          (pagePath.endsWith('/') && pagePath + 'index.html' === path)
+        ) {
+          location.reload()
+        }
+        return
+      } else {
         location.reload()
       }
   }
