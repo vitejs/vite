4f6b3dfb40d2095e3f00a8c7f347db6140569771
Evan You <yyx990803@gmail.com>
2020-05-25 11:21:34 -0400

    revert cwd/root change

--- src/node/config.ts
+++ src/node/config.ts
@@ -267,22 +267,21 @@ const debug = require('debug')('vite:config')
 
 export async function resolveConfig(
   mode: string,
-  specifiedRoot?: string,
   configPath?: string
 ): Promise<ResolvedConfig | undefined> {
   const start = Date.now()
-  const root = specifiedRoot || process.cwd()
+  const cwd = process.cwd()
   let config: ResolvedConfig | undefined
   let resolvedPath: string | undefined
   let isTS = false
   if (configPath) {
-    resolvedPath = path.resolve(root, configPath)
+    resolvedPath = path.resolve(cwd, configPath)
   } else {
-    const jsConfigPath = path.resolve(root, 'vite.config.js')
+    const jsConfigPath = path.resolve(cwd, 'vite.config.js')
     if (fs.existsSync(jsConfigPath)) {
       resolvedPath = jsConfigPath
     } else {
-      const tsConfigPath = path.resolve(root, 'vite.config.ts')
+      const tsConfigPath = path.resolve(cwd, 'vite.config.ts')
       if (fs.existsSync(tsConfigPath)) {
         isTS = true
         resolvedPath = tsConfigPath
@@ -334,14 +333,8 @@ export async function resolveConfig(
     }
 
     // normalize config root to absolute
-    if (config.root) {
-      if (!specifiedRoot) {
-        if (!path.isAbsolute(config.root)) {
-          config.root = path.resolve(config.root)
-        }
-      } else {
-        debug(`configured root option will be ignored`)
-      }
+    if (config.root && !path.isAbsolute(config.root)) {
+      config.root = path.resolve(path.dirname(resolvedPath), config.root)
     }
 
     // resolve plugins
@@ -352,7 +345,7 @@ export async function resolveConfig(
     }
 
     // load environment variables
-    const env = loadEnv(mode, root)
+    const env = loadEnv(mode, cwd)
     debug(`env: %O`, env)
     config.env = env
 
