9d40966882284f3a6370107c670c02ec3ad5276e
likui <2218301630@qq.com>
2020-05-26 14:23:46 +0800

    feat: skip unused assets watch apply

--- src/node/server/serverPluginAssets.ts
+++ src/node/server/serverPluginAssets.ts
@@ -1,13 +1,29 @@
 import { ServerPlugin } from '.'
 import { isImportRequest, isStaticAsset } from '../utils'
 
-export const assetPathPlugin: ServerPlugin = ({ app }) => {
+const usedAssetsSet = new Set<string>()
+
+export const assetPathPlugin: ServerPlugin = ({ app, resolver, watcher }) => {
   app.use(async (ctx, next) => {
-    if (isStaticAsset(ctx.path) && isImportRequest(ctx)) {
-      ctx.type = 'js'
-      ctx.body = `export default ${JSON.stringify(ctx.path)}`
-      return
+    if (isStaticAsset(ctx.path)) {
+      if (isImportRequest(ctx)) {
+        ctx.type = 'js'
+        ctx.body = `export default ${JSON.stringify(ctx.path)}`
+        return
+      }
+      usedAssetsSet.add(ctx.path)
     }
     return next()
+  })
+
+  watcher.on('change', (filePath) => {
+    if (isStaticAsset(filePath)) {
+      const publicPath = resolver.fileToRequest(filePath)
+
+      // skip unused
+      if (!usedAssetsSet.has(publicPath)) return
+
+      watcher.handleJSReload(filePath)
+    }
   })
 }
