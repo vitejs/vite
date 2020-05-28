24b75a166122fe068628cf489e94d9d41a383236
Evan You <yyx990803@gmail.com>
2020-05-28 10:30:59 -0400

    chore: remove debug log [ci skip]

--- test/test.js
+++ test/test.js
@@ -148,7 +148,6 @@ describe('vite', () => {
           () => browserLogs[browserLogs.length - 1],
           'js module hot updated:  /testHmrManual.js'
         )
-        console.log(browserLogs)
         expect(
           browserLogs.slice(browserLogs.length - 4, browserLogs.length - 1)
         ).toEqual([
