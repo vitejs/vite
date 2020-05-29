8d2f6226c21c66134f50f85bc8207bc9970f2c47
likui <2218301630@qq.com>
2020-05-29 10:57:25 +0800

    fix: add network cert

--- src/node/utils/certificate.ts
+++ src/node/utils/certificate.ts
@@ -1,72 +1,79 @@
 // https://github.com/webpack/webpack-dev-server/blob/master/lib/utils/createCertificate.js
 import path from 'path'
 import fs from 'fs-extra'
+import { getIpV4Addresses } from './getIpV4Addresses'
 
 const certificateTtl = 30
 const dayMs = 1000 * 60 * 60 * 24
 
 function createCertificate() {
-  const pems = require('selfsigned').generate(null, {
-    algorithm: 'sha256',
-    days: certificateTtl,
-    keySize: 2048,
-    extensions: [
-      // {
-      //   name: 'basicConstraints',
-      //   cA: true,
-      // },
-      {
-        name: 'keyUsage',
-        keyCertSign: true,
-        digitalSignature: true,
-        nonRepudiation: true,
-        keyEncipherment: true,
-        dataEncipherment: true
-      },
-      {
-        name: 'extKeyUsage',
-        serverAuth: true,
-        clientAuth: true,
-        codeSigning: true,
-        timeStamping: true
-      },
-      {
-        name: 'subjectAltName',
-        altNames: [
-          {
-            // type 2 is DNS
-            type: 2,
-            value: 'localhost'
-          },
-          {
-            type: 2,
-            value: 'localhost.localdomain'
-          },
-          {
-            type: 2,
-            value: 'lvh.me'
-          },
-          {
-            type: 2,
-            value: '*.lvh.me'
-          },
-          {
-            type: 2,
-            value: '[::1]'
-          },
-          {
-            // type 7 is IP
-            type: 7,
-            ip: '127.0.0.1'
-          },
-          {
-            type: 7,
-            ip: 'fe80::1'
-          }
-        ]
-      }
-    ]
-  })
+  const pems = require('selfsigned').generate(
+    [{ name: 'commonName', value: 'localhost' }],
+    {
+      algorithm: 'sha256',
+      days: certificateTtl,
+      keySize: 2048,
+      extensions: [
+        // {
+        //   name: 'basicConstraints',
+        //   cA: true,
+        // },
+        {
+          name: 'keyUsage',
+          keyCertSign: true,
+          digitalSignature: true,
+          nonRepudiation: true,
+          keyEncipherment: true,
+          dataEncipherment: true
+        },
+        {
+          name: 'extKeyUsage',
+          serverAuth: true,
+          clientAuth: true,
+          codeSigning: true,
+          timeStamping: true
+        },
+        {
+          name: 'subjectAltName',
+          altNames: [
+            {
+              // type 2 is DNS
+              type: 2,
+              value: 'localhost'
+            },
+            {
+              type: 2,
+              value: 'localhost.localdomain'
+            },
+            {
+              type: 2,
+              value: 'lvh.me'
+            },
+            {
+              type: 2,
+              value: '*.lvh.me'
+            },
+            {
+              type: 2,
+              value: '[::1]'
+            },
+            {
+              // type 7 is IP
+              type: 7,
+              ip: '127.0.0.1'
+            },
+            ...getIpV4Addresses().map((network) => {
+              return { type: 7, ip: network.address }
+            }),
+            {
+              type: 7,
+              ip: 'fe80::1'
+            }
+          ]
+        }
+      ]
+    }
+  )
   return pems.private + pems.cert
 }
 
---
+++ src/node/utils/getIpV4Addresses.ts
@@ -0,0 +1,10 @@
+import os, { NetworkInterfaceInfoIPv4 } from 'os'
+
+export function getIpV4Addresses(): NetworkInterfaceInfoIPv4[] {
+  return Object.values(os.networkInterfaces())
+    .filter((i) => !!i)
+    .reduce((pre, cur) => (pre = pre!.concat(cur!)), [])!
+    .filter((d) => d.family === 'IPv4') as NetworkInterfaceInfoIPv4[]
+}
+
+console.log(getIpV4Addresses())
