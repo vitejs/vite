ea39ca222b324e3c9a3c259acc77e011f6b1766d
likui <2218301630@qq.com>
2020-05-30 22:33:32 +0800

    test: css modules `@import` hmr

--- playground/css-@import/TestCssAtImport.vue
+++ playground/css-@import/TestCssAtImport.vue
@@ -6,14 +6,28 @@
   <div class="script-at-import">
     &lt;script @import &gt; this should be green
   </div>
+  <h2>CSS Modules @import</h2>
+  <div id="sfc-style-css-module-at-import" :class="$style['sfc-style-css-module-at-import']">
+    &lt;style module @import &gt; this should be red
+  </div>
+  <div id="sfc-script-css-module-at-import" :class="imported['sfc-script-css-module-at-import']">
+    &lt;script module.css @import &gt; this should be green
+  </div>
 </template>
 
 <style scoped>
 @import './testCssAtImportFromStyle.css';
 </style>
 
+<style module>
+@import './testCssModuleAtImportFromStyle.module.css';
+</style>
+
 <script>
 import './testCssAtImportFromScript.css'
+import imported from './testCssModuleAtImportFromScript.module.css'
 
-export default {}
+export default {
+  data: () => ({ imported })
+}
 </script>
