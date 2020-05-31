24278b1fd73dfabb741b35fc6964244a20a84794
kazuya kawaguchi <kawakazu80@gmail.com>
2020-05-31 23:43:33 +0900

    fix: type

--- playground/custom-blocks/i18nServerPlugin.js
+++ playground/custom-blocks/i18nServerPlugin.js
@@ -12,7 +12,7 @@ export const i18nServerPlugin = ({ root, app, server, watcher }) => {
       if (/ya?ml/.test(lang)) {
         resource = yaml.safeLoad(source.trim())
       } else {
-        resource = JSON.parse(srouce.trim())
+        resource = JSON.parse(source.trim())
       }
       let code = `export default function i18n(Component) {\n`
       code += `  Component.i18n = ${JSON.stringify(resource)}\n`
