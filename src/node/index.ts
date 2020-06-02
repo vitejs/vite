c400d2a7e79eb4c493dc91e67751acca8756459b
Evan You <yyx990803@gmail.com>
2020-06-02 15:35:41 -0400

    fix: inject global preambles without breaking doctype
    fix #221

--- src/node/index.ts
+++ src/node/index.ts
@@ -2,4 +2,10 @@ export * from './server'
 export * from './build'
 export * from './depOptimizer'
 export * from './config'
-export { readBody, cachedRead, isStaticAsset, isImportRequest } from './utils'
+export {
+  readBody,
+  cachedRead,
+  isStaticAsset,
+  isImportRequest,
+  injectScriptToHtml
+} from './utils'
