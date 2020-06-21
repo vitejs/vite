baca22fa771ed567cf06a721794ad23b363b892a
likui <2218301630@qq.com>
2020-06-21 11:02:13 +0800

    fix(dev): should use real file path as relative root
    fix #421

--- src/node/resolver.ts
+++ src/node/resolver.ts
@@ -293,7 +293,10 @@ export function createResolver(
       let dirname = path.dirname(publicPath)
       for (const alias in literalDirAlias) {
         if (publicPath.startsWith(alias)) {
-          const relativeFilePath = path.relative(root, literalDirAlias[alias])
+          const relativeFilePath = path.relative(
+            resolver.requestToFile(publicPath),
+            literalDirAlias[alias]
+          )
           if (!relativeFilePath.startsWith('..')) {
             dirname = '/' + slash(relativeFilePath)
           }
