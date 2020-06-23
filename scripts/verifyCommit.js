505fddd4c948358b6af79271e1629c6d39ed5add
Evan You <yyx990803@gmail.com>
2020-06-23 10:18:24 -0400

    workflow: update commit verify script

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -5,7 +5,7 @@ const msgPath = process.env.GIT_PARAMS
 const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 
 const releaseRE = /^v\d/
-const commitRE = /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps): .{1,50}/
+const commitRE = /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
 
 if (!releaseRE.test(msg) && !commitRE.test(msg)) {
   console.log()
