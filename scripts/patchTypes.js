d44cf64d3a71b3e49d21099631740c672107b09f
Evan You <yyx990803@gmail.com>
2020-06-24 23:11:58 -0400

    build: fix build script + package entries

--- scripts/patchTypes.js
+++ scripts/patchTypes.js
@@ -1,6 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-const indexPath = path.resolve(__dirname, '../dist/index.d.ts')
+// copy file
+const metaPath = path.resolve(__dirname, '../importMeta.d.ts')
+fs.writeFileSync(
+  path.resolve(__dirname, '../dist/importMeta.d.ts'),
+  fs.readFileSync(metaPath)
+)
+
+// inject import
+const indexPath = path.resolve(__dirname, '../dist/node/index.d.ts')
 const content = fs.readFileSync(indexPath, 'utf-8')
 fs.writeFileSync(indexPath, content + `\nimport '../importMeta'`)
