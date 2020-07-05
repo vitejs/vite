2d37d518d0abafcc5eb25dbccf7426f3c329c879
likui <2218301630@qq.com>
2020-07-05 20:27:56 +0800

    fix: disable vite internal `rollup-plugin-vue`

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import { vuePlugin } from './serverPlugin'
 export = {
   resolvers: [resolver],
   configureServer: vuePlugin,
+  enableRollupPluginVue: false,
   rollupInputOptions: {
     plugins: [require('rollup-plugin-vue')({})],
   },
