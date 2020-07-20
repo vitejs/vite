74819fbd73bea7a8ba976368c3b504d2d21e24dd
likui <2218301630@qq.com>
2020-07-20 17:28:59 +0800

    fix: fix test

--- src/node/resolver.ts
+++ src/node/resolver.ts
@@ -254,7 +254,7 @@ export function createResolver(
 
       // example id: "@babel/runtime/helpers/esm/slicedToArray"
       // see the test case: /playground/TestNormalizePublicPath.vue
-      const id = publicPath.replace(moduleRE, '')
+      const id = cleanPublicPath.replace(moduleRE, '')
       const { scope, name, inPkgPath } = parseNodeModuleId(id)
       if (!inPkgPath) return publicPath
       let filePathPostFix = ''
--- src/node/server/serverPluginModuleRewrite.ts
+++ src/node/server/serverPluginModuleRewrite.ts
@@ -22,7 +22,13 @@ import {
   hmrDirtyFilesMap,
   latestVersionsMap
 } from './serverPluginHmr'
-import { readBody, cleanUrl, isExternalUrl, bareImportRE } from '../utils'
+import {
+  readBody,
+  cleanUrl,
+  isExternalUrl,
+  bareImportRE,
+  removeQueryTimestamp
+} from '../utils'
 import chalk from 'chalk'
 import { isCSSRequest } from '../utils/cssUtils'
 import { envPublicPath } from './serverPluginEnv'
@@ -76,7 +82,9 @@ export const moduleRewritePlugin: ServerPlugin = ({
         // before we perform hmr analysis.
         // on the other hand, static import is guaranteed to have extension
         // because they must all have gone through module rewrite.
-        const importer = resolver.normalizePublicPath(ctx.path)
+        const importer = removeQueryTimestamp(
+          resolver.normalizePublicPath(ctx.url)
+        )
         ctx.body = rewriteImports(
           root,
           content!,
