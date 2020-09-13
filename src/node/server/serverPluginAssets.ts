ee1b1810ed479145bc7b6fa3d356592d33fdf081
Ian Purvis <ian@purvisresearch.com>
2020-09-13 13:22:25 -0500

    refactor: Add AssetPathPluginOptions interface
      `include` defaults to existing isStaticAsset function

--- src/node/server/serverPluginAssets.ts
+++ src/node/server/serverPluginAssets.ts
@@ -1,10 +1,16 @@
 import { ServerPlugin } from '.'
 import { isImportRequest, isStaticAsset } from '../utils'
 
-export const createAssetPathPlugin = (): ServerPlugin => {
+interface AssetPathPluginOptions {
+  include?: (file: string) => boolean
+}
+
+export const createAssetPathPlugin = ({
+  include = isStaticAsset
+}: AssetPathPluginOptions = {}): ServerPlugin => {
   return ({ app }) => {
     app.use(async (ctx, next) => {
-      if (isStaticAsset(ctx.path) && isImportRequest(ctx)) {
+      if (include(ctx.path) && isImportRequest(ctx)) {
         ctx.type = 'js'
         ctx.body = `export default ${JSON.stringify(ctx.path)}`
         return
