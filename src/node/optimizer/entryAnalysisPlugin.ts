463079e478ec0519ae1f9d532a7588cbadeeb490
Sirui Chen <632882184@qq.com>
2020-09-20 22:21:56 +0800

    fix: delete unused code

--- src/node/optimizer/entryAnalysisPlugin.ts
+++ src/node/optimizer/entryAnalysisPlugin.ts
@@ -29,29 +29,3 @@ export function entryAnalysisPlugin(): Plugin {
     }
   }
 }
-
-async function isCjs(filePath: string) {}
-
-// function getCjsExports(
-//   filePath: string,
-//   result: { [exportName: string]: true }
-// ) {
-//   const content = fs.readFileSync(filePath, 'utf-8')
-//   let parseResult: { exports: string[]; reexports: string[] }
-//   try {
-//     parseResult = parseCjs(content)
-//   } catch (error) {
-//     // not a commonjs module
-//     return result
-//   }
-//   const { exports: cjsExports, reexports: cjsReExports } = parseResult
-//   cjsExports.forEach((exportName: string) => {
-//     result[exportName] = true
-//   })
-//   cjsReExports.forEach((cjsReExport: string) => {
-//     // Example : react/index.js re-export from "./cjs/react.production.min.js"
-//     const reExportsFiles = resolveFrom(path.dirname(filePath), cjsReExport)
-//     getCjsExports(reExportsFiles, result)
-//   })
-//   return result
-// }
