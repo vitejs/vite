16dbf4d7b46f8bd10de6749095e41a5c160ef9bd
Ian Purvis <ian@purvisresearch.com>
2020-09-29 07:45:00 -0500

    refactor: Reference resolver via asset path plugin args

--- src/node/server/serverPluginAssets.ts
+++ src/node/server/serverPluginAssets.ts
@@ -1,9 +1,9 @@
 import { ServerPlugin } from '.'
 import { isImportRequest } from '../utils'
 
-export const assetPathPlugin: ServerPlugin = ({ app }) => {
+export const assetPathPlugin: ServerPlugin = ({ app, resolver }) => {
   app.use(async (ctx, next) => {
-    if (ctx.resolver.isAssetRequest(ctx.path) && isImportRequest(ctx)) {
+    if (resolver.isAssetRequest(ctx.path) && isImportRequest(ctx)) {
       ctx.type = 'js'
       ctx.body = `export default ${JSON.stringify(ctx.path)}`
       return
