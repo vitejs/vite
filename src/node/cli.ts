f0e58b90af613daf8b3d467e2e6d5763b5ecec94
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-11-13 11:48:53 -0500

    fix(cli): bunch of changes

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -29,20 +29,20 @@ cli
     '--jsx',
     `['vue' | 'preact' | 'react']  choose jsx preset (default: 'vue')`
   )
-  .option('--jsx-factory', '[string]  (default: React.createElement)')
-  .option('--jsx-fragment', '[string]  (default: React.Fragment)')
+  .option('--jsx-factory', `[string]  (default: React.createElement)`)
+  .option('--jsx-fragment', `[string]  (default: React.Fragment)`)
 
 // serve
 cli
   .command('[root]') // default command
   .alias('serve')
-  .option('--port', '[number]  port to listen to')
+  .option('--port', `[number]  port to listen to`)
   .option(
     '--force',
-    '[boolean]  force the optimizer to ignore the cache and re-bundle'
+    `[boolean]  force the optimizer to ignore the cache and re-bundle`
   )
-  .option('--open', '[boolean]  open browser on server start')
-  .action(async (root, argv) => {
+  .option('--open', `[boolean]  open browser on server start`)
+  .action(async (root: string, argv: any) => {
     if (root) {
       argv.root = root
     }
@@ -53,27 +53,27 @@ cli
 // build
 cli
   .command('build [root]')
-  .option('--entry', '[string]  entry file for build (default: index.html)')
-  .option('--outDir', '[string]  output directory (default: dist)')
-  .option('--ssr', '[boolean]  build for server-side rendering')
+  .option('--entry', `[string]  entry file for build (default: index.html)`)
+  .option('--outDir', `[string]  output directory (default: dist)`)
+  .option('--ssr', `[boolean]  build for server-side rendering`)
   .option(
     '--assetsDir',
-    '[string]  directory under outDir to place assets in (default: _assets)'
+    `[string]  directory under outDir to place assets in (default: _assets)`
   )
   .option(
     '--sourcemap',
-    '[boolean]  output source maps for build (default: false)'
+    `[boolean]  output source maps for build (default: false)`
   )
   .option(
     '--minify',
     `[boolean | 'terser' | 'esbuild']  enable/disable minification, or specify minifier to use (default: terser)`
   )
-  .option('--base', '[string]  public base path (default: /)')
+  .option('--base', `[string]  public base path (default: /)`)
   .option(
     '--assetsInlineLimit',
     `[number]  static asset base64 inline threshold in bytes (default: 4096)`
   )
-  .action(async (root, argv) => {
+  .action(async (root: string, argv: any) => {
     if (root) {
       argv.root = root
     }
@@ -86,13 +86,12 @@ cli
   .command('optimize [root]')
   .option(
     '--force',
-    '[boolean]  force the optimizer to ignore the cache and re-bundle'
+    `[boolean]  force the optimizer to ignore the cache and re-bundle`
   )
-  .action(async (root, argv) => {
+  .action(async (root: string, argv: any) => {
     if (root) {
       argv.root = root
     }
-    console.log(root, argv)
     const options = await resolveOptions({ argv, defaultMode: 'development' })
     return runOptimize(options)
   })
@@ -105,7 +104,7 @@ async function resolveOptions({
   argv,
   defaultMode
 }: {
-  argv: Partial<UserConfig> & any
+  argv: Partial<UserConfig> & { [key: string]: any }
   defaultMode: string
 }): Promise<UserConfig> {
   // cast xxx=true | false into actual booleans
@@ -131,7 +130,11 @@ async function resolveOptions({
     )
   }
 
-  argv = makeJsxObject(argv)
+  const jsxFactory = argv['jsx-factory']
+  const jsxFragment = argv['jsx-fragment']
+  if (jsxFactory || jsxFragment) {
+    argv.jsx = { factory: jsxFactory, fragment: jsxFragment }
+  }
 
   const userConfig = await resolveConfig(
     argv.mode || defaultMode,
@@ -144,16 +147,6 @@ async function resolveOptions({
     }
   }
 
-  return argv
-}
-
-function makeJsxObject(argv: any) {
-  if (argv['jsxFactory']) {
-    ;(argv.jsx || (argv.jsx = {})).factory = argv['jsx-factory']
-  }
-  if (argv['jsxFragment']) {
-    ;(argv.jsx || (argv.jsx = {})).fragment = argv['jsx-fragment']
-  }
   return argv
 }
 
@@ -181,8 +174,8 @@ function runServe(options: UserConfig) {
     console.log()
     console.log(`  Dev server running at:`)
     const interfaces = os.networkInterfaces()
-    Object.keys(interfaces).forEach((key) => {
-      ;(interfaces[key] || [])
+    Object.keys(interfaces).forEach((key) =>
+      (interfaces[key] || [])
         .filter((details) => details.family === 'IPv4')
         .map((detail) => {
           return {
@@ -196,7 +189,7 @@ function runServe(options: UserConfig) {
           const url = `${protocol}://${host}:${chalk.bold(port)}/`
           console.log(`  > ${type} ${chalk.cyan(url)}`)
         })
-    })
+    )
     console.log()
     require('debug')('vite:server')(`server ready in ${Date.now() - start}ms.`)
 
