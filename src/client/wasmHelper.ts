f7de781e185faf06c41eaaeb834c17c2acc30ad4
Ian Purvis <ian@purvisresearch.com>
2020-11-22 10:30:05 -0600

    feat: Use WebAssembly.instantiateStreaming when safe

--- src/client/wasmHelper.ts
+++ src/client/wasmHelper.ts
@@ -2,26 +2,34 @@ declare const atob: any
 declare const fetch: any
 declare const WebAssembly: any
 
-function initWasm(opts = {}, url: string) {
-  let instance
+async function initWasm(opts = {}, url: string) {
+  let result
   if (url.startsWith('data:')) {
     const binaryString = atob(url.replace(/^data:.*?base64,/, ''))
     const bytes = new Uint8Array(binaryString.length)
     for (let i = 0; i < binaryString.length; i++) {
       bytes[i] = binaryString.charCodeAt(i)
     }
-    instance = WebAssembly.instantiate(bytes.buffer, opts)
+    result = await WebAssembly.instantiate(bytes, opts)
   } else {
     // https://github.com/mdn/webassembly-examples/issues/5
     // WebAssembly.instantiateStreaming requires the server to provide the
     // correct MIME type for .wasm files, which unfortunately doesn't work for
     // a lot of static file servers, so we just work around it by getting the
     // raw buffer.
-    instance = fetch(url)
-      .then((r: any) => r.arrayBuffer())
-      .then((bytes: ArrayBuffer) => WebAssembly.instantiate(bytes, opts))
+    const response = await fetch(url)
+    const contentType = response.headers.get('Content-Type')
+    if (
+      'instantiateStreaming' in WebAssembly &&
+      contentType.startsWith('application/wasm')
+    ) {
+      result = await WebAssembly.instantiateStreaming(response, opts)
+    } else {
+      const buffer = await response.arrayBuffer()
+      result = await WebAssembly.instantiate(buffer, opts)
+    }
   }
-  return instance.then((i: any) => i.instance.exports)
+  return result.instance.exports
 }
 
 export const id = '/vite/wasm-helper'
