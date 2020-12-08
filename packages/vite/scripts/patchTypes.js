d899d6128bb0b7d6b9eeba52400f484bd5f5939c
Evan You <yyx990803@gmail.com>
2020-12-08 02:55:46 -0500

    build: tweak script

--- packages/vite/scripts/patchTypes.js
+++ packages/vite/scripts/patchTypes.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
+// remove temp type dir
+fs.rmdirSync(path.resolve(__dirname, '../dist-types'), { recursive: true })
+// somehow api-extractor generates a duplicated dist dir in its project root
+fs.rmdirSync(path.resolve(__dirname, '../src/node/dist'), { recursive: true })
+
 const importMetaDts = fs.readFileSync(
   path.resolve(__dirname, '../src/client/importMeta.d.ts'),
   'utf-8'
