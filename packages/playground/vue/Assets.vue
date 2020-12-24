f9c7036d884b53255a59218a57b02655df7214e3
Evan You <yyx990803@gmail.com>
2020-12-24 10:25:18 -0500

    test: svg fragment asset references

--- packages/playground/vue/Assets.vue
+++ packages/playground/vue/Assets.vue
@@ -12,4 +12,13 @@
     Absolute import from public dir
     <img class="public-import" src="/icon.png" style="width: 30px;" />
   </p>
+  <p>
+    SVG Fragment reference
+    <img
+      class="svg-frag"
+      style="width:32px;height:32px;"
+      src="./assets/fragment.svg#icon-heart-view"
+      alt=""
+    >
+  </p>
 </template>
