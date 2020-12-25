bd098ca08441e16f7900d1f0eac3ae473bef7547
Evan You <yyx990803@gmail.com>
2020-12-25 18:16:54 -0500

    refactor: strongly cache optimized deps

--- packages/vite/src/node/optimizer/depMetadataPlugin.ts
+++ packages/vite/src/node/optimizer/depMetadataPlugin.ts
@@ -1,16 +1,18 @@
 import { Plugin } from 'rollup'
+import { DepOptimizationMetadata } from '.'
 
-export function recordCjsEntryPlugin(data: Record<string, true>): Plugin {
+export function recordCjsEntryPlugin(data: DepOptimizationMetadata): Plugin {
   return {
     name: 'vite:cjs-entry-named-export',
     async generateBundle(options, bundle) {
       Object.values(bundle).forEach((chunk) => {
         if (chunk.type === 'chunk' && chunk.isEntry) {
+          data.map[chunk.name] = chunk.fileName
           if (chunk.facadeModuleId) {
             const facadeInfo = this.getModuleInfo(chunk.facadeModuleId)
             // this info is exposed by rollup commonjs plugin
             if (facadeInfo?.meta?.commonjs?.isCommonJS) {
-              data[chunk.fileName] = true
+              data.cjsEntries[chunk.fileName] = true
             }
           }
         }
