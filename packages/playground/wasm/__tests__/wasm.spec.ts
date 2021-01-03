919ea14890968be2fb972b82bd4d2476317243c2
Ian Purvis <ian@purvisresearch.com>
2021-01-03 15:33:49 -0600

    test(wasm): should work when inline or output

--- packages/playground/wasm/__tests__/wasm.spec.ts
+++ packages/playground/wasm/__tests__/wasm.spec.ts
@@ -1,6 +1,11 @@
 import { untilUpdated } from '../../testUtils'
 
-test('should work', async () => {
-  await page.click('.run')
-  await untilUpdated(() => page.textContent('.result'), 'Wasm result: 42')
+test('should work when inlined', async () => {
+  await page.click('.inline-wasm .run')
+  await untilUpdated(() => page.textContent('.inline-wasm .result'), '42')
+})
+
+test('should work when output', async () => {
+  await page.click('.output-wasm .run')
+  await untilUpdated(() => page.textContent('.output-wasm .result'), '24')
 })
