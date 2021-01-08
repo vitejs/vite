cf06627926b132dca73e451730d1c2e91228d742
underfin <likui6666666@gmail.com>
2021-01-08 11:12:14 +0800

    fix(build): resolve template assets reference on build
    fix #15

--- template.ts
+++ template.ts
@@ -8,7 +8,7 @@ export function compileSFCTemplate(
   source: string,
   block: SFCBlock,
   filename: string,
-  { root, isProduction, vueTemplateOptions }: ResolvedOptions,
+  { root, isProduction, vueTemplateOptions, devServer }: ResolvedOptions,
   pluginContext: TransformPluginContext
 ): string {
   const { tips, errors, code } = compileTemplate({
@@ -16,9 +16,11 @@ export function compileSFCTemplate(
     filename,
     compiler: vueTemplateCompiler as any,
     transformAssetUrls: true,
-    transformAssetUrlsOptions: {
-      base: path.posix.dirname(path.relative(root, filename)),
-    },
+    transformAssetUrlsOptions: devServer
+      ? {
+          base: path.posix.dirname(path.relative(root, filename)),
+        }
+      : {},
     isProduction,
     isFunctional: !!block.attrs.functional,
     optimizeSSR: false,
@@ -35,5 +37,31 @@ export function compileSFCTemplate(
     // 	errors.forEach((e) => pluginContext.error(e))
   }
 
-  return code + `\nexport { render, staticRenderFns }`
+  if (devServer) {
+    return code + `\nexport { render, staticRenderFns }`
+  }
+  // rewrite require calls to import on build
+  return transformRequireToImport(code) + `\nexport { render, staticRenderFns }`
+}
+
+export function transformRequireToImport(code: string): string {
+  const imports: { [key: string]: string } = {}
+  let strImports = ''
+
+  code = code.replace(
+    /require\(("(?:[^"\\]|\\.)+"|'(?:[^'\\]|\\.)+')\)/g,
+    (_, name): any => {
+      if (!(name in imports)) {
+        imports[name] = `__$_require_${name
+          .replace(/[^a-z0-9]/g, '_')
+          .replace(/_{2,}/g, '_')
+          .replace(/^_|_$/g, '')}__`
+        strImports += 'import ' + imports[name] + ' from ' + name + '\n'
+      }
+
+      return imports[name]
+    }
+  )
+
+  return strImports + code
 }
