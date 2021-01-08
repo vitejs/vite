9c41d7a7b13a4f0996a366804fbd52c1eb312c48
Evan You <yyx990803@gmail.com>
2021-01-08 18:37:49 -0500

    feat: build support for data uri import

---
+++ packages/vite/src/node/plugins/dataUri.ts
@@ -0,0 +1,56 @@
+// This is based on @rollup/plugin-data-uri
+// MIT Licensed https://github.com/rollup/plugins/blob/master/LICENSE
+// ref https://github.com/vitejs/vite/issues/1428#issuecomment-757033808
+import { Plugin } from '../plugin'
+import { URL } from 'url'
+
+const dataUriRE = /^([^/]+\/[^;,]+)(;base64)?,([\s\S]*)$/
+
+const dataUriPrefix = `/@data-uri/`
+
+export function dataURIPlugin(): Plugin {
+  const resolved: {
+    [key: string]: string
+  } = {}
+
+  return {
+    name: 'vite:data-uri',
+    resolveId(id) {
+      if (!dataUriRE.test(id)) {
+        return null
+      }
+
+      const uri = new URL(id)
+      if (uri.protocol !== 'data:') {
+        return null
+      }
+
+      const match = uri.pathname.match(dataUriRE)
+      if (!match) {
+        return null
+      }
+
+      const [, mime, format, data] = match
+      if (mime !== 'text/javascript') {
+        throw new Error(
+          `data URI with non-JavaScript mime type is not supported.`
+        )
+      }
+
+      // decode data
+      const base64 = format && /base64/i.test(format.substring(1))
+      const content = base64
+        ? Buffer.from(data, 'base64').toString('utf-8')
+        : data
+      resolved[id] = content
+      return dataUriPrefix + id
+    },
+
+    load(id) {
+      if (id.startsWith(dataUriPrefix)) {
+        id = id.slice(dataUriPrefix.length)
+        return resolved[id] || null
+      }
+    }
+  }
+}
