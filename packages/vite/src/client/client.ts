bbfe06ce1af8f89490032e609377c6516f7da773
Evan You <yyx990803@gmail.com>
2021-01-09 23:54:30 -0500

    fix: fix dynamic import with parent relative paths
    fix #1461

--- packages/vite/src/client/client.ts
+++ packages/vite/src/client/client.ts
@@ -438,7 +438,9 @@ export const createHotContext = (ownerPath: string) => {
 }
 
 export function injectQuery(url: string, queryToInject: string) {
-  const { pathname, search, hash } = new URL(url, 'http://vitejs.dev')
+  // can't use pathname from URL since it may be relative like ../
+  const pathname = url.replace(/#.*$/, '').replace(/\?.*$/, '')
+  const { search, hash } = new URL(url, 'http://vitejs.dev')
   return `${pathname}?${queryToInject}${search ? `&` + search.slice(1) : ''}${
     hash || ''
   }`
