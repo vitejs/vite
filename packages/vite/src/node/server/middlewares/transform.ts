66e57e7719f9b6e4c41742166f22504ad43f7152
Evan You <yyx990803@gmail.com>
2021-01-12 17:55:59 -0500

    fix: Revert "feat: allow browser new window view source (#1496)"
    This reverts commit 281f63c0f21b007980fa1f239f9b5e1bc46a2381.
    
    close #1507

--- packages/vite/src/node/server/middlewares/transform.ts
+++ packages/vite/src/node/server/middlewares/transform.ts
@@ -39,6 +39,7 @@ export function transformMiddleware(
   return async (req, res, next) => {
     if (
       req.method !== 'GET' ||
+      req.headers.accept?.includes('text/html') ||
       knownIgnoreList.has(req.url!)
     ) {
       return next()
