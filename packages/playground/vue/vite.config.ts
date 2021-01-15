d8754deeb16ef0d86b17dfa2a3394d0919bcd72e
underfin <likui6666666@gmail.com>
2021-01-15 22:39:18 +0800

    fix(plugin-vue): sfc src import respect alias (#1544)
    fix #1542

--- packages/playground/vue/vite.config.ts
+++ packages/playground/vue/vite.config.ts
@@ -1,8 +1,12 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vuePlugin from '@vitejs/plugin-vue'
 import { vueI18nPlugin } from './CustomBlockPlugin'
 
 export default defineConfig({
+  alias: {
+    '/@': __dirname
+  },
   plugins: [vuePlugin(), vueI18nPlugin],
   build: {
     // to make tests faster
