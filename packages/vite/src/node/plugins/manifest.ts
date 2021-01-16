4aa152cf38adb2cb7f803c8f822287b9885e05b8
choysen <582511362@qq.com>
2021-01-16 23:06:18 +0800

    fix(build): manifest should not be override when chunk name is same

--- packages/vite/src/node/plugins/manifest.ts
+++ packages/vite/src/node/plugins/manifest.ts
@@ -1,28 +1,33 @@
 import { Plugin } from '../plugin'
 
 export function manifestPlugin(): Plugin {
-  const manifest: Record<string, { file: string; imports?: string[] }> = {}
+  type Manifest = Record<string, { file: string; imports?: string[] }>
+  const manifestList: Manifest[] = []
 
   return {
     name: 'vite:manifest',
     generateBundle(_options, bundle) {
       for (const file in bundle) {
         const chunk = bundle[file]
+        const manifest: Manifest = {}
         if (chunk.type === 'chunk') {
           if (chunk.isEntry) {
             manifest[chunk.name + '.js'] = {
               file: chunk.fileName,
               imports: chunk.imports
             }
+            manifestList.push(manifest)
           }
         } else if (chunk.name) {
           manifest[chunk.name] = { file: chunk.fileName }
+          manifestList.push(manifest)
         }
       }
+
       this.emitFile({
         fileName: 'manifest.json',
         type: 'asset',
-        source: JSON.stringify(manifest, null, 2)
+        source: JSON.stringify(manifestList, null, 2)
       })
     }
   }
