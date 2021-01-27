8b543a7f22a7218f088f2b09abb515c3414b6d0f
Evan You <yyx990803@gmail.com>
2021-01-26 22:01:09 -0500

    chore(create-app): add a note about using workspace TS

--- packages/create-app/template-vue-ts/src/main.ts
+++ packages/create-app/template-vue-ts/src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+// TypeScript error? Run VSCode command
+// TypeScript: Select TypeScript version - > Use Workspace Version
 import App from './App.vue'
 
 createApp(App).mount('#app')
