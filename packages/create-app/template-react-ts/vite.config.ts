ce71d71e83785a1072204bf8117a61ab7e4e772c
Evan You <yyx990803@gmail.com>
2021-01-26 22:03:20 -0500

    feat: add link to config docs

--- packages/create-app/template-react-ts/vite.config.ts
+++ packages/create-app/template-react-ts/vite.config.ts
@@ -1,6 +1,7 @@
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import { defineConfig } from 'vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()]
 })
