b7d68b300fb6625b36902234270803d55684bc64
Evan You <yyx990803@gmail.com>
2021-01-28 13:30:27 -0500

    fix(optimizer): invalidate all modules on deps rebundle

--- packages/vite/src/node/optimizer/registerMissing.ts
+++ packages/vite/src/node/optimizer/registerMissing.ts
@@ -12,7 +12,6 @@ export function createMissingImpoterRegisterFn(server: ViteDevServer) {
   const { logger } = server.config
   let knownOptimized = server._optimizeDepsMetadata!.optimized
   let currentMissing: Record<string, string> = {}
-  let currentImporters = new Set<string>()
   let handle: NodeJS.Timeout
 
   let pendingResolve: (() => void) | null = null
@@ -64,14 +63,10 @@ export function createMissingImpoterRegisterFn(server: ViteDevServer) {
       server._pendingReload = pendingResolve = null
     }
 
-    // Importers' cached transform results have stale imports (resolved to
+    // Cached transform results have stale imports (resolved to
     // old locations) so they need to be invalidated before the page is
     // reloaded.
-    currentImporters.forEach((importer) => {
-      const mod = server.moduleGraph.getModuleById(importer)
-      if (mod) server.moduleGraph.invalidateModule(mod)
-    })
-    currentImporters.clear()
+    server.moduleGraph.invalidateAll()
 
     server.ws.send({
       type: 'full-reload',
@@ -79,14 +74,9 @@ export function createMissingImpoterRegisterFn(server: ViteDevServer) {
     })
   }
 
-  return function registerMissingImport(
-    id: string,
-    resolved: string,
-    importer?: string
-  ) {
+  return function registerMissingImport(id: string, resolved: string) {
     if (!knownOptimized[id]) {
       currentMissing[id] = resolved
-      if (importer) currentImporters.add(importer)
       if (handle) clearTimeout(handle)
       handle = setTimeout(rerun, debounceMs)
       server._pendingReload = new Promise((r) => {
