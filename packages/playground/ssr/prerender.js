40be502db072c5a664ec93d15b134c11d2fe96b1
Evan You <yyx990803@gmail.com>
2021-02-02 15:12:10 -0500

    chore: add ssr playground pre-render example

---
+++ packages/playground/ssr/prerender.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+const toAbsolute = (p) => path.resolve(__dirname, p)
+
+const manifest = require('./dist/static/ssr-manifest.json')
+const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8')
+const { render } = require('./dist/server/entry-server.js')
+
+const routesToPrerender = ['/', '/vue', '/react']
+
+;(async () => {
+  // pre-render each route...
+  for (const url of routesToPrerender) {
+    const [appHtml, preloadLinks] = await render(url, manifest)
+
+    const html = `
+        ${preloadLinks}
+        ${template.replace(`<!--ssr-outlet-->`, appHtml)}
+        `
+
+    const filePath = `dist/static${url === '/' ? '/index' : url}.html`
+    fs.writeFileSync(toAbsolute(filePath), html)
+    console.log('pre-rendered:', filePath)
+  }
+
+  // done, delete ssr manifest
+  fs.unlinkSync(toAbsolute('dist/static/ssr-manifest.json'))
+})()
