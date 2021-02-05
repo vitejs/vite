d6bf066e3a18b749bed1a7fc622dd2551404e29f
Johnson Chu <johnsoncodehk@gmail.com>
2021-02-06 00:26:39 +0800

    feat(create-app): clearer vue-ts setup recommend (#1896) [skip ci]

--- packages/create-app/template-vue-ts/src/main.ts
+++ packages/create-app/template-vue-ts/src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue'
-// TypeScript error? Run VSCode command
-// TypeScript: Select TypeScript version - > Use Workspace Version
 import App from './App.vue'
 
 createApp(App).mount('#app')
