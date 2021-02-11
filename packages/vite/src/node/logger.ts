f95cad0995325be68cc5f63f5f7e38b14e60f252
Evan You <yyx990803@gmail.com>
2021-02-11 17:29:02 -0500

    Update logger.ts

--- packages/vite/src/node/logger.ts
+++ packages/vite/src/node/logger.ts
@@ -31,7 +31,7 @@ let sameCount = 0
 
 function clearScreen() {
   const repeatCount = process.stdout.rows - 2
-  const blank = '\n'.repeat(repeatCount >= 0 ? repeatCount : 0)
+  const blank = repeatCount > 0 ? '\n'.repeat(repeatCount) : ''
   console.log(blank)
   readline.cursorTo(process.stdout, 0, 0)
   readline.clearScreenDown(process.stdout)
