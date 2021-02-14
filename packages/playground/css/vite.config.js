f32830bb71076fbea52494f01a8ea52117f96f88
Bernardo Uriarte Blanco <berublan@gmail.com>
2021-02-14 14:32:21 +0100

    docs: add example usage of `getJSON` to generate css module typings

--- packages/playground/css/vite.config.js
+++ packages/playground/css/vite.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+const fs = require('fs')
 /**
  * @type {import('vite').UserConfig}
  */
@@ -9,6 +11,21 @@ module.exports = {
   },
   css: {
     modules: {
+      // example of how getJSON can be used to generate
+      // typescript typings for css modules class names
+      getJSON(cssFileName, json, _outputFileName) {
+        let typings = 'declare const classNames: {\n'
+        for (let className in json) {
+          typings += `    "${className}": string;\n`
+        }
+        typings += '};\n'
+        typings += 'export default classNames;\n'
+        const typingsFile = path.join(
+          path.dirname(cssFileName),
+          path.basename(cssFileName) + '.d.ts'
+        )
+        fs.writeFileSync(typingsFile, typings)
+      },
       generateScopedName: '[name]__[local]___[hash:base64:5]'
     },
     preprocessorOptions: {
