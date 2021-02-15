8a0dcb330475794f8d0a7719d40b882ffd7167cd
vemoo <berublan@gmail.com>
2021-02-15 16:00:13 +0100

    feat: allow `getJSON` option on `css.modules` (#2025)

--- packages/playground/css/vite.config.js
+++ packages/playground/css/vite.config.js
@@ -10,6 +10,24 @@ module.exports = {
   css: {
     modules: {
       generateScopedName: '[name]__[local]___[hash:base64:5]'
+
+      // example of how getJSON can be used to generate
+      // typescript typings for css modules class names
+
+      // getJSON(cssFileName, json, _outputFileName) {
+      //   let typings = 'declare const classNames: {\n'
+      //   for (let className in json) {
+      //     typings += `    "${className}": string;\n`
+      //   }
+      //   typings += '};\n'
+      //   typings += 'export default classNames;\n'
+      //   const { join, dirname, basename } = require('path')
+      //   const typingsFile = join(
+      //     dirname(cssFileName),
+      //     basename(cssFileName) + '.d.ts'
+      //   )
+      //   require('fs').writeFileSync(typingsFile, typings)
+      // },
     },
     preprocessorOptions: {
       scss: {
