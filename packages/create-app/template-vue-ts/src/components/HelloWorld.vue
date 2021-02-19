891d0c221695e98c3a56934654e57010fb4389a9
xon52 <30713944+xon52@users.noreply.github.com>
2021-02-20 09:25:00 +1000

    fix: vue-ts template checkbox type mismatch

--- packages/create-app/template-vue-ts/src/components/HelloWorld.vue
+++ packages/create-app/template-vue-ts/src/components/HelloWorld.vue
@@ -1,49 +1,65 @@
 <template>
   <h1>{{ msg }}</h1>
+  <label>
+    <input type="checkbox" :checked="useScriptSetup" @input="setUseScriptSetup" /> Use
+    <code>&lt;script setup&gt;</code>
+  </label>
+  <label> <input type="checkbox" :checked="useTsPlugin" @input="setUseTsPlugin" /> Provide types for <code>*.vue</code> imports </label>
 
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
     +
-    <a
-      href="https://marketplace.visualstudio.com/items?itemName=octref.vetur"
-      target="_blank"
-    >Vetur</a>
-    or
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    (if using
-    <code>&ltscript setup&gt;</code>)
+    <template v-if="!useScriptSetup">
+      <a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank">Vetur</a>
+      +
+      <a href="https://marketplace.visualstudio.com/items?itemName=znck.vue-language-features" target="_blank"
+        >Vue DX</a
+      >
+    </template>
+    <template v-else>
+      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    </template>
   </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  <p v-if="useTsPlugin">
+    tsconfig setup:
+    <br />1. Install and add <code>@vuedx/typescript-plugin-vue</code> to tsconfig plugins <br />2. Delete
+    <code>src/shims-vue.d.ts</code> <br />3. Open <code>src/main.ts</code> in VSCode <br />4. Open VSCode command input
+    <br />5. Search and run "Select TypeScript version" -> "Use workspace version"
   </p>
-
   <button @click="count++">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Docs</a> |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+  </p>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent } from "vue";
 export default defineComponent({
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   setup: () => {
-    const count = ref(0)
-    return { count }
-  }
-})
+    const count = ref(0);
+    const useScriptSetup = ref(false);
+    const useTsPlugin = ref(false);
+    const setUseScriptSetup = (v: any): void =>
+      (useScriptSetup.value = v.target.checked);
+    const setUseTsPlugin = (v: any): void =>
+      (useTsPlugin.value = v.target.checked);
+    return { count, useScriptSetup, useTsPlugin, setUseScriptSetup, setUseTsPlugin };
+  },
+});
 </script>
 
 <style scoped>
