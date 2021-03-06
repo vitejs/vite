eb8f4cd29480313ef058f2b764c8eca0d1fbaa19
Jonas Galvez <jonasgalvez@gmail.com>
2021-03-06 10:04:38 -0300

    add new server

--- packages/playground/ssr-vue-fastify/server.js
+++ packages/playground/ssr-vue-fastify/server.js
@@ -1,7 +1,7 @@
-// @ts-check
 const fs = require('fs')
-const path = require('path')
-const express = require('express')
+const { resolve } = require('path')
+const Fastify = require('fastify')
+const FastifyVitePlugin = require('./plugin')
 
 const isTest = process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD
 
@@ -9,78 +9,25 @@ async function createServer(
   root = process.cwd(),
   isProd = process.env.NODE_ENV === 'production'
 ) {
-  const resolve = (p) => path.resolve(__dirname, p)
+  const fastify = new Fastify()
 
-  const indexProd = isProd
-    ? fs.readFileSync(resolve('dist/client/index.html'), 'utf-8')
-    : ''
-
-  const manifest = isProd
-    ? // @ts-ignore
-      require('./dist/client/ssr-manifest.json')
-    : {}
-
-  const app = express()
-
-  /**
-   * @type {import('vite').ViteDevServer}
-   */
-  let vite
-  if (!isProd) {
-    vite = await require('vite').createServer({
-      root,
-      logLevel: isTest ? 'error' : 'info',
-      server: {
-        middlewareMode: true
-      }
-    })
-    // use vite's connect instance as middleware
-    app.use(vite.middlewares)
-  } else {
-    app.use(require('compression')())
-    app.use(
-      require('serve-static')(resolve('dist/client'), {
-        index: false
-      })
-    )
-  }
-
-  app.use('*', async (req, res) => {
-    try {
-      const url = req.originalUrl
-
-      let template, render
-      if (!isProd) {
-        // always read fresh template in dev
-        template = fs.readFileSync(resolve('index.html'), 'utf-8')
-        template = await vite.transformIndexHtml(url, template)
-        render = (await vite.ssrLoadModule('/src/entry-server.js')).render
-      } else {
-        template = indexProd
-        render = require('./dist/server/entry-server.js').render
-      }
-
-      const [appHtml, preloadLinks] = await render(url, manifest)
-
-      const html = template
-        .replace(`<!--preload-links-->`, preloadLinks)
-        .replace(`<!--app-html-->`, appHtml)
-
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
-    } catch (e) {
-      vite && vite.ssrFixStacktrace(e)
-      console.log(e.stack)
-      res.status(500).end(e.stack)
-    }
+  await fastify.register(FastifyVitePlugin, {
+    dev: true,
+    rootDir: resolve(__dirname),
+    srcDir: resolve(__dirname, 'src'),
   })
 
-  return { app, vite }
+  return { fastify, vite: fastify.$vite }
 }
 
 if (!isTest) {
-  createServer().then(({ app }) =>
-    app.listen(3000, () => {
-      console.log('http://localhost:3000')
+  createServer().then(({ fastify }) =>
+    fastify.listen(3000, (err, address) => {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+      console.log(`Server listening on ${address}`)
     })
   )
 }
