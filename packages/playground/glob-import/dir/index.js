ba2c7954957405949f587e096647a6731667f959
vimcaw <vimcaw@gmail.com>
2021-03-18 13:35:24 +0800

    test: fix missing normalizing result order of async glob importer

--- packages/playground/glob-import/dir/index.js
+++ packages/playground/glob-import/dir/index.js
@@ -1,5 +1,3 @@
-import sortObjectDeep from '../sortObjectDeep'
-
-const modules = sortObjectDeep(import.meta.globEager('./*.js', './_*.js'))
+const modules = import.meta.globEager('./*.js', './_*.js')
 
 export { modules }
