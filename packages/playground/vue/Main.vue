3e26855488716870763596b718f65cee579d2942
Evan You <yyx990803@gmail.com>
2021-03-26 18:05:27 -0400

    test: add vue slotted test case

--- packages/playground/vue/Main.vue
+++ packages/playground/vue/Main.vue
@@ -11,6 +11,9 @@
   <Assets />
   <CustomBlock />
   <SrcImport />
+  <Slotted>
+    <div class="slotted">this should be red</div>
+  </Slotted>
 </template>
 
 <script setup lang="ts">
@@ -21,6 +24,7 @@ import CssModules from './CssModules.vue'
 import Assets from './Assets.vue'
 import CustomBlock from './CustomBlock.vue'
 import SrcImport from './src-import/SrcImport.vue'
+import Slotted from './Slotted.vue'
 
 import { ref } from 'vue'
 
