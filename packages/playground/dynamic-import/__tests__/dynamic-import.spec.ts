a4708384f46653a40f5f50f9736a9341e0eae9f6
Pig Fang <g-plane@hotmail.com>
2021-03-29 22:43:46 +0800

    fix(client): don't inject queries for data URLs (#2703), fix #2658

--- packages/playground/dynamic-import/__tests__/dynamic-import.spec.ts
+++ packages/playground/dynamic-import/__tests__/dynamic-import.spec.ts
@@ -10,6 +10,18 @@ test('should load full dynamic import from public', async () => {
   await untilUpdated(() => page.textContent('.view'), 'Qux view', true)
 })
 
+test('should load data URL of `blob:`', async () => {
+  await page.click('.issue-2658-1')
+  await untilUpdated(() => page.textContent('.view'), 'blob', true)
+})
+
+test('should load data URL of `data:`', async () => {
+  await page.click('.issue-2658-2')
+  await untilUpdated(() => page.textContent('.view'), 'data', true)
+})
+
+// since this test has a timeout, it should be put last so that it
+// does not bleed on the last
 test('should load dynamic import with vars', async () => {
   await page.click('.foo')
   await untilUpdated(() => page.textContent('.view'), 'Foo view', true)
