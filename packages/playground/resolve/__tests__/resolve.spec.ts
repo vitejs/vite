669c591696788df844e7b26e600df86ffc70792c
Timsonrobl <timson.robl@gmail.com>
2021-03-29 17:45:05 +0300

    fix(resolve): fix resolver not following node resolve algorithm (#2718), fix #2695

--- packages/playground/resolve/__tests__/resolve.spec.ts
+++ packages/playground/resolve/__tests__/resolve.spec.ts
@@ -42,6 +42,14 @@ test('implicit dir/index.js vs explicit file', async () => {
   expect(await page.textContent('.dir-vs-file')).toMatch('[success]')
 })
 
+test('exact extension vs. duplicated (.js.js)', async () => {
+  expect(await page.textContent('.exact-extension')).toMatch('[success]')
+})
+
+test('dont add extension to directory name (./dir-with-ext.js/index.js)', async () => {
+  expect(await page.textContent('.dir-with-ext')).toMatch('[success]')
+})
+
 test('filename with dot', async () => {
   expect(await page.textContent('.dot')).toMatch('[success]')
 })
