edcaf85a852f25d34730ab1ddc1c6fe2d0cb5eae
Shinigami <chrissi92@hotmail.de>
2021-03-31 23:20:10 +0200

    chore: use eslint-define-config (#2688)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
-module.exports = {
+// @ts-check
+const { defineConfig } = require('eslint-define-config')
+
+module.exports = defineConfig({
   root: true,
   extends: ['plugin:node/recommended'],
   parser: '@typescript-eslint/parser',
@@ -80,4 +83,4 @@ module.exports = {
       }
     }
   ]
-}
+})
