61b3400f131908d9edbd08ee593766ff1c7c7681
Anthony Fu <anthonyfu117@hotmail.com>
2021-04-01 06:09:01 +0800

    test: provide failed test for #2810

--- packages/playground/ssr-vue/src/pages/Home.vue
+++ packages/playground/ssr-vue/src/pages/Home.vue
@@ -6,11 +6,14 @@
   <button @click="state.count++">count is: {{ state.count }}</button>
   <Foo />
   <p class="virtual">msg from virtual module: {{ foo.msg }}</p>
+
+  <ImportType/>
 </template>
 
 <script setup>
 import foo from '@foo'
 import { reactive, defineAsyncComponent } from 'vue'
+import ImportType from '../components/ImportType.vue'
 const Foo = defineAsyncComponent(() =>
   import('../components/Foo').then((mod) => mod.Foo)
 )
