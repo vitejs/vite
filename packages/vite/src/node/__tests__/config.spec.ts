df5092c58126885ed8943559a0f49230daee2829
Chris Santamaria <chris@santamaria.me>
2021-04-03 16:03:04 -0400

    test(build): add test for assetsInclude key

--- packages/vite/src/node/__tests__/config.spec.ts
+++ packages/vite/src/node/__tests__/config.spec.ts
@@ -1,44 +1,62 @@
 import { mergeConfig, UserConfigExport } from '../config'
 
-test('merge configs with different alias schemas', () => {
-  const baseConfig: UserConfigExport = {
-    resolve: {
-      alias: [
-        {
-          find: 'foo',
-          replacement: 'foo-value'
-        }
-      ]
-    }
-  }
-
-  const newConfig: UserConfigExport = {
-    resolve: {
-      alias: {
-        bar: 'bar-value',
-        baz: 'baz-value'
+describe('mergeConfig', () => {
+  test('handles configs with different alias schemas', () => {
+    const baseConfig: UserConfigExport = {
+      resolve: {
+        alias: [
+          {
+            find: 'foo',
+            replacement: 'foo-value'
+          }
+        ]
       }
     }
-  }
 
-  const mergedConfig: UserConfigExport = {
-    resolve: {
-      alias: [
-        {
-          find: 'foo',
-          replacement: 'foo-value'
-        },
-        {
-          find: 'bar',
-          replacement: 'bar-value'
-        },
-        {
-          find: 'baz',
-          replacement: 'baz-value'
+    const newConfig: UserConfigExport = {
+      resolve: {
+        alias: {
+          bar: 'bar-value',
+          baz: 'baz-value'
         }
-      ]
+      }
     }
-  }
 
-  expect(mergeConfig(baseConfig, newConfig)).toEqual(mergedConfig)
+    const mergedConfig: UserConfigExport = {
+      resolve: {
+        alias: [
+          {
+            find: 'foo',
+            replacement: 'foo-value'
+          },
+          {
+            find: 'bar',
+            replacement: 'bar-value'
+          },
+          {
+            find: 'baz',
+            replacement: 'baz-value'
+          }
+        ]
+      }
+    }
+
+    expect(mergeConfig(baseConfig, newConfig)).toEqual(mergedConfig)
+  })
+
+  test('handles assetsInclude', () => {
+    const baseConfig: UserConfigExport = {
+      assetsInclude: 'some-string'
+    }
+
+    const newConfig: UserConfigExport = {
+      assetsInclude: ['some-other-string', /regexp?/]
+    }
+
+    const mergedConfig: UserConfigExport = {
+      assetsInclude: ['some-string', 'some-other-string', /regexp?/]
+    }
+
+    expect(mergeConfig(baseConfig, newConfig)).toEqual(mergedConfig)
+  })
 })
