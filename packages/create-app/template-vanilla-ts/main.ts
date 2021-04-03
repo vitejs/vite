eec9c58c562971e5900e25da08d9e303bffe7464
Michael Rienstra <mrienstra@gmail.com>
2021-04-03 15:33:57 -0700

    feat: @vitejs/create-app: add 'vanilla-ts' template

---
+++ packages/create-app/template-vanilla-ts/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+const el = document.querySelector('#app');
+
+if (el) {
+  el.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+}
