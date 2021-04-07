81aa087fc775ef94f31fbc63e9b8f0a53b52d69c
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-07 19:19:54 -0400

    fix: ignore ENOENT in `injectSourcesContent`
    This is important for virtual modules and malformed source maps, both of which can throw ENOENT errors when injecting `sourcesContent`.

--- packages/vite/src/node/server/sourcemap.ts
+++ packages/vite/src/node/server/sourcemap.ts
@@ -5,16 +5,26 @@ export async function injectSourcesContent(
   map: { sources: string[]; sourcesContent?: string[]; sourceRoot?: string },
   file: string
 ): Promise<void> {
-  const sourceRoot = await fs.realpath(
-    path.resolve(path.dirname(file), map.sourceRoot || '')
-  )
+  try {
+    var sourceRoot = await fs.realpath(
+      path.resolve(path.dirname(file), map.sourceRoot || '')
+    )
+  } catch (e) {
+    if (e.code == 'ENOENT') return
+    throw e
+  }
   map.sourcesContent = []
   await Promise.all(
     map.sources.filter(Boolean).map(async (sourcePath, i) => {
-      map.sourcesContent![i] = await fs.readFile(
-        path.resolve(sourceRoot, decodeURI(sourcePath)),
-        'utf-8'
-      )
+      try {
+        map.sourcesContent![i] = await fs.readFile(
+          path.resolve(sourceRoot, decodeURI(sourcePath)),
+          'utf-8'
+        )
+      } catch (e) {
+        if (e.code == 'ENOENT') return
+        throw e
+      }
     })
   )
 }
