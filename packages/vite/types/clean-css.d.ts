8ff8b179bee7e1b4bb78959f9e95c9f99ac2dae7
Markus Lanthaler <mark_lanthaler@gmx.net>
2021-04-13 20:54:02 +0200

    Also allow '1' and '0'

--- packages/vite/types/clean-css.d.ts
+++ packages/vite/types/clean-css.d.ts
@@ -453,7 +453,7 @@ export namespace CleanCSS {
       /**
        * denotes a number of /*! ... * / comments preserved; defaults to `all`
        */
-      specialComments?: 'all' | 1 | 0
+      specialComments?: 'all' | '1' | 1 | '0' | 0
 
       /**
        * Controls at-rules (e.g. `@charset`, `@import`) optimizing; defaults to `true`
