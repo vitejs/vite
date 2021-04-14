34c56812eaeaac0fca8df7b6d92045c3406f4aa3
Chris Santamaria <chris@santamaria.me>
2021-04-14 15:30:32 -0400

    fix(build): properly handle alias key in config merge (#2847)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -502,11 +502,11 @@ function resolveBaseUrl(
   return base
 }
 
-export function mergeConfig(
+function mergeConfigRecursively(
   a: Record<string, any>,
   b: Record<string, any>,
-  isRoot = true
-): Record<string, any> {
+  rootPath: string
+) {
   const merged: Record<string, any> = { ...a }
   for (const key in b) {
     const value = b[key]
@@ -520,16 +520,20 @@ export function mergeConfig(
       continue
     }
     if (isObject(existing) && isObject(value)) {
-      merged[key] = mergeConfig(existing, value, false)
+      merged[key] = mergeConfigRecursively(
+        existing,
+        value,
+        rootPath ? `${rootPath}.${key}` : key
+      )
       continue
     }
 
-    // root fields that require special handling
-    if (existing != null && isRoot) {
-      if (key === 'alias') {
+    // fields that require special handling
+    if (existing != null) {
+      if (key === 'alias' && (rootPath === 'resolve' || rootPath === '')) {
         merged[key] = mergeAlias(existing, value)
         continue
-      } else if (key === 'assetsInclude') {
+      } else if (key === 'assetsInclude' && rootPath === '') {
         merged[key] = [].concat(existing, value)
         continue
       }
@@ -538,6 +542,14 @@ export function mergeConfig(
     merged[key] = value
   }
   return merged
+}
+
+export function mergeConfig(
+  a: Record<string, any>,
+  b: Record<string, any>,
+  isRoot = true
+): Record<string, any> {
+  return mergeConfigRecursively(a, b, isRoot ? '' : '.')
 }
 
 function mergeAlias(a: AliasOptions = [], b: AliasOptions = []): Alias[] {
