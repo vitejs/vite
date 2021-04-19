79f9ba0091c826cac240356da3d38b9c89cc24fe
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-04-18 20:21:54 -0400

    fix(emptyOutDir): never remove .git
    ..and empty the out dir in `build --watch` mode when `emptyOutDir` is null.

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -323,11 +323,15 @@ export function writeFile(
   fs.writeFileSync(filename, content)
 }
 
-export function emptyDir(dir: string): void {
-  if (!fs.existsSync(dir)) {
-    return
-  }
+/**
+ * Delete every file and subdirectory. **The given directory must exist.**
+ * Pass an optional `skip` array to preserve files in the root directory.
+ */
+export function emptyDir(dir: string, skip?: string[]): void {
   for (const file of fs.readdirSync(dir)) {
+    if (skip?.includes(file)) {
+      continue
+    }
     const abs = path.resolve(dir, file)
     // baseline is Node 12 so can't use rmSync :(
     if (fs.lstatSync(abs).isDirectory()) {
