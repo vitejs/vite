d696b54d8af0a786fd3efecb169b38ce5a4af297
Alex Kozack <kozackunisoft@gmail.com>
2021-04-23 17:30:34 +0300

    feat: Allow overwrite `Terser.MinifyOptions.safari10` option from `UserConfig`

--- packages/vite/src/node/plugins/terser.ts
+++ packages/vite/src/node/plugins/terser.ts
@@ -21,11 +21,11 @@ export function terserPlugin(options: Terser.MinifyOptions): Plugin {
 
     async renderChunk(code, _chunk, outputOptions) {
       const res = await worker.run(__dirname, code, {
+        safari10: true,
         ...options,
         sourceMap: !!outputOptions.sourcemap,
         module: outputOptions.format.startsWith('es'),
-        toplevel: outputOptions.format === 'cjs',
-        safari10: true
+        toplevel: outputOptions.format === 'cjs'
       })
       return {
         code: res.code!,
