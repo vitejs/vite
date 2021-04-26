c043e115fc90ca30a610fe17c02dfa29a8349d8b
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-04-26 09:39:58 +0200

    chore(lint): configure consistent quote style

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = defineConfig({
         destructuring: 'all'
       }
     ],
+    quotes: ['warn', 'single', { avoidEscape: true }],
 
     'node/no-missing-import': [
       'error',
