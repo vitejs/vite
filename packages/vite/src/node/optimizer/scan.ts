1e785d1af54ae5e305dd1bef9af513f2c3a91ad3
Vben <anncwb@126.com>
2021-04-27 18:10:40 +0800

    fix(scan): improve script regular matching (fixes #2942) (#2961)

--- packages/vite/src/node/optimizer/scan.ts
+++ packages/vite/src/node/optimizer/scan.ts
@@ -134,7 +134,8 @@ function globEntries(pattern: string | string[], config: ResolvedConfig) {
 }
 
 const scriptModuleRE = /(<script\b[^>]*type\s*=\s*(?:"module"|'module')[^>]*>)(.*?)<\/script>/gims
-const scriptRE = /(<script\b[^>]*>)(.*?)<\/script>/gims
+export const scriptRE = /(<script\b(\s[^>]*>|>))(.*?)<\/script>/gims
+export const commentRE = /<!--(.|[\r\n])*?-->/
 const srcRE = /\bsrc\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
 const langRE = /\blang\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
 
@@ -196,14 +197,18 @@ function esbuildScanPlugin(
       build.onLoad(
         { filter: htmlTypesRE, namespace: 'html' },
         async ({ path }) => {
-          const raw = fs.readFileSync(path, 'utf-8')
-          const regex = path.endsWith('.html') ? scriptModuleRE : scriptRE
+          let raw = fs.readFileSync(path, 'utf-8')
+          // Avoid matching the content of the comment
+          raw = raw.replace(commentRE, '')
+          const isHtml = path.endsWith('.html')
+          const regex = isHtml ? scriptModuleRE : scriptRE
           regex.lastIndex = 0
           let js = ''
           let loader: Loader = 'js'
           let match
           while ((match = regex.exec(raw))) {
-            const [, openTag, content] = match
+            const [, openTag, htmlContent, scriptContent] = match
+            const content = isHtml ? htmlContent : scriptContent
             const srcMatch = openTag.match(srcRE)
             const langMatch = openTag.match(langRE)
             const lang =
