9438a9da609cf0018ee72d7aa8574290dbdea7f5
Rivaldo Junior <jrcavalcantejr@gmail.com>
2021-05-05 01:09:12 -0300

    docs: update package name (#3266)
    Co-authored-by: Rivaldo Junior <rivaldojunior@sas.com.br>

--- packages/plugin-react-refresh/index.js
+++ packages/plugin-react-refresh/index.js
@@ -136,8 +136,8 @@ function reactRefreshPlugin(opts) {
 
   if (!window.__vite_plugin_react_preamble_installed__) {
     throw new Error(
-      "vite-plugin-react can't detect preamble. Something is wrong. " +
-      "See https://github.com/vitejs/vite-plugin-react/pull/11#discussion_r430879201"
+      "@vitejs/plugin-react-refresh can't detect preamble. Something is wrong. " +
+      "See https://github.com/vitejs/@vitejs/plugin-react-refresh/pull/11#discussion_r430879201"
     );
   }
 
