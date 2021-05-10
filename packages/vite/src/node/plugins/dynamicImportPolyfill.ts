e2a363356ca8a9bf722c0d5a425a53031671e8a8
patak <matias.capeletto@gmail.com>
2021-05-10 06:49:29 +0200

    fix: warning for vite/dynamic-import-polyfill (#3328)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/vite/src/node/plugins/dynamicImportPolyfill.ts
+++ packages/vite/src/node/plugins/dynamicImportPolyfill.ts
@@ -1,3 +1,26 @@
-throw new Error(
-  `Vite's dynamic import polyfill has been removed. Please install and import https://github.com/GoogleChromeLabs/dynamic-import-polyfill instead.`
-)
+import { ResolvedConfig } from '..'
+import { Plugin } from '../plugin'
+
+export const polyfillId = 'vite/dynamic-import-polyfill'
+
+/**
+ * @deprecated 
+ */
+export function dynamicImportPolyfillPlugin(config: ResolvedConfig): Plugin {
+  return {
+    name: 'vite:dynamic-import-polyfill',
+    resolveId(id) {
+      if (id === polyfillId) {
+        return id
+      }
+    },
+    load(id) {
+      if (id === polyfillId) {
+        config.logger.warn(
+          `\n'vite/dynamic-import-polyfill' is no longer needed, refer to https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md#230-2021-05-10`
+        )
+        return ''
+      }
+    }
+  }
+}
--- packages/vite/src/node/plugins/index.ts
+++ packages/vite/src/node/plugins/index.ts
@@ -10,6 +10,7 @@ import { assetPlugin } from './asset'
 import { clientInjectionsPlugin } from './clientInjections'
 import { htmlInlineScriptProxyPlugin } from './html'
 import { wasmPlugin } from './wasm'
+import { dynamicImportPolyfillPlugin } from './dynamicImportPolyfill'
 import { webWorkerPlugin } from './worker'
 import { preAliasPlugin } from './preAlias'
 import { definePlugin } from './define'
@@ -30,6 +31,7 @@ export async function resolvePlugins(
     isBuild ? null : preAliasPlugin(),
     aliasPlugin({ entries: config.resolve.alias }),
     ...prePlugins,
+    dynamicImportPolyfillPlugin(config),
     resolvePlugin({
       ...config.resolve,
       root: config.root,
