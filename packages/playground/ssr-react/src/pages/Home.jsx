daef373a6cf5cf9915e51f0158ed0563ccc17dc8
Greg Fairbanks <gregfa@zillowgroup.com>
2021-05-19 13:42:06 -0400

    fix: deadlock in ssrLoadModule with circular dependencies (fix #2491, fix #2258)

--- packages/playground/ssr-react/src/pages/Home.jsx
+++ packages/playground/ssr-react/src/pages/Home.jsx
@@ -1,3 +1,12 @@
+import { addAndMultiply } from '../add'
+import { multiplyAndAdd } from '../multiply'
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <>
+      <h1>Home</h1>
+      <div>{addAndMultiply(1, 2, 3)}</div>
+      <div>{multiplyAndAdd(1, 2, 3)}</div>
+    </>
+  )
 }
