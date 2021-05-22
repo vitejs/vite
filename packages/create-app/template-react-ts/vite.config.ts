3282f51a31ba7ab1c1c296a52933b4702193ae8a
Mia <vottus@vott.us>
2021-05-22 02:23:48 +0200

    chore: add jsxInject note to react templates

--- packages/create-app/template-react-ts/vite.config.ts
+++ packages/create-app/template-react-ts/vite.config.ts
@@ -4,4 +4,9 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()]
+  // To automatically inject React you can uncomment the following lines
+  // (you also have to set "jsx" to true in tsconfig.json):
+  // esbuild: {
+  //   jsxInject: "import React from 'react';",
+  // }
 })
--- packages/create-app/template-react/vite.config.js
+++ packages/create-app/template-react/vite.config.js
@@ -4,4 +4,8 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()]
+  // To automatically inject React you can uncomment the following lines:
+  // esbuild: {
+  //   jsxInject: "import React from 'react';",
+  // }
 })
