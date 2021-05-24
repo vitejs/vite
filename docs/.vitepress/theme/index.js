5a75e09090b3b13cc0f37a8f47f43462e734d959
Evan You <yyx990803@gmail.com>
2021-05-24 11:45:48 -0400

    chore: new sponsor

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -19,7 +19,7 @@ export default {
             },
             [h('span', 'Sponsors')]
           ),
-          ...sponsors.map(({ href, src, name }) =>
+          ...sponsors.map(({ href, src, name, id }) =>
             h(
               'a',
               {
@@ -28,7 +28,7 @@ export default {
                 rel: 'noopener',
                 'aria-label': 'sponsor-img'
               },
-              [h('img', { src, alt: name })]
+              [h('img', { src, alt: name, id: `sponsor-${id}` })]
             )
           )
         ])
