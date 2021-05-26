c6f99611d41e3186a3219a9343a4b212b2a35290
Fernando Ferreira <fernando.ferreira@gmail.com>
2021-05-26 19:43:38 +0100

    feat(ssr): include non-CSS assets in the manifest

--- packages/playground/ssr-vue/src/App.vue
+++ packages/playground/ssr-vue/src/App.vue
@@ -11,6 +11,17 @@
 </template>
 
 <style>
+@font-face {
+  font-family: 'Inter';
+  font-style: italic;
+  font-weight: 400;
+  font-display: swap;
+  src: url('./assets/fonts/Inter-Italic.woff2?#iefix') format('woff2'),
+    url('./assets/fonts/Inter-Italic.woff') format('woff');
+}
+.inter {
+  font-family: 'Inter';
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
