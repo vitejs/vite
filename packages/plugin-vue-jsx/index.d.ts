047785bb3f734d32c13338eb5fffa270a3d59243
virgosoy <virgosoy@126.com>
2021-05-28 13:15:43 +0800

    fix: include/exclude options for vue-jsx .d.ts

--- packages/plugin-vue-jsx/index.d.ts
+++ packages/plugin-vue-jsx/index.d.ts
@@ -1,6 +1,12 @@
 import { Plugin } from 'vite'
 import { VueJSXPluginOptions } from '@vue/babel-plugin-jsx'
+import { FilterPattern } from '@rollup/pluginutils'
 
-declare function createPlugin(options?: VueJSXPluginOptions): Plugin
+declare interface CommonOptions{
+    include?: FilterPattern;
+    exclude?: FilterPattern;
+}
+
+declare function createPlugin(options?: VueJSXPluginOptions & CommonOptions): Plugin
 
 export default createPlugin
