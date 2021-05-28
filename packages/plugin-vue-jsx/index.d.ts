8382294ac0fd11142c3424453d36c6f44f52bfb6
Shinigami <chrissi92@hotmail.de>
2021-05-28 14:12:00 +0200

    chore: apply formatting

--- packages/plugin-vue-jsx/index.d.ts
+++ packages/plugin-vue-jsx/index.d.ts
@@ -2,7 +2,7 @@ import { Plugin } from 'vite'
 import { VueJSXPluginOptions } from '@vue/babel-plugin-jsx'
 import { FilterPattern } from '@rollup/pluginutils'
 
-declare interface CommonOptions{
+declare interface CommonOptions {
     include?: FilterPattern;
     exclude?: FilterPattern;
 }
