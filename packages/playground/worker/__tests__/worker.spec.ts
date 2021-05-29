d78191c5ce5f9c0e5a2329c7b113fc25b27535b9
fi3ework <fi3ework@gmail.com>
2021-05-29 11:09:38 +0800

    feat(plugins/worker): support SharedWorker (resolve #2093) (#2505)

--- packages/playground/worker/__tests__/worker.spec.ts
+++ packages/playground/worker/__tests__/worker.spec.ts
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import { untilUpdated, isBuild, testDir } from '../../testUtils'
+import { Page } from 'playwright-chromium'
 
 test('normal', async () => {
   await page.click('.ping')
@@ -16,17 +17,42 @@ test('inlined', async () => {
   await untilUpdated(() => page.textContent('.pong-inline'), 'pong')
 })
 
+const waitSharedWorkerTick = (
+  (resolvedSharedWorkerCount: number) => async (page: Page) => {
+    await untilUpdated(async () => {
+      const count = await page.textContent('.tick-count')
+      // ignore the initial 0
+      return count === '1' ? 'page loaded' : ''
+    }, 'page loaded')
+    // test.concurrent sequential is not guaranteed
+    // force page to wait to ensure two pages overlap in time
+    resolvedSharedWorkerCount++
+
+    await untilUpdated(() => {
+      return resolvedSharedWorkerCount === 2 ? 'all pages loaded' : ''
+    }, 'all pages loaded')
+  }
+)(0)
+
+test.concurrent.each([[true], [false]])('shared worker', async (doTick) => {
+  if (doTick) {
+    await page.click('.tick-shared')
+  }
+  await waitSharedWorkerTick(page)
+})
+
 if (isBuild) {
   // assert correct files
   test('inlined code generation', async () => {
     const assetsDir = path.resolve(testDir, 'dist/assets')
     const files = fs.readdirSync(assetsDir)
-    // should have only 1 worker chunk
-    expect(files.length).toBe(2)
+    // should have 2 worker chunk
+    expect(files.length).toBe(3)
     const index = files.find((f) => f.includes('index'))
     const content = fs.readFileSync(path.resolve(assetsDir, index), 'utf-8')
     // chunk
     expect(content).toMatch(`new Worker("/assets`)
+    expect(content).toMatch(`new SharedWorker("/assets`)
     // inlined
     expect(content).toMatch(`(window.URL||window.webkitURL).createObjectURL`)
   })
