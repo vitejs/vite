d92af2b58329acce5449c44874880c3bd6187113
ygj6 <yuangongji@foxmail.com>
2021-05-29 22:04:50 +0800

    fix(playground): fix an eslint error in testUtils (#3584)

--- packages/playground/testUtils.ts
+++ packages/playground/testUtils.ts
@@ -23,7 +23,7 @@ Object.keys(colors).forEach((color) => {
 })
 
 function componentToHex(c: number): string {
-  var hex = c.toString(16)
+  const hex = c.toString(16)
   return hex.length == 1 ? '0' + hex : hex
 }
 
