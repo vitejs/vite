98445e2cb9a6ba279d989b9ea6d1a505a4bc4613
Anthony Fu <anthonyfu117@hotmail.com>
2021-05-30 11:44:29 +0800

    chore: rename

--- packages/plugin-vue-jsx/index.d.ts
+++ packages/plugin-vue-jsx/index.d.ts
@@ -2,11 +2,13 @@ import { Plugin } from 'vite'
 import { VueJSXPluginOptions } from '@vue/babel-plugin-jsx'
 import { FilterPattern } from '@rollup/pluginutils'
 
-declare interface CommonOptions {
+declare interface FilterOptions {
   include?: FilterPattern
   exclude?: FilterPattern
 }
 
-declare function createPlugin(options?: VueJSXPluginOptions & CommonOptions): Plugin
+declare function createPlugin(
+  options?: VueJSXPluginOptions & FilterOptions
+): Plugin
 
 export default createPlugin
