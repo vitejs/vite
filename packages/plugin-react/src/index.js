10806d58181ad038a2776d0c45afb013db3ce3b5
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-05-31 12:56:29 -0400

    fix: export preambleCode

--- packages/plugin-react/src/index.js
+++ packages/plugin-react/src/index.js
@@ -210,6 +210,8 @@ export default function viteReact(opts = {}) {
   ]
 }
 
+viteReact.preambleCode = preambleCode
+
 function interopDefault(promise) {
   return promise.then((module) => module.default || module)
 }
