f1aab74727562c10c9dcfe12e41cbdca6aa0d9fd
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2021-06-05 10:23:08 +0200

    feat: client event target improvements

--- packages/playground/hmr/listener.js
+++ packages/playground/hmr/listener.js
@@ -1,11 +1,11 @@
 import { viteEventTarget } from '/@vite/client'
 
-viteEventTarget.addEventListener('vite:connected', () => {
-  console.log(`>>> vite:connected`)
+viteEventTarget.addEventListener('vite:connected', (event) => {
+  console.log(`>>> vite:connected -- ${event.detail.type}`)
 })
-viteEventTarget.addEventListener('vite:update', () => {
-  console.log(`>>> vite:update`)
+viteEventTarget.addEventListener('vite:update', (event) => {
+  console.log(`>>> vite:update -- ${event.detail.type}`)
 })
-viteEventTarget.addEventListener('vite:error', () => {
-  console.log(`>>> vite:error`)
+viteEventTarget.addEventListener('vite:error', (event) => {
+  console.log(`>>> vite:error -- ${event.detail.type}`)
 })
