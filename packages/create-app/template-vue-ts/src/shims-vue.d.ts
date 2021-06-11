4f22703528b80e36cffbb35d527a5e12f7479efb
falstack <icesilt@outlook.com>
2021-06-11 14:23:51 +0800

    Update shims-vue.d.ts

--- packages/create-app/template-vue-ts/src/shims-vue.d.ts
+++ packages/create-app/template-vue-ts/src/shims-vue.d.ts
@@ -1,5 +1,9 @@
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent<
+    Record<string, unknown>,
+    Record<string, unknown>,
+    unknown
+  >
   export default component
 }
