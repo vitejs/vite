7efd40b4b1c67ae344d63b203ec722cf0ab32a87
Dominik G <dominik.goepel@gmx.de>
2021-06-11 19:52:10 +0200

    fix(create-app): update svelte templates to use named export of vite-… (#3760)

--- packages/create-app/template-svelte-ts/svelte.config.js
+++ packages/create-app/template-svelte-ts/svelte.config.js
@@ -1,6 +1,6 @@
-const sveltePreprocess = require('svelte-preprocess')
+import sveltePreprocess from 'svelte-preprocess'
 
-module.exports = {
+export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess()
