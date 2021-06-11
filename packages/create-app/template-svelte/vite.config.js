8afbeb97c18e9166f30eefebf10bc10778e4c968
dominikg <dominik.goepel@gmx.de>
2021-06-11 14:52:24 +0200

    fix(create-app): update svelte templates to use named export of vite-plugin-svelte

--- packages/create-app/template-svelte/vite.config.js
+++ packages/create-app/template-svelte/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import svelte from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
