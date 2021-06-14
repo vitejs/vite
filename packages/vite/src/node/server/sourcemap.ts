bd5d6c082b8ad85d2c1f1ec1aa59b1a89c808710
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-06-14 18:27:05 -0400

    fix(sourcemap): use `moduleGraph` to fix url sources

--- packages/vite/src/node/server/sourcemap.ts
+++ packages/vite/src/node/server/sourcemap.ts
@@ -1,21 +1,28 @@
 import { promises as fs } from 'fs'
 import path from 'path'
+import { ModuleGraph } from './moduleGraph'
 
 export async function injectSourcesContent(
   map: { sources: string[]; sourcesContent?: string[]; sourceRoot?: string },
   file: string,
-  useResolvedSources?: boolean
+  moduleGraph?: ModuleGraph
 ): Promise<void> {
   const sourceRoot = await fs.realpath(
     path.resolve(path.dirname(file), map.sourceRoot || '')
   )
-  map.sourcesContent = []
+  const needsContent = !map.sourcesContent
+  if (needsContent) {
+    map.sourcesContent = []
+  }
   await Promise.all(
     map.sources.filter(Boolean).map(async (sourcePath, i) => {
-      const source = path.resolve(sourceRoot, decodeURI(sourcePath))
-      map.sourcesContent![i] = await fs.readFile(source, 'utf-8')
-      if (useResolvedSources) {
-        map.sources[i] = source
+      const mod = await moduleGraph?.getModuleByUrl(sourcePath)
+      sourcePath = mod?.file || path.resolve(sourceRoot, decodeURI(sourcePath))
+      if (moduleGraph) {
+        map.sources[i] = sourcePath
+      }
+      if (needsContent) {
+        map.sourcesContent![i] = await fs.readFile(sourcePath, 'utf-8')
       }
     })
   )
