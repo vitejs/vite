3cc19f08b02ee9ce9de54da7eb618e97ddf144b4
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-06-15 14:09:20 -0400

    fix(ssr): ignore "vm.js" frames in stacktrace

--- packages/vite/src/node/ssr/ssrStacktrace.ts
+++ packages/vite/src/node/ssr/ssrStacktrace.ts
@@ -18,6 +18,8 @@ export function ssrRewriteStacktrace(
     .map((line, i) => {
       return line.replace(stackFrameRE, (input, varName, url, line, column) => {
         if (!url) return input
+        // Ignore frames for internal "vm" module
+        if (url === 'vm.js') return ''
 
         const mod = moduleGraph.urlToModuleMap.get(url)
         const rawSourceMap = mod?.ssrTransformResult?.map
@@ -63,5 +65,5 @@ export function ssrRewriteStacktrace(
       })
     : error.message
 
-  return message + '\n\n' + stackFrames.join('\n')
+  return message + '\n\n' + stackFrames.filter(Boolean).join('\n')
 }
