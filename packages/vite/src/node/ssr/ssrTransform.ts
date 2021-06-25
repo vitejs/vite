0ad4425effd3dc815675c2050d9883f293309c0e
Glen Huang <heyhgl@gmail.com>
2021-06-25 10:47:07 +0800

    fix(ssr): not flatten export * as (fix #3934)

--- packages/vite/src/node/ssr/ssrTransform.ts
+++ packages/vite/src/node/ssr/ssrTransform.ts
@@ -133,9 +133,15 @@ export async function ssrTransform(
 
     // export * from './foo'
     if (node.type === 'ExportAllDeclaration') {
-      const importId = defineImport(node, node.source.value as string)
-      s.remove(node.start, node.end)
-      s.append(`\n${ssrExportAllKey}(${importId})`)
+      if (!node.exported) {
+        const importId = defineImport(node, node.source.value as string)
+        s.remove(node.start, node.end)
+        s.append(`\n${ssrExportAllKey}(${importId})`)
+      } else {
+        const importId = defineImport(node, node.source.value as string)
+        defineExport(node.exported.name, `${importId}`)
+        s.remove(node.start, node.end)
+      }
     }
   }
 
