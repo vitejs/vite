8467f64aee218dcb3135bb832ccaed36d647052f
Yelmor <yelmor@outlook.com>
2021-06-27 19:49:42 +0800

    fix: resolve nested dependencies (#3254) (#3753)
    * fix: resolve nested dependencies (#3254)
    
    * chore: inline packages
    
    * chore: force optimize
    
    * chore: cleanup
    
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- packages/vite/src/node/optimizer/esbuildDepPlugin.ts
+++ packages/vite/src/node/optimizer/esbuildDepPlugin.ts
@@ -94,7 +94,7 @@ export function esbuildDepPlugin(
                 namespace: 'dep'
               }
             : {
-                path: require.resolve(qualified[flatId], {
+                path: require.resolve(id, {
                   paths: [resolveDir]
                 })
               }
