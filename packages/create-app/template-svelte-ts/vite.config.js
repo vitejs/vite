f8ea78ec610512b126fa23d3a9b4212760921810
Jonathan Dickinson <jonathan@dickinson.id>
2021-06-30 11:58:48 -0400

    fix: redirect src/ to /src/ for svelte-ts build
    Add alias for "src/" and redirect to "/src/" for svelte-ts template.
    This fixes a conflict between VS Intellisense and Rollup

--- packages/create-app/template-svelte-ts/vite.config.js
+++ packages/create-app/template-svelte-ts/vite.config.js
@@ -3,5 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      // Allow imports to specify, e.g. import 'src/example'
+      { find: /^src\/(.*)/, replacement: '/src/$1' }
+    ]
+  },
   plugins: [svelte()]
 })
