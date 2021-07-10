486b0182304499d840e38901d600c11ef84b67ab
CSY54 <18496305+CSY54@users.noreply.github.com>
2021-07-10 20:15:18 +0800

    fix: update link to vue rfc #4200 (#4203)

--- packages/create-app/template-vue/src/App.vue
+++ packages/create-app/template-vue/src/App.vue
@@ -7,7 +7,7 @@
 import HelloWorld from './components/HelloWorld.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
 </script>
 
 <style>
