de7b5adcbba3938090e743c9811244e0cc23d607
François Wouts <f@zenc.io>
2021-07-17 08:05:48 +1000

    fix: run everything in a single Jest test to ensure module cache isn't cleared

--- packages/playground/css-config-caching-b/__tests__/css.spec.ts
+++
@@ -1,8 +0,0 @@
-import { getColor } from '../../testUtils'
-
-test('postcss config', async () => {
-  const a = await page.$('.postcss-a')
-  expect(await getColor(a)).toBe('black')
-  const b = await page.$('.postcss-b')
-  expect(await getColor(b)).toBe('green')
-})
