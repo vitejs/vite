885496bf4b1e447775709bb43fdcc7f50bdea05e
ygj6 <yuangongji@foxmail.com>
2021-07-19 16:05:55 +0800

    fix(util): copyDir may cause an infinite loop (#4310)

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -351,6 +351,9 @@ export function copyDir(srcDir: string, destDir: string): void {
   fs.mkdirSync(destDir, { recursive: true })
   for (const file of fs.readdirSync(srcDir)) {
     const srcFile = path.resolve(srcDir, file)
+    if (srcFile === destDir) {
+      continue
+    }
     const destFile = path.resolve(destDir, file)
     const stat = fs.statSync(srcFile)
     if (stat.isDirectory()) {
