7a3c6e616385cbc069620ae583d6739a972c0ead
OneNail <31649110+OneNail@users.noreply.github.com>
2021-07-21 04:06:28 +0800

    fix: reuse the old preprocessor after changing the lang attr (#4224)
    * fix(plugin-vue): reuse the old preprocessor after changing the lang attr
    
    * feat(playground): add stylus + change lang test in vue
    
    * chore: add eol line break
    
    Co-authored-by: Shinigami <chrissi92@hotmail.de>

--- packages/playground/vue/__tests__/vue.spec.ts
+++ packages/playground/vue/__tests__/vue.spec.ts
@@ -56,6 +56,21 @@ describe('pre-processors', () => {
     )
     await untilUpdated(() => getColor(el), 'blue')
   })
+
+  test('stylus + change lang', async () => {
+    expect(await getColor('p.pug-stylus')).toBe('orange')
+    editFile('PreProcessors.vue', (code) =>
+      code
+        .replace('<style lang="stylus">', '<style lang="scss">')
+        .replace('color = orange', '$color: yellow;')
+        .replace('color: color', '{ color: $color; }')
+    )
+    await untilUpdated(() => getColor('p.pug-stylus'), 'yellow')
+    editFile('PreProcessors.vue', (code) =>
+      code.replace('$color: yellow;', '$color: orange;')
+    )
+    await untilUpdated(() => getColor('p.pug-stylus'), 'orange')
+  })
 })
 
 describe('css modules', () => {
