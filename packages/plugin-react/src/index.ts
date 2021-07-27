7c4d6106e09901507524ad06b5f43c9e4d56177d
Peng Xiao <pengxiao@outlook.com>
2021-07-27 23:48:04 +0800

    fix: tests

--- packages/plugin-react/src/index.ts
+++ packages/plugin-react/src/index.ts
@@ -248,13 +248,6 @@ export default function viteReact(opts: Options = {}): PluginOption[] {
   const viteReactJsx: Plugin = {
     name: 'vite:react-jsx',
     enforce: 'pre',
-    config: () => ({
-      // react/jsx-runtime is in CJS format
-      // we want to explicitly cast it to ESM
-      optimizeDeps: {
-        include: ['react/jsx-runtime']
-      }
-    }),
     resolveId(id: string) {
       return id === runtimeId ? id : null
     },
