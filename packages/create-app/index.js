def20b2ae50f5fd840c222e9d726577d0c5b9bee
odan <odan3240@gmail.com>
2021-08-08 22:11:35 +0900

    fix(create-app): fix alternative command of pnpm

--- packages/create-app/index.js
+++ packages/create-app/index.js
@@ -5,7 +5,7 @@ const { yellow, green } = require('kolorist')
 
 const alternativeCommands = {
   yarn: 'yarn create vite',
-  pnpm: 'pnpx create-react-app vite',
+  pnpm: 'pnpx create-vite-app',
   npm: 'npm init vite',
   unknown: 'npm init vite'
 }
