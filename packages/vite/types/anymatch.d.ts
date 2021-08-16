de113954355c952726f697e71ed5facb643a6083
heyunfei.i <heyunfei.i@bytedance.com>
2021-08-16 11:40:15 +0800

    fix: delete unused types and fix api-extrcor warning

--- packages/vite/types/anymatch.d.ts
+++ packages/vite/types/anymatch.d.ts
@@ -1,28 +1,5 @@
-type AnymatchFn = (testString: string) => boolean
-type AnymatchPattern = string | RegExp | AnymatchFn
+export type AnymatchFn = (testString: string) => boolean
+export type AnymatchPattern = string | RegExp | AnymatchFn
 type AnymatchMatcher = AnymatchPattern | AnymatchPattern[]
-type AnymatchTester = {
-  (testString: string | any[], returnIndex: true): number
-  (testString: string | any[]): boolean
-}
-
-type PicomatchOptions = { dot: boolean }
-
-declare const anymatch: {
-  (matchers: AnymatchMatcher): AnymatchTester
-  (
-    matchers: AnymatchMatcher,
-    testString: null,
-    returnIndex: true | PicomatchOptions
-  ): AnymatchTester
-  (
-    matchers: AnymatchMatcher,
-    testString: string | any[],
-    returnIndex: true | PicomatchOptions
-  ): number
-  (matchers: AnymatchMatcher, testString: string | any[]): boolean
-}
 
 export { AnymatchMatcher as Matcher }
-export { AnymatchTester as Tester }
-export default anymatch
