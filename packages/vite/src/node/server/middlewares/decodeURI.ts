200652c954cac2e39cb7f72ace39be79cb1d8797
hanlee <me@hanlee.co>
2021-08-25 19:57:49 +0900

    fix: decode url for transform middleware

---
+++ packages/vite/src/node/server/middlewares/decodeURI.ts
@@ -0,0 +1,10 @@
+import { Connect } from 'types/connect'
+
+export function decodeURIMiddleware(): Connect.NextHandleFunction {
+  // Keep the named function. The name is visible in debug logs via `DEBUG=connect:dispatcher ...`
+  return function viteDecoreURIMiddleware(req, _, next) {
+    // #2195
+    req.url = decodeURI(req.url!)
+    next()
+  }
+}
