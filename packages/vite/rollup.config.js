a854a8687006a287aaee9c08431dba82397325fd
hanlee <me@hanlee.co>
2021-08-26 08:54:50 +0900

    chore: sort licenses

--- packages/vite/rollup.config.js
+++ packages/vite/rollup.config.js
@@ -346,7 +346,11 @@ function licensePlugin() {
         coreLicense +
         `\n# Licenses of bundled dependencies\n` +
         `The published Vite artifact additionally contains code with the following licenses:\n` +
-        `${Array.from(licenses).join(', ')}\n\n` +
+        `${Array.from(licenses)
+          .sort((a, b) => {
+            return a.startsWith('(') ? 1 : b.startsWith('(') ? -1 : b - a
+          })
+          .join(', ')}\n\n` +
         `# Bundled dependencies:\n` +
         dependencyLicenseTexts
       const existingLicenseText = fs.readFileSync('LICENSE.md', 'utf8')
