d8c5c6c955f6bbfe7a12cc7ec8512c579bf30380
Han（ハン） <me@hanlee.co>
2021-08-25 23:29:53 +0900

    Update packages/vite/src/node/server/middlewares/decodeURI.ts
    Co-authored-by: Jeff Yang <32727188+ydcjeff@users.noreply.github.com>

--- packages/vite/src/node/server/middlewares/decodeURI.ts
+++ packages/vite/src/node/server/middlewares/decodeURI.ts
@@ -2,7 +2,7 @@ import type { Connect } from 'types/connect'
 
 export function decodeURIMiddleware(): Connect.NextHandleFunction {
   // Keep the named function. The name is visible in debug logs via `DEBUG=connect:dispatcher ...`
-  return function viteDecoreURIMiddleware(req, _, next) {
+  return function viteDecodeURIMiddleware(req, _, next) {
     // #2195
     req.url = decodeURI(req.url!)
     next()
