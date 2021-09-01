c87763c1418d1ba876eae13d139eba83ac6f28b2
milahu <milahu@gmail.com>
2021-09-01 15:23:07 +0200

    fix: handle error in numberToPos and formatError (#4782)

--- packages/vite/src/node/server/pluginContainer.ts
+++ packages/vite/src/node/server/pluginContainer.ts
@@ -51,7 +51,6 @@ import {
 } from 'rollup'
 import * as acorn from 'acorn'
 import acornClassFields from 'acorn-class-fields'
-import acornNumericSeparator from 'acorn-numeric-separator'
 import acornStaticClassFeatures from 'acorn-static-class-features'
 import { RawSourceMap } from '@ampproject/remapping/dist/types/types'
 import { combineSourcemaps } from '../utils'
@@ -116,8 +115,7 @@ type PluginContext = Omit<
 
 export let parser = acorn.Parser.extend(
   acornClassFields,
-  acornStaticClassFeatures,
-  acornNumericSeparator
+  acornStaticClassFeatures
 )
 
 export async function createPluginContainer(
@@ -285,10 +283,22 @@ export async function createPluginContainer(
           : // some rollup plugins, e.g. json, sets position instead of pos
             (err as any).position
       if (pos != null) {
-        err.loc = err.loc || {
-          file: err.id,
-          ...numberToPos(ctx._activeCode, pos)
+        let errLocation;
+        try {
+          errLocation = numberToPos(ctx._activeCode, pos);
         }
+        catch (err2) {
+          logger.error(
+            chalk.red(`Error in error handler:\n${err2.stack || err2.message}\n`),
+            // print extra newline to separate the two errors
+            { error: err2 }
+          )
+          throw err;
+        }
+        err.loc = err.loc || {
+            file: err.id,
+            ...errLocation
+        };
         err.frame = err.frame || generateCodeFrame(ctx._activeCode, pos)
       } else if (err.loc) {
         // css preprocessors may report errors in an included file
@@ -387,8 +397,7 @@ export async function createPluginContainer(
         parser = acorn.Parser.extend(
           ...[
             acornClassFields,
-            acornStaticClassFeatures,
-            acornNumericSeparator
+            acornStaticClassFeatures
           ].concat(options.acornInjectPlugins)
         )
       }
