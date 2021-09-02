29bb101ac539451eaed1b74b08beab330146e44b
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-09-02 13:13:32 -0700

    chore: surface package resolution failures (#4822)

--- packages/vite/src/node/ssr/ssrExternal.ts
+++ packages/vite/src/node/ssr/ssrExternal.ts
@@ -63,6 +63,9 @@ export function resolveSSRExternal(
       requireEntry = require.resolve(id, { paths: [root] })
     } catch (e) {
       // resolve failed, assume include
+      config.logger.warn(
+        `Bundling package for SSR due to resolve failure. ${e.message}`
+      )
       continue
     }
     if (!entry) {
