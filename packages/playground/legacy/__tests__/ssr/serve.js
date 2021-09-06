vitejs/vite

commit 1f068fcec38fc07c34e75a19821064386e460907

fix(plugin-legacy): skip in SSR build (#4536)

packages/plugin-legacy/index.js
@@ -97,7 +97,7 @@ function viteLegacyPlugin(options = {}) {
     apply: 'build',
 
     configResolved(config) {
-      if (config.build.minify === 'esbuild') {
+      if (!config.build.ssr && config.build.minify === 'esbuild') {
         throw new Error(
           `Can't use esbuild as the minifier when targeting legacy browsers ` +
             `because esbuild minification is not legacy safe.`
@@ -106,6 +106,10 @@ function viteLegacyPlugin(options = {}) {
     },
 
     async generateBundle(opts, bundle) {
+      if (config.build.ssr) {
+        return
+      }
+
       if (!isLegacyBundle(bundle, opts)) {
         if (!modernPolyfills.size) {
           return
@@ -170,7 +174,7 @@ function viteLegacyPlugin(options = {}) {
       }
       config = _config
 
-      if (!genLegacy) {
+      if (!genLegacy || config.build.ssr) {
         return
       }
 
@@ -226,6 +230,10 @@ function viteLegacyPlugin(options = {}) {
     },
 
     renderChunk(raw, chunk, opts) {
+      if (config.build.ssr) {
+        return
+      }
+
       if (!isLegacyChunk(chunk, opts)) {
         if (
           options.modernPolyfills &&
@@ -315,6 +323,7 @@ function viteLegacyPlugin(options = {}) {
     },
 
     transformIndexHtml(html, { chunk }) {
+      if (config.build.ssr) return
       if (!chunk) return
       if (chunk.fileName.includes('-legacy')) {
         // The legacy bundle is built first, and its index.html isn't actually
@@ -420,6 +429,10 @@ function viteLegacyPlugin(options = {}) {
     },
 
     generateBundle(opts, bundle) {
+      if (config.build.ssr) {
+        return
+      }
+
       if (isLegacyBundle(bundle, opts)) {
         // avoid emitting duplicate assets
         for (const name in bundle) {

commit e5954c739a74961ec39a72ffa0bafb8bef025032

fix(plugin-legacy): fix regression introduced in #4536

packages/playground/legacy/entry-server.js
@@ -1,0 +1,7 @@
+// This counts as 'server-side' rendering, yes?
+export async function render() {
+  return /* html */ `
+    <div id="app">Hello</div>
+    <div id="env">${import.meta.env.LEGACY}</div>
+  `
+}

packages/plugin-legacy/index.js
@@ -451,12 +451,14 @@ function viteLegacyPlugin(options = {}) {
   const legacyEnvPlugin = {
     name: 'legacy-env',
 
-    config(_, env) {
+    config(config, env) {
       if (env) {
         return {
           define: {
             'import.meta.env.LEGACY':
-              env.command === 'serve' ? false : legacyEnvVarMarker
+              env.command === 'serve' || config.build.ssr
+                ? false
+                : legacyEnvVarMarker
           }
         }
       } else {

packages/playground/legacy/__tests__/ssr/legacy-ssr.spec.ts
@@ -1,0 +1,20 @@
+import { isBuild } from '../../../testUtils'
+import { port } from './serve'
+
+const url = `http://localhost:${port}`
+
+if (isBuild) {
+  test('should work', async () => {
+    await page.goto(url)
+    expect(await page.textContent('#app')).toMatch('Hello')
+  })
+
+  test('import.meta.env.LEGACY', async () => {
+    // SSR build is always modern
+    expect(await page.textContent('#env')).toMatch('false')
+  })
+} else {
+  // this test doesn't support serve mode
+  // must contain at least one test
+  test('should work', () => void 0)
+}

packages/playground/legacy/__tests__/ssr/serve.js
@@ -1,0 +1,52 @@
+// @ts-check
+// this is automtically detected by scripts/jestPerTestSetup.ts and will replace
+// the default e2e test serve behavior
+const path = require('path')
+
+const port = (exports.port = 9527)
+
+/**
+ * @param {string} root
+ * @param {boolean} _isProd
+ */
+exports.serve = async function serve(root, _isProd) {
+  const { build } = require('vite')
+  await build({
+    root,
+    logLevel: 'silent',
+    build: {
+      target: 'esnext',
+      ssr: 'entry-server.js',
+      outDir: 'dist/server'
+    }
+  })
+
+  const express = require('express')
+  const app = express()
+
+  app.use('/', async (_req, res) => {
+    const { render } = require(path.resolve(
+      root,
+      './dist/server/entry-server.js'
+    ))
+    const html = await render()
+    res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+  })
+
+  return new Promise((resolve, reject) => {
+    try {
+      const server = app.listen(port, () => {
+        resolve({
+          // for test teardown
+          async close() {
+            await new Promise((resolve) => {
+              server.close(resolve)
+            })
+          }
+        })
+      })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}