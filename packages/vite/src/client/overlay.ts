e5b472d12e4bdaf7c0a2bd4015e73b40a716dac8
Naitik Shah <n@daaku.org>
2021-09-06 15:19:25 +0400

    fix(overlay): handle missing customElements (#4856)

--- packages/vite/src/client/overlay.ts
+++ packages/vite/src/client/overlay.ts
@@ -182,4 +182,6 @@ export class ErrorOverlay extends HTMLElement {
 }
 
 export const overlayId = 'vite-error-overlay'
-!customElements.get(overlayId) && customElements.define(overlayId, ErrorOverlay)
+if (customElements && !customElements.get(overlayId)) {
+  customElements.define(overlayId, ErrorOverlay)
+}
