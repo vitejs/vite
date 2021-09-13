fa3d8f05111de9d8fc3c39baaf7a74c869c22fc9
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-09-12 20:18:32 -0700

    chore: move spa fallback middleware to new file

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -15,12 +15,12 @@ import { FSWatcher, WatchOptions } from 'types/chokidar'
 import { createWebSocketServer, WebSocketServer } from './ws'
 import { baseMiddleware } from './middlewares/base'
 import { proxyMiddleware, ProxyOptions } from './middlewares/proxy'
+import { spaFallbackMiddleware } from './middlewares/spaFallback'
 import { transformMiddleware } from './middlewares/transform'
 import {
   createDevHtmlTransformFn,
   indexHtmlMiddleware
 } from './middlewares/indexHtml'
-import history from 'connect-history-api-fallback'
 import {
   serveRawFsMiddleware,
   servePublicMiddleware,
@@ -29,7 +29,7 @@ import {
 import { timeMiddleware } from './middlewares/time'
 import { ModuleGraph, ModuleNode } from './moduleGraph'
 import { Connect } from 'types/connect'
-import { createDebugger, ensureLeadingSlash, normalizePath } from '../utils'
+import { ensureLeadingSlash, normalizePath } from '../utils'
 import { errorMiddleware, prepareError } from './middlewares/error'
 import { handleHMRUpdate, HmrOptions, handleFileAddUnlink } from './hmr'
 import { openBrowser } from './openBrowser'
@@ -500,33 +500,9 @@ export async function createServer(
   middlewares.use(serveRawFsMiddleware(server))
   middlewares.use(serveStaticMiddleware(root, config))
 
-  function viteSpaFallbackMiddleware(
-    req: http.IncomingMessage,
-    res: http.ServerResponse,
-    next: Connect.NextFunction
-  ) {
-    return history({
-      logger: createDebugger('vite:spa-fallback'),
-      // support /dir/ without explicit index.html
-      rewrites: [
-        {
-          from: /\/$/,
-          to({ parsedUrl }: any) {
-            const rewritten = parsedUrl.pathname + 'index.html'
-            if (fs.existsSync(path.join(root, rewritten))) {
-              return rewritten
-            } else {
-              return `/index.html`
-            }
-          }
-        }
-      ]
-    })(req, res, next)
-  }
-
   // spa fallback
   if (!middlewareMode || middlewareMode === 'html') {
-    middlewares.use(viteSpaFallbackMiddleware)
+    middlewares.use(spaFallbackMiddleware(root))
   }
 
   // run post config hooks
