23799dc91ca3d5f238212ae5a4719d7edceaf246
Francois Valdy <francois.valdy@gmail.com>
2021-09-17 10:42:24 +0200

    Add some exports to node bundle

--- packages/vite/src/node/index.ts
+++ packages/vite/src/node/index.ts
@@ -1,11 +1,13 @@
 export * from './config'
 export { createServer } from './server'
+export { createPluginContainer } from './server/pluginContainer'
 export { build } from './build'
 export { optimizeDeps } from './optimizer'
+export { scanImports } from './optimizer/scan'
 export { send } from './server/send'
 export { createLogger } from './logger'
 export { resolvePackageData, resolvePackageEntry } from './plugins/resolve'
-export { normalizePath } from './utils'
+export { normalizePath, flattenId } from './utils'
 
 // additional types
 export type {
