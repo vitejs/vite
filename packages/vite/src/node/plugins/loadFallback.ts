0260a0047c9790ceb8ccdc2eb3f3606bf970b573
patak <matias.capeletto@gmail.com>
2021-09-19 07:52:56 +0200

    fix: normalize internal plugin names (#4976)

--- packages/vite/src/node/plugins/loadFallback.ts
+++ packages/vite/src/node/plugins/loadFallback.ts
@@ -2,9 +2,12 @@ import { promises as fs } from 'fs'
 import { Plugin } from '..'
 import { cleanUrl } from '../utils'
 
+/**
+ * A plugin to provide build load fallback for arbitrary request with queries.
+ */
 export function loadFallbackPlugin(): Plugin {
   return {
-    name: 'load-fallback',
+    name: 'vite:load-fallback',
     async load(id) {
       try {
         return fs.readFile(cleanUrl(id), 'utf-8')
