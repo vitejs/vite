9c0ffea8371f605f09230c33caedf56d7b5ec039
ygj6 <chieffochen2021>
2021-09-22 19:57:17 +0800

    fix: modify JavaScript parser's ecmaVersion option to 'latest'

--- packages/vite/src/node/ssr/ssrTransform.ts
+++ packages/vite/src/node/ssr/ssrTransform.ts
@@ -33,7 +33,7 @@ export async function ssrTransform(
 
   const ast = parser.parse(code, {
     sourceType: 'module',
-    ecmaVersion: 2022,
+    ecmaVersion: 'latest',
     locations: true
   }) as any
 
