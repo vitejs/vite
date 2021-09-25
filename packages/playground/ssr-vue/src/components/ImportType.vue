fe8e95990f4752eb987c8884c50b1f7f259d4fb0
Pavel <pbuzdin@users.noreply.github.com>
2021-09-25 18:52:58 +0300

    these files are not in use anymore

--- packages/playground/ssr-vue/src/components/ImportType.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <p>import type should be removed without side-effect</p>
-</template>
-
-<script setup lang="ts">
-import { Foo } from 'dep-import-type/deep'
-const msg: Foo = {}
-</script>
