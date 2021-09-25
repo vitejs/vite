ddae45dbaed87b333f13b1a8077736bacc01e80c
Pavel <pbuzdin@users.noreply.github.com>
2021-09-25 18:50:29 +0300

    removed not working jsx plugin

--- packages/playground/ssr-vue/vite.config.js
+++ packages/playground/ssr-vue/vite.config.js
@@ -1,5 +1,4 @@
 const vuePlugin = require('@vitejs/plugin-vue')
-const vueJsx = require('@vitejs/plugin-vue-jsx')
 
 /**
  * @type {import('vite').UserConfig}
@@ -7,7 +6,6 @@ const vueJsx = require('@vitejs/plugin-vue-jsx')
 module.exports = {
   plugins: [
     vuePlugin(),
-    vueJsx(),
     {
       name: 'virtual',
       resolveId(id) {
