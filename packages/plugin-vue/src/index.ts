94f3630eed76cbef2f877e61fba67f33e1bc5126
Haoqun Jiang <haoqunjiang@gmail.com>
2021-09-30 22:10:15 +0800

    fix(plugin-vue): don't use object spread in the config hook
    The config object will be merged recursively in vite core, so object
    spread shouldn't be used:
    https://github.com/vitejs/vite/blob/5cf4e69cd3afc7f960e02072171c7c441747e8f0/packages/vite/src/node/config.ts#L308

--- packages/plugin-vue/src/index.ts
+++ packages/plugin-vue/src/index.ts
@@ -118,8 +118,7 @@ export default function vuePlugin(rawOptions: Options = {}): Plugin {
       return {
         define: {
           __VUE_OPTIONS_API__: true,
-          __VUE_PROD_DEVTOOLS__: false,
-          ...config.define
+          __VUE_PROD_DEVTOOLS__: false
         },
         ssr: {
           external: ['vue', '@vue/server-renderer']
