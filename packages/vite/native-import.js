44872c32919041c681eb3f638953fdc809e83355
Nate Moore <nate@skypack.dev>
2021-10-05 14:31:18 -0500

    refactor: remove unused file

--- packages/vite/native-import.js
+++
@@ -1,9 +0,0 @@
-/**
- * Typescript automatically compiles dynamic `import()` to `resolve()`
- * so this needs to be a raw JS file until `typescript@4.5.0` lands with
- * `module: "node12"` support.
- *
- * @param {string} fileUrl
- * @returns {any}
- */
-module.exports = (fileUrl) => import(fileUrl);
