d43b530d84354de29288f8b32df49960e7339a2c
patak <matias.capeletto@gmail.com>
2021-10-06 15:04:40 +0200

    chore: spacing

--- packages/vite/src/node/cli.ts
+++ packages/vite/src/node/cli.ts
@@ -150,7 +150,7 @@ cli
   .option(
     '--minify [minifier]',
     `[boolean | "terser" | "esbuild"] enable/disable minification, ` +
-    `or specify minifier to use (default: esbuild)`
+      `or specify minifier to use (default: esbuild)`
   )
   .option('--manifest', `[boolean] emit build manifest json`)
   .option('--ssrManifest', `[boolean] emit ssr manifest json`)
