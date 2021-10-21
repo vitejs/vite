bac8caec5f8f49584cfc1b8f9c23122e49a485bd
Máté Borkesz <mate.borkesz@feat.agency>
2021-10-21 16:48:09 +0200

    plugin-vue: vue-class-component HMR Component is missing template or render function bug fix

--- packages/plugin-vue/src/helper.ts
+++ packages/plugin-vue/src/helper.ts
@@ -2,9 +2,15 @@ export const EXPORT_HELPER_ID = 'plugin-vue:export-helper'
 
 export const helperCode = `
 export default (sfc, props) => {
+
+  if (typeof sfc === 'function' ) {
+    sfc = {};
+  }
+
   for (const [key, val] of props) {
     sfc[key] = val
   }
-  return sfc
+
+  return sfc;
 }
 `
