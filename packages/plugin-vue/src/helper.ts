e1db6215584d8362360b7968983157318efab01b
Máté Borkesz <mate.borkesz@feat.agency>
2021-11-09 13:32:42 +0100

    (plugin-vue):vue-class-component fix. helper.ts use sfc.__vccOpts

--- packages/plugin-vue/src/helper.ts
+++ packages/plugin-vue/src/helper.ts
@@ -3,14 +3,10 @@ export const EXPORT_HELPER_ID = 'plugin-vue:export-helper'
 export const helperCode = `
 export default (sfc, props) => {
 
-  if (!sfc || typeof sfc !== 'object') {
-    sfc = {};
-  }
-
+  const target = sfc.__vccOpts || sfc
   for (const [key, val] of props) {
-    sfc[key] = val
+    target[key] = val
   }
-
-  return sfc;
+  return target
 }
 `
