ae410522c2231ee19637c3ecf7da9f63377dc82f
Máté Borkesz <mate.borkesz@feat.agency>
2021-11-09 14:30:14 +0100

    closing ; (rerun checks)

--- packages/plugin-vue/src/helper.ts
+++ packages/plugin-vue/src/helper.ts
@@ -2,11 +2,10 @@ export const EXPORT_HELPER_ID = 'plugin-vue:export-helper'
 
 export const helperCode = `
 export default (sfc, props) => {
-
-  const target = sfc.__vccOpts || sfc
+  const target = sfc.__vccOpts || sfc;
   for (const [key, val] of props) {
-    target[key] = val
+    target[key] = val;
   }
-  return target
+  return target;
 }
 `
