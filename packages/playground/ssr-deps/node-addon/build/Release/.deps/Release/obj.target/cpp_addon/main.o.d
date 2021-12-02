b677cfa970f1ea2bd980672aeeedc837b72e7860
patak-js <matias.capeletto@gmail.com>
2021-12-02 22:08:55 +0100

    release: v2.7.0-beta.10

---
+++ packages/playground/ssr-deps/node-addon/build/Release/.deps/Release/obj.target/cpp_addon/main.o.d
@@ -0,0 +1,23 @@
+cmd_Release/obj.target/cpp_addon/main.o := g++ -o Release/obj.target/cpp_addon/main.o ../main.cpp '-DNODE_GYP_MODULE_NAME=cpp_addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DBUILDING_NODE_EXTENSION' -I/home/matias/.cache/node-gyp/16.9.1/include/node -I/home/matias/.cache/node-gyp/16.9.1/src -I/home/matias/.cache/node-gyp/16.9.1/deps/openssl/config -I/home/matias/.cache/node-gyp/16.9.1/deps/openssl/openssl/include -I/home/matias/.cache/node-gyp/16.9.1/deps/uv/include -I/home/matias/.cache/node-gyp/16.9.1/deps/zlib -I/home/matias/.cache/node-gyp/16.9.1/deps/v8/include  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++14 -MMD -MF ./Release/.deps/Release/obj.target/cpp_addon/main.o.d.raw   -c
+Release/obj.target/cpp_addon/main.o: ../main.cpp \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/node.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/v8.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/cppgc/common.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/v8config.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/v8-internal.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/v8-version.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/v8config.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/v8-platform.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/node_version.h \
+ /home/matias/.cache/node-gyp/16.9.1/include/node/v8.h
+../main.cpp:
+/home/matias/.cache/node-gyp/16.9.1/include/node/node.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/v8.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/cppgc/common.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/v8config.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/v8-internal.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/v8-version.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/v8config.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/v8-platform.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/node_version.h:
+/home/matias/.cache/node-gyp/16.9.1/include/node/v8.h:
