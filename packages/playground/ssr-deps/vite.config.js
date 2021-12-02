58dc6710f2a2ceebb986937b829f37c364380aa6
ygj6 <yuangongji@foxmail.com>
2021-12-02 14:32:52 +0800

    chore: update annotation

--- packages/playground/ssr-deps/vite.config.js
+++ packages/playground/ssr-deps/vite.config.js
@@ -3,8 +3,8 @@
  */
 module.exports = {
   resolve: {
-    // linked dependency `node-addon` need to preserve symlinks as node_modules dependency
-    // it cannot file to node_modules due to `.node` file cannot be unlink in Windows
+    // The dependency `node-addon` needs to be linked to node_modules and preserve symlinks,
+    // because the `.node` file cannot be unlinked in Windows after being filed to node_modules
     // ref: https://github.com/nodejs/node/issues/24878/
     preserveSymlinks: true
   }
