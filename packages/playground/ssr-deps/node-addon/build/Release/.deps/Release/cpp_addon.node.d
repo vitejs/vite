fb12992988940bf278fce5444f9b908b848560b0
ygj6 <7699524+ygj6@users.noreply.github.com>
2021-12-03 17:42:20 +0800

    chore: git ignores the compilation output dir (#5951)

--- packages/playground/ssr-deps/node-addon/build/Release/.deps/Release/cpp_addon.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/cpp_addon.node := ln -f "Release/obj.target/cpp_addon.node" "Release/cpp_addon.node" 2>/dev/null || (rm -rf "Release/cpp_addon.node" && cp -af "Release/obj.target/cpp_addon.node" "Release/cpp_addon.node")
