8d3df31b289420ab75a0962d1e9413e20a14cac0
yuuangzhang <yuuangzhang@tencent.com>
2021-12-09 16:17:48 +0800

    chore: update example

--- packages/playground/ssr-vue/src/pages/Home.vue
+++ packages/playground/ssr-vue/src/pages/Home.vue
@@ -5,6 +5,7 @@
   </p>
   <button @click="state.count++">count is: {{ state.count }}</button>
   <Foo />
+  <Button>externalBtn</Button>
   <p class="virtual">msg from virtual module: {{ foo.msg }}</p>
   <p class="inter">this will be styled with a font-face</p>
   <p class="import-meta-url">{{ state.url }}</p>
