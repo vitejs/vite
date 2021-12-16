633da271c5446ede6161b646a5bc7d6c438d86bd
patr0nus <dk4rest@gmail.com>
2021-12-16 23:14:21 +0800

    prettier

--- packages/playground/worker/classic-worker.ts
+++ packages/playground/worker/classic-worker.ts
@@ -1,7 +1,7 @@
 self.onmessage = (e) => {
   if (e.data === 'ping') {
     // Ensure that we are not in a module worker (calling importScripts in module workers throws an error).
-    self.importScripts();
+    self.importScripts()
     self.postMessage('pong')
   }
 }
