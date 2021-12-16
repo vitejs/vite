68627950c8b8f4efffbe90442d2136eedda5fe74
Jeff Yang <32727188+ydcjeff@users.noreply.github.com>
2021-12-17 04:45:30 +0630

    chore: remove node addon dir (#6107)

--- packages/playground/ssr-deps/node-addon/build/Release/.deps/Release/obj.target/cpp_addon.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/cpp_addon.node := g++ -o Release/obj.target/cpp_addon.node -shared -pthread -rdynamic -m64  -Wl,-soname=cpp_addon.node -Wl,--start-group Release/obj.target/cpp_addon/main.o -Wl,--end-group 
