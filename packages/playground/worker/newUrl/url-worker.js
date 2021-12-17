7b27c6934faf4a84767850ad6c2686e3c075efc0
poyoho <907415276@qq.com>
2021-12-17 20:24:27 +0800

    chore: Useless code

--- packages/playground/worker/newUrl/url-worker.js
+++ packages/playground/worker/newUrl/url-worker.js
@@ -1,5 +1,3 @@
 import constant from './module'
 
-// the comment to make this file not to be base64
-/*************************************************************************************/
 self.postMessage(constant)
