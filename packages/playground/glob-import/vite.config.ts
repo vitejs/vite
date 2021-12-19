a2136b12294371a18353b27ece1ba18e5001fcc4
poyoho <907415276@qq.com>
2021-12-19 11:02:11 +0800

    fix: alias option

--- packages/playground/glob-import/vite.config.ts
+++ packages/playground/glob-import/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   resolve: {
     alias: {
-      '@dir': path.resolve('../dir/')
+      '@dir': path.resolve('./dir/')
     }
   }
 })
