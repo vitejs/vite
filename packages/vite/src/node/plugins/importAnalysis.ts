ac06a46f2da7775b5123bbc51407c60c7a716e9f
Evan Boehs <evan@boehs.org>
2021-12-27 17:34:29 -0500

    Update importAnalysis.ts

--- packages/vite/src/node/plugins/importAnalysis.ts
+++ packages/vite/src/node/plugins/importAnalysis.ts
@@ -135,7 +135,7 @@ export function importAnalysisPlugin(config: ResolvedConfig): Plugin {
           ? `If you are using JSX, make sure to name the file with the .jsx or .tsx extension.`
           : `You may need to install appropriate plugins to handle the ${path.extname(
               importer
-            )} file format, or add "**/*${path.extname(importer)}" to \`assetsInclude\` to your configuration.`
+            )} file format, or if it's an asset, add "**/*${path.extname(importer)}" to \`assetsInclude\` in your configuration.`
 
         this.error(
           `Failed to parse source for import analysis because the content ` +
