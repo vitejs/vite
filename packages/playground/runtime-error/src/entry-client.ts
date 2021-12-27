3a51bc1427694f5f268a2e9164b2d2c617dd1613
Aaron Bassett <arbassett4@outlook.com>
2021-12-27 01:29:24 -0500

    chore: add more testing samples

--- packages/playground/runtime-error/src/entry-client.ts
+++ packages/playground/runtime-error/src/entry-client.ts
@@ -1,3 +1,7 @@
 document.querySelector<HTMLButtonElement>('#throwBtn').onclick = () => {
   throw new Error('Why did you click the button')
 }
+
+document.querySelector<HTMLButtonElement>('#invalidAccessor').onclick = () => {
+  window.doesnt.exists = 5
+}
