25f7a1fce1265e6c31ff9f0803bb05a4976dbbd3
Aron Griffis <aron@scampersand.com>
2021-12-27 20:30:40 -0500

    feat: lazy load terser worker

--- packages/vite/src/node/plugins/terser.ts
+++ packages/vite/src/node/plugins/terser.ts
@@ -4,18 +4,21 @@ import type { Terser } from 'types/terser'
 import type { ResolvedConfig } from '..'
 
 export function terserPlugin(config: ResolvedConfig): Plugin {
-  const worker = new Worker(
-    (basedir: string, code: string, options: Terser.MinifyOptions) => {
-      // when vite is linked, the worker thread won't share the same resolve
-      // root with vite itself, so we have to pass in the basedir and resolve
-      // terser first.
-      // eslint-disable-next-line node/no-restricted-require
-      const terserPath = require.resolve('terser', {
-        paths: [basedir]
-      })
-      return require(terserPath).minify(code, options) as Terser.MinifyOutput
-    }
-  )
+  const makeWorker = () =>
+    new Worker(
+      (basedir: string, code: string, options: Terser.MinifyOptions) => {
+        // when vite is linked, the worker thread won't share the same resolve
+        // root with vite itself, so we have to pass in the basedir and resolve
+        // terser first.
+        // eslint-disable-next-line node/no-restricted-require
+        const terserPath = require.resolve('terser', {
+          paths: [basedir]
+        })
+        return require(terserPath).minify(code, options) as Terser.MinifyOutput
+      }
+    )
+
+  let worker: ReturnType<typeof makeWorker>
 
   return {
     name: 'vite:terser',
@@ -33,10 +36,13 @@ export function terserPlugin(config: ResolvedConfig): Plugin {
       }
 
       // Do not minify ES lib output since that would remove pure annotations
-      // and break tree-shaking
+      // and break tree-shaking.
       if (config.build.lib && outputOptions.format === 'es') {
         return null
       }
+
+      // Lazy load worker.
+      worker ||= makeWorker()
 
       const res = await worker.run(__dirname, code, {
         safari10: true,
@@ -52,7 +58,7 @@ export function terserPlugin(config: ResolvedConfig): Plugin {
     },
 
     closeBundle() {
-      worker.stop()
+      worker?.stop()
     }
   }
 }
