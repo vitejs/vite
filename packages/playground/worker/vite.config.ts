a2a5f96b03015c8f3647ddd57aa3b2b0248876c3
poyoho <907415276@qq.com>
2021-12-28 21:14:11 +0800

    chore: remove useless code

--- packages/playground/worker/vite.config.ts
+++ packages/playground/worker/vite.config.ts
@@ -1,11 +1,7 @@
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 
-/**
- * @type {import('vite').UserConfig}
- */
 export default defineConfig({
-  plugins: [],
   build: {
     target: process.env.NODE_ENV === 'production' ? 'chrome60' : 'esnext'
   },
