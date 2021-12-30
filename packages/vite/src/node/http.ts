bd5149876b686926164e7e2228569fb8840c9acc
Bogdan Chadkin <trysound@yandex.ru>
2021-12-31 02:16:31 +0300

    chore: use forge partially to generate certificates (#6325)
    > watching The Matrix while hacking these certificates
    
    Alternative to https://github.com/vitejs/vite/pull/6173
    
    Since sharing same certificate for all users is not the best approach
    I suggest to try decompose node-forge which is used internally by
    selfsigned package. We can just use part of the package which already
    gives a good result.
    
    Eventually we will deprecate this feature but would be good to have less
    impact on current major too.
    
    ```
    du -sk dist/node
    ```
    before: 10760
    after: 9944

--- packages/vite/src/node/http.ts
+++ packages/vite/src/node/http.ts
@@ -152,82 +152,13 @@ function readFileIfExists(value?: string | Buffer | any[]) {
   return value
 }
 
-/**
- * https://github.com/webpack/webpack-dev-server/blob/master/lib/utils/createCertificate.js
- *
- * Copyright JS Foundation and other contributors
- * This source code is licensed under the MIT license found in the
- * LICENSE file at
- * https://github.com/webpack/webpack-dev-server/blob/master/LICENSE
- */
-async function createCertificate() {
-  const { generate } = await import('selfsigned')
-  const pems = generate(null, {
-    algorithm: 'sha256',
-    days: 30,
-    keySize: 2048,
-    extensions: [
-      // {
-      //   name: 'basicConstraints',
-      //   cA: true,
-      // },
-      {
-        name: 'keyUsage',
-        keyCertSign: true,
-        digitalSignature: true,
-        nonRepudiation: true,
-        keyEncipherment: true,
-        dataEncipherment: true
-      },
-      {
-        name: 'extKeyUsage',
-        serverAuth: true,
-        clientAuth: true,
-        codeSigning: true,
-        timeStamping: true
-      },
-      {
-        name: 'subjectAltName',
-        altNames: [
-          {
-            // type 2 is DNS
-            type: 2,
-            value: 'localhost'
-          },
-          {
-            type: 2,
-            value: 'localhost.localdomain'
-          },
-          {
-            type: 2,
-            value: 'lvh.me'
-          },
-          {
-            type: 2,
-            value: '*.lvh.me'
-          },
-          {
-            type: 2,
-            value: '[::1]'
-          },
-          {
-            // type 7 is IP
-            type: 7,
-            ip: '127.0.0.1'
-          },
-          {
-            type: 7,
-            ip: 'fe80::1'
-          }
-        ]
-      }
-    ]
-  })
-  return pems.private + pems.cert
+async function createCertificateLazily() {
+  const { createCertificate } = await import('./certificate')
+  return createCertificate()
 }
 
 async function getCertificate(cacheDir?: string) {
-  if (!cacheDir) return await createCertificate()
+  if (!cacheDir) return await createCertificateLazily()
 
   const cachePath = path.join(cacheDir, '_cert.pem')
 
@@ -243,7 +174,7 @@ async function getCertificate(cacheDir?: string) {
 
     return content
   } catch {
-    const content = await createCertificate()
+    const content = await createCertificateLazily()
     fsp
       .mkdir(cacheDir, { recursive: true })
       .then(() => fsp.writeFile(cachePath, content))
