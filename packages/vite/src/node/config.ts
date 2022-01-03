f3d56f144f923dd610e1dac18941f2803ed88a8c
poyoho <907415276@qq.com>
2022-01-03 17:28:19 +0800

    remove: change

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -527,9 +527,7 @@ export async function resolveConfig(
   )
 
   // call configResolved hooks
-  await Promise.all(
-    userPlugins.concat().map((p) => p.configResolved?.(resolved))
-  )
+  await Promise.all(userPlugins.map((p) => p.configResolved?.(resolved)))
 
   if (process.env.DEBUG) {
     debug(`using resolved config: %O`, {
