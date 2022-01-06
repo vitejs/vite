a34561490b4b866d8d4f98c697435dcb68a5c3ed
poyoho <907415276@qq.com>
2022-01-06 16:40:35 +0800

    feat: new Worker can bundle URL('path', import.meta.url) script (fix #5979) (#6356)

--- packages/playground/worker/__tests__/worker.spec.ts
+++ packages/playground/worker/__tests__/worker.spec.ts
@@ -52,11 +52,11 @@ test.concurrent.each([[true], [false]])('shared worker', async (doTick) => {
 })
 
 if (isBuild) {
+  const assetsDir = path.resolve(testDir, 'dist/assets')
   // assert correct files
   test('inlined code generation', async () => {
-    const assetsDir = path.resolve(testDir, 'dist/assets')
     const files = fs.readdirSync(assetsDir)
-    expect(files.length).toBe(4)
+    expect(files.length).toBe(6)
     const index = files.find((f) => f.includes('index'))
     const content = fs.readFileSync(path.resolve(assetsDir, index), 'utf-8')
     const worker = files.find((f) => f.includes('my-worker'))
@@ -74,5 +74,17 @@ if (isBuild) {
     // inlined
     expect(content).toMatch(`(window.URL||window.webkitURL).createObjectURL`)
     expect(content).toMatch(`window.Blob`)
+  })
+
+  test('worker need bundle', () => {
+    fs.readdirSync(assetsDir)
+      .filter(
+        (file) =>
+          file.includes('url-worker') || file.includes('url-shared-worker')
+      )
+      .forEach((file) => {
+        const content = fs.readFileSync(path.resolve(assetsDir, file), 'utf-8')
+        expect(content.startsWith('(function(){')).toBe(true)
+      })
   })
 }
