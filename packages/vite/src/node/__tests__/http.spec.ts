da883b17afd8fd71a3f57c84f838ff6033881e50
赵東澔 <zhaodonghao@zhaodonghaodeMacBook-Pro.local>
2022-01-16 12:23:49 +0800

    feat: Check port usage correctly

---
+++ packages/vite/src/node/__tests__/http.spec.ts
@@ -0,0 +1,44 @@
+import { httpServerStart } from '../http'
+import { resolveConfig } from '..'
+
+describe('start preview server', () => {
+  test('[strictPort=false, host=127.0.0.1] should use another port when already start a server on localhost.', async () => {
+    const originalPort = 5000
+    const server = require('http').createServer()
+    server.listen(originalPort, 'localhost')
+
+    const testServer = require('http').createServer()
+    const config = await resolveConfig({}, 'serve', 'production')
+    const port = await httpServerStart(testServer, {
+      port: originalPort,
+      strictPort: false,
+      host: '127.0.0.1',
+      logger: config.logger
+    })
+
+    expect(port).not.toEqual(originalPort)
+
+    server.close()
+    testServer.close()
+  })
+
+  test('[strictPort=true, host=127.0.0.1] should use another port when already start a server on localhost.', async () => {
+    const originalPort = 5000
+    const server = require('http').createServer()
+    server.listen(originalPort, 'localhost')
+
+    const config = await resolveConfig({}, 'serve', 'production')
+    const testServer = require('http').createServer()
+    expect(
+      httpServerStart(testServer, {
+        port: originalPort,
+        strictPort: true,
+        host: '127.0.0.1',
+        logger: config.logger
+      })
+    ).rejects.toEqual(new Error(`Port ${originalPort} is already in use`))
+
+    server.close()
+    testServer.close()
+  })
+})
--- packages/vite/src/node/http.ts
+++ packages/vite/src/node/http.ts
@@ -183,6 +183,39 @@ async function getCertificate(cacheDir?: string) {
   }
 }
 
+function checkPortOccupied(
+  port: number,
+  host?: string
+): Promise<{ port: number; occupied: boolean }> {
+  return new Promise((resolve, reject) => {
+    const tempServer = require('http').createServer()
+
+    const onError = (e: Error & { code?: string }) => {
+      if (e.code === 'EADDRINUSE') {
+        checkPortOccupied(++port, host)
+          .then((r) => {
+            resolve({
+              port: r.port,
+              occupied: true
+            })
+          })
+          .catch(reject)
+      } else {
+        reject(e)
+      }
+      tempServer.removeListener('error', onError)
+      tempServer.close()
+    }
+
+    tempServer.on('error', onError)
+
+    tempServer.listen(port, host, () => {
+      tempServer.removeListener('error', onError)
+      tempServer.close(() => resolve({ port, occupied: false }))
+    })
+  })
+}
+
 export async function httpServerStart(
   httpServer: HttpServer,
   serverOptions: {
@@ -192,28 +225,39 @@ export async function httpServerStart(
     logger: Logger
   }
 ): Promise<number> {
-  return new Promise((resolve, reject) => {
-    let { port, strictPort, host, logger } = serverOptions
+  const { strictPort, host, logger } = serverOptions
+  // Check the port usage of serverOptions.host
+  let checkInfo = await checkPortOccupied(serverOptions.port, host)
 
-    const onError = (e: Error & { code?: string }) => {
-      if (e.code === 'EADDRINUSE') {
-        if (strictPort) {
-          httpServer.removeListener('error', onError)
-          reject(new Error(`Port ${port} is already in use`))
-        } else {
-          logger.info(`Port ${port} is in use, trying another one...`)
-          httpServer.listen(++port, host)
-        }
-      } else {
-        httpServer.removeListener('error', onError)
-        reject(e)
-      }
+  if (host === '127.0.0.1') {
+    // Check the port usage of localhost
+    checkInfo = await checkPortOccupied(serverOptions.port, 'localhost')
+  }
+
+  return new Promise((resolve, reject) => {
+    const { port, occupied } = checkInfo
+
+    if (occupied && strictPort) {
+      reject(new Error(`Port ${serverOptions.port} is already in use`))
+      return
+    }
+
+    const onError = (e: Error) => {
+      httpServer.removeListener('error', onError)
+      reject(e)
     }
 
     httpServer.on('error', onError)
 
     httpServer.listen(port, host, () => {
       httpServer.removeListener('error', onError)
+
+      if (occupied) {
+        logger.info(
+          `Port ${serverOptions.port} is in use, trying another one...`
+        )
+      }
+
       resolve(port)
     })
   })
