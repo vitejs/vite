d3032644ccdc91848e90e593ebb9395d5d3b49a6
patak <matias.capeletto@gmail.com>
2022-01-16 21:08:50 +0100

    chore: inline selfsigned license (#6530)

--- packages/vite/src/node/certificate.ts
+++ packages/vite/src/node/certificate.ts
@@ -1,13 +1,37 @@
-// simplified fork of
+// Simplified fork of selfsigned with inlined options and partial
+// node-forge usage to achieve smaller bundle. See:
 // https://github.com/jfromaniello/selfsigned/blob/da38146f8d02183c35f49f91659a744a243e8707/index.js
-// with inlined options and partial node-forge usage
-// to achieve smaller bundle
 //
 // this utility create untrusted certificate which still
 // allows to access page after proceeding a wall with warning
 //
 // should be deprecated eventually and replaced with recipes
 // about generating secure trusted certificates
+//
+// ## selfsigned
+// License: MIT
+// By: José F. Romaniello, Paolo Fragomeni, Charles Bushong
+// Repository: git://github.com/jfromaniello/selfsigned.git
+// MIT License
+// Copyright (c) 2013 José F. Romaniello
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 // @ts-ignore
 import forge from 'node-forge/lib/forge'
