c7b24f3d03f80b8787ea8624f8e271db40d63d17
lighthouse-worker <zhouhaolei@bilibili.com>
2022-01-18 12:02:20 +0800

    fix(playground-ssr): use createApp in client

--- packages/playground/ssr-vue/src/pages/Home.vue
+++ packages/playground/ssr-vue/src/pages/Home.vue
@@ -8,7 +8,7 @@
   <p class="virtual">msg from virtual module: {{ foo.msg }}</p>
   <p class="inter">this will be styled with a font-face</p>
   <p class="import-meta-url">{{ state.url }}</p>
-  <p class="protocol">{{ state.protocol }}</p>
+  <p class="protocol">{{ protocol }}</p>
   <p class="nested-virtual">msg from nested virtual module: {{ virtualMsg }}</p>
   <Button>CommonButton</Button>
   <div>
@@ -22,8 +22,9 @@
 <script setup>
 import foo from '@foo'
 import { msg as virtualMsg } from '@virtual-file'
-import { reactive, defineAsyncComponent } from 'vue'
+import { reactive, defineAsyncComponent, onMounted, computed } from 'vue'
 import Button from '../components/button'
+
 const ImportType = load('ImportType')
 const Foo = defineAsyncComponent(() =>
   import('../components/Foo').then((mod) => mod.Foo)
@@ -33,13 +34,17 @@ function load(file) {
 }
 const url = import.meta.env.SSR
   ? import.meta.url
-  : document.querySelector('.import-meta-url').textContent
-const protocol = new URL(url).protocol
+  : ''
 
 const state = reactive({
   count: 0,
-  protocol,
   url
+})
+
+const protocol = computed(() => state.url ? new URL(state.url).protocol : '')
+
+onMounted(() => {
+  state.url = document.querySelector('.import-meta-url').textContent
 })
 </script>
 
