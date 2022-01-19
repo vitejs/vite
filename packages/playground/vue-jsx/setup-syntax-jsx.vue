78f3bc34b8719ee2f6e36041a115e86127976c1c
poyoho <907415276@qq.com>
2022-01-19 21:13:07 +0800

    wip: add unit test for vue-jsx setup syntax

---
+++ packages/playground/vue-jsx/setup-syntax-jsx.vue
@@ -0,0 +1,23 @@
+<script setup lang="jsx">
+import { ref, onMounted, defineProps } from 'vue'
+
+const props = defineProps({
+  msg: String
+})
+onMounted(() => console.log('mounted in vue with jsx2'))
+let count = ref(100)
+const increment = () => {
+  console.log('increase')
+  count.value++
+}
+const Render = () => (
+  <div>
+    <button onClick={increment}> click!!! </button>hello {props.msg}
+    {count.value}
+  </div>
+)
+</script>
+
+<template>
+  <Render />
+</template>
