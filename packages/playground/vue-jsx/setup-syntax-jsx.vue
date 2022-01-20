c3b5abc47f3011490aab94d618c5b5d58de39395
poyoho <907415276@qq.com>
2022-01-20 16:22:49 +0800

    test: hmr

--- packages/playground/vue-jsx/setup-syntax-jsx.vue
+++ packages/playground/vue-jsx/setup-syntax-jsx.vue
@@ -1,29 +1,17 @@
 <script setup lang="jsx">
-import { ref, onMounted, defineProps } from 'vue'
-
-const props = defineProps({
-  msg: String
-})
-onMounted(() => console.log('mounted in vue with jsx2'))
+import { ref } from 'vue'
 let count = ref(100)
 const increment = () => {
   count.value++
 }
 const Render = () => (
   <div>
-    <button onClick={increment}> click!!! </button>hello {props.msg}
-    {count.value}
+    <button onClick={increment}> click!!! </button>
   </div>
 )
 </script>
 
 <template>
-  <p>{{ count }}</p>
+  <p class="setup-jsx">{{ count }}</p>
   <Render />
 </template>
-
-<style scoped>
-p {
-  color: red;
-}
-</style>
