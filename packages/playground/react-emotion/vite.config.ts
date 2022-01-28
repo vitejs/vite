ba5a6df0646950f5e5351d292e451850fc3b0b87
patak-dev <matias.capeletto@gmail.com>
2022-01-28 16:01:55 +0100

    fix: apply sodatea fix for plugin-react

--- packages/playground/react-emotion/vite.config.ts
+++ packages/playground/react-emotion/vite.config.ts
@@ -4,9 +4,6 @@ import type { UserConfig } from 'vite'
 const config: UserConfig = {
   plugins: [
     react({
-      // TODO: When bundling with esbuild >0.14.4 or rollup, jsxRuntime: 'automatic' no
-      // longer works for this example. See #6639
-      jsxRuntime: 'classic',
       jsxImportSource: '@emotion/react',
       babel: {
         plugins: ['@emotion/babel-plugin']
