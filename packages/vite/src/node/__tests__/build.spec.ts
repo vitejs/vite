9775e70d84fe6d72dbbef0676ea7b5178e570b77
Sachin Raja <sachinraja2349@gmail.com>
2022-02-09 16:11:33 -0800

    test: package extensions

--- packages/vite/src/node/__tests__/build.spec.ts
+++ packages/vite/src/node/__tests__/build.spec.ts
@@ -1,6 +1,12 @@
+import type { LibraryFormats, LibraryOptions } from '../build'
 import { resolveLibFilename } from '../build'
 import { resolve } from 'path'
-import { resolveConfig } from '..'
+
+type FormatsToFileNames = [LibraryFormats, string][]
+const baseLibOptions: LibraryOptions = {
+  fileName: 'my-lib',
+  entry: 'mylib.js'
+}
 
 describe('resolveLibFilename', () => {
   test('custom filename function', () => {
@@ -64,5 +70,43 @@ describe('resolveLibFilename', () => {
         resolve(__dirname, 'packages/noname')
       )
     }).toThrow()
+  })
+
+  test('commonjs package extensions', () => {
+    const formatsToFilenames: FormatsToFileNames = [
+      ['es', 'my-lib.es.mjs'],
+      ['umd', 'my-lib.umd.js'],
+      ['cjs', 'my-lib.cjs.js'],
+      ['iife', 'my-lib.iife.js']
+    ]
+
+    for (const [format, expectedFilename] of formatsToFilenames) {
+      const filename = resolveLibFilename(
+        baseLibOptions,
+        format,
+        resolve(__dirname, 'packages/noname')
+      )
+
+      expect(filename).toBe(expectedFilename)
+    }
+  })
+
+  test('module package extensions', () => {
+    const formatsToFilenames: FormatsToFileNames = [
+      ['es', 'my-lib.es.js'],
+      ['umd', 'my-lib.umd.cjs'],
+      ['cjs', 'my-lib.cjs.cjs'],
+      ['iife', 'my-lib.iife.js']
+    ]
+
+    for (const [format, expectedFilename] of formatsToFilenames) {
+      const filename = resolveLibFilename(
+        baseLibOptions,
+        format,
+        resolve(__dirname, 'packages/module')
+      )
+
+      expect(filename).toBe(expectedFilename)
+    }
   })
 })
