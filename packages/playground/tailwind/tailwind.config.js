dec69647bfaeaba3407f0e4bb15264b6d889b712
Nurettin Kaya <sibbngheid@gmail.com>
2022-02-17 13:24:44 -0800

    fix: normalize postcss dependency messages (#6959)

--- packages/playground/tailwind/tailwind.config.js
+++ packages/playground/tailwind/tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   mode: 'jit',
-  purge: [__dirname + '/src/**/*.vue'],
+  purge: [
+    // Before editing this section, make sure no paths are matching with `/src/App.vue`
+    // Look https://github.com/vitejs/vite/pull/6959 for more details
+    __dirname + '/src/{components,views}/**/*.vue',
+    __dirname + '/src/App.vue'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
