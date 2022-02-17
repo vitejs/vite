7d04de741c0d9f7f8720be4dea01a2a676f85f13
sibbng <sibbngheid@gmail.com>
2022-02-17 21:11:49 +0300

    chore: add note

--- packages/playground/tailwind/tailwind.config.js
+++ packages/playground/tailwind/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   mode: 'jit',
   purge: [
+    // Before editing this section, make sure no paths are matching with `/src/App.vue`
+    // Look https://github.com/vitejs/vite/pull/6959 for more details
     __dirname + '/src/{components,views}/**/*.vue',
     __dirname + '/src/App.vue'
   ],
