584a0530717d4f5b6ff0993fa13e1ff87aab4080
yoho <907415276@qq.com>
2022-02-20 21:46:26 +0800

    fix(glob): css imports injecting a ?used query to export the css string (#6949)

--- packages/playground/css/__tests__/css.spec.ts
+++ packages/playground/css/__tests__/css.spec.ts
@@ -13,6 +13,14 @@ import {
 
 // note: tests should retrieve the element at the beginning of test and reuse it
 // in later assertions to ensure CSS HMR doesn't reload the page
+test('imported css', async () => {
+  const css = await page.textContent('.imported-css')
+  expect(css).toContain('.imported {')
+  const glob = await page.textContent('.imported-css-glob')
+  expect(glob).toContain('.dir-import')
+  const globEager = await page.textContent('.imported-css-globEager')
+  expect(globEager).toContain('.dir-import')
+})
 
 test('linked css', async () => {
   const linked = await page.$('.linked')
