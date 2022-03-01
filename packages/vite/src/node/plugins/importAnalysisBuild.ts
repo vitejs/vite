c101c243399529e0a366426a7daf4907772e1a1c
Vladimir <sheremet.va@icloud.com>
2022-03-01 20:01:47 +0300

    fix: throw Error when can't preload CSS (#7108)

--- packages/vite/src/node/plugins/importAnalysisBuild.ts
+++ packages/vite/src/node/plugins/importAnalysisBuild.ts
@@ -75,7 +75,9 @@ function preload(baseModule: () => Promise<{}>, deps?: string[]) {
       if (isCss) {
         return new Promise((res, rej) => {
           link.addEventListener('load', res)
-          link.addEventListener('error', rej)
+          link.addEventListener('error', () =>
+            rej(new Error(`Unable to preload CSS for ${dep}`))
+          )
         })
       }
     })
