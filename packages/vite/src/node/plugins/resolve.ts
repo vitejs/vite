0cf802fc6afb1271533a825f7e18dc681f109293
sinoon <sinoon1218@gmail.com>
2022-03-02 18:59:41 +0800

    refactor: remove debug

--- packages/vite/src/node/plugins/resolve.ts
+++ packages/vite/src/node/plugins/resolve.ts
@@ -142,11 +142,6 @@ export function resolvePlugin(baseOptions: InternalResolveOptions): Plugin {
           return res
         }
       }
-      debug(
-        `[all] ${colors.cyan(id)} -> ${colors.dim(importer)}, ${colors.blue(
-          preferRelative.toString()
-        )}`
-      )
 
       // relative
       if (id.startsWith('.') || (preferRelative && /^\w/.test(id))) {
