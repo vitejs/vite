4c64caecc5ce29f9066678d48e0fe1bd8ad61a76
sinoon <sinoon1218@gmail.com>
2022-03-02 17:22:18 +0800

    test(example): add test case

--- packages/playground/css/index.vue
+++ packages/playground/css/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="css-in-less">test for import css in less with SFC</div>
+  <div class="css-in-less">
+    test for import css in less with SFC, color will be yellow
+  </div>
+  <div class="css-in-less-2">
+    test for import less in less with SFC, color will be blue
+  </div>
 </template>
 
 <script setup></script>
