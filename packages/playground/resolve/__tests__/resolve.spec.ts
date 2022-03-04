4485a2c4bb9f0d746cde24599cd4f7b18760fad8
sapphi-red <green@sapphi.red>
2022-03-04 18:03:19 +0900

    chore: add comment about import with ?url

--- packages/playground/resolve/__tests__/resolve.spec.ts
+++ packages/playground/resolve/__tests__/resolve.spec.ts
@@ -17,6 +17,7 @@ test('deep import with exports field', async () => {
 })
 
 test('deep import with query with exports field', async () => {
+  // since it is imported with `?url` it should return a url
   expect(await page.textContent('.exports-deep-query')).toMatch(
     isBuild ? /base64/ : '/exports-path/deep.json'
   )
