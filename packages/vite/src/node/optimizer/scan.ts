3f5b645b1df7251fdbeeda5229388a81cfd04753
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-03-07 01:54:24 -0800

    fix: handle files with multiple comments (#7202)

--- packages/vite/src/node/optimizer/scan.ts
+++ packages/vite/src/node/optimizer/scan.ts
@@ -143,7 +143,7 @@ function globEntries(pattern: string | string[], config: ResolvedConfig) {
 const scriptModuleRE =
   /(<script\b[^>]*type\s*=\s*(?:"module"|'module')[^>]*>)(.*?)<\/script>/gims
 export const scriptRE = /(<script\b(?:\s[^>]*>|>))(.*?)<\/script>/gims
-export const commentRE = /<!--(.|[\r\n])*?-->/
+export const commentRE = /<!--.*?-->/gs
 const srcRE = /\bsrc\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
 const typeRE = /\btype\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
 const langRE = /\blang\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
