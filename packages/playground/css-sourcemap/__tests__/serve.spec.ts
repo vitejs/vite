bf10f635e2a1769f5982d5478c41cce1d1fa1ca4
sapphi-red <green@sapphi.red>
2022-03-24 19:42:46 +0900

    test: add "missing source file" test

--- packages/playground/css-sourcemap/__tests__/serve.spec.ts
+++ packages/playground/css-sourcemap/__tests__/serve.spec.ts
@@ -207,6 +207,12 @@ if (!isBuild) {
       }
     `)
   })
+
+  test('should not output missing source file warning', () => {
+    serverLogs.forEach((log) => {
+      expect(log).not.toMatch(/Sourcemap for .+ points to missing source files/)
+    })
+  })
 } else {
   test('this file only includes test for serve', () => {
     expect(true).toBe(true)
