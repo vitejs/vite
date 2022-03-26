64ddff03f7ff693e8a9ed1217d401b628a940852
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-27 00:23:30 +0800

    refactor(types): share hot context type (#7475)

--- packages/vite/types/importMeta.d.ts
+++ packages/vite/types/importMeta.d.ts
@@ -20,48 +20,7 @@ interface GlobOptions {
 interface ImportMeta {
   url: string
 
-  readonly hot?: {
-    readonly data: any
-
-    accept(): void
-    accept(cb: (mod: any) => void): void
-    accept(dep: string, cb: (mod: any) => void): void
-    accept(deps: readonly string[], cb: (mods: any[]) => void): void
-
-    /**
-     * @deprecated
-     */
-    acceptDeps(): never
-
-    dispose(cb: (data: any) => void): void
-    decline(): void
-    invalidate(): void
-
-    on: {
-      (
-        event: 'vite:beforeUpdate',
-        cb: (payload: import('./hmrPayload').UpdatePayload) => void
-      ): void
-      (
-        event: 'vite:beforePrune',
-        cb: (payload: import('./hmrPayload').PrunePayload) => void
-      ): void
-      (
-        event: 'vite:beforeFullReload',
-        cb: (payload: import('./hmrPayload').FullReloadPayload) => void
-      ): void
-      (
-        event: 'vite:error',
-        cb: (payload: import('./hmrPayload').ErrorPayload) => void
-      ): void
-      <T extends string>(
-        event: import('./customEvent').CustomEventName<T>,
-        cb: (data: any) => void
-      ): void
-    }
-
-    send(event: string, data?: any): void
-  }
+  readonly hot?: import('./hot').ViteHotContext
 
   readonly env: ImportMetaEnv
 
