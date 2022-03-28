71f4b0d3f654b23e74a55f9d7a39d10e0e935613
KallynGowdy <kallyngowdy@gmail.com>
2022-03-28 12:54:30 -0400

    chore: Remove unused test code

--- packages/playground/worker/__tests__/worker.spec.ts
+++ packages/playground/worker/__tests__/worker.spec.ts
@@ -82,58 +82,6 @@ if (isBuild) {
     expect(content).toMatch(`(window.URL||window.webkitURL).createObjectURL`)
     expect(content).toMatch(`window.Blob`)
   })
-
-  // // assert correct files
-  // test('sourcemap generation for web workers', async () => {
-  //   const files = fs.readdirSync(assetsDir)
-  //   // should have 2 worker chunk
-  //   expect(files.length).toBe(13)
-  //   const index = files.find((f) => /^index\.\w+\.js$/.test(f))
-  //   const content = fs.readFileSync(path.resolve(assetsDir, index), 'utf-8')
-  //   const indexSourcemap = getSourceMapUrl(content)
-  //   const worker = files.find((f) => /^my-worker\.\w+\.js$/.test(f))
-  //   const workerContent = fs.readFileSync(
-  //     path.resolve(assetsDir, worker),
-  //     'utf-8'
-  //   )
-  //   const workerSourcemap = getSourceMapUrl(workerContent)
-  //   const sharedWorker = files.find((f) =>
-  //     /^my-shared-worker\.\w+\.js$/.test(f)
-  //   )
-  //   const sharedWorkerContent = fs.readFileSync(
-  //     path.resolve(assetsDir, sharedWorker),
-  //     'utf-8'
-  //   )
-  //   const sharedWorkerSourcemap = getSourceMapUrl(sharedWorkerContent)
-
-  //   expect(files).toContainEqual(expect.stringMatching(/^index\.\w+\.js\.map$/))
-  //   expect(files).toContainEqual(
-  //     expect.stringMatching(/^my-worker\.\w+\.js\.map$/)
-  //   )
-  //   expect(files).toContainEqual(
-  //     expect.stringMatching(/^my-shared-worker\.\w+\.js\.map$/)
-  //   )
-
-  //   // sourcemap should exist and have a data URL
-  //   expect(indexSourcemap).toMatch(/^index\.\w+\.js\.map$/)
-  //   expect(workerSourcemap).toMatch(/^my-worker\.\w+\.js\.map$/)
-  //   expect(sharedWorkerSourcemap).toMatch(/^my-shared-worker\.\w+\.js\.map$/)
-
-  //   // worker should have all imports resolved and no exports
-  //   expect(workerContent).not.toMatch(`import`)
-  //   expect(workerContent).not.toMatch(`export`)
-
-  //   // shared worker should have all imports resolved and no exports
-  //   expect(sharedWorkerContent).not.toMatch(`import`)
-  //   expect(sharedWorkerContent).not.toMatch(`export`)
-
-  //   // chunk
-  //   expect(content).toMatch(`new Worker("/assets`)
-  //   expect(content).toMatch(`new SharedWorker("/assets`)
-  //   // inlined
-  //   expect(content).toMatch(`(window.URL||window.webkitURL).createObjectURL`)
-  //   expect(content).toMatch(`window.Blob`)
-  // })
 }
 
 test('module worker', async () => {
