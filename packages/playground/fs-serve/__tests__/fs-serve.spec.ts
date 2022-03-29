6f49a6cfda844b536544851d3e8577799943ff2f
Artur <artur@vaadin.com>
2022-03-29 12:16:52 +0300

    fix: make @fs URLs work with special characters (#7510)

--- packages/playground/fs-serve/__tests__/fs-serve.spec.ts
+++ packages/playground/fs-serve/__tests__/fs-serve.spec.ts
@@ -23,6 +23,15 @@ describe('main', () => {
       expect(await page.textContent('.safe-fetch-status')).toBe('200')
     })
 
+    test('safe fetch with special characters', async () => {
+      expect(
+        await page.textContent('.safe-fetch-subdir-special-characters')
+      ).toMatch('KEY=safe')
+      expect(
+        await page.textContent('.safe-fetch-subdir-special-characters-status')
+      ).toBe('200')
+    })
+
     test('unsafe fetch', async () => {
       expect(await page.textContent('.unsafe-fetch')).toMatch('403 Restricted')
       expect(await page.textContent('.unsafe-fetch-status')).toBe('403')
@@ -30,6 +39,13 @@ describe('main', () => {
 
     test('safe fs fetch', async () => {
       expect(await page.textContent('.safe-fs-fetch')).toBe(stringified)
+      expect(await page.textContent('.safe-fs-fetch-status')).toBe('200')
+    })
+
+    test('safe fs fetch with special characters', async () => {
+      expect(await page.textContent('.safe-fs-fetch-special-characters')).toBe(
+        stringified
+      )
       expect(await page.textContent('.safe-fs-fetch-status')).toBe('200')
     })
 
