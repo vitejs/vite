60c1b78ad0707260a4064ec3a375e491bcde52c0
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-30 22:55:07 +0800

    chore: setup vite-plugin-glob

--- packages/playground/glob-import/vite.config.ts
+++ packages/playground/glob-import/vite.config.ts
@@ -1,10 +1,12 @@
 import path from 'path'
 import { defineConfig } from 'vite'
+import Glob from 'vite-plugin-glob'
 
 export default defineConfig({
   resolve: {
     alias: {
       '@dir': path.resolve(__dirname, './dir/')
     }
-  }
+  },
+  plugins: [Glob({ takeover: true })]
 })
