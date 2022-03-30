8efc6a6d5fa73612c69a8539be0c81231b144b4e
patak <matias.capeletto@gmail.com>
2022-03-30 10:11:09 +0200

    fix: revert #7463 and #6624 (#7522)

--- packages/vite/src/node/plugins/clientInjections.ts
+++ packages/vite/src/node/plugins/clientInjections.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import type { Plugin } from '../plugin'
 import type { ResolvedConfig } from '../config'
 import { CLIENT_ENTRY, ENV_ENTRY } from '../constants'
-import { normalizePath } from '../utils'
+import { normalizePath, isObject } from '../utils'
 
 // ids in transform are normalized to unix style
 const normalizedClientEntry = normalizePath(CLIENT_ENTRY)
@@ -23,31 +23,32 @@ export function clientInjectionsPlugin(config: ResolvedConfig): Plugin {
         const protocol = options.protocol || null
         const timeout = options.timeout || 30000
         const overlay = options.overlay !== false
-        let port: number | string | undefined | null
-        port = options.clientPort || options.port
-        if (config.server.middlewareMode) {
-          port = port || 24678
+        let port: number | string | undefined
+        if (isObject(config.server.hmr)) {
+          port = config.server.hmr.clientPort || config.server.hmr.port
         }
-        port = port ? String(port) : null
-
+        if (config.server.middlewareMode) {
+          port = String(port || 24678)
+        } else {
+          port = String(port || options.port || config.server.port!)
+        }
         let hmrBase = config.base
         if (options.path) {
           hmrBase = path.posix.join(hmrBase, options.path)
         }
-        if (hmrBase === '/') {
-          hmrBase = ''
+        if (hmrBase !== '/') {
+          port = path.posix.normalize(`${port}${hmrBase}`)
         }
 
         return code
-          .replace(/__MODE__/g, JSON.stringify(config.mode))
-          .replace(/__BASE__/g, JSON.stringify(config.base))
-          .replace(/__DEFINES__/g, serializeDefine(config.define || {}))
-          .replace(/__HMR_PROTOCOL__/g, JSON.stringify(protocol))
-          .replace(/__HMR_HOSTNAME__/g, JSON.stringify(host))
-          .replace(/__HMR_PORT__/g, JSON.stringify(port))
-          .replace(/__HMR_BASE__/g, JSON.stringify(hmrBase))
-          .replace(/__HMR_TIMEOUT__/g, JSON.stringify(timeout))
-          .replace(/__HMR_ENABLE_OVERLAY__/g, JSON.stringify(overlay))
+          .replace(`__MODE__`, JSON.stringify(config.mode))
+          .replace(`__BASE__`, JSON.stringify(config.base))
+          .replace(`__DEFINES__`, serializeDefine(config.define || {}))
+          .replace(`__HMR_PROTOCOL__`, JSON.stringify(protocol))
+          .replace(`__HMR_HOSTNAME__`, JSON.stringify(host))
+          .replace(`__HMR_PORT__`, JSON.stringify(port))
+          .replace(`__HMR_TIMEOUT__`, JSON.stringify(timeout))
+          .replace(`__HMR_ENABLE_OVERLAY__`, JSON.stringify(overlay))
       } else if (!options?.ssr && code.includes('process.env.NODE_ENV')) {
         // replace process.env.NODE_ENV instead of defining a global
         // for it to avoid shimming a `process` object during dev,
