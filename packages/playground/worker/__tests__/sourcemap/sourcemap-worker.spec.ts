234aeefabaccddd81d1b1f4d25ff8c57e566a1bb
sapphi-red <green@sapphi.red>
2022-03-30 22:29:08 +0900

    test: replace fs/promises with fs.promises

--- packages/playground/worker/__tests__/sourcemap/sourcemap-worker.spec.ts
+++ packages/playground/worker/__tests__/sourcemap/sourcemap-worker.spec.ts
@@ -1,4 +1,4 @@
-import fs from 'fs/promises'
+import { promises as fs } from 'fs'
 import path from 'path'
 import { untilUpdated, isBuild, testDir } from '../../../testUtils'
 import { Page } from 'playwright-chromium'
