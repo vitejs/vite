a742b157f853f75b271803d84325240e8c87e44e
Bjorn Lu <bjornlu.dev@gmail.com>
2022-04-04 03:21:46 +0800

    fix: detect env hmr (#7595)

--- packages/vite/src/node/server/hmr.ts
+++ packages/vite/src/node/server/hmr.ts
@@ -44,6 +44,7 @@ export async function handleHMRUpdate(
 ): Promise<any> {
   const { ws, config, moduleGraph } = server
   const shortFile = getShortName(file, config.root)
+  const fileName = path.basename(file)
 
   const isConfig = file === config.configFile
   const isConfigDependency = config.configFileDependencies.some(
@@ -51,7 +52,7 @@ export async function handleHMRUpdate(
   )
   const isEnv =
     config.inlineConfig.envFile !== false &&
-    (file === '.env' || file.startsWith('.env.'))
+    (fileName === '.env' || fileName.startsWith('.env.'))
   if (isConfig || isConfigDependency || isEnv) {
     // auto restart server
     debugHmr(`[config change] ${colors.dim(shortFile)}`)
