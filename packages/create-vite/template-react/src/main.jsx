89fe2752ae168da2efc8b924f0cffe0a09a50002
Shahriar Shojib <shahriar_shojib@hotmail.com>
2022-04-03 20:42:57 +0600

    chore(create-vite-app): upgrade react to 18

--- packages/create-vite/template-react/src/main.jsx
+++ packages/create-vite/template-react/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+import { createRoot } from 'react-dom/client'
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
