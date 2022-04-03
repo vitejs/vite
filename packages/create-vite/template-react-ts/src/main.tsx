b88a1a91f201ce8e9d71bccf2bce57d8cdbf08fb
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-04-03 17:07:49 -0400

    chore: tweak code style

--- packages/create-vite/template-react-ts/src/main.tsx
+++ packages/create-vite/template-react-ts/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 
-import { createRoot } from 'react-dom/client'
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
