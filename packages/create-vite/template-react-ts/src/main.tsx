0461d1072fd91fb5742479a81ab260ea4f2e1272
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-04-03 17:37:16 -0400

    chore: fix typescript warning

--- packages/create-vite/template-react-ts/src/main.tsx
+++ packages/create-vite/template-react-ts/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
