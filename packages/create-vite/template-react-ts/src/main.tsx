3da401f0c8ad38af7513bc5e2abfd878aa57be89
franciscomarin98 <francmarinc@gmail.com>
2022-04-04 14:09:50 -0500

    Code improvement proposal created in pull request #7597

--- packages/create-vite/template-react-ts/src/main.tsx
+++ packages/create-vite/template-react-ts/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root') as HTMLElement;
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
