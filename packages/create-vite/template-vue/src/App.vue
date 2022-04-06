aa9b1131963a30f477641464d8c42ce34e8a7fbd
agoni1212 <22545824+agoni1212@users.noreply.github.com>
2022-04-06 21:24:09 +0800

    chore: update vue template setup api doc url (#7628)

--- packages/create-vite/template-vue/src/App.vue
+++ packages/create-vite/template-vue/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
