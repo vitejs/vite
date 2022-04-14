5e61f4cff9135c109fdb5221dd71569f9648ba9f
CA Gustavo <84424661+gustavocadev@users.noreply.github.com>
2022-04-14 09:59:37 -0500

    chore(create-vite-app): update base code to react 18
    What this PR do is to change the base code of react ( main.jsx - main.tsx ) to make it easier to read

--- packages/create-vite/template-react/src/main.jsx
+++ packages/create-vite/template-react/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
