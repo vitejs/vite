8263e25eec03a1421d523b4d9eda079bb04216af
Shinigami92 <chrissi92@hotmail.de>
2022-04-22 17:21:08 +0200

    ci: fix jest windows node 18

--- scripts/jestGlobalTeardown.cjs
+++ scripts/jestGlobalTeardown.cjs
@@ -4,6 +4,6 @@ const path = require('path')
 module.exports = async () => {
   await global.__BROWSER_SERVER__.close()
   if (!process.env.VITE_PRESERVE_BUILD_ARTIFACTS) {
-    rimraf(path.resolve(__dirname, '../packages/temp'))
+    rimraf.sync(path.resolve(__dirname, '../packages/temp'))
   }
 }
