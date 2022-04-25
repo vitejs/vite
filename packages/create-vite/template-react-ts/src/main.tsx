c9ececb0593d66ad3947d900c4535b84e1f7465b
purpleeeee <h.purpleeeee@gmail.com>
2022-04-25 17:09:59 +0900

    chore: format

--- packages/create-vite/template-react-ts/src/main.tsx
+++ packages/create-vite/template-react-ts/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-const rootElement = document.getElementById("root")
-if (!rootElement) throw new Error("Failed to find the root element")
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
