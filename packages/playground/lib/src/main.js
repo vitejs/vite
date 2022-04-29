a36be20db0179594a2fdd8caf313ded6e95a67ad
patak <matias.capeletto@gmail.com>
2022-04-29 15:05:27 +0200

    chore: link to esbuild issue

--- packages/playground/lib/src/main.js
+++ packages/playground/lib/src/main.js
@@ -1,5 +1,5 @@
 export default function myLib(sel) {
-  // Force esbuild helpers
+  // Force esbuild spread helpers (https://github.com/evanw/esbuild/issues/951)
   console.log({ ...'foo' })
 
   document.querySelector(sel).textContent = 'It works'
