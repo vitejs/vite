fbdb21646acd72e355693d38c3f20e824c183af3
patak <matias.capeletto@gmail.com>
2022-04-30 06:13:30 +0200

    fix: inject esbuild helpers in IIFE and UMD wrappers (#7948)

--- packages/playground/lib/__tests__/lib.spec.ts
+++ packages/playground/lib/__tests__/lib.spec.ts
@@ -9,10 +9,22 @@ if (isBuild) {
 
   test('umd', async () => {
     expect(await page.textContent('.umd')).toBe('It works')
+    const code = fs.readFileSync(
+      path.join(testDir, 'dist/my-lib-custom-filename.umd.js'),
+      'utf-8'
+    )
+    // esbuild helpers are injected inside of the UMD wrapper
+    expect(code).toMatch(/^\(function\(/)
   })
 
   test('iife', async () => {
     expect(await page.textContent('.iife')).toBe('It works')
+    const code = fs.readFileSync(
+      path.join(testDir, 'dist/my-lib-custom-filename.iife.js'),
+      'utf-8'
+    )
+    // esbuild helpers are injected inside of the IIFE wrapper
+    expect(code).toMatch(/^var MyLib=function\(\){"use strict";/)
   })
 
   test('Library mode does not include `preload`', async () => {
--- packages/playground/lib/src/main.js
+++ packages/playground/lib/src/main.js
@@ -1,3 +1,6 @@
 export default function myLib(sel) {
+  // Force esbuild spread helpers (https://github.com/evanw/esbuild/issues/951)
+  console.log({ ...'foo' })
+
   document.querySelector(sel).textContent = 'It works'
 }
