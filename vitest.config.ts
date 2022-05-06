ba02475cb37eb77f363158b6ecc302691e458739
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-06 15:05:17 +0800

    fix: empty regex for clean string

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
-      './packages/playground/**/*.*'
+      './packages/playground/**/*.*',
+      './packages/temp/**/*.*'
     ]
   }
 })
