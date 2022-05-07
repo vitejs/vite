80acd460dd48a94045eaafe0705c72a6c79520e1
yoho <panyoho@gmail.com>
2022-05-07 22:10:49 +0800

    fix: cache same src if no scoped

--- packages/plugin-vue/src/utils/descriptorCache.ts
+++ packages/plugin-vue/src/utils/descriptorCache.ts
@@ -69,11 +69,24 @@ export function getSrcDescriptor(
   filename: string,
   query: VueQuery
 ): SFCDescriptor {
-  return cache.get(`${filename}?src=${query.src}`)!
+  if (query.scoped) {
+    return cache.get(`${filename}?src=${query.src}`)!
+  }
+  // console.log('get', filename)
+  return cache.get(filename)!
 }
 
-export function setSrcDescriptor(filename: string, entry: SFCDescriptor): void {
-  // if multiple Vue files use the same src file, they will be overwritten
-  // should use other key
-  cache.set(`${filename}?src=${entry.id}`, entry)
+export function setSrcDescriptor(
+  filename: string,
+  entry: SFCDescriptor,
+  scoped?: boolean
+): void {
+  if (scoped) {
+    // if multiple Vue files use the same src file, they will be overwritten
+    // should use other key
+    cache.set(`${filename}?src=${entry.id}`, entry)
+    return
+  }
+  // console.log('set', filename, entry)
+  cache.set(filename, entry)
 }
