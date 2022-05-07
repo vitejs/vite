0fbd233b0d0078a97850754e2cc5fdf75d6ce35a
patak <matias.capeletto@gmail.com>
2022-05-07 07:20:00 +0200

    fix: graceful rename in windows (#8036)

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -775,8 +775,8 @@ function gracefulRename(
     ) {
       setTimeout(function () {
         fs.stat(to, function (stater, st) {
-          if (stater && stater.code === 'ENOENT') gracefulRename(from, to, CB)
-          else cb(er)
+          if (stater && stater.code === 'ENOENT') fs.rename(from, to, CB)
+          else CB(er)
         })
       }, backoff)
       if (backoff < 100) backoff += 10
