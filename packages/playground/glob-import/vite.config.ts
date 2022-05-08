5f9388bf43430b8863366abc6241f5af5d47fca7
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-08 09:05:30 +0800

    chore: update

--- packages/playground/glob-import/vite.config.ts
+++ packages/playground/glob-import/vite.config.ts
@@ -1,14 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import Glob from 'vite-plugin-glob'
 
 export default defineConfig({
   resolve: {
     alias: {
       '@dir': path.resolve(__dirname, './dir/')
     }
-  },
-  optimizeDeps: {
-    entries: []
   }
 })
