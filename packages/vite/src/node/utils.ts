5e178449aabca810a1dd9e2aae9bf0e4690f386f
patak <matias.capeletto@gmail.com>
2022-05-10 08:00:21 +0200

    fix: image-set with base64 images (fix #8028) (#8035)

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -560,8 +560,7 @@ export async function processSrcSet(
   srcs: string,
   replacer: (arg: ImageCandidate) => Promise<string>
 ): Promise<string> {
-  const imageCandidates: ImageCandidate[] = srcs
-    .split(',')
+  const imageCandidates: ImageCandidate[] = splitSrcSet(srcs)
     .map((s) => {
       const src = s.replace(escapedSpaceCharacters, ' ').trim()
       const [url] = imageSetUrlRE.exec(src) || []
@@ -587,6 +586,25 @@ export async function processSrcSet(
     return (prev +=
       url + ` ${descriptor}${index === ret.length - 1 ? '' : ', '}`)
   }, '')
+}
+
+function splitSrcSet(srcs: string) {
+  const parts: string[] = []
+  // There could be a ',' inside of url(data:...), linear-gradient(...) or "data:..."
+  const cleanedSrcs = srcs.replace(
+    /(?:url|image|gradient|cross-fade)\([^\)]*\)|"([^"]|(?<=\\)")*"|'([^']|(?<=\\)')*'/g,
+    blankReplacer
+  )
+  let startIndex = 0
+  let splitIndex: number
+  do {
+    splitIndex = cleanedSrcs.indexOf(',', startIndex)
+    parts.push(
+      srcs.slice(startIndex, splitIndex !== -1 ? splitIndex : undefined)
+    )
+    startIndex = splitIndex + 1
+  } while (splitIndex !== -1)
+  return parts
 }
 
 function escapeToLinuxLikePath(path: string) {
