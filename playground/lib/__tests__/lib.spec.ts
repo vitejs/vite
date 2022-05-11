4fbe0b91e02ba113835f942e8f20761f9b4b8845
Rongjian Zhang <pd4d10@gmail.com>
2022-05-12 01:00:38 +0800

    fix: add hash to lib chunk names (#7190)
    Co-authored-by: patak-dev <matias.capeletto@gmail.com>

--- playground/lib/__tests__/lib.spec.ts
+++ playground/lib/__tests__/lib.spec.ts
@@ -44,6 +44,9 @@ describe.runIf(isBuild)('build', () => {
       'utf-8'
     )
     expect(code).not.toMatch('__vitePreload')
+
+    // Test that library chunks are hashed
+    expect(code).toMatch(/await import\("\.\/message.[a-z\d]{8}.js"\)/)
   })
 
   test('@import hoist', async () => {
