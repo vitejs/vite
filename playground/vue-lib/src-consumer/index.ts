abf6b7a60cc2efb91a3381eaa71abab94cb723e2
Sachin Raja <sachinraja2349@gmail.com>
2022-05-12 05:58:55 -0700

    fix: ts error

--- playground/vue-lib/src-consumer/index.ts
+++ playground/vue-lib/src-consumer/index.ts
@@ -1,7 +1,7 @@
-/* eslint-disable node/no-missing-import */
+import { createApp } from 'vue'
+// @ts-ignore
 import { CompA } from '../dist/lib/my-vue-lib'
 import '../dist/lib/style.css'
-import { createApp } from 'vue'
 
 const app = createApp(CompA)
 app.mount('#app')
