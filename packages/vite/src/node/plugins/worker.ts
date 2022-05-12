f27204fa0d91db1ad9ebb0e09055f849700a8c56
yoho <panyoho@gmail.com>
2022-05-12 21:06:49 +0800

    fix: build error

--- packages/vite/src/node/plugins/worker.ts
+++ packages/vite/src/node/plugins/worker.ts
@@ -5,9 +5,9 @@ import type { ResolvedConfig } from '../config'
 import type { Plugin } from '../plugin'
 import type { ViteDevServer } from '../server'
 import { ENV_ENTRY, ENV_PUBLIC_PATH } from '../constants'
-import { cleanUrl, injectQuery, parseRequest } from '../utils'
+import { cleanUrl, getHash, injectQuery, parseRequest } from '../utils'
 import { onRollupWarning } from '../build'
-import { fileToUrl, getAssetHash } from './asset'
+import { fileToUrl } from './asset'
 
 interface WorkerCache {
   // save worker bundle emitted files avoid overwrites the same file.
@@ -146,7 +146,7 @@ function emitSourcemapForWorkerEntry(
       const basename = path.parse(cleanUrl(id)).name
       const data = sourcemap.toString()
       const content = Buffer.from(data)
-      const contentHash = getAssetHash(content)
+      const contentHash = getHash(content)
       const fileName = `${basename}.${contentHash}.js.map`
       const filePath = path.posix.join(config.build.assetsDir, fileName)
       emitWorkerSourcemap(context, config, {
@@ -189,7 +189,7 @@ export async function workerFileToUrl(
   }
   const code = await bundleWorkerEntry(ctx, config, id, query)
   const basename = path.parse(cleanUrl(id)).name
-  const contentHash = getAssetHash(code)
+  const contentHash = getHash(code)
   const fileName = path.posix.join(
     config.build.assetsDir,
     `${basename}.${contentHash}.js`
