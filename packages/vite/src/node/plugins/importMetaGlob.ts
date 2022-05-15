533510c332dc360e32097e6e8860f8ba71b3c146
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-15 12:39:50 +0800

    fix(glob): properly handles tailing comma (#8181)

--- packages/vite/src/node/plugins/importMetaGlob.ts
+++ packages/vite/src/node/plugins/importMetaGlob.ts
@@ -1,7 +1,13 @@
 import { isAbsolute, posix } from 'path'
 import { isMatch, scan } from 'micromatch'
 import { stripLiteral } from 'strip-literal'
-import type { ArrayExpression, CallExpression, Literal, Node } from 'estree'
+import type {
+  ArrayExpression,
+  CallExpression,
+  Literal,
+  Node,
+  SequenceExpression
+} from 'estree'
 import { parseExpressionAt } from 'acorn'
 import MagicString from 'magic-string'
 import fg from 'fast-glob'
@@ -111,20 +117,44 @@ export async function parseImportGlob(
       return e
     }
 
-    let ast: CallExpression
+    let ast: CallExpression | SequenceExpression
+    let lastTokenPos: number | undefined
 
     try {
       ast = parseExpressionAt(code, start, {
         ecmaVersion: 'latest',
         sourceType: 'module',
-        ranges: true
+        ranges: true,
+        onToken: (token) => {
+          lastTokenPos = token.end
+        }
       }) as any
     } catch (e) {
       const _e = e as any
       if (_e.message && _e.message.startsWith('Unterminated string constant'))
         return undefined!
-      throw _e
+      if (lastTokenPos == null || lastTokenPos <= start) throw _e
+
+      // tailing comma in object or array will make the parser think it's a comma operation
+      // we try to parse again removing the comma
+      try {
+        const statement = code.slice(start, lastTokenPos).replace(/[,\s]*$/, '')
+        ast = parseExpressionAt(
+          ' '.repeat(start) + statement, // to keep the ast position
+          start,
+          {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            ranges: true
+          }
+        ) as any
+      } catch {
+        throw _e
+      }
     }
+
+    if (ast.type === 'SequenceExpression')
+      ast = ast.expressions[0] as CallExpression
 
     if (ast.type !== 'CallExpression')
       throw err(`Expect CallExpression, got ${ast.type}`)
