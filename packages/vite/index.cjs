96622cacbec43842a57bcf457ad9a0aef54cbaa5
Anthony Fu <anthonyfu117@hotmail.com>
2022-05-15 12:59:07 +0800

    chore: cleanup

--- packages/vite/index.cjs
+++ packages/vite/index.cjs
@@ -1,39 +1,32 @@
 /* eslint-disable no-restricted-globals */
 
-// proxy cjs utils
-Object.assign(module.exports, require('./dist/node-cjs/publicUtils.cjs'))
-
 // type utils
 module.exports.defineConfig = (config) => config
 
+// proxy cjs utils (sync functions)
+Object.assign(module.exports, require('./dist/node-cjs/publicUtils.cjs'))
+
 // async functions, can be redirect from ESM build
-module.exports.build = (...args) =>
-  import('./dist/node/index.js').then((i) => i.build(...args))
-
-module.exports.createServer = (...args) =>
-  import('./dist/node/index.js').then((i) => i.createServer(...args))
-
-module.exports.preview = (...args) =>
-  import('./dist/node/index.js').then((i) => i.preview(...args))
-
-module.exports.transformWithEsbuild = (...args) =>
-  import('./dist/node/index.js').then((i) => i.transformWithEsbuild(...args))
-
-module.exports.resolveConfig = (...args) =>
-  import('./dist/node/index.js').then((i) => i.resolveConfig(...args))
-
-module.exports.optimizeDeps = (...args) =>
-  import('./dist/node/index.js').then((i) => i.optimizeDeps(...args))
-
-module.exports.formatPostcssSourceMap = (...args) =>
-  import('./dist/node/index.js').then((i) => i.formatPostcssSourceMap(...args))
+const asyncFunctions = [
+  'build',
+  'createServer',
+  'preview',
+  'transformWithEsbuild',
+  'resolveConfig',
+  'optimizeDeps',
+  'formatPostcssSourceMap'
+]
+asyncFunctions.forEach((name) => {
+  module.exports[name] = (...args) =>
+    import('./dist/node/index.js').then((i) => i[name](...args))
+})
 
 // some sync functions are marked not supported due to their complexity and uncommon usage
 const unsupportedCJS = ['resolvePackageEntry', 'resolvePackageData']
 unsupportedCJS.forEach((name) => {
-  module.exports[name] = (...args) => {
-    throw new Error(`"${name}" is not supported in CJS build of Vite 3.
-Please use ESM or dynamic imports \`const { ${name} } = await import('vite')\`.
-`)
+  module.exports[name] = () => {
+    throw new Error(
+      `"${name}" is not supported in CJS build of Vite 3.\nPlease use ESM or dynamic imports \`const { ${name} } = await import('vite')\`.`
+    )
   }
 })
