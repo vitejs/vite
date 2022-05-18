6fca8f509f3fe822cca13afc6da8097a7dbfbe72
Shen Lu <shenlu89dev@gmail.com>
2022-05-18 13:53:36 +0800

    Update main.tsx
    chore: update destructuring syntax

--- packages/create-vite/template-react-ts/src/main.tsx
+++ packages/create-vite/template-react-ts/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
