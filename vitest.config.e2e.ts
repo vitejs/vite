0ce86a611a83e2efaea361dcd6375de91d7283f9
bluwy <bjornlu.dev@gmail.com>
2022-05-19 00:36:48 +0800

    test(e2e): set 4 max threads

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -20,7 +20,9 @@ export default defineConfig({
     onConsoleLog(log) {
       if (log.match(/experimental|jit engine|emitted file|tailwind/i))
         return false
-    }
+    },
+    maxThreads: process.env.CI ? 4 : undefined,
+    minThreads: process.env.CI ? 1 : undefined
   },
   esbuild: {
     target: 'node14'
