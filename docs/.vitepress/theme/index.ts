d994214be41ce5de3d8b16d228bab3bcad6f49ff
Kia King Ishii <kia.king.08@gmail.com>
2022-05-22 00:52:33 +0900

    docs: adjust styling

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './styles/vars.css'
+import './styles/custom.css'
 import HomeSponsors from './components/HomeSponsors.vue'
 
 export default {
