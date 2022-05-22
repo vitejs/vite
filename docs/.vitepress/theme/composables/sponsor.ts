68f17b1b3d9d0a9fd75f6b32ac6e48df554a3078
Kia King Ishii <kia.king.08@gmail.com>
2022-05-22 15:20:43 +0900

    docs: vitepress next updates (#8260)

---
+++ docs/.vitepress/theme/composables/sponsor.ts
@@ -0,0 +1,61 @@
+import { ref, onMounted } from 'vue'
+
+interface Sponsors {
+  special: Sponsor[]
+  platinum: Sponsor[]
+  platinum_china: Sponsor[]
+  gold: Sponsor[]
+  silver: Sponsor[]
+  bronze: Sponsor[]
+}
+
+interface Sponsor {
+  name: string
+  img: string
+  url: string
+}
+
+// shared data across instances so we load only once.
+const data = ref()
+
+const dataHost = 'https://sponsors.vuejs.org'
+const dataUrl = `${dataHost}/vite.json`
+
+export function useSponsor() {
+  onMounted(async () => {
+    if (data.value) {
+      return
+    }
+
+    const result = await fetch(dataUrl)
+    const json = await result.json()
+
+    data.value = mapSponsors(json)
+  })
+
+  return {
+    data
+  }
+}
+
+function mapSponsors(sponsors: Sponsors) {
+  return [
+    {
+      tier: 'Platinum Sponsor',
+      size: 'big',
+      items: mapImgPath(sponsors['platinum'])
+    },
+    {
+      tier: 'Gold Sponsors',
+      size: 'medium',
+      items: mapImgPath(sponsors['gold'])
+    }
+  ]
+}
+
+function mapImgPath(sponsors: Sponsor[]) {
+  return sponsors.map((sponsor) => ({
+    ...sponsor,
+    img: `${dataHost}/images/${sponsor.img}`
+  }))
+}
