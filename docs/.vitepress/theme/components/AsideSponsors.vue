379f25673be7a725cba03aab733e49744a52923b
Kia King Ishii <kia.king.08@gmail.com>
2022-05-29 04:42:47 +0900

    docs: upgrade vitepress and add aside sponsors (#8363)

---
+++ docs/.vitepress/theme/components/AsideSponsors.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { VPDocAsideSponsors } from 'vitepress/theme'
+import { useSponsor } from '../composables/sponsor'
+
+const { data } = useSponsor()
+
+const sponsors = computed(() => {
+  return (
+    data?.value.map((sponsor) => {
+      return {
+        size: sponsor.size === 'big' ? 'mini' : 'xmini',
+        items: sponsor.items
+      }
+    }) ?? []
+  )
+})
+</script>
+
+<template>
+  <VPDocAsideSponsors v-if="data" :data="sponsors" />
+</template>
