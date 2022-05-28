1fe6c274e9ffb3faa3c5eab1e557f7e1bc8482e8
Kia King Ishii <kia.king.08@gmail.com>
2022-05-28 17:52:18 +0900

    docs: upgrade vitepress and add aside sponsors

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import Theme from 'vitepress/theme'
 import './styles/vars.css'
 import './styles/custom.css'
 import HomeSponsors from './components/HomeSponsors.vue'
+import AsideSponsors from './components/AsideSponsors.vue'
 
 export default {
   ...Theme,
   Layout() {
     return h(Theme.Layout, null, {
-      'home-features-after': () => h(HomeSponsors)
+      'home-features-after': () => h(HomeSponsors),
+      'aside-outline-after': () => h(AsideSponsors)
     })
   }
 }
