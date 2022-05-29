ae677d5dc8135e76ce67107570b52ab34f60a53c
yoho <panyoho@gmail.com>
2022-05-29 19:14:56 +0800

    fix: dynamic import

--- packages/vite/src/node/plugins/dynamicImportVars.ts
+++ packages/vite/src/node/plugins/dynamicImportVars.ts
@@ -204,9 +204,12 @@ export function dynamicImportVarsPlugin(config: ResolvedConfig): Plugin {
             `import __variableDynamicImportRuntimeHelper from "${dynamicImportHelperId}";`
           )
         }
-        // and then will be transformed by importMetaGlob.ts
-        // so we don't need to generate map
-        return s.toString()
+        return {
+          code: s.toString(),
+          map: config.build.sourcemap
+            ? s.generateMap({ hires: true, source: importer })
+            : null
+        }
       }
     }
   }
