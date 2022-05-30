adc2d77c3412ee58dd12c36dcb1914e62af1f13f
草鞋没号 <308487730@qq.com>
2022-05-30 13:44:37 +0800

    chore: update comments (#8394)

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -485,7 +485,7 @@ export function writeFile(
 }
 
 /**
- * Use instead of fs.existsSync(filename)
+ * Use fs.statSync(filename) instead of fs.existsSync(filename)
  * #2051 if we don't have read permission on a directory, existsSync() still
  * works and will result in massively slow subsequent checks (which are
  * unnecessary in the first place)
