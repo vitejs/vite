da7ddb78b8fb0386ec75781305ce010092eb046b
patak-dev <matias.capeletto@gmail.com>
2022-06-06 21:13:15 +0200

    chore: update vanilla templates

--- packages/create-vite/template-vanilla/main.js
+++ packages/create-vite/template-vanilla/main.js
@@ -5,12 +5,8 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img class="logo" alt="Vite logo" src="${viteLogo}" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img class="logo" alt="JavaScript logo" src="${javascriptLogo}" />
-    </a>
+    <a href="https://vitejs.dev" target="_blank"><img class="logo" alt="Vite logo" src="${viteLogo}" /></a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><img class="logo" alt="JavaScript logo" src="${javascriptLogo}" /></a>
     <h1>Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
