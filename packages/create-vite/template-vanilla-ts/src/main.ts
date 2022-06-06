f3a6256a5a95403c0400191807124ad85886b1fb
patak <matias.capeletto@gmail.com>
2022-06-06 20:52:58 +0200

    chore: update type
    Co-authored-by: 翠 / green <green@sapphi.red>

--- packages/create-vite/template-vanilla-ts/src/main.ts
+++ packages/create-vite/template-vanilla-ts/src/main.ts
@@ -21,4 +21,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector<HTMLDivElement>('#counter'))
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
