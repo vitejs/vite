336b49b1fbcda464d605dd0b5f55bc35442f0260
patak-dev <matias.capeletto@gmail.com>
2022-06-07 11:42:05 +0200

    chore: format

--- packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
+++ packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
@@ -19,9 +19,7 @@ const count = ref(0)
 
   <p>
     Check out
-    <a
-      href="https://vuejs.org/guide/quick-start.html#local"
-      target="_blank"
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
   </p>
