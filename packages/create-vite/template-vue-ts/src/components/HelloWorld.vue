0f670d2ecd8f8a76c42b0113c79e8e2a6f740e5c
patak <matias.capeletto@gmail.com>
2022-06-07 09:35:09 +0200

    chore: better anchor
    Co-authored-by: Tony Trinh <tony19@gmail.com>

--- packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
+++ packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
@@ -20,7 +20,7 @@ const count = ref(0)
   <p>
     Check out
     <a
-      href="https://vuejs.org/guide/quick-start.html#with-build-tools"
+      href="https://vuejs.org/guide/quick-start.html#local"
       target="_blank"
       >create-vue</a
     >, the official Vue + Vite starter
