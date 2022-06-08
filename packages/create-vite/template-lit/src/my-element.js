c1036f1f029e73ca3e5025678e0248c00647856d
bluwy <bjornlu.dev@gmail.com>
2022-06-08 15:49:57 +0800

    chore: add will-change for logo

--- packages/create-vite/template-lit/src/my-element.js
+++ packages/create-vite/template-lit/src/my-element.js
@@ -65,6 +65,7 @@ export class MyElement extends LitElement {
       .logo {
         height: 6em;
         padding: 1.5em;
+        will-change: filter;
       }
       .logo:hover {
         filter: drop-shadow(0 0 2em #646cffaa);
--- packages/create-vite/template-vue-ts/src/App.vue
+++ packages/create-vite/template-vue-ts/src/App.vue
@@ -20,6 +20,7 @@ import HelloWorld from './components/HelloWorld.vue'
 .logo {
   height: 6em;
   padding: 1.5em;
+  will-change: filter;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
