9d5baf007656f4e226fa5a47dbfe0cc50fc1d84c
patak-dev <matias.capeletto@gmail.com>
2022-06-09 22:56:18 +0200

    test: add dep in worker

--- playground/worker/my-worker.ts
+++ playground/worker/my-worker.ts
@@ -1,9 +1,10 @@
 import { mode, msg } from './modules/workerImport'
 import { bundleWithPlugin } from './modules/test-plugin'
+import { msg as msgFromDep } from 'dep-to-optimize'
 
 self.onmessage = (e) => {
   if (e.data === 'ping') {
-    self.postMessage({ msg, mode, bundleWithPlugin })
+    self.postMessage({ msg, mode, bundleWithPlugin, msgFromDep })
   }
 }
 
