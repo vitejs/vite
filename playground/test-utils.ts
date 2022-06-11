4291d387a241785525bd2a462568dc299bf14ab6
yoho <panyoho@gmail.com>
2022-06-11 14:58:41 +0800

    test: worker update test

--- playground/test-utils.ts
+++ playground/test-utils.ts
@@ -162,12 +162,12 @@ export async function untilUpdated(
   }
 }
 
-export const extractSourcemap = (content: string) => {
+export const extractSourcemap = (content: string): any => {
   const lines = content.trim().split('\n')
   return fromComment(lines[lines.length - 1]).toObject()
 }
 
-export const formatSourcemapForSnapshot = (map: any) => {
+export const formatSourcemapForSnapshot = (map: any): any => {
   const root = normalizePath(testDir)
   const m = { ...map }
   delete m.file
