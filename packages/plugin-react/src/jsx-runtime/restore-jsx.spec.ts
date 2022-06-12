21c95c316d9d148f099c40cacf75ef7b921853ba
patak-dev <matias.capeletto@gmail.com>
2022-06-12 14:17:08 +0200

    chore: update snapshots

--- packages/plugin-react/src/jsx-runtime/restore-jsx.spec.ts
+++ packages/plugin-react/src/jsx-runtime/restore-jsx.spec.ts
@@ -6,36 +6,36 @@ describe('parseReactAlias', () => {
   it('handles cjs require', () => {
     expect(parseReactAlias(`const React = require("react")`))
       .toMatchInlineSnapshot(`
-      Array [
-        "React",
-        true,
-      ]
-    `)
+        [
+          "React",
+          true,
+        ]
+      `)
   })
 
   it('handles cjs require (minified)', () => {
     expect(parseReactAlias(`var F=require('foo');var R=require('react')`))
       .toMatchInlineSnapshot(`
-      Array [
-        "R",
-        true,
-      ]
-    `)
+        [
+          "R",
+          true,
+        ]
+      `)
   })
 
   it('does not handle destructured cjs require', () => {
     expect(parseReactAlias(`var {createElement} = require("react")`))
       .toMatchInlineSnapshot(`
-      Array [
-        undefined,
-        false,
-      ]
-    `)
+        [
+          undefined,
+          false,
+        ]
+      `)
   })
 
   it('handles esm import', () => {
     expect(parseReactAlias(`import React from 'react'`)).toMatchInlineSnapshot(`
-      Array [
+      [
         "React",
         false,
       ]
@@ -45,21 +45,21 @@ describe('parseReactAlias', () => {
   it('handles esm import namespace', () => {
     expect(parseReactAlias(`import * as React from "react"`))
       .toMatchInlineSnapshot(`
-      Array [
-        "React",
-        false,
-      ]
-    `)
+        [
+          "React",
+          false,
+        ]
+      `)
   })
 
   it('does not handle destructured esm import', () => {
     expect(parseReactAlias(`import {createElement} from "react"`))
       .toMatchInlineSnapshot(`
-      Array [
-        undefined,
-        false,
-      ]
-    `)
+        [
+          undefined,
+          false,
+        ]
+      `)
   })
 })
 
