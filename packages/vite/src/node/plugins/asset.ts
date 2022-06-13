81fd41b4482ba6e0c15b4a3b9a39d925e2e8e56c
sapphi-red <green@sapphi.red>
2022-06-13 22:11:58 +0900

    fix: add mime data for missing ones

--- packages/vite/src/node/plugins/asset.ts
+++ packages/vite/src/node/plugins/asset.ts
@@ -23,6 +23,18 @@ const assetHashToFilenameMap = new WeakMap<
 // save hashes of the files that has been emitted in build watch
 const emittedHashMap = new WeakMap<ResolvedConfig, Set<string>>()
 
+// add own dictionary entry by directly assigning mrmime
+export function registerCustomMime(): void {
+  // https://github.com/lukeed/mrmime/issues/3
+  mrmime.mimes['ico'] = 'image/x-icon'
+  // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Containers#flac
+  mrmime.mimes['flac'] = 'audio/flac'
+  // mrmime and mime-db is not released yet: https://github.com/jshttp/mime-db/commit/c9242a9b7d4bb25d7a0c9244adec74aeef08d8a1
+  mrmime.mimes['aac'] = 'audio/aac'
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+  mrmime.mimes['eot'] = 'application/vnd.ms-fontobject'
+}
+
 /**
  * Also supports loading plain strings with import text from './foo.txt?raw'
  */
@@ -31,9 +43,8 @@ export function assetPlugin(config: ResolvedConfig): Plugin {
   assetHashToFilenameMap.set(config, new Map())
   const relativeBase = isRelativeBase(config.base)
 
-  // add own dictionary entry by directly assigning mrmine
-  // https://github.com/lukeed/mrmime/issues/3
-  mrmime.mimes['ico'] = 'image/x-icon'
+  registerCustomMime()
+
   return {
     name: 'vite:asset',
 
