4720768a5684236991b3547792e5092389d6a2f3
Kia King Ishii <kia.king.08@gmail.com>
2022-06-22 22:28:32 +0900

    docs: add team page

---
+++ docs/_data/team.js
@@ -0,0 +1,155 @@
+export const core = [
+  {
+    avatar: 'https://www.github.com/yyx990803.png',
+    name: 'Evan You',
+    title: 'Creator',
+    org: 'Vue.js',
+    orgLink: 'https://vuejs.org/',
+    desc: 'Husband, father of two, independent open source developer.',
+    links: [
+      { icon: 'github', link: 'https://github.com/yyx990803' },
+      { icon: 'twitter', link: 'https://twitter.com/youyuxi' }
+    ],
+    sponsor: 'https://github.com/sponsors/yyx990803'
+  },
+  {
+    avatar: 'https://www.github.com/patak-dev.png',
+    name: 'Patak',
+    title: 'A collaborative being',
+    org: 'StackBlitz',
+    orgLink: 'https://stackblitz.com/',
+    desc: 'Core team member of Vite. Team member of Vue.',
+    links: [
+      { icon: 'github', link: 'https://github.com/patak-dev' },
+      { icon: 'twitter', link: 'https://twitter.com/patak_dev' }
+    ],
+    sponsor: 'https://github.com/sponsors/patak-dev'
+  },
+  {
+    avatar: 'https://www.github.com/antfu.png',
+    name: 'Anthony Fu',
+    title: 'A fanatical open sourceror',
+    org: 'NuxtLabs',
+    orgLink: 'https://nuxtlabs.com/',
+    desc: 'Core team member of Vite & Vue Working at NuxtLabs.',
+    links: [
+      { icon: 'github', link: 'https://github.com/antfu' },
+      { icon: 'twitter', link: 'https://twitter.com/antfu7' }
+    ],
+    sponsor: 'https://github.com/sponsors/antfu'
+  },
+  {
+    avatar: 'https://github.com/sodatea.png',
+    name: 'Haoqun Jiang',
+    title: 'Developer',
+    org: 'Vue.js',
+    orgLink: 'https://vuejs.org/',
+    desc: 'Vite/Vite core team member. Full-time open sourcerer.',
+    links: [
+      { icon: 'github', link: 'https://github.com/sodatea' },
+      { icon: 'twitter', link: 'https://twitter.com/haoqunjiang' }
+    ],
+    sponsor: 'https://github.com/sponsors/sodatea'
+  },
+  {
+    avatar: 'https://github.com/Shinigami92.png',
+    name: 'Shinigami',
+    title: 'Maintainer',
+    org: 'Faker',
+    orgLink: 'https://fakerjs.dev',
+    desc: 'Frontend developer who loves Material Design and Vue.',
+    links: [
+      { icon: 'github', link: 'https://github.com/Shinigami92' },
+      { icon: 'twitter', link: 'https://twitter.com/Shini_92' }
+    ],
+    sponsor: 'https://github.com/sponsors/Shinigami92'
+  },
+  {
+    avatar: 'https://github.com/aleclarson.png',
+    name: 'Shinigami',
+    title: 'Open sourceror',
+    desc: 'Risk taker, Free thinker, App builder, Board game believer.',
+    links: [
+      { icon: 'github', link: 'https://github.com/aleclarson' },
+      { icon: 'twitter', link: 'https://twitter.com/retropragma' }
+    ],
+    sponsor: 'https://github.com/sponsors/aleclarson'
+  },
+  {
+    avatar: 'https://github.com/bluwy.png',
+    name: 'Bjorn Lu',
+    title: 'Frontend Web Developer',
+    org: 'Opslock',
+    desc: 'Web dev. svelte and vite team member. Something something opinions.',
+    links: [
+      { icon: 'github', link: 'hhttps://github.com/bluwy' },
+      { icon: 'twitter', link: 'https://twitter.com/bluwyoo' },
+      {
+        icon: 'linkedin',
+        link: 'https://www.linkedin.com/in/bjorn-lu-3302a31b5'
+      }
+    ]
+  },
+  {
+    avatar: 'https://github.com/poyoho.png',
+    name: 'yoho',
+    title: 'Frontend Developer',
+    desc: 'Frontend. Vite team member.',
+    links: [
+      { icon: 'github', link: 'https://github.com/poyoho' },
+      { icon: 'twitter', link: 'https://twitter.com/yoho_po' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/sapphi-red.png',
+    name: 'green',
+    title: 'Developer',
+    org: 'Tokyo Institute of Technology',
+    desc: 'tech19 JC, raP SysAd. My name is "Midori".',
+    links: [
+      { icon: 'github', link: 'hhttps://github.com/sapphi-red' },
+      { icon: 'twitter', link: 'https://twitter.com/sapphi_red' }
+    ]
+  },
+  {
+    avatar: 'https://github.com/ygj6.png',
+    name: 'ygj6',
+    title: 'Developer',
+    desc: '...?',
+    links: [{ icon: 'github', link: 'hhttps://github.com/ygj6' }]
+  },
+  {
+    avatar: 'https://github.com/Niputi.png',
+    name: 'green',
+    title: 'Developer',
+    org: 'Computershare Denmark',
+    desc: 'weeb/javascript lover.',
+    links: [
+      { icon: 'github', link: 'hhttps://github.com/Niputi' },
+      { icon: 'twitter', link: 'https://twitter.com/Niputi_' }
+    ]
+  }
+]
+
+export const emeriti = [
+  {
+    avatar: 'https://github.com/underfin.png',
+    name: 'underfin',
+    title: 'Developer',
+    links: [{ icon: 'github', link: 'hhttps://github.com/underfin' }]
+  },
+  {
+    avatar: 'https://github.com/GrygrFlzr.png',
+    name: 'GrygrFlzr',
+    title: 'Developer',
+    links: [{ icon: 'github', link: 'hhttps://github.com/GrygrFlzr' }]
+  },
+  {
+    avatar: 'https://github.com/nihalgonsalves.png',
+    name: 'GrygrFlzr',
+    title: 'Senior Software Engineer',
+    org: 'Delivery Hero',
+    orgLink: 'https://www.deliveryhero.com',
+    links: [{ icon: 'github', link: 'hhttps://github.com/nihalgonsalves' }]
+  }
+]
