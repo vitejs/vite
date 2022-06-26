1596f425233ce18d36bd415dd6fa6b7a2869f8bd
patak-dev <matias.capeletto@gmail.com>
2022-06-26 12:38:44 +0200

    perf: avoid sourcemap generation during dev for line-stable transforms

--- packages/vite/src/node/plugins/importMetaGlob.ts
+++ packages/vite/src/node/plugins/importMetaGlob.ts
@@ -17,7 +17,7 @@ import type { Plugin } from '../plugin'
 import type { ViteDevServer } from '../server'
 import type { ModuleNode } from '../server/moduleGraph'
 import type { ResolvedConfig } from '../config'
-import { normalizePath, slash, transformResult } from '../utils'
+import { normalizePath, slash, transformStableResult } from '../utils'
 
 const { isMatch, scan } = micromatch
 
@@ -75,7 +75,7 @@ export function importGlobPlugin(config: ResolvedConfig): Plugin {
             server!.watcher.add(dirname(file))
           })
         }
-        return transformResult(result.s, id, config)
+        return transformStableResult(result.s, id, config)
       }
     }
   }
