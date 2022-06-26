8b02b05dfc9a8c16e1d80f27bd5b57f62e3ccfac
yoho <panyoho@gmail.com>
2022-06-27 00:02:02 +0800

    feat: update comment

--- scripts/reporter.ts
+++ scripts/reporter.ts
@@ -66,11 +66,13 @@ export default function ReporterPlugin(): Plugin {
 process.on('exit', () => {
   writeFileSync(
     path.join(__dirname, '../report.md'),
-    '|hooks|file|timing|\n' +
+    '<!--report-->\n' +
+      '|hooks|file|timing|\n' +
       '|-----|----|------|\n' +
       Object.entries(res)
         .sort((a, b) => b[1].timing - a[1].timing)
         .map((dat) => `|${dat[1].hooks}|${dat[0]}|${dat[1].timing}|`)
-        .join('\n')
+        .join('\n'),
+    { encoding: 'utf8' }
   )
 })
