432dd94e271a5afc2410d98afa4811a042e09e80
yoho <panyoho@gmail.com>
2022-06-27 13:49:15 +0800

    feat: report timing

---
+++ scripts/reporter.ts
@@ -0,0 +1,76 @@
+import { writeFileSync } from 'node:fs'
+import path from 'node:path'
+import type { Plugin, ResolvedConfig } from 'vite'
+
+interface ReportInfo {
+  hooks: string
+  timing: number
+}
+
+const res = {} as Record<string, ReportInfo>
+
+export default function ReporterPlugin(): Plugin {
+  function injectTimeCollect(plugin: Plugin, config: ResolvedConfig) {
+    if (plugin.transform) {
+      const _transform = plugin.transform
+      plugin.transform = async function (...args) {
+        const id = args[1].replace(config.root, '')
+        const start = Date.now()
+        const result = await _transform.apply(this, args)
+        const end = Date.now()
+
+        if (result != null) {
+          if (!res[id]) {
+            res[id] = { hooks: 'transform', timing: 0 }
+          }
+          res[id].timing += end - start
+        }
+
+        return result
+      }
+    }
+
+    if (plugin.load) {
+      const _load = plugin.load
+      plugin.load = async function (...args) {
+        const id = args[0].replace(config.root, '')
+        const start = Date.now()
+        const result = await _load.apply(this, args)
+        const end = Date.now()
+
+        if (result != null) {
+          if (!res[id]) {
+            res[id] = { hooks: 'load', timing: 0 }
+          }
+          res[id].timing += end - start
+        }
+
+        return result
+      }
+    }
+  }
+
+  return <Plugin>{
+    name: 'vite-plugin-inspect',
+    apply: 'serve',
+    configResolved(config) {
+      config.plugins.forEach((plugin) => injectTimeCollect(plugin, config))
+    }
+  }
+}
+
+process.on('exit', () => {
+  writeFileSync(
+    path.join(__dirname, '../report.md'),
+    '<!--report-->\n' +
+      '## Top 10 (change in each commit)\n' +
+      '|hooks|file|timing|\n' +
+      '|-----|----|------|\n' +
+      Object.entries(res)
+        .sort((a, b) => b[1].timing - a[1].timing)
+        .slice(0, 10)
+        .map((dat) => `|${dat[1].hooks}|${dat[0]}|${dat[1].timing}|`)
+        .join('\n'),
+    { encoding: 'utf8' }
+  )
+})
