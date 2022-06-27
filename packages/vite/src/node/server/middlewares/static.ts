236660a494ca18f8cced49a3b1f66ca763051272
sapphi-red <green@sapphi.red>
2022-06-27 12:25:49 +0900

    fix: /@fs/ dir traversal with escaped chars (static)

--- packages/vite/src/node/server/middlewares/static.ts
+++ packages/vite/src/node/server/middlewares/static.ts
@@ -78,7 +78,7 @@ export function serveStaticMiddleware(
       return next()
     }
 
-    const url = decodeURI(req.url!)
+    const url = decodeURIComponent(req.url!)
 
     // apply aliases to static requests as well
     let redirected: string | undefined
