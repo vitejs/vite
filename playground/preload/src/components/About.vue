fd46ccfccb668632cd2fce77fce0a07ee2efefc3
patak-dev <matias.capeletto@gmail.com>
2022-06-30 22:40:31 +0200

    test: add example of full-reload with 'lazy' in preload playground

--- playground/preload/src/components/About.vue
+++ playground/preload/src/components/About.vue
@@ -1,9 +1,13 @@
+<script setup>
+import { msg } from 'dep-including-a'
+</script>
+
 <template>
   <div>
     This is <b>about</b> page.
     <br />
     Go to <router-link to="/">Home</router-link> page
+    <br />
+    {{ msg }}
   </div>
 </template>
-
-<script setup></script>
