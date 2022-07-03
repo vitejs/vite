23a0d62cef18f97d87ec9d03b872f011abfb53a8
bluwy <bjornlu.dev@gmail.com>
2022-07-03 14:40:57 +0800

    chore: fix merge

--- playground/external/src/main.js
+++ playground/external/src/main.js
@@ -1,2 +1,9 @@
 import '@vitejs/dep-that-imports-vue'
 import '@vitejs/dep-that-requires-vue'
+import { hello } from 'external-dep'
+
+text('.external', hello)
+
+function text(el, text) {
+  document.querySelector(el).textContent = text
+}
