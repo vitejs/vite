e8c1e07db9a5c7f4ff177245d310ed95d218c4b9
patak-dev <matias.capeletto@gmail.com>
2022-07-05 23:00:24 +0200

    chore: reduce change set

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  optimizeDeps: {
-    // vitepress is an aliased with replacement `join(DIST_CLIENT_PATH, '/index')`
-    // This needs to be excluded from optimization
-    exclude: ['vitepress']
-  },
   ssr: {
-    // And it is also marked as noExternal, so it is safer to exclude it in SSR too
-    // Right now in Vite we are bailing out for aliased deps during SSR by default
-    // but this may change in the future
-    optimizeDeps: {
-      exclude: ['vitepress']
-    },
     format: 'cjs'
   },
   legacy: {
     buildSsrCjsExternalHeuristics: true
+  },
+  optimizeDeps: {
+    // vitepress is aliased with replacement `join(DIST_CLIENT_PATH, '/index')`
+    // This needs to be excluded from optimization
+    exclude: ['vitepress']
   }
 })
