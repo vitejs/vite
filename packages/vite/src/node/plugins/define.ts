ad6659d1f4fb9f379d17f8bdac2cca9180694be1
Derek Arner <arner.derek@gmail.com>
2022-07-05 21:46:45 -0700

    feat: allow import.meta.hot define override

--- packages/vite/src/node/plugins/define.ts
+++ packages/vite/src/node/plugins/define.ts
@@ -45,13 +45,15 @@ export function definePlugin(config: ResolvedConfig): Plugin {
       ...config.env,
       SSR: !!config.build.ssr
     }
+    // put import.meta.hot replacement in importMetaKeys to allow
+    // users to override with config.define.
+    importMetaKeys['import.meta.hot'] = `false`
     for (const key in env) {
       importMetaKeys[`import.meta.env.${key}`] = JSON.stringify(env[key])
     }
     Object.assign(importMetaFallbackKeys, {
       'import.meta.env.': `({}).`,
-      'import.meta.env': JSON.stringify(config.env),
-      'import.meta.hot': `false`
+      'import.meta.env': JSON.stringify(config.env)
     })
   }
 
