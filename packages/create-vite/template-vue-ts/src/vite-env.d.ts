dcd8cb1423e7fac3d8aca25df8fee4877a3d6a26
Bjorn Lu <bjornlu.dev@gmail.com>
2022-07-08 17:40:01 +0800

    chore: use vite-env.d.ts convention (#8988)

--- packages/create-vite/template-vue-ts/src/vite-env.d.ts
+++ packages/create-vite/template-vue-ts/src/vite-env.d.ts
@@ -2,7 +2,6 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
 }
