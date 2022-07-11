519f7debe176d99f464a96240842c303d8ec5e57
Kia King Ishii <kia.king.08@gmail.com>
2022-07-11 21:59:03 +0900

    docs: add team page (#8720)
    Co-authored-by: Shinigami <chrissi92@hotmail.de>
    Co-authored-by: patak <matias.capeletto@gmail.com>
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>
    Co-authored-by: 翠 / green <green@sapphi.red>
    Co-authored-by: Alec Larson <1925840+aleclarson@users.noreply.github.com>
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>
    Co-authored-by: ygj6 <7699524+ygj6@users.noreply.github.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -72,31 +72,36 @@ export default defineConfig({
       { text: 'Config', link: '/config/', activeMatch: '/config/' },
       { text: 'Plugins', link: '/plugins/', activeMatch: '/plugins/' },
       {
-        text: 'Links',
+        text: 'Resources',
         items: [
+          { text: 'Team', link: '/team' },
           {
-            text: 'Twitter',
-            link: 'https://twitter.com/vite_js'
-          },
-          {
-            text: 'Discord Chat',
-            link: 'https://chat.vitejs.dev'
-          },
-          {
-            text: 'Awesome Vite',
-            link: 'https://github.com/vitejs/awesome-vite'
-          },
-          {
-            text: 'DEV Community',
-            link: 'https://dev.to/t/vite'
-          },
-          {
-            text: 'Rollup Plugins Compat',
-            link: 'https://vite-rollup-plugins.patak.dev/'
-          },
-          {
-            text: 'Changelog',
-            link: 'https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md'
+            items: [
+              {
+                text: 'Twitter',
+                link: 'https://twitter.com/vite_js'
+              },
+              {
+                text: 'Discord Chat',
+                link: 'https://chat.vitejs.dev'
+              },
+              {
+                text: 'Awesome Vite',
+                link: 'https://github.com/vitejs/awesome-vite'
+              },
+              {
+                text: 'DEV Community',
+                link: 'https://dev.to/t/vite'
+              },
+              {
+                text: 'Rollup Plugins Compat',
+                link: 'https://vite-rollup-plugins.patak.dev/'
+              },
+              {
+                text: 'Changelog',
+                link: 'https://github.com/vitejs/vite/blob/main/packages/vite/CHANGELOG.md'
+              }
+            ]
           }
         ]
       },
