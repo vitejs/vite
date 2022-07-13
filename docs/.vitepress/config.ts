a0c9de584274f9607f2423467f2aadd9c64840ce
翠 / green <green@sapphi.red>
2022-07-13 20:32:39 +0900

    docs: version toggle label (#9069)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -106,10 +106,10 @@ export default defineConfig({
         ]
       },
       {
-        text: 'v3 (next)',
+        text: 'Version',
         items: [
           {
-            text: 'v2.x (stable)',
+            text: 'Vite 2 Docs',
             link: 'https://v2.vitejs.dev'
           }
         ]
