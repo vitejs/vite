0bd053d4f9e6ab1f991fac3b8fc34c40ac8feab1
bluwy <bjornlu.dev@gmail.com>
2022-07-15 23:11:08 +0800

    fix: externalize workspace relative import when bundle config

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -900,11 +900,40 @@ async function bundleConfigFile(
       {
         name: 'externalize-deps',
         setup(build) {
-          build.onResolve({ filter: /.*/ }, (args) => {
-            const id = args.path
+          build.onResolve({ filter: /.*/ }, ({ path: id, importer }) => {
+            // bundle all relative paths
             if (id[0] !== '.' && !path.isAbsolute(id)) {
               return {
                 external: true
+              }
+            }
+            // if the file is outside of a vite project, make sure that the we can
+            // also access it's third-party dependencies. externalize if not.
+            // monorepo/
+            // ├─ package.json
+            // ├─ utils.js -----------> bundle (share same node_modules)
+            // ├─ vite-project/
+            // │  ├─ vite.config.js --> entry
+            // │  ├─ package.json
+            // ├─ foo-project/
+            // │  ├─ utils.js --------> external (has own node_modules)
+            // │  ├─ package.json
+            if (id.startsWith('..')) {
+              const idFsPath = path.resolve(path.dirname(importer), id)
+              const idPkgPath = lookupFile(idFsPath, [`package.json`], {
+                pathOnly: true
+              })
+              if (idPkgPath) {
+                const idPkgDir = path.dirname(idPkgPath)
+                // if this file needs to go up one or more directory to reach the vite config,
+                // that means it has it's own node_modules (e.g. foo-project)
+                if (path.relative(idPkgDir, fileName).startsWith('..')) {
+                  return {
+                    // normalize actual relative import after bundled as a single vite config
+                    path: path.relative(path.dirname(fileName), idFsPath),
+                    external: true
+                  }
+                }
               }
             }
           })
