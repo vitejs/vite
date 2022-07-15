d6b3e95f33c3825b0cc8f955d8305dcd1863d0b1
Nostalgia <10909568@qq.com>
2022-07-15 17:27:47 +0800

    fix(types): fix type checking error with eslint
    pass any to DefineComponent occurs typescript eslint type checking error

--- packages/create-vite/template-vue-ts/src/vite-env.d.ts
+++ packages/create-vite/template-vue-ts/src/vite-env.d.ts
@@ -2,6 +2,6 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  const component: DefineComponent
   export default component
 }
