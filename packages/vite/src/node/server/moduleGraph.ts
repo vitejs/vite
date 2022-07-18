1236a998d592113ff29bfb31ea83ce756e788cee
sapphi-red <green@sapphi.red>
2022-07-18 21:53:49 +0900

    fix: prevent null pathname error

--- packages/vite/src/node/server/moduleGraph.ts
+++ packages/vite/src/node/server/moduleGraph.ts
@@ -237,10 +237,16 @@ export class ModuleGraph {
     url = removeImportQuery(removeTimestampQuery(url))
     const resolved = await this.resolveId(url, !!ssr)
     const resolvedId = resolved?.id || url
-    const ext = extname(cleanUrl(resolvedId))
-    const { pathname, search, hash } = parseUrl(url)
-    if (ext && !pathname!.endsWith(ext)) {
-      url = pathname + ext + (search || '') + (hash || '')
+    if (
+      url !== resolvedId &&
+      !url.includes('\0') &&
+      !url.startsWith(`virtual:`)
+    ) {
+      const ext = extname(cleanUrl(resolvedId))
+      const { pathname, search, hash } = parseUrl(url)
+      if (ext && !pathname!.endsWith(ext)) {
+        url = pathname + ext + (search || '') + (hash || '')
+      }
     }
     return [url, resolvedId, resolved?.meta]
   }
