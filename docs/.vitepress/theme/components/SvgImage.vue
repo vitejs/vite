2c2db083fdb48a60b7eab5ccafa7d7a03b8c93d7
Tony Trinh <tony19@gmail.com>
2022-07-18 19:35:49 -0500

    docs: add SvgImage.vue global component

---
+++ docs/.vitepress/theme/components/SvgImage.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    svg: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
+
+<template>
+  <figure class="svg-image-root" v-html="svg" />
+</template>
+
+<style>
+.svg-image-root {
+  background-color: #eee;
+  border: solid 1px #ccc;
+  border-radius: 8px;
+  padding: 1ch;
+  margin: 1ch 0;
+}
+html.dark .svg-image-root {
+  background-color: #313641;
+}
+</style>
