29db3ea4e6d6d5cb1d6891768789063a9bcdbd0f
Hang Liang <90079525+xlianghang@users.noreply.github.com>
2022-07-20 23:38:07 +0800

    docs: fix server options link (#9242)

--- packages/vite/src/node/server/middlewares/static.ts
+++ packages/vite/src/node/server/middlewares/static.ts
@@ -193,7 +193,7 @@ function ensureServingAccess(
     const hintMessage = `
 ${server.config.server.fs.allow.map((i) => `- ${i}`).join('\n')}
 
-Refer to docs https://vitejs.dev/config/#server-fs-allow for configurations and more details.`
+Refer to docs https://vitejs.dev/config/server-options.html#server-fs-allow for configurations and more details.`
 
     server.config.logger.error(urlMessage)
     server.config.logger.warnOnce(hintMessage + '\n')
