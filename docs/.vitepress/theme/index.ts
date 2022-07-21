6adb90695ba0de5b5cdc3edcc4a9fb63835efded
Tony Trinh <tony19@gmail.com>
2022-07-21 04:32:41 -0500

    docs: convert png diagrams to svg (#9208)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,7 @@ import Theme from 'vitepress/theme'
 import './styles/vars.css'
 import HomeSponsors from './components/HomeSponsors.vue'
 import AsideSponsors from './components/AsideSponsors.vue'
+import SvgImage from './components/SvgImage.vue'
 
 export default {
   ...Theme,
@@ -11,5 +12,8 @@ export default {
       'home-features-after': () => h(HomeSponsors),
       'aside-ads-before': () => h(AsideSponsors)
     })
+  },
+  enhanceApp({ app }) {
+    app.component('SvgImage', SvgImage)
   }
 }
