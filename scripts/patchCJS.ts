8e5fdf77a3545ce4fc2b26fe46a30ac783322c4e
Joaquín Sánchez <joaquin.sanchez@fi2net.es>
2022-07-24 08:03:41 +0200

    fix: build command fails in Windows (#9329)

--- scripts/patchCJS.ts
+++ scripts/patchCJS.ts
@@ -41,7 +41,7 @@ if (matchMixed) {
   writeFileSync(indexPath, lines.join('\n'))
 
   console.log(colors.bold(`${indexPath} CJS patched`))
-  process.exit()
+  process.exit(0)
 }
 
 const matchDefault = code.match(/\nmodule.exports = (\w+);/)
@@ -50,7 +50,7 @@ if (matchDefault) {
   code += `module.exports["default"] = ${matchDefault[1]};\n`
   writeFileSync(indexPath, code)
   console.log(colors.bold(`${indexPath} CJS patched`))
-  process.exit()
+  process.exit(0)
 }
 
 console.error(colors.red(`${indexPath} CJS patch failed`))
