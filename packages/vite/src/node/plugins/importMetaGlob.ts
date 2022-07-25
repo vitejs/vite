fc4661fae07b0bbdf72f6d11772a1b2b5a26c718
qing.deng <dengqing0821@gmail.com>
2022-07-25 12:43:22 +0800

    fix: support `import.meta.glob` to use template strings but not contain expressions.

--- packages/vite/src/node/plugins/importMetaGlob.ts
+++ packages/vite/src/node/plugins/importMetaGlob.ts
@@ -14,6 +14,7 @@ import MagicString from 'magic-string'
 import fg from 'fast-glob'
 import { stringifyQuery } from 'ufo'
 import type { GeneralImportGlobOptions } from 'types/importGlob'
+import type { TemplateLiteral } from '@babel/types'
 import type { Plugin } from '../plugin'
 import type { ViteDevServer } from '../server'
 import type { ModuleNode } from '../server/moduleGraph'
@@ -168,29 +169,40 @@ export async function parseImportGlob(
     if (ast.arguments.length < 1 || ast.arguments.length > 2)
       throw err(`Expected 1-2 arguments, but got ${ast.arguments.length}`)
 
-    const arg1 = ast.arguments[0] as ArrayExpression | Literal
+    const arg1 = ast.arguments[0] as ArrayExpression | Literal | TemplateLiteral
     const arg2 = ast.arguments[1] as Node | undefined
 
     const globs: string[] = []
-    if (arg1.type === 'ArrayExpression') {
-      for (const element of arg1.elements) {
-        if (!element) continue
-        if (element.type !== 'Literal') throw err('Could only use literals')
+
+    const validateLiteral = (
+      element: Literal | TemplateLiteral | ArrayExpression['elements'][0]
+    ) => {
+      if (!element) return
+      if (element.type === 'Literal') {
         if (typeof element.value !== 'string')
           throw err(
             `Expected glob to be a string, but got "${typeof element.value}"`
           )
-
         globs.push(element.value)
+      } else if (element.type === 'TemplateLiteral') {
+        if (element.expressions.length !== 0) {
+          throw err(
+            `Expected glob to be a string, but got contains expressions`
+          )
+        }
+        globs.push(element.quasis[0].value.raw)
+      } else {
+        throw err('Could only use literals')
       }
-    } else if (arg1.type === 'Literal') {
-      if (typeof arg1.value !== 'string')
-        throw err(
-          `Expected glob to be a string, but got "${typeof arg1.value}"`
-        )
-      globs.push(arg1.value)
+    }
+
+    if (arg1.type === 'ArrayExpression') {
+      for (const element of arg1.elements) {
+        if (!element) continue
+        validateLiteral(element)
+      }
     } else {
-      throw err('Could only use literals')
+      validateLiteral(arg1)
     }
 
     // arg2
