ae43c0d9cb893d80b7ca5fe44cbdb5a3ac11e4b4
yzydeveloper <wqrdw@qq.com>
2022-08-01 17:01:09 +0800

    fix: css-post/emitFile uses chunkName

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -543,11 +543,12 @@ export function cssPostPlugin(config: ResolvedConfig): Plugin {
           chunkCSS = await finalizeCss(chunkCSS, true, config)
 
           // emit corresponding css file
+          const chunkFile = chunk.name + '.css'
           const fileHandle = this.emitFile({
             name: isPreProcessor(lang) ? cssAssetName : cssFileName,
             fileName: assetFileNamesToFileName(
               resolveAssetFileNames(config),
-              cssFileName,
+              chunkFile,
               getHash(chunkCSS),
               chunkCSS
             ),
