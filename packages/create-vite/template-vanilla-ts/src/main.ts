dd40a97a636139850f730bb11dbdb8e328389b7d
Divyamrit <69889565+sacarvy@users.noreply.github.com>
2022-08-01 21:25:46 +0530

    remove vanilla classname small typo fix
    It's must be a type,

--- packages/create-vite/template-vanilla-ts/src/main.ts
+++ packages/create-vite/template-vanilla-ts/src/main.ts
@@ -8,7 +8,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      <img src="${typescriptLogo}" class="logo" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
