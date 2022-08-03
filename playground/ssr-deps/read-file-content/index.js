30fe52655bf4aa32c6e46c5cedde6fed5c01e05c
sapphi-red <green@sapphi.red>
2022-08-03 20:38:09 +0900

    chore: remove condition for node 12 support
    refs #5248

--- playground/ssr-deps/read-file-content/index.js
+++ playground/ssr-deps/read-file-content/index.js
@@ -1,9 +1,6 @@
 const path = require('node:path')
 
 module.exports = async function readFileContent(filePath) {
-  const fs =
-    process.versions.node.split('.')[0] >= '14'
-      ? require('node:fs/promises')
-      : require('node:fs').promises
+  const fs = require('node:fs/promises')
   return await fs.readFile(path.resolve(filePath), 'utf-8')
 }
