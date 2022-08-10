fb734fa66a910692364ae24e5d39096731af4de8
yoho <panyoho@gmail.com>
2022-08-10 19:53:22 +0800

    chore: update
    Co-authored-by: 翠 / green <green@sapphi.red>

--- playground/vue-legacy/__tests__/vue-legacy.spec.ts
+++ playground/vue-legacy/__tests__/vue-legacy.spec.ts
@@ -1,5 +1,5 @@
 import { untilUpdated } from '~utils'
 
 test('vue legacy assets', async () => {
-  untilUpdated(() => '.container', 'asset')
+  untilUpdated(() => getBg('.container'), 'asset', true)
 })
