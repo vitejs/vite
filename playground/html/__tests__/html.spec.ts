81b4123778c0a105667a864c391c1374268fd12d
patak-dev <matias.capeletto@gmail.com>
2022-08-15 21:45:12 +0200

    refactor: valid.html

--- playground/html/__tests__/html.spec.ts
+++ playground/html/__tests__/html.spec.ts
@@ -242,3 +242,10 @@ describe.runIf(isServe)('invalid', () => {
     expect(content).toBeTruthy()
   })
 })
+
+describe('valid html', () => {
+  test('duplicated attrs', async () => {
+    await page.goto(viteTestUrl + '/valid.html')
+    expect(await getColor('#duplicated-attrs')).toBe('green')
+  })
+})
