297b7657cfba214b5563375df63ddc5090e63e86
yoho <panyoho@gmail.com>
2022-08-17 19:36:51 +0800

    chore: update
    Co-authored-by: 翠 / green <green@sapphi.red>

--- playground/vue-legacy/Main.vue
+++ playground/vue-legacy/Main.vue
@@ -1,5 +1,5 @@
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, defineAsyncComponent } from 'vue'
 import css from './inline.css?inline'
 
 export default defineComponent({
