ce283dca336b2ab4a8d095d014af3fc9df96f878
dominikg <dominik.goepel@gmx.de>
2022-08-25 20:53:33 +0200

    fix: remove multi char test, windows didn't like the directory name

--- playground/glob-import/escape/chars_*|(){}[]/glob.js
+++
@@ -1,5 +0,0 @@
-const relative = import.meta.glob('./**/*.js', { eager: true })
-const alias = import.meta.glob('@escape_chars_*|(){}[]_mod/**/*.js', {
-  eager: true
-})
-export { relative, alias }
