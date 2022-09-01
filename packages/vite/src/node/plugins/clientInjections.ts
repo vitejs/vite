03836066612f5e738066ffdbd47350b68747130f
Tony Trinh <tony19@gmail.com>
2022-08-31 23:27:21 -0500

    fix(client-inject): don't modify string literals
    fix #9829

--- packages/vite/src/node/plugins/clientInjections.ts
+++ packages/vite/src/node/plugins/clientInjections.ts
@@ -2,7 +2,12 @@ import path from 'node:path'
 import type { Plugin } from '../plugin'
 import type { ResolvedConfig } from '../config'
 import { CLIENT_ENTRY, ENV_ENTRY } from '../constants'
-import { isObject, normalizePath, resolveHostname } from '../utils'
+import {
+  isObject,
+  normalizePath,
+  replaceInCode,
+  resolveHostname
+} from '../utils'
 
 // ids in transform are normalized to unix style
 const normalizedClientEntry = normalizePath(CLIENT_ENTRY)
@@ -48,28 +53,35 @@ export function clientInjectionsPlugin(config: ResolvedConfig): Plugin {
           hmrBase = path.posix.join(hmrBase, hmrConfig.path)
         }
 
-        return code
-          .replace(`__MODE__`, JSON.stringify(config.mode))
-          .replace(`__BASE__`, JSON.stringify(devBase))
-          .replace(`__DEFINES__`, serializeDefine(config.define || {}))
-          .replace(`__SERVER_HOST__`, JSON.stringify(serverHost))
-          .replace(`__HMR_PROTOCOL__`, JSON.stringify(protocol))
-          .replace(`__HMR_HOSTNAME__`, JSON.stringify(host))
-          .replace(`__HMR_PORT__`, JSON.stringify(port))
-          .replace(`__HMR_DIRECT_TARGET__`, JSON.stringify(directTarget))
-          .replace(`__HMR_BASE__`, JSON.stringify(hmrBase))
-          .replace(`__HMR_TIMEOUT__`, JSON.stringify(timeout))
-          .replace(`__HMR_ENABLE_OVERLAY__`, JSON.stringify(overlay))
-      } else if (!options?.ssr && code.includes('process.env.NODE_ENV')) {
+        const replacements: Record<string, string> = {
+          __MODE__: JSON.stringify(config.mode),
+          __BASE__: JSON.stringify(devBase),
+          __DEFINES__: serializeDefine(config.define || {}),
+          __SERVER_HOST__: JSON.stringify(serverHost),
+          __HMR_PROTOCOL__: JSON.stringify(protocol),
+          __HMR_HOSTNAME__: JSON.stringify(host),
+          __HMR_PORT__: JSON.stringify(port),
+          __HMR_DIRECT_TARGET__: JSON.stringify(directTarget),
+          __HMR_BASE__: JSON.stringify(hmrBase),
+          __HMR_TIMEOUT__: JSON.stringify(timeout),
+          __HMR_ENABLE_OVERLAY__: JSON.stringify(overlay)
+        }
+        const pattern = new RegExp(Object.keys(replacements).join('|'), 'gu')
+        return replaceInCode(code, pattern, replacements)?.toString() ?? null
+      } else if (!options?.ssr) {
         // replace process.env.NODE_ENV instead of defining a global
         // for it to avoid shimming a `process` object during dev,
         // avoiding inconsistencies between dev and build
-        return code.replace(
-          /\bprocess\.env\.NODE_ENV\b/g,
-          config.define?.['process.env.NODE_ENV'] ||
-            JSON.stringify(process.env.NODE_ENV || config.mode)
+        return (
+          replaceInCode(
+            code,
+            /\bprocess\.env\.NODE_ENV\b/g,
+            config.define?.['process.env.NODE_ENV'] ||
+              JSON.stringify(process.env.NODE_ENV || config.mode)
+          )?.toString() ?? null
         )
       }
+      return null
     }
   }
 }
