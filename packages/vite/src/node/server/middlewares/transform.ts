7de66249bf1d50b5b553f1e6ad84a278564ca1c0
Jonas Kuske <mail@jonaskuske.com>
2022-09-04 16:38:26 +0200

    fix: read sec-fetch-dest to detect JS in transform

--- packages/vite/src/node/server/middlewares/transform.ts
+++ packages/vite/src/node/server/middlewares/transform.ts
@@ -148,6 +148,7 @@ export function transformMiddleware(
       }
 
       if (
+        req.headers['sec-fetch-dest'] === 'script' ||
         isJSRequest(url) ||
         isImportRequest(url) ||
         isCSSRequest(url) ||
