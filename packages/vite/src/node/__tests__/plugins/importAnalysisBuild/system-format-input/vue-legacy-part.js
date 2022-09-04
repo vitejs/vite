5e1ba19d755e5c67818c78b3ca1e804c809469e7
Tal Hadad <tal_hd@hotmail.com>
2022-09-04 13:30:03 +0300

    test: (hopefully) fix line ending issues

--- packages/vite/src/node/__tests__/plugins/importAnalysisBuild/system-format-input/vue-legacy-part.js
+++ packages/vite/src/node/__tests__/plugins/importAnalysisBuild/system-format-input/vue-legacy-part.js
@@ -1,12 +1,12 @@
 const _sfc_main = defineComponent({
-    components: {
-      module: defineAsyncComponent(() => __vitePreload(() => module.import('./module-legacy.de1a4764.js'), "__VITE_PRELOAD__", module.meta.url))
-    },
+  components: {
+    module: defineAsyncComponent(() => __vitePreload(() => module.import('./module-legacy.de1a4764.js'), "__VITE_PRELOAD__", module.meta.url))
+  },
 
-    setup() {
-      return {
-        css
-      };
-    }
+  setup() {
+    return {
+      css
+    };
+  }
 
-  });
+});
