85ed65d191b0580db641f398405cd93ebb546831
yangxingyuan <yangxingyuan@bytedance.com>
2022-09-14 18:11:16 +0800

    feat: add test case and remove react

--- playground/async-chunk-css-order/components/BlueButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Button } from './Button'
-import './BlueButton.css'
-
-export function BlueButton() {
-  return <Button className="blue">I'm a blue button</Button>
-}
--- playground/async-chunk-css-order/components/GreenButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Button } from './Button'
-import './GreenButton.css'
-
-export function GreenButton() {
-  return <Button className="green">I'm a green button</Button>
-}
