34aac6db0453c58bda79b5fabfb845e21f90ed91
Remus Mate <mrm_dev@outlook.com>
2022-09-21 18:11:57 +1000

    fix(build): fix resolution algorithm when `build.ssr` is true (#9989)

---
+++ playground/ssr-resolve/entries/dir/index.js
@@ -0,0 +1,1 @@
+module.exports = __filename.slice(__filename.lastIndexOf('entries'))
---
+++ playground/ssr-resolve/main.js
@@ -0,0 +1,12 @@
+// no `exports` key, should resolve to entries/dir/index.js
+import dirEntry from 'entries/dir'
+// no `exports` key, should resolve to entries/file.js
+import fileEntry from 'entries/file'
+// has `exports` key, should resolve to pkg-exports/entry
+import pkgExportsEntry from 'pkg-exports/entry'
+
+export default `
+  entries/dir: ${dirEntry}
+  entries/file: ${fileEntry}
+  pkg-exports/entry: ${pkgExportsEntry}
+`
