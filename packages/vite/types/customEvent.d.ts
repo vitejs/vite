7c225e52233f47b28049c04dc3fde60607d323b1
bluwy <bjornlu.dev@gmail.com>
2022-09-25 19:06:47 +0800

    chore: move back client types

--- packages/vite/types/customEvent.d.ts
+++ packages/vite/types/customEvent.d.ts
@@ -1,1 +1,16 @@
-export type { CustomEventMap, InferCustomEventPayload } from '../client/types'
+import type {
+  ErrorPayload,
+  FullReloadPayload,
+  PrunePayload,
+  UpdatePayload
+} from './hmrPayload'
+
+export interface CustomEventMap {
+  'vite:beforeUpdate': UpdatePayload
+  'vite:beforePrune': PrunePayload
+  'vite:beforeFullReload': FullReloadPayload
+  'vite:error': ErrorPayload
+}
+
+export type InferCustomEventPayload<T extends string> =
+  T extends keyof CustomEventMap ? CustomEventMap[T] : any
--- packages/vite/types/importMeta.d.ts
+++ packages/vite/types/importMeta.d.ts
@@ -1,13 +1,25 @@
-import type {
-  ImportMeta as ViteImportMeta,
-  ImportMetaEnv as ViteImportMetaEnv
-} from '../client/types'
+import type { ViteHotContext } from './hot'
+import type { ImportGlobEagerFunction, ImportGlobFunction } from './importGlob'
 
-declare global {
-  interface GlobOptions {
-    as?: string
-  }
+export interface ImportMetaEnv {
+  [key: string]: any
+  BASE_URL: string
+  MODE: string
+  DEV: boolean
+  PROD: boolean
+  SSR: boolean
+}
 
-  interface ImportMeta extends ViteImportMeta {}
-  interface ImportMetaEnv extends ViteImportMetaEnv {}
+export interface ImportMeta {
+  url: string
+
+  readonly hot?: ViteHotContext
+
+  readonly env: ImportMetaEnv
+
+  glob: ImportGlobFunction
+  /**
+   * @deprecated Use `import.meta.glob('*', { eager: true })` instead
+   */
+  globEager: ImportGlobEagerFunction
 }
