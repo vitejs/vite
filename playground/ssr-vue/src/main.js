4b9bbb3e6e89c4db5691d320262ef54af51e812f
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-10-06 13:53:06 -0700

    chore: migrate from vuex to pinia (#10366)

--- playground/ssr-vue/src/main.js
+++ playground/ssr-vue/src/main.js
@@ -1,3 +1,4 @@
+import { createPinia } from 'pinia'
 import { createSSRApp } from 'vue'
 import App from './App.vue'
 import { createRouter } from './router'
@@ -7,6 +8,8 @@ import { createRouter } from './router'
 // fresh store here.
 export function createApp() {
   const app = createSSRApp(App)
+  const pinia = createPinia()
+  app.use(pinia)
   const router = createRouter()
   app.use(router)
   return { app, router }
