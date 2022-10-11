3806a9872c21dc943366722732fd3a4459e3f6a4
bluwy <bjornlu.dev@gmail.com>
2022-10-11 15:27:35 +0800

    chore: fix lint

--- playground/cli-module/vite.config.js
+++ playground/cli-module/vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
+// eslint-disable-next-line import/no-nodejs-modules
 import { URL } from 'url'
+import { defineConfig } from 'vite'
 
 // make sure bundling works even if `url` refers to the locally installed
 // `url` package instead of the built-in `url` nodejs module
