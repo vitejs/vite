15c27f0a1e7b4297300bbcabb4cbe2263a85873b
pengboUESTC <pengbotju@163.com>
2022-10-13 15:02:14 +0800

    feat: add rollup config test

--- playground/config/__tests__/load.spec.ts
+++ playground/config/__tests__/load.spec.ts
@@ -26,65 +26,63 @@ it('loadConfigFromFile', async () => {
 it('loadConfigFromFile with rollupconfig', async () => {
   const { config } = await loadConfigFromFile(
     {} as any,
-    resolve(__dirname, '../packages/entry/vite.config.ts')
+    resolve(__dirname, '../packages/entry/vite.config.rollup.ts'),
+    resolve(__dirname, '../packages/entry/')
   )
-  expect(config).toMatchObject({
-    array: [
-      [1, 3],
-      [2, 4]
-    ],
-    build: {
+  const { output, plugins, external, input } = config.build.rollupOptions
+  expect(plugins).toMatchObject([])
+  expect(external).toMatchObject([])
+  expect(input).equal('src/index.ts')
+  // @ts-ignore
+  expect(config.array).toMatchObject([
+    [1, 3],
+    [2, 4]
+  ])
+  expect(output).toMatchObject([
+    {
+      amd: undefined,
+      assetFileNames: undefined,
+      banner: undefined,
+      chunkFileNames: undefined,
+      compact: undefined,
+      dir: 'dist',
+      dynamicImportFunction: undefined,
+      entryFileNames: undefined,
+      esModule: undefined,
+      exports: undefined,
+      extend: undefined,
+      externalLiveBindings: undefined,
+      file: undefined,
+      footer: undefined,
+      format: 'cjs',
+      freeze: undefined,
+      generatedCode: undefined,
+      globals: undefined,
+      hoistTransitiveImports: undefined,
+      indent: undefined,
+      inlineDynamicImports: undefined,
+      interop: undefined,
+      intro: undefined,
+      manualChunks: undefined,
+      minifyInternalExports: undefined,
+      name: undefined,
+      namespaceToStringTag: undefined,
+      noConflict: undefined,
+      outro: undefined,
+      paths: undefined,
       plugins: [],
-      external: [],
-      onwarn: function add(msg) {
-        console.log(msg)
-      },
-      input: 'src/index.ts',
-      output: {
-        amd: undefined,
-        assetFileNames: undefined,
-        banner: undefined,
-        chunkFileNames: undefined,
-        compact: undefined,
-        dir: 'dist',
-        dynamicImportFunction: undefined,
-        entryFileNames: undefined,
-        esModule: undefined,
-        exports: undefined,
-        extend: undefined,
-        externalLiveBindings: undefined,
-        file: undefined,
-        footer: undefined,
-        format: 'cjs',
-        freeze: undefined,
-        generatedCode: undefined,
-        globals: undefined,
-        hoistTransitiveImports: undefined,
-        indent: undefined,
-        inlineDynamicImports: undefined,
-        interop: undefined,
-        intro: undefined,
-        manualChunks: undefined,
-        minifyInternalExports: undefined,
-        name: undefined,
-        namespaceToStringTag: undefined,
-        noConflict: undefined,
-        outro: undefined,
-        paths: undefined,
-        plugins: [],
-        preferConst: undefined,
-        preserveModules: undefined,
-        preserveModulesRoot: undefined,
-        sanitizeFileName: undefined,
-        sourcemap: undefined,
-        sourcemapBaseUrl: undefined,
-        sourcemapExcludeSources: undefined,
-        sourcemapFile: undefined,
-        sourcemapPathTransform: undefined,
-        strict: undefined,
-        systemNullSetters: undefined,
-        validate: undefined
-      }
+      preferConst: undefined,
+      preserveModules: undefined,
+      preserveModulesRoot: undefined,
+      sanitizeFileName: undefined,
+      sourcemap: undefined,
+      sourcemapBaseUrl: undefined,
+      sourcemapExcludeSources: undefined,
+      sourcemapFile: undefined,
+      sourcemapPathTransform: undefined,
+      strict: undefined,
+      systemNullSetters: undefined,
+      validate: undefined
     }
-  })
+  ])
 })
