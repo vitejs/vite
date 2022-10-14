a7d8400baae166a2ceb2ec0238b0e91073529bb2
yucccc <feyucccc@gmail.com>
2022-10-15 02:31:41 +0800

    fix: type error

--- packages/vite/src/node/cli.ts
+++ packages/vite/src/node/cli.ts
@@ -28,9 +28,7 @@ interface GlobalCLIOptions {
   force?: boolean
 }
 
-const normalizeOptionsConfig: {
-  [K in keyof ServerOptions]: (v: any[]) => any
-} = {
+const normalizeOptionsConfig: Record<string, (v: any[]) => any> = {
   port: (v) => {
     return v.find((p) => typeof p === 'number' || typeof p === 'string') || 5173
   }
