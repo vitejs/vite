1a9028ae254a38032b683a6ba7fd967a980bf681
Manu MA <manu.mtza@gmail.com>
2022-10-19 13:05:14 +0200

    feat: include line and column in error format

--- packages/vite/src/node/server/middlewares/error.ts
+++ packages/vite/src/node/server/middlewares/error.ts
@@ -26,7 +26,11 @@ export function buildErrorMessage(
   includeStack = true
 ): string {
   if (err.plugin) args.push(`  Plugin: ${colors.magenta(err.plugin)}`)
-  if (err.id) args.push(`  File: ${colors.cyan(err.id)}`)
+  let loc = ''
+  if (err.loc) {
+    loc = `:${err.loc.line}:${err.loc.column}`
+  }
+  if (err.id) args.push(`  File: ${colors.cyan(err.id)}${loc}`)
   if (err.frame) args.push(colors.yellow(pad(err.frame)))
   if (includeStack && err.stack) args.push(pad(cleanStack(err.stack)))
   return args.join('\n')
