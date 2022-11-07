aed74c6ef664fa49a35c043a4dcde60b98dc334c
bluwy <bjornlu.dev@gmail.com>
2022-11-07 17:48:04 +0800

    fix: prevent cache on optional package resolve

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -951,12 +951,23 @@ export const requireResolveFromRootWithFallback = (
   root: string,
   id: string
 ): string => {
+  const paths = _require.resolve.paths?.(id) || []
   // Search in the root directory first, and fallback to the default require paths.
-  const fallbackPaths = _require.resolve.paths?.(id) || []
-  const path = _require.resolve(id, {
-    paths: [root, ...fallbackPaths]
-  })
-  return path
+  paths.unshift(root)
+
+  // Use `resolve` package to check existence first, so if the package is not found,
+  // it won't be cached by nodejs, since there isn't a way to invalidate them:
+  // https://github.com/nodejs/node/issues/44663
+  if (!resolve.sync(id, { paths })) {
+    const err: any = new Error(
+      `Cannot find module '${root}' imported from '${root}'`
+    )
+    err.code = 'ERR_MODULE_NOT_FOUND'
+    throw new err()
+  }
+
+  // Use `require.resolve` again as the `resolve` package doesn't support the `exports` field
+  return _require.resolve(id, { paths })
 }
 
 // Based on node-graceful-fs
