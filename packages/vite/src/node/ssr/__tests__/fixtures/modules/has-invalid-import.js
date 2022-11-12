dca2f7e549f8ee532faceb85ff4117b6b6fb18ec
Bjorn Lu <bjornlu.dev@gmail.com>
2022-11-12 15:17:10 +0800

    fix(ssr): improve missing file error (#10880)

---
+++ packages/vite/src/node/ssr/__tests__/fixtures/modules/has-invalid-import.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line node/no-missing-import
+import { foo } from './non-existent.js'
+
+export const hello = 'world'
---
+++ packages/vite/src/node/ssr/__tests__/ssrLoadModule.spec.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'node:url'
+import { expect, test } from 'vitest'
+import { createServer } from '../../server'
+
+const root = fileURLToPath(new URL('./', import.meta.url))
+
+async function createDevServer() {
+  const server = await createServer({ configFile: false, root })
+  server.pluginContainer.buildStart({})
+  return server
+}
+
+test('ssrLoad', async () => {
+  expect.assertions(1)
+  const server = await createDevServer()
+  try {
+    await server.ssrLoadModule('/fixtures/modules/has-invalid-import.js')
+  } catch (e) {
+    expect(e.message).toBe(
+      'Failed to load url ./non-existent.js (resolved id: ./non-existent.js). Does the file exist?'
+    )
+  }
+})
