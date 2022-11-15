cbd08422b0e837a8b0b659bd77f128aa6c367981
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2022-11-15 15:20:42 -0500

    fix: support `export type` rewrites

--- packages/vite/scripts/emitCjsTypes.ts
+++ packages/vite/scripts/emitCjsTypes.ts
@@ -14,9 +14,14 @@ async function main() {
     let text = fs.readFileSync(file, 'utf8')
 
     const [imports] = lexer.parse(text)
-    for (const i of [...imports].reverse()) {
+    const typeImports = parseTypeImports(text, true)
+    const allImports = [...imports, ...typeImports]
+      // In reverse order
+      .sort((a, b) => b.s - a.s)
+
+    for (const i of allImports) {
       let id = i.n
-      if (!id || !/^\.\.?(?:\/|$)/.test(id)) {
+      if (!id || !/^\.\.?(?:\/|$)/.test(id) || id.endsWith('.cjs')) {
         continue
       }
 
@@ -46,6 +51,46 @@ function isDirectory(file: string) {
   } catch (e) {
     return false
   }
+}
+
+// This assumes no each import/export statement is on its own line.
+function parseTypeImports(code: string, exportsOnly?: boolean) {
+  const imports = []
+
+  const openKeywords = exportsOnly ? ['export'] : ['import', 'export']
+  const openPattern = [['', '\n'], openKeywords, ['type']]
+  const fromPattern = [['from'], ['"', "'"]]
+
+  let cursor = 0
+  let pattern = openPattern
+  let patternIndex = 0
+
+  const words = code.split(/([\w/\-@.]+|\n)/g)
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i].replace(/ /g, '')
+    if (pattern[patternIndex].includes(word)) {
+      if (++patternIndex === pattern.length) {
+        patternIndex = 0
+        if (pattern === openPattern) {
+          pattern = fromPattern
+        } else if (pattern === fromPattern) {
+          pattern = openPattern
+          const moduleSpecifier = words[i + 1]
+          const start = cursor + words[i].length
+          imports.push({
+            s: start,
+            e: start + moduleSpecifier.length,
+            n: moduleSpecifier
+          })
+        }
+      }
+    } else if (patternIndex > 0 && word) {
+      patternIndex = 0
+    }
+    cursor += words[i].length
+  }
+
+  return imports
 }
 
 main()
