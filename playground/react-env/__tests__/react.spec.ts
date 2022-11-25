b4c8abf04e485d6dde3dcf76556845da88a0454e
翠 / green <green@sapphi.red>
2022-11-26 01:23:43 +0900

    test: extend react-env timeout (#11078)

--- playground/react-env/__tests__/react.spec.ts
+++ playground/react-env/__tests__/react.spec.ts
@@ -2,9 +2,5 @@ import { expect, test } from 'vitest'
 import { page } from '~utils'
 
 test('should work', async () => {
-  expect(
-    await page.textContent('h1', {
-      timeout: 100
-    })
-  ).toMatch('Hello Vite + React')
+  expect(await page.textContent('h1')).toMatch('Hello Vite + React')
 })
