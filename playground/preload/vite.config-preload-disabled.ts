868e54c0ee9406b1faf7cd4564b9b0ad24e992ea
sapphi-red <green@sapphi.red>
2022-12-07 15:35:39 +0900

    test: fix preload flakiness

--- playground/preload/vite.config-preload-disabled.ts
+++ playground/preload/vite.config-preload-disabled.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vuePlugin()],
   build: {
+    outDir: 'dist/preload-disabled',
     minify: 'terser',
     terserOptions: {
       format: {
