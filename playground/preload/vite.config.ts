8307cae07004723531d5aed929761e9dd1f43b90
翠 / green <green@sapphi.red>
2022-12-08 14:50:08 +0900

    test: fix preload flakiness (#11252)

--- playground/preload/vite.config.ts
+++ playground/preload/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vuePlugin()],
   build: {
+    outDir: 'dist/normal',
     minify: 'terser',
     terserOptions: {
       format: {
