5af8641970f515697131a7cc3e9628c57d718b23
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2022-12-13 05:07:43 +0100

    Update counter.js

--- packages/create-vite/template-vanilla/counter.js
+++ packages/create-vite/template-vanilla/counter.js
@@ -1,9 +1,6 @@
-export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+let counter = -1;
+const getCount = (el) => el.innerHTML = `count is ${counter++}`
+export function setupCounter(el) {
+  getCount(el);  
+  element.addEventListener('click', ({ target: el }) => getcount(el));
 }
