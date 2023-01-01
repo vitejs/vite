704c4be645ac36147a4bfa3273877f9c4c62efb3
栗嘉男 <574980606@qq.com>
2023-01-01 23:12:31 +0800

    update type

--- packages/create-vite/template-preact-ts/src/main.tsx
+++ packages/create-vite/template-preact-ts/src/main.tsx
@@ -2,4 +2,4 @@ import { render } from 'preact'
 import { App } from './app'
 import './index.css'
 
-render(<App />, document.getElementById('app') as HTMLDivElement)
+render(<App />, document.getElementById('app')!)
