3e20e0cd3e5e0609b45782268b9b9f152408b636
sapphi-red <green@sapphi.red>
2023-01-22 14:02:01 +0900

    test: fix Node 14

--- playground/ssr-html/test-stacktrace.js
+++ playground/ssr-html/test-stacktrace.js
@@ -6,6 +6,19 @@ const isSourceMapEnabled = process.argv[2] === 'true'
 process.setSourceMapsEnabled(isSourceMapEnabled)
 console.log('# sourcemaps enabled:', isSourceMapEnabled)
 
+const version = (() => {
+  const m = process.version.match(/^v(\d+)\.(\d+)\.\d+$/)
+  if (!m) throw new Error(`Failed to parse version: ${process.version}`)
+
+  return { major: m[1], minor: m[2] }
+})()
+
+// https://github.com/nodejs/node/pull/43428
+const isFunctionSourceMapSupported =
+  (version.major === 16 && version.minor >= 17) ||
+  (version.major === 18 && version.minor >= 6) ||
+  version.major >= 19
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const isTest = process.env.VITEST
 
@@ -23,9 +36,9 @@ try {
   mod.error()
 } catch (e) {
   // this should not be called
-  // when running on Node.js "^16.17.0 || >=18.6.0" and sourcemap is enabled
-  // because the stacktrace is already rewritten
-  if (!isSourceMapEnabled) {
+  // when sourcemap support for `new Function` is supported and sourcemap is enabled
+  // because the stacktrace is already rewritten by Node.js
+  if (!(isSourceMapEnabled && isFunctionSourceMapSupported)) {
     vite.ssrFixStacktrace(e)
   }
   console.log(e)
