cc00b522fbe0722fc2abbaa225eb48c0154fe585
翠 / green <green@sapphi.red>
2023-02-01 18:52:45 +0900

    docs: add troubleshooting for browser compat (#11877)
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- packages/vite/src/node/plugins/resolve.ts
+++ packages/vite/src/node/plugins/resolve.ts
@@ -388,7 +388,7 @@ export function resolvePlugin(resolveOptions: InternalResolveOptions): Plugin {
           return `\
 export default new Proxy({}, {
   get(_, key) {
-    throw new Error(\`Module "${id}" has been externalized for browser compatibility. Cannot access "${id}.\${key}" in client code.\`)
+    throw new Error(\`Module "${id}" has been externalized for browser compatibility. Cannot access "${id}.\${key}" in client code.  See http://vitejs.dev/guide/troubleshooting.html#module-externalized-for-browser-compatibility for more details.\`)
   }
 })`
         }
