9e9a2a8f956f836f6da8dafcaa32512e92a5653c
sapphi-red <green@sapphi.red>
2023-02-14 17:22:55 +0900

    feat: support `import.meta.hot?.accept`

--- playground/hmr/hmr.ts
+++ playground/hmr/hmr.ts
@@ -3,6 +3,7 @@ import { foo as depFoo, nestedFoo } from './hmrDep'
 import './importing-updated'
 import './invalidation/parent'
 import './file-delete-restore'
+import './optional-chaining/parent'
 
 export const foo = 1
 text('.app', foo)
