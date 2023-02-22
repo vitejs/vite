622f03b6952069b08f4748f6fc34dcfd73f2bb10
Shayne O'Sullivan <shayneosull@gmail.com>
2023-02-22 00:04:39 -0600

    chore: update volar link in helloworld components

--- packages/create-vite/template-vue/src/components/HelloWorld.vue
+++ packages/create-vite/template-vue/src/components/HelloWorld.vue
@@ -27,7 +27,11 @@ const count = ref(0)
   </p>
   <p>
     Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a
+      href="https://marketplace.visualstudio.com/items?itemName=Vue.volar"
+      target="_blank"
+      >Volar</a
+    >
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
