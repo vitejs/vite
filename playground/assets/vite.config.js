ffa57956212ce32e7201430808c1ff6880460070
fi3ework <fi3ework@gmail.com>
2023-02-27 13:36:57 +0800

    refactor: friendly to git log

--- playground/assets/vite.config.js
+++ playground/assets/vite.config.js
@@ -1,21 +1,21 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
-export default /** @type {import('vite').UserConfig} */ (
-  defineConfig({
-    base: '/foo',
-    publicDir: 'static',
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'nested'),
-      },
+/** @type {import('vite').UserConfig} */
+// @ts-expect-error typecast
+export default defineConfig({
+  base: '/foo',
+  publicDir: 'static',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'nested'),
     },
-    assetsInclude: ['**/*.unknown'],
-    build: {
-      outDir: 'dist/foo',
-      assetsInlineLimit: 8192, // 8kb
-      manifest: true,
-      watch: {},
-    },
-  })
-)
+  },
+  assetsInclude: ['**/*.unknown'],
+  build: {
+    outDir: 'dist/foo',
+    assetsInlineLimit: 8192, // 8kb
+    manifest: true,
+    watch: {},
+  },
+})
--- playground/lib/vite.config.js
+++ playground/lib/vite.config.js
@@ -2,41 +2,41 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { defineConfig } from 'vite'
 
-export default /** @type {import('vite').UserConfig} */ (
-  defineConfig({
-    esbuild: {
-      supported: {
-        // Force esbuild inject helpers to test regex
-        'object-rest-spread': false,
+/** @type {import('vite').UserConfig} */
+// @ts-expect-error typecast
+export default defineConfig({
+  esbuild: {
+    supported: {
+      // Force esbuild inject helpers to test regex
+      'object-rest-spread': false,
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        banner: `/*!\nMayLib\n*/`,
       },
     },
-    build: {
-      rollupOptions: {
-        output: {
-          banner: `/*!\nMayLib\n*/`,
-        },
-      },
-      lib: {
-        entry: path.resolve(__dirname, 'src/main.js'),
-        name: 'MyLib',
-        formats: ['es', 'umd', 'iife'],
-        fileName: 'my-lib-custom-filename',
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.js'),
+      name: 'MyLib',
+      formats: ['es', 'umd', 'iife'],
+      fileName: 'my-lib-custom-filename',
+    },
+  },
+  plugins: [
+    {
+      name: 'emit-index',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: 'index.html',
+          source: fs.readFileSync(
+            path.resolve(__dirname, 'index.dist.html'),
+            'utf-8',
+          ),
+        })
       },
     },
-    plugins: [
-      {
-        name: 'emit-index',
-        generateBundle() {
-          this.emitFile({
-            type: 'asset',
-            fileName: 'index.html',
-            source: fs.readFileSync(
-              path.resolve(__dirname, 'index.dist.html'),
-              'utf-8',
-            ),
-          })
-        },
-      },
-    ],
-  })
-)
+  ],
+})
