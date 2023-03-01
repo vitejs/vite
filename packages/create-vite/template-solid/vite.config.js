274653535c24bf5481dece1011018d36b18efd28
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-03-01 19:02:52 +0200

    fix(solid template): Removed unneeded configurations

--- packages/create-vite/template-solid/vite.config.js
+++ packages/create-vite/template-solid/vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
+import solid from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+  plugins: [solid()],
 })
