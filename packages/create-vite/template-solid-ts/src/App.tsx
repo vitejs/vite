38dde55be20ee7242fe3f925804af87f0ddcf7ef
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-03-01 19:01:03 +0200

    fix(solid template): fixed a typo
    React => Solid

--- packages/create-vite/template-solid-ts/src/App.tsx
+++ packages/create-vite/template-solid-ts/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and Solid logos to learn more
       </p>
     </div>
   )
