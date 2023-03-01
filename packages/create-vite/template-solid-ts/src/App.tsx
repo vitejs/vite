72ed3c5fe088d8ddf66f52be415675cbcc3ff636
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-03-01 18:59:42 +0200

    fix(solid template): fixed code error in template

--- packages/create-vite/template-solid-ts/src/App.tsx
+++ packages/create-vite/template-solid-ts/src/App.tsx
@@ -18,7 +18,7 @@ function App() {
       <h1>Vite + Solid</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count()}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
