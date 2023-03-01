b925aff9e91bc8b105b3bfa957f7e4bbd0466f45
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-03-01 02:38:15 +0200

    fix(solid template): start following the style of vite projects

--- packages/create-vite/solid-ts/src/App.tsx
+++ packages/create-vite/solid-ts/src/App.tsx
@@ -1,25 +1,32 @@
-import type { Component } from 'solid-js'
+import { useSignal } from 'solid-js'
+import solidLogo from './assets/solid.svg'
+import './App/module.css'
 
-import logo from './logo.svg'
-import styles from './App.module.css'
+function App() {
+  const [count, setCount] = useSignal(0)
 
-const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-      </header>
+        <a href="https://solidjs.com" target="_blank">
+          <img src={solidLogo} className="logo solid" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + Solid</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   )
 }
