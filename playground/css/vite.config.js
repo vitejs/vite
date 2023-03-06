afea8850528ba144a619423032aa6ce12d3b529e
François Risoud <francois.risoud@gmail.com>
2023-03-06 18:58:14 +0100

    feat(css): add preprocessor option to define stylus vars & funcs (#7227)
    Co-authored-by: bluwy <bjornlu.dev@gmail.com>

--- playground/css/vite.config.js
+++ playground/css/vite.config.js
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import stylus from 'stylus'
 import { defineConfig } from 'vite'
 
 // trigger scss bug: https://github.com/sass/dart-sass/issues/710
@@ -68,6 +69,10 @@ export default defineConfig({
           './options/relative-import.styl',
           path.join(__dirname, 'options/absolute-import.styl'),
         ],
+        define: {
+          $definedColor: new stylus.nodes.RGBA(51, 197, 255, 1),
+          definedFunction: () => new stylus.nodes.RGBA(255, 0, 98, 1),
+        },
       },
     },
   },
