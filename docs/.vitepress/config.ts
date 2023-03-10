ce4e8525d8c7d4e18d52c09f23cc6b053509f014
Nazaré da Piedade <31008635+nazarepiedady@users.noreply.github.com>
2023-03-10 19:09:16 +0000

    chore: add `português` to locales in `.vitepress/config.ts` (#12105)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -91,6 +91,7 @@ export default defineConfig({
     zh: { label: '简体中文', link: 'https://cn.vitejs.dev' },
     ja: { label: '日本語', link: 'https://ja.vitejs.dev' },
     es: { label: 'Español', link: 'https://es.vitejs.dev' },
+    pt: { label: 'Português', link: 'https://pt.vitejs.dev' },
   },
 
   vue: {
