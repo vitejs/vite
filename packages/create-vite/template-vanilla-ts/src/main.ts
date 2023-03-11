8281c0fb9b3f24db5193ed65ce58cc4958643f37
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-03-11 18:36:37 +0000

    fix(create-vite): fix broken Vite logo when base is configured

--- packages/create-vite/template-vanilla-ts/src/main.ts
+++ packages/create-vite/template-vanilla-ts/src/main.ts
@@ -1,11 +1,12 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
 import { setupCounter } from './counter'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src=${viteLogo} class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
