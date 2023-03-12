4aafa6bc26e6f22fd99c5a4e83c7e9b4bff019f8
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-03-12 14:50:39 +0000

    style(create-vite): use quotes for attributes consistently
    As part of my changes in #12374, I unintentionally removed the quotes
    for the Vite logos from the vanilla templates, perhaps because the rest
    of the templates didn't need them.
    
    While HTML5 allows no quotes for attribute values if the value doesn't contain
    a space, keeping a consistent style is important for cognitive load when
    reading and maintaining. Hence, I've marked this as a style change.
    
    While not a functional change in these templates, it could be if someone
    later changed the import path to contain a space (perhaps after
    generating a project using the template).

--- packages/create-vite/template-vanilla-ts/src/main.ts
+++ packages/create-vite/template-vanilla-ts/src/main.ts
@@ -6,7 +6,7 @@ import { setupCounter } from './counter'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src=${viteLogo} class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
