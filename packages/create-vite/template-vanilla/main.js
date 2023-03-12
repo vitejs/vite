1f82c017ce95a12ab16aaadae835cd7827e7bd87
Alex Palmer <XanderXAJ@users.noreply.github.com>
2023-03-12 15:00:43 +0000

    style(create-vite): use quotes for attributes consistently (#12383)

--- packages/create-vite/template-vanilla/main.js
+++ packages/create-vite/template-vanilla/main.js
@@ -6,7 +6,7 @@ import { setupCounter } from './counter.js'
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src=${viteLogo} class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
