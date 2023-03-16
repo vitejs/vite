72a7132aa366365770fcd53cc834c3bb6e8c5178
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-16 18:24:45 +0100

    fix: add `virtual:` to virtual module source map ignore (#12444)

--- packages/vite/src/node/server/sourcemap.ts
+++ packages/vite/src/node/server/sourcemap.ts
@@ -12,7 +12,7 @@ const debug = createDebugger('vite:sourcemap', {
 // Virtual modules should be prefixed with a null byte to avoid a
 // false positive "missing source" warning. We also check for certain
 // prefixes used for special handling in esbuildDepPlugin.
-const virtualSourceRE = /^(?:\0|dep:|browser-external:)/
+const virtualSourceRE = /^(?:dep:|browser-external:|virtual:)|\0/
 
 interface SourceMapLike {
   sources: string[]
