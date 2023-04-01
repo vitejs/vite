a3150ee1b67c96f8bf88a20c2a7c4d2befed7b97
翠 / green <green@sapphi.red>
2023-04-02 08:08:36 +0900

    refactor: remove `ensureVolumeInPath` (#12690)

--- packages/vite/src/node/server/middlewares/transform.ts
+++ packages/vite/src/node/server/middlewares/transform.ts
@@ -7,7 +7,6 @@ import type { ViteDevServer } from '..'
 import {
   cleanUrl,
   createDebugger,
-  ensureVolumeInPath,
   fsPathFromId,
   injectQuery,
   isImportRequest,
@@ -79,9 +78,7 @@ export function transformMiddleware(
           // means that the dependency has already been pre-bundled and loaded
           const sourcemapPath = url.startsWith(FS_PREFIX)
             ? fsPathFromId(url)
-            : normalizePath(
-                ensureVolumeInPath(path.resolve(root, url.slice(1))),
-              )
+            : normalizePath(path.resolve(root, url.slice(1)))
           try {
             const map = JSON.parse(
               await fsp.readFile(sourcemapPath, 'utf-8'),
