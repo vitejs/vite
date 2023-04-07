ffad51c41bd932281c2d349c7f1c072718d4de3c
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-04-07 19:17:59 +0200

    fix: remove unneeded error throw

--- packages/create-vite/template-solid/src/index.jsx
+++ packages/create-vite/template-solid/src/index.jsx
@@ -6,10 +6,4 @@ import App from './App'
 
 const root = document.getElementById('root')
 
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?',
-  )
-}
-
 render(() => <App />, root)
