90923062f0e8f13ef0504e8e1d4c7a7e2343b9be
AbdelrahmanDwedar <dedou2005@gmail.com>
2023-04-07 15:44:33 +0200

    fix: fix all the left issues

--- packages/create-vite/template-solid-ts/README.md
+++ packages/create-vite/template-solid-ts/README.md
@@ -13,7 +13,7 @@ In the project directory, you can run:
 ### `npm run dev`
 
 Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Open [http://localhost:5173](http://localhost:5173) to view it in the browser.
 
 ### `npm run build`
 
--- packages/create-vite/template-solid/src/App.jsx
+++ packages/create-vite/template-solid/src/App.jsx
@@ -1,22 +1,23 @@
 import { useSignal } from 'solid-js'
 import solidLogo from './assets/solid.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useSignal(0)
 
   return (
-    <div className="App">
+    <div class="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
         <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} className="logo solid" alt="Solid logo" />
+          <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
       </div>
       <h1>Vite + Solid</h1>
-      <div className="card">
+      <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count()}
         </button>
@@ -24,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p class="read-the-docs">
         Click on the Vite and Solid logos to learn more
       </p>
     </div>
