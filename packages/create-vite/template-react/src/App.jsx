1607f4ac10715a60a32a1a6029a4fea9eaa33b14
Daniel de Andrade Lopes <danieldeandradelopes@gmail.com>
2023-04-13 01:09:53 -0300

    chore(create-vite): remove unnecessary App class Reference (#12771)

--- packages/create-vite/template-react/src/App.jsx
+++ packages/create-vite/template-react/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
