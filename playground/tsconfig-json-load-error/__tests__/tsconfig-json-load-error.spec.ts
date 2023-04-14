28090ea23bfa4146a23ad1bee5c5792e150fb3e5
patak <matias.capeletto@gmail.com>
2023-04-14 09:47:41 +0200

    test: relax tsconfig json load error regex

--- playground/tsconfig-json-load-error/__tests__/tsconfig-json-load-error.spec.ts
+++ playground/tsconfig-json-load-error/__tests__/tsconfig-json-load-error.spec.ts
@@ -14,7 +14,7 @@ describe.runIf(isBuild)('build', () => {
   test('should throw an error on build', () => {
     expect(serveError).toBeTruthy()
     expect(serveError.message).toMatch(
-      /^parsing .* failed: SyntaxError: Unexpected token \} in JSON at position \d+$/,
+      /^parsing .* failed: SyntaxError: Unexpected token/,
     )
     clearServeError() // got expected error, null it here so testsuite does not fail from rethrow in afterAll
   })
@@ -45,9 +45,7 @@ describe.runIf(isServe)('server', () => {
       return m[0].innerHTML
     })
     // use regex with variable filename and position values because they are different on win
-    expect(message).toMatch(
-      /^parsing .* failed: SyntaxError: Unexpected token \} in JSON at position \d+$/,
-    )
+    expect(message).toMatch(/^parsing .* failed: SyntaxError: Unexpected token/)
   })
 
   test('should reload when tsconfig is changed', async () => {
