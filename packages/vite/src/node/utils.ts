ca3cd56aebf032e23c357b5362301a054b0d5beb
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-04-17 10:08:30 +0200

    Fix lint warnings

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -251,8 +251,8 @@ export function isParentDirectory(dir: string, file: string): boolean {
  */
 export function isSameFileUri(file1: string, file2: string): boolean {
   return (
-    file1 == file2 ||
-    (isCaseInsensitiveFS && file1.toLowerCase() ==  file2.toLowerCase())
+    file1 === file2 ||
+    (isCaseInsensitiveFS && file1.toLowerCase() === file2.toLowerCase())
   )
 }
 
