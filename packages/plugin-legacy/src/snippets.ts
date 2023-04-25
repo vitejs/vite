9b3dc6aab4775edb372b958e62330ecfb147b6ec
钢翼 <659240788@qq.com>
2023-04-25 20:51:37 +0800

    feat(plugin-legacy): support file protocol (#8524)
    Co-authored-by: sapphi-red <green@sapphi.red>
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- packages/plugin-legacy/src/snippets.ts
+++ packages/plugin-legacy/src/snippets.ts
@@ -9,7 +9,7 @@ export const systemJSInlineCode = `System.import(document.getElementById('${lega
 const detectModernBrowserVarName = '__vite_is_modern_browser'
 export const detectModernBrowserDetector =
   'import.meta.url;import("_").catch(()=>1);async function* g(){};'
-export const detectModernBrowserCode = `${detectModernBrowserDetector}window.${detectModernBrowserVarName}=true;`
+export const detectModernBrowserCode = `${detectModernBrowserDetector}if(location.protocol!="file:"){window.${detectModernBrowserVarName}=true}`
 export const dynamicFallbackInlineCode = `!function(){if(window.${detectModernBrowserVarName})return;console.warn("vite: loading legacy chunks, syntax error above and the same error below should be ignored");var e=document.getElementById("${legacyPolyfillId}"),n=document.createElement("script");n.src=e.src,n.onload=function(){${systemJSInlineCode}},document.body.appendChild(n)}();`
 
 export const modernChunkLegacyGuard = `export function __vite_legacy_guard(){${detectModernBrowserDetector}};`
