765058c6b54270a3dc8d339ab5efa9376ac694d9
btea <2356281422@qq.com>
2023-04-26 21:53:05 +0800

    fix(resolve): deep import resolvedId error (#13010)

--- packages/vite/src/node/plugins/resolve.ts
+++ packages/vite/src/node/plugins/resolve.ts
@@ -773,8 +773,15 @@ export function tryNodeResolve(
     }
     let resolvedId = id
     if (deepMatch && !pkg?.data.exports && path.extname(id) !== resolvedExt) {
-      resolvedId = resolved.id.slice(resolved.id.indexOf(id))
-      debug?.(`[processResult] ${colors.cyan(id)} -> ${colors.dim(resolvedId)}`)
+      // id date-fns/locale
+      // resolve.id ...date-fns/esm/locale/index.js
+      const index = resolved.id.indexOf(id)
+      if (index > -1) {
+        resolvedId = resolved.id.slice(index)
+        debug?.(
+          `[processResult] ${colors.cyan(id)} -> ${colors.dim(resolvedId)}`,
+        )
+      }
     }
     return { ...resolved, id: resolvedId, external: true }
   }
