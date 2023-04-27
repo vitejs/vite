9e4603f5003b3416b67d5f1e8a7a9d78419c8bc0
sun0day <sun_day500@163.com>
2023-04-27 20:35:43 +0800

    test(resolve): ssr deep import resolve (#13022)

--- playground/ssr-resolve/main.js
+++ playground/ssr-resolve/main.js
@@ -4,9 +4,13 @@ import dirEntry from '@vitejs/test-entries/dir'
 import fileEntry from '@vitejs/test-entries/file'
 // has `exports` key, should resolve to pkg-exports/entry
 import pkgExportsEntry from '@vitejs/test-resolve-pkg-exports/entry'
+import deepFoo from '@vitejs/test-deep-import/foo'
+import deepBar from '@vitejs/test-deep-import/bar'
 
 export default `
   entries/dir: ${dirEntry}
   entries/file: ${fileEntry}
   pkg-exports/entry: ${pkgExportsEntry}
+  deep-import/foo: ${deepFoo}
+  deep-import/bar: ${deepBar}
 `
