88d64af5567e6d90b1b3941db93b11ce491617e2
linl33 <linlee1997@gmail.com>
2023-05-05 12:54:48 +0800

    test: add tests for modulepreload

---
+++ packages/vite/src/node/__tests__/plugins/modulePreloadPolyfill/modulePreloadPolyfill.spec.ts
@@ -0,0 +1,58 @@
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { describe, it } from 'vitest'
+import type { ModuleFormat, RollupOutput } from 'rollup'
+import { build } from '../../../build'
+import { modulePreloadPolyfillId } from '../../../plugins/modulePreloadPolyfill'
+
+const __dirname = resolve(fileURLToPath(import.meta.url), '..')
+
+const buildProject = ({ format = 'es' as ModuleFormat } = {}) =>
+  build({
+    root: resolve(__dirname, 'packages/build-project'),
+    logLevel: 'silent',
+    build: {
+      write: false,
+      rollupOptions: {
+        input: 'main.js',
+        output: {
+          format,
+        },
+        treeshake: {
+          moduleSideEffects: false,
+        },
+      },
+      minify: false,
+    },
+    plugins: [
+      {
+        name: 'test',
+        resolveId(id) {
+          if (id === 'main.js') {
+            return `\0${id}`
+          }
+        },
+        load(id) {
+          if (id === '\0main.js') {
+            return `import '${modulePreloadPolyfillId}'`
+          }
+        },
+      },
+    ],
+  }) as Promise<RollupOutput>
+
+describe('load', () => {
+  it('loads modulepreload polyfill', async ({ expect }) => {
+    const { output } = await buildProject()
+    expect(output).toHaveLength(1)
+    expect(output[0].code).toMatchSnapshot()
+  })
+
+  it("doesn't load modulepreload polyfill when format is cjs", async ({
+    expect,
+  }) => {
+    const { output } = await buildProject({ format: 'cjs' })
+    expect(output).toHaveLength(1)
+    expect(output[0].code).toMatchSnapshot()
+  })
+})
