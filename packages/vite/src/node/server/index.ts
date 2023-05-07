5d57541ca4d46106035e75ae136d53926a3d17a1
gtmnayan <gtmnayan@gmail.com>
2023-05-07 08:07:02 +0545

    fix: handle ping requests before they reach user middleware

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -608,6 +608,15 @@ export async function _createServer(
   // open in editor support
   middlewares.use('/__open-in-editor', launchEditorMiddleware())
 
+  // ping request handler
+  middlewares.use((req, res, next) => {
+    if (req.headers['accept'] === 'text/x-vite-ping') {
+      res.writeHead(204).end()
+    } else {
+      next()
+    }
+  })
+
   // serve static files under /public
   // this applies before the transform middleware so that these files are served
   // as-is without transforms.
