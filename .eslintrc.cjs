8d47b4c168e5f7a5e5bc0df4853d67254f8ecd09
fi3ework <fi3ework@gmail.com>
2023-05-09 01:48:18 +0800

    revert playgournd/tailwind to CJS

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -190,6 +190,7 @@ module.exports = defineConfig({
         'playground/**/*dep*/**',
         'playground/resolve/browser-module-field2/index.web.js',
         'playground/resolve/browser-field/**',
+        'playground/tailwind/**', // blocked by https://github.com/postcss/postcss-load-config/issues/239
       ],
       rules: {
         'import/no-commonjs': 'error',
--- playground/tailwind/tailwind.config.js
+++ playground/tailwind/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-export default {
+module.exports = {
   content: [
     // Before editing this section, make sure no paths are matching with `/src/App.vue`
     // Look https://github.com/vitejs/vite/pull/6959 for more details
