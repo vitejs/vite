a36c88f971583d06941e45d31316bbfece1c876d
Gumball12 <to@shj.rip>
2023-05-21 22:44:30 +0900

    docs: link to ko translation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -92,6 +92,7 @@ export default defineConfig({
     ja: { label: '日本語', link: 'https://ja.vitejs.dev' },
     es: { label: 'Español', link: 'https://es.vitejs.dev' },
     pt: { label: 'Português', link: 'https://pt.vitejs.dev' },
+    ko: { label: '한국어', link: 'https://ko.vitejs.dev' },
   },
 
   themeConfig: {
