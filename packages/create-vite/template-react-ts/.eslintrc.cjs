595f3236d334776dbbfc2dc57e3c817599802b07
Rick Schultz <rick-schultz@hotmail.com>
2023-06-17 13:00:09 -0300

    fix: fixed module is not defined.eslint error

--- packages/create-vite/template-react-ts/.eslintrc.cjs
+++ packages/create-vite/template-react-ts/.eslintrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
