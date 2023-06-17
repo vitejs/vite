0f1a11ffa603eaeca1bfd791559652ccdda0d943
Rick Schultz <rick-schultz@hotmail.com>
2023-06-17 13:31:38 -0300

    fix: fixed import React and ReactDOM error and removed tsx from App so we don't get an error

--- packages/create-vite/template-react-ts/src/main.tsx
+++ packages/create-vite/template-react-ts/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
