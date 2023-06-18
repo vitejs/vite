ffb02f4bf6e8627d0b909c27818e993d6e906dd0
Arnaud Barré <arnaud.barre@carbometrix.com>
2023-06-18 21:22:33 +0200

    feat: update eslint config in React templates

--- packages/create-vite/template-react-ts/.eslintrc.cjs
+++ packages/create-vite/template-react-ts/.eslintrc.cjs
@@ -1,14 +1,27 @@
 module.exports = {
+  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unnecessary-condition': 'warn',
+    '@typescript-eslint/non-nullable-type-assertion-style': 'warn',
   },
 }
--- packages/create-vite/template-react/.eslintrc.cjs
+++ packages/create-vite/template-react/.eslintrc.cjs
@@ -10,6 +10,9 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
 }
