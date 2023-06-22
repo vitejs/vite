f13ad43d273f5ac87bc49db4d6f6b0e594558a3c
ivan <sun_day500@163.com>
2023-06-22 23:10:27 +0800

    chore: __vite__define__${key}__define__vite__

--- packages/vite/src/node/plugins/define.ts
+++ packages/vite/src/node/plugins/define.ts
@@ -41,7 +41,7 @@ export function definePlugin(config: ResolvedConfig): Plugin {
     if (isBuild) {
       const match = key.match(metaEnvRe)
       if (match) {
-        userDefineEnv[match[1]] = `__vite__define__${key}__vite__define__`
+        userDefineEnv[match[1]] = `__vite__define__${key}__define__vite__`
       }
     }
   }
@@ -62,7 +62,7 @@ export function definePlugin(config: ResolvedConfig): Plugin {
         SSR: '__vite__ssr__',
         ...userDefineEnv,
       }).replace(
-        /"__vite__define__(.+?)__vite__define__"/g,
+        /"__vite__define__(.+?)__define__vite__"/g,
         (_, key) => userDefine[key],
       ),
     })
