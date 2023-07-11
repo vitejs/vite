cc5d124d9d8e7b6cfb278cc97da7ff3a0d080058
이현재 <71202076+2-NOW@users.noreply.github.com>
2023-07-11 23:12:22 +0900

    Update .eslintrc.cjs

--- packages/create-vite/template-react-ts/.eslintrc.cjs
+++ packages/create-vite/template-react-ts/.eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
