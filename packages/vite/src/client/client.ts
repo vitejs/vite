8992e99a87d1453033e59be9f4c8efb404d275ff
cunzaizhuyi <877824709@qq.com>
2023-07-15 10:10:27 +0800

    chore: link protocol

--- packages/vite/src/client/client.ts
+++ packages/vite/src/client/client.ts
@@ -626,7 +626,7 @@ export function injectQuery(url: string, queryToInject: string): string {
 
   // can't use pathname from URL since it may be relative like ../
   const pathname = url.replace(/#.*$/, '').replace(/\?.*$/, '')
-  const { search, hash } = new URL(url, 'http://vitejs.dev')
+  const { search, hash } = new URL(url, 'https://vitejs.dev')
 
   return `${pathname}?${queryToInject}${search ? `&` + search.slice(1) : ''}${
     hash || ''
