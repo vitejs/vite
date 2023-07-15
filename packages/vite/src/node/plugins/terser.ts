0f152dec83a2e0d7ecd883110e9cbf2cc62304fb
Sang Quang Nguyen <sang.quang.nguyen@employmenthero.com>
2023-07-15 11:46:09 +0700

    using worker

--- packages/vite/src/node/plugins/terser.ts
+++ packages/vite/src/node/plugins/terser.ts
@@ -36,6 +36,9 @@ export function terserPlugin(config: ResolvedConfig): Plugin {
         const terser = require(terserPath)
         return terser.minify(code, options) as Terser.MinifyOutput
       },
+      {
+        max: config?.build?.terserOptions?.workers,
+      }
     )
 
   let worker: ReturnType<typeof makeWorker>
