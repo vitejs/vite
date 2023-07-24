dc658f04756f5e194c1a92114ed33cf0f45a23ad
翠 / green <green@sapphi.red>
2023-07-24 21:10:38 +0900

    fix(html): support `import.meta.env` define replacement without quotes (#13425)

--- playground/html/vite.config.js
+++ playground/html/vite.config.js
@@ -36,6 +36,10 @@ export default defineConfig({
 
   define: {
     'import.meta.env.VITE_NUMBER': 5173,
+    'import.meta.env.VITE_STRING': JSON.stringify('string'),
+    'import.meta.env.VITE_OBJECT_STRING': '{ "foo": "bar" }',
+    'import.meta.env.VITE_TEMPLATE_LITERAL': '`template literal`',
+    'import.meta.env.VITE_NULL_STRING': 'null',
   },
 
   plugins: [
