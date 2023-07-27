24c12fef604438826d76f49c244ae8e76574b929
Vincent François <vincent.francois@inato.com>
2023-07-27 08:48:28 +0200

    refactor: add new overload to the type of defineConfig (#13958)

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -119,6 +119,7 @@ export type UserConfigExport =
  */
 export function defineConfig(config: UserConfig): UserConfig
 export function defineConfig(config: Promise<UserConfig>): Promise<UserConfig>
+export function defineConfig(config: UserConfigFnObject): UserConfigFnObject
 export function defineConfig(config: UserConfigExport): UserConfigExport
 export function defineConfig(config: UserConfigExport): UserConfigExport {
   return config
