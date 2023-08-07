0e9be79de816c139116aa47500c87193ecc9395e
Bjorn Lu <bjornlu.dev@gmail.com>
2023-08-07 17:03:39 +0800

    chore: fix eslint warnings (#14031)

--- packages/vite/types/importMeta.d.ts
+++ packages/vite/types/importMeta.d.ts
@@ -2,8 +2,6 @@
 // Thus cannot contain any top-level imports
 // <https://www.typescriptlang.org/docs/handbook/declaration-merging.html#module-augmentation>
 
-/* eslint-disable @typescript-eslint/consistent-type-imports */
-
 interface ImportMetaEnv {
   [key: string]: any
   BASE_URL: string
