acf32f5e64e6b27cd797f1de2469e29e73f8f602
sapphi-red <green@sapphi.red>
2023-08-11 02:37:00 +0900

    test: remove plugin-vue from tailwind playground

--- playground/tailwind/src/main.js
+++ playground/tailwind/src/main.js
@@ -1,6 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-// import '../index.css';
+import { view1 } from './views/view1'
 
-createApp(App).use(router).mount('#app')
+export const main = (view1Content) => /* html */ `
+  <h1 id="pagetitle" class="text-3xl text-violet-600">Page title</h1>
+  ${view1Content}
+`
+
+document.getElementById('app').innerHTML = main(view1)
+
+import.meta.hot?.accept((mod) => {
+  document.getElementById('app').innerHTML = mod.main(view1)
+})
+
+import.meta.hot?.accept(['./views/view1'], ([mod]) => {
+  document.getElementById('app').innerHTML = main(mod.view1)
+})
