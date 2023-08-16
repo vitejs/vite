552c1dae6fe9f8a6973b14a9379744c63155dfdf
Jamie Sinclair <jamsinclairnz@gmail.com>
2023-08-17 02:36:00 +0900

    test: switch to asserting the values are different

--- playground/worker/__tests__/serial/serial-worker-build.spec.ts
+++ playground/worker/__tests__/serial/serial-worker-build.spec.ts
@@ -1,18 +1,26 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { isBuild, page, untilUpdated } from '~utils'
 
 describe.runIf(isBuild)('build', () => {
   test('expect the plugin state to be polluted and not match across worker builds', async () => {
     await untilUpdated(
       () => page.textContent('.nested-worker-plugin-state'),
-      '"data":2',
+      '"type":"plugin-state"',
       true,
     )
-    // The plugin state is polluted and should have a different data value from the sub worker
     await untilUpdated(
       () => page.textContent('.sub-worker-plugin-state'),
-      '"data":1',
+      '"type":"plugin-state-sub"',
       true,
     )
+
+    const nestedWorkerPluginState = JSON.parse(
+      await page.textContent('.nested-worker-plugin-state'),
+    )
+    const subWorkerPluginState = JSON.parse(
+      await page.textContent('.sub-worker-plugin-state'),
+    )
+    // The plugin state is polluted and should have a different data value from the sub worker
+    expect(nestedWorkerPluginState.data).not.toEqual(subWorkerPluginState.data)
   })
 })
