9449d65691aa686d4796dc9b5cd992e9786c864a
Jamie Sinclair <jamsinclairnz@gmail.com>
2023-08-17 02:23:07 +0900

    test: flip expected state value order

--- playground/worker/__tests__/serial/serial-worker-build.spec.ts
+++ playground/worker/__tests__/serial/serial-worker-build.spec.ts
@@ -5,13 +5,13 @@ describe.runIf(isBuild)('build', () => {
   test('expect the plugin state to be polluted and not match across worker builds', async () => {
     await untilUpdated(
       () => page.textContent('.nested-worker-plugin-state'),
-      '"data":1',
+      '"data":2',
       true,
     )
     // The plugin state is polluted and should have a different data value from the sub worker
     await untilUpdated(
       () => page.textContent('.sub-worker-plugin-state'),
-      '"data":2',
+      '"data":1',
       true,
     )
   })
