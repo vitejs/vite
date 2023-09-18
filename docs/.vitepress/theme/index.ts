a7c9f08c8b89f5227ebabeebddcbf43aa9da3b41
翠 / green <green@sapphi.red>
2023-09-18 16:04:19 +0900

    docs: use extends instead of spreading default theme (#14403)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,14 +1,15 @@
 import { h } from 'vue'
-import Theme from 'vitepress/theme'
+import type { Theme } from 'vitepress'
+import DefaultTheme from 'vitepress/theme'
 import './styles/vars.css'
 import HomeSponsors from './components/HomeSponsors.vue'
 import AsideSponsors from './components/AsideSponsors.vue'
 import SvgImage from './components/SvgImage.vue'
 
 export default {
-  ...Theme,
+  extends: DefaultTheme,
   Layout() {
-    return h(Theme.Layout, null, {
+    return h(DefaultTheme.Layout, null, {
       'home-features-after': () => h(HomeSponsors),
       'aside-ads-before': () => h(AsideSponsors),
     })
@@ -16,4 +17,4 @@ export default {
   enhanceApp({ app }) {
     app.component('SvgImage', SvgImage)
   },
-}
+} satisfies Theme
