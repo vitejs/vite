2d88e8546cd63f979aa73dad5c460a058a50b457
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-09-25 18:05:38 +0900

    fix(preview): allow path containing . to fallback to index.html

--- playground/assets/__tests__/assets.spec.ts
+++ playground/assets/__tests__/assets.spec.ts
@@ -41,15 +41,19 @@ test('should get a 404 when using incorrect case', async () => {
     'image/png',
   )
   // fallback to index.html
-  expect((await fetchPath('ICON.png')).headers.get('Content-Type')).toBe(
-    isBuild ? 'text/html; charset=utf-8' : 'text/html',
+  const iconPngResult = await fetchPath('ICON.png')
+  expect(iconPngResult.headers.get('Content-Type')).toBe(
+    isBuild ? 'text/html;charset=utf-8' : 'text/html',
   )
+  expect(iconPngResult.status).toBe(200)
 
   expect((await fetchPath('bar')).headers.get('Content-Type')).toBe('')
   // fallback to index.html
-  expect((await fetchPath('BAR')).headers.get('Content-Type')).toContain(
+  const barResult = await fetchPath('BAR')
+  expect(barResult.headers.get('Content-Type')).toContain(
     isBuild ? 'text/html;charset=utf-8' : 'text/html',
   )
+  expect(barResult.status).toBe(200)
 })
 
 test('should fallback to index.html when accessing non-existant html file', async () => {
--- playground/legacy/__tests__/legacy.spec.ts
+++ playground/legacy/__tests__/legacy.spec.ts
@@ -52,13 +52,13 @@ test('generates assets', async () => {
     () => page.textContent('#assets'),
     isBuild
       ? [
-          'index: text/html; charset=utf-8',
-          'index-legacy: text/html; charset=utf-8',
-          'chunk-async: text/html; charset=utf-8',
-          'chunk-async-legacy: text/html; charset=utf-8',
+          'index: text/html;charset=utf-8',
+          'index-legacy: text/html;charset=utf-8',
+          'chunk-async: text/html;charset=utf-8',
+          'chunk-async-legacy: text/html;charset=utf-8',
           'immutable-chunk: application/javascript',
           'immutable-chunk-legacy: application/javascript',
-          'polyfills-legacy: text/html; charset=utf-8',
+          'polyfills-legacy: text/html;charset=utf-8',
         ].join('\n')
       : [
           'index: text/html',
