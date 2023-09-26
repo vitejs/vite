f15449296f03a91cf8095d372a45d726f8553937
Kyℓe Hensel <k-yle@users.noreply.github.com>
2023-09-26 20:43:08 +1300

    fix(legacy): fix broken build when renderModernChunks=false & polyfills=false (fix #14324) (#14346)

---
+++ playground/legacy/vite.config-no-polyfills-no-systemjs.js
@@ -0,0 +1,30 @@
+import path from 'node:path'
+import legacy from '@vitejs/plugin-legacy'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',
+  plugins: [
+    legacy({
+      renderModernChunks: false,
+      polyfills: false,
+      externalSystemJS: true,
+    }),
+    {
+      name: 'remove crossorigin attribute',
+      transformIndexHtml: (html) => html.replaceAll('crossorigin', ''),
+      enforce: 'post',
+    },
+  ],
+
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, 'no-polyfills-no-systemjs.html'),
+      },
+    },
+  },
+  testConfig: {
+    baseRoute: '/no-polyfills-no-systemjs/',
+  },
+})
