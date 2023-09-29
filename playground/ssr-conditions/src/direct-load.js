399d824754653fd2aaa7c2ee26e4d9802d51c985
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2023-09-29 14:54:12 -0500

    add some tests
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

---
+++ playground/ssr-conditions/src/direct-load.js
@@ -0,0 +1,4 @@
+import noExternalReactServerMessage from '@vitejs/test-ssr-conditions-no-external/server'
+import externalReactServerMessage from '@vitejs/test-ssr-conditions-external/server'
+
+export { noExternalReactServerMessage, externalReactServerMessage }
