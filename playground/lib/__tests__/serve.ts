fba0b9b3f5dbf71092002300ad5229677ea18c2f
翠 / green <green@sapphi.red>
2023-10-09 17:10:39 +0900

    fix(lib): esbuild helper functions injection not working with named exports (#14539)

--- playground/lib/__tests__/serve.ts
+++ playground/lib/__tests__/serve.ts
@@ -82,6 +82,12 @@ export async function serve(): Promise<{ close(): Promise<void> }> {
       ),
     })
 
+    await build({
+      root: rootDir,
+      logLevel: 'warn', // output esbuild warns
+      configFile: path.resolve(__dirname, '../vite.named-exports.config.js'),
+    })
+
     // start static file server
     const serve = sirv(path.resolve(rootDir, 'dist'))
     const httpServer = http.createServer((req, res) => {
