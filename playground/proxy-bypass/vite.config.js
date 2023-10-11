261633a1a1d22706728701b42b4b113662122802
Dunqing <dengqing0821@gmail.com>
2023-10-11 02:07:24 -0500

    fix(proxy): correct the logic of bypass returning false (#14579)

---
+++ playground/proxy-bypass/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: 9606,
+    proxy: {
+      '/anotherApp': {
+        target: 'http://localhost:9607',
+        bypass: () => {
+          return false
+        },
+      },
+    },
+  },
+})
