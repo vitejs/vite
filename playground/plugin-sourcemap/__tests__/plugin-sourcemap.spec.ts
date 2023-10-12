3bbb423fe222c90565ae76ad765c1e8640c96acc
Dunqing <dengqing0821@gmail.com>
2023-10-12 16:28:40 +0800

    fix: test

--- playground/plugin-sourcemap/__tests__/plugin-sourcemap.spec.ts
+++ playground/plugin-sourcemap/__tests__/plugin-sourcemap.spec.ts
@@ -7,24 +7,22 @@ import {
   page,
 } from '~utils'
 
-if (!isBuild) {
-  test('correct sourcemap', async () => {
-    const res = await page.request.get(new URL('./foo.js', page.url()).href)
-    const js = await res.text()
-    expect(js).toContain('// add comment')
-    const map = extractSourcemap(js)
-    expect(formatSourcemapForSnapshot(map)).toMatchInlineSnapshot(`
-      {
-        "mappings": "AAAA,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC;",
-        "sources": [
-          "foo.js",
-        ],
-        "sourcesContent": [
-          "export const foo = 'foo'
-      ",
-        ],
-        "version": 3,
-      }
-    `)
-  })
-}
+test.runIf(!isBuild)('correct sourcemap', async () => {
+  const res = await page.request.get(new URL('./foo.js', page.url()).href)
+  const js = await res.text()
+  expect(js).toContain('// add comment')
+  const map = extractSourcemap(js)
+  expect(formatSourcemapForSnapshot(map)).toMatchInlineSnapshot(`
+    {
+      "mappings": "AAAA,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC,CAAC;",
+      "sources": [
+        "foo.js",
+      ],
+      "sourcesContent": [
+        "export const foo = 'foo'
+    ",
+      ],
+      "version": 3,
+    }
+  `)
+})
