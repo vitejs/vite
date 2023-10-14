6b373114bff4cec8ab6e9b97a7c328f55c0a106f
Dunqing <dengqing0821@gmail.com>
2023-10-14 05:53:21 -0500

    test: update test for inline sourcemap (#14597)

--- playground/js-sourcemap/foo-with-sourcemap.js
+++ playground/js-sourcemap/foo-with-sourcemap.js
@@ -1,5 +1,1 @@
 export const foo = 'foo'
-
-// default boundary sourcemap with magic-string
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxNQUFNLENBQUMsS0FBSyxDQUFDLEdBQUcsQ0FBQyxDQUFDLENBQUMsQ0FBQyxHQUFHIn0=
