275907b6a3a226198e8243e43b21e82dc9532dd7
Bjorn Lu <bjornlu.dev@gmail.com>
2023-11-02 00:14:33 +0800

    refactor: simplify build optimizer node_env handling (#14829)

--- packages/vite/src/node/optimizer/index.ts
+++ packages/vite/src/node/optimizer/index.ts
@@ -755,13 +755,12 @@ async function prepareEsbuildOptimizerRun(
   if (optimizerContext.cancelled) return { context: undefined, idToExports }
 
   // esbuild automatically replaces process.env.NODE_ENV for platform 'browser'
-  // In lib mode, we need to keep process.env.NODE_ENV untouched, so to at build
-  // time we replace it by __vite_process_env_NODE_ENV. This placeholder will be
-  // later replaced by the define plugin
+  // But in lib mode, we need to keep process.env.NODE_ENV untouched
   const define = {
-    'process.env.NODE_ENV': isBuild
-      ? '__vite_process_env_NODE_ENV'
-      : JSON.stringify(process.env.NODE_ENV || config.mode),
+    'process.env.NODE_ENV':
+      isBuild && config.build.lib
+        ? 'process.env.NODE_ENV'
+        : JSON.stringify(process.env.NODE_ENV || config.mode),
   }
 
   const platform =
