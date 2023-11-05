8f5f17bf48c5ce13237885dbc1724a0f71c493e3
bluwy <bjornlu.dev@gmail.com>
2023-11-05 16:57:34 +0800

    chore: refactor as functions

--- packages/vite/rollup.dts.config.ts
+++ packages/vite/rollup.dts.config.ts
@@ -1,7 +1,8 @@
 import { readFileSync } from 'node:fs'
 import { fileURLToPath } from 'node:url'
 import { findStaticImports } from 'mlly'
-import { type Plugin, defineConfig } from 'rollup'
+import { defineConfig } from 'rollup'
+import type { Plugin, PluginContext, RenderedChunk } from 'rollup'
 import dts from 'rollup-plugin-dts'
 
 const depTypesDir = new URL('./src/types/', import.meta.url)
@@ -80,89 +81,106 @@ function patchTypes(): Plugin {
       }
     },
     renderChunk(code, chunk) {
-      // Validate that chunk imports do not import dev deps
-      const deps = Object.keys(pkg.dependencies)
-      for (const id of chunk.imports) {
-        if (
-          !id.startsWith('./') &&
-          !id.startsWith('../') &&
-          !id.startsWith('node:') &&
-          !deps.includes(id) &&
-          !deps.some((name) => id.startsWith(name + '/'))
-        ) {
-          // If validation failed, only warn and set exit code 1 so that files
-          // are written to disk for inspection, but the build will fail
-          this.warn(`${chunk.fileName} imports "${id}" which is not allowed`)
-          process.exitCode = 1
-        }
-      }
-
-      // Rollup deduplicate type names with a trailing `$1` or `$2`, which can be
-      // confusing when showed in autocompletions. Try to replace with a better name
-      const imports = findStaticImports(code)
-      for (const modName in identifierReplacements) {
-        const imp = imports.find(
-          (imp) => imp.specifier === modName && imp.imports.includes('{'),
-        )
-        // Validate that `identifierReplacements` is not outdated if there's no match
-        if (!imp) {
-          this.warn(
-            `${chunk.fileName} does not import "${modName}" for replacement`,
-          )
-          process.exitCode = 1
-          continue
-        }
-
-        const replacements = identifierReplacements[modName]
-        for (const id in replacements) {
-          // Validate that `identifierReplacements` is not outdated if there's no match
-          if (!imp.imports.includes(id)) {
-            this.warn(
-              `${chunk.fileName} does not import "${id}" from "${modName}" for replacement`,
-            )
-            process.exitCode = 1
-            continue
-          }
-
-          const betterId = replacements[id]
-          const regexEscapedId = escapeRegex(id)
-          // If the better id accesses a namespace, the existing `Foo as Foo$1`
-          // named import cannot be replaced with `Foo as Namespace.Foo`, so we
-          // pre-emptively remove the whole named import
-          if (betterId.includes('.')) {
-            code = code.replace(
-              new RegExp(`\\b\\w+\\b as ${regexEscapedId},?\\s?`),
-              '',
-            )
-          }
-          code = code.replace(
-            new RegExp(`\\b${regexEscapedId}\\b`, 'g'),
-            betterId,
-          )
-        }
-      }
-      const unreplacedIds = unique(
-        Array.from(code.matchAll(identifierWithTrailingDollarRE), (m) => m[0]),
-      )
-      if (unreplacedIds.length) {
-        const unreplacedStr = unreplacedIds.map((id) => `\n- ${id}`).join('')
-        this.warn(
-          `${chunk.fileName} contains confusing identifier names${unreplacedStr}`,
-        )
-        process.exitCode = 1
-      }
-
-      // Clean unnecessary comments
-      code = code
-        .replace(singlelineCommentsRE, '')
-        .replace(multilineCommentsRE, (m) => {
-          return licenseCommentsRE.test(m) ? '' : m
-        })
-        .replace(consecutiveNewlinesRE, '\n\n')
-
+      validateChunkImports.call(this, chunk)
+      code = replaceConfusingTypeNames.call(this, code, chunk)
+      code = cleanUnnecessaryComments(code)
       return code
     },
   }
+}
+
+/**
+ * Validate that chunk imports do not import dev deps
+ */
+function validateChunkImports(this: PluginContext, chunk: RenderedChunk) {
+  const deps = Object.keys(pkg.dependencies)
+  for (const id of chunk.imports) {
+    if (
+      !id.startsWith('./') &&
+      !id.startsWith('../') &&
+      !id.startsWith('node:') &&
+      !deps.includes(id) &&
+      !deps.some((name) => id.startsWith(name + '/'))
+    ) {
+      // If validation failed, only warn and set exit code 1 so that files
+      // are written to disk for inspection, but the build will fail
+      this.warn(`${chunk.fileName} imports "${id}" which is not allowed`)
+      process.exitCode = 1
+    }
+  }
+}
+
+/**
+ * Rollup deduplicate type names with a trailing `$1` or `$2`, which can be
+ * confusing when showed in autocompletions. Try to replace with a better name
+ */
+function replaceConfusingTypeNames(
+  this: PluginContext,
+  code: string,
+  chunk: RenderedChunk,
+) {
+  const imports = findStaticImports(code)
+
+  for (const modName in identifierReplacements) {
+    const imp = imports.find(
+      (imp) => imp.specifier === modName && imp.imports.includes('{'),
+    )
+    // Validate that `identifierReplacements` is not outdated if there's no match
+    if (!imp) {
+      this.warn(
+        `${chunk.fileName} does not import "${modName}" for replacement`,
+      )
+      process.exitCode = 1
+      continue
+    }
+
+    const replacements = identifierReplacements[modName]
+    for (const id in replacements) {
+      // Validate that `identifierReplacements` is not outdated if there's no match
+      if (!imp.imports.includes(id)) {
+        this.warn(
+          `${chunk.fileName} does not import "${id}" from "${modName}" for replacement`,
+        )
+        process.exitCode = 1
+        continue
+      }
+
+      const betterId = replacements[id]
+      const regexEscapedId = escapeRegex(id)
+      // If the better id accesses a namespace, the existing `Foo as Foo$1`
+      // named import cannot be replaced with `Foo as Namespace.Foo`, so we
+      // pre-emptively remove the whole named import
+      if (betterId.includes('.')) {
+        code = code.replace(
+          new RegExp(`\\b\\w+\\b as ${regexEscapedId},?\\s?`),
+          '',
+        )
+      }
+      code = code.replace(new RegExp(`\\b${regexEscapedId}\\b`, 'g'), betterId)
+    }
+  }
+
+  const unreplacedIds = unique(
+    Array.from(code.matchAll(identifierWithTrailingDollarRE), (m) => m[0]),
+  )
+  if (unreplacedIds.length) {
+    const unreplacedStr = unreplacedIds.map((id) => `\n- ${id}`).join('')
+    this.warn(
+      `${chunk.fileName} contains confusing identifier names${unreplacedStr}`,
+    )
+    process.exitCode = 1
+  }
+
+  return code
+}
+
+function cleanUnnecessaryComments(code: string) {
+  return code
+    .replace(singlelineCommentsRE, '')
+    .replace(multilineCommentsRE, (m) => {
+      return licenseCommentsRE.test(m) ? '' : m
+    })
+    .replace(consecutiveNewlinesRE, '\n\n')
 }
 
 const escapeRegexRE = /[-/\\^$*+?.()|[\]{}]/g
