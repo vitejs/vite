37e1a062d1ae5552aafa8104bfd62b121acedfc8
Shaoyu Meng <shaoyu>
2023-11-11 15:32:37 -0600

    fix lint issue

--- packages/create-vite/template-react-ts/eslint.config.js
+++ packages/create-vite/template-react-ts/eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
 export default [
   {
@@ -29,4 +29,4 @@ export default [
       ],
     },
   },
-];
+]
