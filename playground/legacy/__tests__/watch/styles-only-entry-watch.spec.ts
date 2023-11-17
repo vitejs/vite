5087c7ff8d1709c2aef6f6e97582fd76d0262c2d
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-11-18 00:03:31 +0900

    chore: set build.outDir

--- playground/legacy/__tests__/watch/styles-only-entry-watch.spec.ts
+++ playground/legacy/__tests__/watch/styles-only-entry-watch.spec.ts
@@ -9,10 +9,14 @@ import {
 } from '~utils'
 
 test.runIf(isBuild)('rebuilds styles only entry on change', async () => {
-  expect(findAssetFile(/style-only-entry-.+\.css/)).toContain('hotpink')
-  expect(findAssetFile(/style-only-entry-legacy-.+\.js/)).toContain('hotpink')
-  expect(findAssetFile(/polyfills-legacy-.+\.js/)).toBeTruthy()
-  const numberOfManifestEntries = Object.keys(readManifest()).length
+  expect(findAssetFile(/style-only-entry-.+\.css/, 'watch')).toContain(
+    'hotpink',
+  )
+  expect(findAssetFile(/style-only-entry-legacy-.+\.js/, 'watch')).toContain(
+    'hotpink',
+  )
+  expect(findAssetFile(/polyfills-legacy-.+\.js/, 'watch')).toBeTruthy()
+  const numberOfManifestEntries = Object.keys(readManifest('watch')).length
   expect(numberOfManifestEntries).toBe(3)
 
   editFile(
@@ -22,20 +26,22 @@ test.runIf(isBuild)('rebuilds styles only entry on change', async () => {
   )
   await notifyRebuildComplete(watcher)
 
-  const updatedManifest = readManifest()
+  const updatedManifest = readManifest('watch')
   expect(Object.keys(updatedManifest)).toHaveLength(numberOfManifestEntries)
 
   // We must use the file referenced in the manifest here,
   // since there'll be different versions of the file with different hashes.
   const reRenderedCssFile = findAssetFile(
     updatedManifest['style-only-entry.css']!.file.substring('assets/'.length),
+    'watch',
   )
   expect(reRenderedCssFile).toContain('lightpink')
   const reRenderedCssLegacyFile = findAssetFile(
     updatedManifest['style-only-entry-legacy.css']!.file.substring(
       'assets/'.length,
     ),
+    'watch',
   )
   expect(reRenderedCssLegacyFile).toContain('lightpink')
-  expect(findAssetFile(/polyfills-legacy-.+\.js/)).toBeTruthy()
+  expect(findAssetFile(/polyfills-legacy-.+\.js/, 'watch')).toBeTruthy()
 })
--- playground/legacy/vite.config-watch.js
+++ playground/legacy/vite.config-watch.js
@@ -12,5 +12,6 @@ export default defineConfig({
       },
     },
     watch: {},
+    outDir: 'dist/watch',
   },
 })
