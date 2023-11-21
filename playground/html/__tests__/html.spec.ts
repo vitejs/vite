17f424b3018300eeac62e63fcfa0f5a0609ab4b2
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-11-21 22:04:00 +0900

    fix: make htmlFallback more permissive

--- playground/html/__tests__/html.spec.ts
+++ playground/html/__tests__/html.spec.ts
@@ -414,3 +414,17 @@ test('html serve behavior', async () => {
   expect(bothSlashIndexHtml.status).toBe(200)
   expect(await bothSlashIndexHtml.text()).toContain('both/index.html')
 })
+
+test('html fallback works non browser accept header', async () => {
+  expect((await fetch('/', { headers: { Accept: '' } })).status).toBe(200)
+  // defaults to "Accept: */*"
+  expect((await fetch('/')).status).toBe(200)
+  // wait-on uses axios and axios sends this accept header
+  expect(
+    (
+      await fetch('/', {
+        headers: { Accept: 'application/json, text/plain, */*' },
+      })
+    ).status,
+  ).toBe(200)
+})
