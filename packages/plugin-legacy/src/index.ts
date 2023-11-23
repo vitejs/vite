bdaf9e3362f4a9714b5f96f03ea6a0b1a0eb9675
jiadesen <jiadesen1993@163.com>
2023-11-23 16:27:32 +0800

    Revert "refactor(plugin-legacy): build product file name optimization"
    This reverts commit 1b50187bba7e604ec397fa5b108191e5e8b231ff.

--- packages/plugin-legacy/src/index.ts
+++ packages/plugin-legacy/src/index.ts
@@ -324,23 +324,31 @@ function viteLegacyPlugin(options: Options = {}): Plugin[] {
           | string
           | ((chunkInfo: PreRenderedChunk) => string)
           | undefined,
-        defaultFileName = '[name]-[hash]-legacy.js',
+        defaultFileName = '[name]-legacy-[hash].js',
       ): string | ((chunkInfo: PreRenderedChunk) => string) => {
         if (!fileNames) {
           return path.posix.join(config.build.assetsDir, defaultFileName)
         }
 
         return (chunkInfo) => {
-          const fileName =
+          let fileName =
             typeof fileNames === 'function' ? fileNames(chunkInfo) : fileNames
 
-          // [name]-[hash].[format] -> [name]-[hash]-legacy.[format]
-          // [hash].[format] -> [hash]-legacy.[format]
-          // custom[hash].[format] -> custom[hash]-legacy.[format]
-          // custom-[hash].[format] -> custom-[hash]-legacy.[format]
-          // custom.[hash].[format] -> custom.[hash]-legacy.[format]
-          // [hash].min.[format] -> [hash]-legacy.min.[format]
-          return fileName.replace(/(\.min\.|\.)[^.]*$/, "-legacy$&")
+          if (fileName.includes('[name]')) {
+            // [name]-[hash].[format] -> [name]-legacy-[hash].[format]
+            fileName = fileName.replace('[name]', '[name]-legacy')
+          } else if (fileName.includes('[hash]')) {
+            // custom[hash].[format] -> [name]-legacy[hash].[format]
+            // custom-[hash].[format] -> [name]-legacy-[hash].[format]
+            // custom.[hash].[format] -> [name]-legacy.[hash].[format]
+            fileName = fileName.replace(/[.-]?\[hash\]/, '-legacy$&')
+          } else {
+            // entry.js -> entry-legacy.js
+            // entry.min.js -> entry-legacy.min.js
+            fileName = fileName.replace(/(.+?)\.(.+)/, '$1-legacy.$2')
+          }
+
+          return fileName
         }
       }
 
