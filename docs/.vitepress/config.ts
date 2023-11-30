521eff4e42c364bc995efaf48c9755896d0615c2
patak <matias.capeletto@gmail.com>
2023-11-30 06:52:18 +0100

    docs: verify Mastodon account (#15186)

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -71,6 +71,7 @@ export default defineConfig({
       'link',
       { rel: 'alternate', type: 'application/rss+xml', href: '/blog.rss' },
     ],
+    ['link', { rel: 'me', href: 'https://m.webtoo.ls/@vite' }],
     ['meta', { property: 'og:type', content: 'website' }],
     ['meta', { property: 'og:title', content: ogTitle }],
     ['meta', { property: 'og:image', content: ogImage }],
