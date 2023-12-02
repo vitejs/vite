eebcbe58beb729cbae69ebd7aac01eebce6d029d
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2023-12-02 23:23:42 +0900

    Revert "test: reduce optimize-deps playground flaky fail"
    This reverts commit b7fa4ab18af4d24309041218586954f54d9b62b4.

--- playground/optimize-deps/__tests__/optimize-deps.spec.ts
+++ playground/optimize-deps/__tests__/optimize-deps.spec.ts
@@ -9,254 +9,175 @@ import {
   readDepOptimizationMetadata,
   serverLogs,
   viteTestUrl,
-  withRetry,
 } from '~utils'
 
 test('default + named imports from cjs dep (react)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs button')).toBe('count is 0'),
-  )
+  expect(await page.textContent('.cjs button')).toBe('count is 0')
   await page.click('.cjs button')
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs button')).toBe('count is 1'),
-  )
+  expect(await page.textContent('.cjs button')).toBe('count is 1')
 })
 
 test('named imports from webpacked cjs (phoenix)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-phoenix')).toBe('ok'),
-  )
+  expect(await page.textContent('.cjs-phoenix')).toBe('ok')
 })
 
 test('default import from webpacked cjs (clipboard)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-clipboard')).toBe('ok'),
-  )
+  expect(await page.textContent('.cjs-clipboard')).toBe('ok')
 })
 
 test('dynamic imports from cjs dep (react)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-dynamic button')).toBe('count is 0'),
-  )
+  expect(await page.textContent('.cjs-dynamic button')).toBe('count is 0')
   await page.click('.cjs-dynamic button')
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-dynamic button')).toBe('count is 1'),
-  )
+  expect(await page.textContent('.cjs-dynamic button')).toBe('count is 1')
 })
 
 test('dynamic named imports from webpacked cjs (phoenix)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-dynamic-phoenix')).toBe('ok'),
-  )
+  expect(await page.textContent('.cjs-dynamic-phoenix')).toBe('ok')
 })
 
 test('dynamic default import from webpacked cjs (clipboard)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-dynamic-clipboard')).toBe('ok'),
-  )
+  expect(await page.textContent('.cjs-dynamic-clipboard')).toBe('ok')
 })
 
 test('dynamic default import from cjs (cjs-dynamic-dep-cjs-compiled-from-esm)', async () => {
-  await withRetry(async () =>
-    expect(
-      await page.textContent('.cjs-dynamic-dep-cjs-compiled-from-esm'),
-    ).toBe('ok'),
+  expect(await page.textContent('.cjs-dynamic-dep-cjs-compiled-from-esm')).toBe(
+    'ok',
   )
 })
 
 test('dynamic default import from cjs (cjs-dynamic-dep-cjs-compiled-from-cjs)', async () => {
-  await withRetry(async () =>
-    expect(
-      await page.textContent('.cjs-dynamic-dep-cjs-compiled-from-cjs'),
-    ).toBe('ok'),
+  expect(await page.textContent('.cjs-dynamic-dep-cjs-compiled-from-cjs')).toBe(
+    'ok',
   )
 })
 
 test('dedupe', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.dedupe button')).toBe('count is 0'),
-  )
+  expect(await page.textContent('.dedupe button')).toBe('count is 0')
   await page.click('.dedupe button')
-  await withRetry(async () =>
-    expect(await page.textContent('.dedupe button')).toBe('count is 1'),
-  )
+  expect(await page.textContent('.dedupe button')).toBe('count is 1')
 })
 
 test('cjs browser field (axios)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-browser-field')).toBe('pong'),
-  )
+  expect(await page.textContent('.cjs-browser-field')).toBe('pong')
 })
 
 test('cjs browser field bare', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.cjs-browser-field-bare')).toBe('pong'),
-  )
+  expect(await page.textContent('.cjs-browser-field-bare')).toBe('pong')
 })
 
 test('dep from linked dep (lodash-es)', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.deps-linked')).toBe('fooBarBaz'),
-  )
+  expect(await page.textContent('.deps-linked')).toBe('fooBarBaz')
 })
 
 test('forced include', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.force-include')).toMatch(`[success]`),
-  )
+  expect(await page.textContent('.force-include')).toMatch(`[success]`)
 })
 
 test('import * from optimized dep', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.import-star')).toMatch(`[success]`),
-  )
+  expect(await page.textContent('.import-star')).toMatch(`[success]`)
 })
 
 test('import from dep with process.env.NODE_ENV', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.node-env')).toMatch(
-      isBuild ? 'prod' : 'dev',
-    ),
-  )
+  expect(await page.textContent('.node-env')).toMatch(isBuild ? 'prod' : 'dev')
 })
 
 test('import from dep with .notjs files', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.not-js')).toMatch(`[success]`),
-  )
+  expect(await page.textContent('.not-js')).toMatch(`[success]`)
 })
 
 test('Import from dependency which uses relative path which needs to be resolved by main field', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.relative-to-main')).toMatch(`[success]`),
-  )
+  expect(await page.textContent('.relative-to-main')).toMatch(`[success]`)
 })
 
 test('dep with dynamic import', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.dep-with-dynamic-import')).toMatch(
-      `[success]`,
-    ),
+  expect(await page.textContent('.dep-with-dynamic-import')).toMatch(
+    `[success]`,
   )
 })
 
 test('dep with optional peer dep', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.dep-with-optional-peer-dep')).toMatch(
-      `[success]`,
-    ),
+  expect(await page.textContent('.dep-with-optional-peer-dep')).toMatch(
+    `[success]`,
   )
   if (isServe) {
-    await withRetry(async () =>
-      expect(browserErrors.map((error) => error.message)).toEqual(
-        expect.arrayContaining([
-          'Could not resolve "foobar" imported by "@vitejs/test-dep-with-optional-peer-dep". Is it installed?',
-        ]),
-      ),
+    expect(browserErrors.map((error) => error.message)).toEqual(
+      expect.arrayContaining([
+        'Could not resolve "foobar" imported by "@vitejs/test-dep-with-optional-peer-dep". Is it installed?',
+      ]),
     )
   }
 })
 
 test('dep with optional peer dep submodule', async () => {
-  await withRetry(async () =>
-    expect(
-      await page.textContent('.dep-with-optional-peer-dep-submodule'),
-    ).toMatch(`[success]`),
-  )
+  expect(
+    await page.textContent('.dep-with-optional-peer-dep-submodule'),
+  ).toMatch(`[success]`)
   if (isServe) {
-    await withRetry(async () =>
-      expect(browserErrors.map((error) => error.message)).toEqual(
-        expect.arrayContaining([
-          'Could not resolve "foobar/baz" imported by "@vitejs/test-dep-with-optional-peer-dep-submodule". Is it installed?',
-        ]),
-      ),
+    expect(browserErrors.map((error) => error.message)).toEqual(
+      expect.arrayContaining([
+        'Could not resolve "foobar/baz" imported by "@vitejs/test-dep-with-optional-peer-dep-submodule". Is it installed?',
+      ]),
     )
   }
 })
 
 test('dep with css import', async () => {
-  await withRetry(async () =>
-    expect(await getColor('.dep-linked-include')).toBe('red'),
-  )
+  expect(await getColor('.dep-linked-include')).toBe('red')
 })
 
 test('CJS dep with css import', async () => {
-  await withRetry(async () =>
-    expect(await getColor('.cjs-with-assets')).toBe('blue'),
-  )
+  expect(await getColor('.cjs-with-assets')).toBe('blue')
 })
 
 test('externalize known non-js files in optimize included dep', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.externalize-known-non-js')).toMatch(
-      `[success]`,
-    ),
+  expect(await page.textContent('.externalize-known-non-js')).toMatch(
+    `[success]`,
   )
 })
 
 test('vue + vuex', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.vue')).toMatch(`[success]`),
-  )
+  expect(await page.textContent('.vue')).toMatch(`[success]`)
 })
 
 // When we use the Rollup CommonJS plugin instead of esbuild prebundling,
 // the esbuild plugins won't apply to dependencies
 test('esbuild-plugin', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.esbuild-plugin')).toMatch(
-      `Hello from an esbuild plugin`,
-    ),
+  expect(await page.textContent('.esbuild-plugin')).toMatch(
+    `Hello from an esbuild plugin`,
   )
 })
 
 test('import from hidden dir', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.hidden-dir')).toBe('hello!'),
-  )
+  expect(await page.textContent('.hidden-dir')).toBe('hello!')
 })
 
 test('import optimize-excluded package that imports optimized-included package', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.nested-include')).toBe('nested-include'),
-  )
+  expect(await page.textContent('.nested-include')).toBe('nested-include')
 })
 
 test('import aliased package with colon', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.url')).toBe('vitejs.dev'),
-  )
+  expect(await page.textContent('.url')).toBe('vitejs.dev')
 })
 
 test('import aliased package using absolute path', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.alias-using-absolute-path')).toBe(
-      'From dep-alias-using-absolute-path',
-    ),
+  expect(await page.textContent('.alias-using-absolute-path')).toBe(
+    'From dep-alias-using-absolute-path',
   )
 })
 
 test('variable names are reused in different scripts', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.reused-variable-names')).toBe('reused'),
-  )
+  expect(await page.textContent('.reused-variable-names')).toBe('reused')
 })
 
 test('flatten id should generate correctly', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.clonedeep-slash')).toBe('clonedeep-slash'),
-  )
-  await withRetry(async () =>
-    expect(await page.textContent('.clonedeep-dot')).toBe('clonedeep-dot'),
-  )
+  expect(await page.textContent('.clonedeep-slash')).toBe('clonedeep-slash')
+  expect(await page.textContent('.clonedeep-dot')).toBe('clonedeep-dot')
 })
 
 test('non optimized module is not duplicated', async () => {
-  await withRetry(async () =>
-    expect(
-      await page.textContent('.non-optimized-module-is-not-duplicated'),
-    ).toBe('from-absolute-path, from-relative-path'),
-  )
+  expect(
+    await page.textContent('.non-optimized-module-is-not-duplicated'),
+  ).toBe('from-absolute-path, from-relative-path')
 })
 
 test.runIf(isServe)('error on builtin modules usage', () => {
@@ -306,12 +227,8 @@ test('pre bundle css require', async () => {
     )
   }
 
-  await withRetry(async () =>
-    expect(await getColor('.css-require')).toBe('red'),
-  )
-  await withRetry(async () =>
-    expect(await getColor('.css-module-require')).toBe('red'),
-  )
+  expect(await getColor('.css-require')).toBe('red')
+  expect(await getColor('.css-module-require')).toBe('red')
 })
 
 test.runIf(isBuild)('no missing deps during build', async () => {
@@ -359,7 +276,5 @@ describe.runIf(isServe)('optimizeDeps config', () => {
 })
 
 test('long file name should work', async () => {
-  await withRetry(async () =>
-    expect(await page.textContent('.long-file-name')).toMatch(`hello world`),
-  )
+  expect(await page.textContent('.long-file-name')).toMatch(`hello world`)
 })
