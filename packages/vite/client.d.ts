88de5c53c23630db78e6e32c02f489e14d67cf96
davidleger95 <davidleger95@gmail.com>
2023-12-01 20:42:52 -0400

    feat: add type for .mov files

--- packages/vite/client.d.ts
+++ packages/vite/client.d.ts
@@ -104,6 +104,10 @@ declare module '*.mp4' {
   const src: string
   export default src
 }
+declare module '*.mov' {
+  const src: string
+  export default src
+}
 declare module '*.webm' {
   const src: string
   export default src
