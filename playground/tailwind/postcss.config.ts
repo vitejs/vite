778b69c9c5e588b64df0074620a54424bf30105e
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2023-12-04 10:38:50 +0900

    avoid dirname in esm

--- playground/tailwind/postcss.config.ts
+++ playground/tailwind/postcss.config.ts
@@ -1,8 +1,11 @@
 // postcss.config.ts
+import { fileURLToPath } from 'node:url'
 
 export default {
   plugins: {
-    tailwindcss: { config: __dirname + '/tailwind.config.ts' },
+    tailwindcss: {
+      config: fileURLToPath(new URL('./tailwind.config.ts', import.meta.url)),
+    },
     autoprefixer: {},
   },
 }
