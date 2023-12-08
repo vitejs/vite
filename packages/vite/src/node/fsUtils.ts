93572c86f3d0b041485d1069daf9ad8486db0164
patak <matias.capeletto@gmail.com>
2023-12-08 22:27:39 +0100

    chore: lint

--- packages/vite/src/node/fsUtils.ts
+++ packages/vite/src/node/fsUtils.ts
@@ -1,7 +1,5 @@
 import fs from 'node:fs'
-// import fsp from 'node:fs/promises'
 import path from 'node:path'
-import colors from 'picocolors'
 import type { ResolvedConfig } from './config'
 import {
   isInNodeModules,
