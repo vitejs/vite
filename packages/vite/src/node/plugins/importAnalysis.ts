87b27360fbfe8c3521ca935e26b55279e5958740
patak <matias.capeletto@gmail.com>
2023-12-12 18:42:06 +0100

    perf: temporary hack to avoid fs checks for /@react-refresh (#15299)

--- packages/vite/src/node/plugins/importAnalysis.ts
+++ packages/vite/src/node/plugins/importAnalysis.ts
@@ -338,7 +338,11 @@ export function importAnalysisPlugin(config: ResolvedConfig): Plugin {
           url = resolved.id.slice(root.length)
         } else if (
           depsOptimizer?.isOptimizedDepFile(resolved.id) ||
-          (path.isAbsolute(cleanUrl(resolved.id)) &&
+          // vite-plugin-react isn't following the leading \0 virtual module convention.
+          // This is a temporary hack to avoid expensive fs checks for React apps.
+          // We'll remove this as soon we're able to fix the react plugins.
+          (resolved.id !== '/@react-refresh' &&
+            path.isAbsolute(resolved.id) &&
             fsUtils.existsSync(cleanUrl(resolved.id)))
         ) {
           // an optimized deps may not yet exists in the filesystem, or
