cb062310bb2441358a11df8bb3cdafa3f04f900a
Chaejun Lee <chaejunlee@knu.ac.kr>
2023-12-12 01:43:01 -0800

    chore: re-rerun ci 2

--- packages/vite/src/node/plugins/asset.ts
+++ packages/vite/src/node/plugins/asset.ts
@@ -419,16 +419,16 @@ export async function urlToBuiltUrl(
 function svgToDataURL(content: Buffer): string {
   const stringContent = content.toString()
 
-  const singleQuoteInDoubleQuotes = /"[^"']*'[^"]*"/
   const doubleQuoteInSingleQuotes = /'[^'"]*"[^']*'/
+  const singleQuoteInDoubleQuotes = /"[^"']*'[^"]*"/
 
   // If the SVG contains some text or HTML, any transformation is unsafe, and given that double quotes would then
   // need to be escaped, the gain to use a data URI would be ridiculous if not negative
   if (
     stringContent.includes('<text') ||
     stringContent.includes('<foreignObject') ||
-    singleQuoteInDoubleQuotes.test(stringContent) ||
-    doubleQuoteInSingleQuotes.test(stringContent)
+    doubleQuoteInSingleQuotes.test(stringContent) ||
+    singleQuoteInDoubleQuotes.test(stringContent)
   ) {
     return `data:image/svg+xml;base64,${content.toString('base64')}`
   } else {
