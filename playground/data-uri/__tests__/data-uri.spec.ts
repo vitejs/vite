ada87e406051e620f44808d4ef4dcdeb8f376ca6
이채준 - CJ Lee <chaejunlee@knu.ac.kr>
2023-12-15 07:35:39 -0800

    fix(build): use base64 for inline SVG if it contains both single and double quotes (#15271)

--- playground/data-uri/__tests__/data-uri.spec.ts
+++ playground/data-uri/__tests__/data-uri.spec.ts
@@ -9,6 +9,18 @@ test('base64', async () => {
   expect(await page.textContent('.base64')).toBe('hi')
 })
 
+test('svg data uri minify', async () => {
+  const sqdqs = await page.getByTestId('sqdqs').boundingBox()
+  const sqsdqs = await page.getByTestId('sqsdqs').boundingBox()
+  const dqsqs = await page.getByTestId('dqsqs').boundingBox()
+  const dqssqs = await page.getByTestId('dqssqs').boundingBox()
+
+  expect(sqdqs.height).toBe(100)
+  expect(sqsdqs.height).toBe(100)
+  expect(dqsqs.height).toBe(100)
+  expect(dqssqs.height).toBe(100)
+})
+
 test.runIf(isBuild)('should compile away the import for build', async () => {
   const file = findAssetFile('index')
   expect(file).not.toMatch('import')
