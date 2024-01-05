26bdc17028b08e02c3c481f84e2af506aaf7266e
Simon Le Marchant <simon@marchantweb.com>
2024-01-05 11:03:46 -0500

    feat: base for the hero section

---
+++ docs/.vitepress/theme/components/landing/Hero.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="hero">
+    <div class="container">
+      <a
+        href="https://viteconf.org/23/replay?utm=vite-homepage"
+        class="hero__pill"
+        target="_blank"
+      >
+        <img src="/viteconf.svg" alt="Viteconf logo" width="20" height="20" />
+        <span>Vite conf 2023</span>
+      </a>
+      <h1>Vite is the next generation frontend tooling</h1>
+      <h2>
+        Get ready for a development environment that can finally catch up with
+        you.
+      </h2>
+      <div class="hero__actions">
+        <a href="/guide/" class="btn btn--primary">Get started</a>
+        <a
+          href="https://github.com/vitejs/vite"
+          target="_blank"
+          class="btn btn--outline"
+          ><img src="/github.svg" alt="GitHub logo" width="20" height="20" />
+          GitHub</a
+        >
+      </div>
+    </div>
+    <div class="hero__glow glow--purple" />
+    <div class="hero__glow glow--blue" />
+    <div class="hero__diagram" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@600&display=swap');
+
+.hero {
+  position: relative;
+  z-index: 2;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  max-width: 902px;
+  margin: 95px auto 0;
+  align-items: center;
+  position: relative;
+  z-index: 2;
+}
+
+.hero__pill {
+  display: inline-flex;
+  padding: 6px 16px;
+  align-items: center;
+  gap: 8px;
+  border-radius: 100px;
+  border: 1px solid rgba(189, 52, 254, 0.5);
+  background: radial-gradient(
+    1686.42% 113.39% at 83.25% 2.56%,
+    rgba(189, 52, 254, 0.1) 0%,
+    rgba(189, 52, 254, 0) 100%
+  );
+  box-shadow: none;
+  margin-bottom: 25px;
+
+  span {
+    color: #fff;
+    font-family: Inter, sans-serif;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: -0.28px;
+  }
+
+  &:hover {
+    animation: hero-pill-glow 1600ms ease-out infinite alternate;
+  }
+}
+
+@keyframes hero-pill-glow {
+  0% {
+    border-color: rgba(189, 52, 254, 0.5);
+    box-shadow:
+      0 0 5px rgba(189, 52, 254, 0.01),
+      inset 0 0 5px rgba(189, 52, 254, 0.1);
+  }
+  100% {
+    border-color: rgba(189, 52, 254, 0.6);
+    box-shadow:
+      0 0 20px rgba(189, 52, 254, 0.2),
+      inset 0 0 10px rgba(189, 52, 254, 0.2);
+  }
+}
+
+h1 {
+  text-align: center;
+  font-family: 'Manrope', sans-serif;
+  font-size: 72px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 81px;
+  letter-spacing: -1.44px;
+  background: linear-gradient(180deg, #fff 0%, rgba(255, 255, 255, 0.31) 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  padding-bottom: 8px; /* Fix for hanging descender on "g" in "tooling" */
+  text-wrap: balance;
+  margin-bottom: 15px;
+  cursor: default;
+}
+
+h2 {
+  color: #a3a3a3;
+  text-align: center;
+  font-family: Inter, sans-serif;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 150%;
+  letter-spacing: -0.4px;
+  max-width: 500px;
+  text-wrap: balance;
+  cursor: default;
+  margin-bottom: 25px;
+}
+
+.hero__glow {
+  &.glow--blue {
+    position: absolute;
+    left: -10vw;
+    width: 40vw;
+    aspect-ratio: 3;
+    pointer-events: none;
+    z-index: -2;
+    border-radius: 50% 100% 100% 50%;
+    background: linear-gradient(180deg, #61d9ff 0%, rgba(0, 0, 0, 0) 100%);
+    filter: blur(10vw);
+  }
+
+  &.glow--purple {
+    position: absolute;
+    left: 20vw;
+    width: 70vw;
+    aspect-ratio: 4;
+    pointer-events: none;
+    z-index: -1;
+    border-radius: 100%;
+    background: linear-gradient(
+      90deg,
+      rgba(113, 94, 189, 0.9) 0%,
+      rgba(128, 86, 194, 0.8) 80%,
+      rgba(189, 52, 254, 0) 100%
+    );
+    filter: blur(10vw);
+    transform: rotate(10deg);
+  }
+}
+
+.hero__actions {
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+}
+
+.hero__diagram {
+  position: relative;
+  z-index: 2;
+  min-height: 40vh;
+}
+</style>
+<script setup lang="ts"></script>
--- docs/index.md
+++ docs/index.md
@@ -1,60 +1,23 @@
 ---
-layout: home
-
 title: Vite
 titleTemplate: Next Generation Frontend Tooling
+pageClass: landing
 
-hero:
-  name: Vite
-  text: Next Generation Frontend Tooling
-  tagline: Get ready for a development environment that can finally catch up with you.
-  image:
-    src: /logo-with-shadow.png
-    alt: Vite
-  actions:
-    - theme: brand
-      text: Get Started
-      link: /guide/
-    - theme: alt
-      text: Why Vite?
-      link: /guide/why
-    - theme: alt
-      text: View on GitHub
-      link: https://github.com/vitejs/vite
-    - theme: brand
-      text: ⚡ ViteConf 24!
-      link: https://viteconf.org/?utm=vite-homepage
-
-features:
-  - icon: 💡
-    title: Instant Server Start
-    details: On demand file serving over native ESM, no bundling required!
-  - icon: ⚡️
-    title: Lightning Fast HMR
-    details: Hot Module Replacement (HMR) that stays fast regardless of app size.
-  - icon: 🛠️
-    title: Rich Features
-    details: Out-of-the-box support for TypeScript, JSX, CSS and more.
-  - icon: 📦
-    title: Optimized Build
-    details: Pre-configured Rollup build with multi-page and library mode support.
-  - icon: 🔩
-    title: Universal Plugins
-    details: Rollup-superset plugin interface shared between dev and build.
-  - icon: 🔑
-    title: Fully Typed APIs
-    details: Flexible programmatic APIs with full TypeScript typing.
+layout: home
+aside: false
+editLink: false
 ---
 
 <script setup>
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  const urlParams = new URLSearchParams(window.location.search)
-  if (urlParams.get('uwu') != null) {
-    const img = document.querySelector('.VPHero .VPImage.image-src')
-    img.src = '/logo-uwu.png'
-    img.alt = 'Vite Kawaii Logo by @icarusgkx'
-  }
-})
+import Hero from './.vitepress/theme/components/landing/Hero.vue';
+import HomeSponsors from './.vitepress/theme/components/HomeSponsors.vue';
 </script>
+
+<div class="VPHome">
+<Hero/>
+<HomeSponsors/>
+</div>
+
+<style>
+
+</style>
