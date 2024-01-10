vitejs/vite

commit 202308507c9da835b9035b035477dcac24ff13ab

fix: only watch needed env files (#15365)

packages/vite/src/node/server/hmr.ts
@@ -74,7 +74,7 @@ export async function handleHMRUpdate(
 
   const isEnv =
     config.inlineConfig.envFile !== false &&
-    getEnvFilesForMode(config.mode).includes(fileName)
+    getEnvFilesForMode(config.mode, config.envDir).includes(fileName)
   if (isConfig || isConfigDependency || isPackageJson || isEnv) {
     // auto restart server
     debugHmr?.(`[config change] ${colors.dim(shortFile)}`)

packages/vite/src/node/env.ts
@@ -5,13 +5,13 @@ import { expand } from 'dotenv-expand'
 import { arraify, tryStatSync } from './utils'
 import type { UserConfig } from './config'
 
-export function getEnvFilesForMode(mode: string): string[] {
+export function getEnvFilesForMode(mode: string, envDir: string): string[] {
   return [
     /** default file */ `.env`,
     /** local file */ `.env.local`,
     /** mode file */ `.env.${mode}`,
     /** mode local file */ `.env.${mode}.local`,
-  ]
+  ].map((file) => path.join(envDir, file))
 }
 
 export function loadEnv(
@@ -27,11 +27,10 @@ export function loadEnv(
   }
   prefixes = arraify(prefixes)
   const env: Record<string, string> = {}
-  const envFiles = getEnvFilesForMode(mode)
+  const envFiles = getEnvFilesForMode(mode, envDir)
 
   const parsed = Object.fromEntries(
-    envFiles.flatMap((file) => {
-      const filePath = path.join(envDir, file)
+    envFiles.flatMap((filePath) => {
       if (!tryStatSync(filePath)?.isFile()) return []
 
       return Object.entries(parse(fs.readFileSync(filePath)))

packages/vite/src/node/server/index.ts
@@ -52,6 +52,7 @@ import type { Logger } from '../logger'
 import { printServerUrls } from '../logger'
 import { createNoopWatcher, resolveChokidarOptions } from '../watch'
 import { initPublicFiles } from '../publicDir'
+import { getEnvFilesForMode } from '../env'
 import type { PluginContainer } from './pluginContainer'
 import { ERR_CLOSED_SERVER, createPluginContainer } from './pluginContainer'
 import type { WebSocketServer } from './ws'
@@ -414,7 +415,11 @@ export async function _createServer(
   const watcher = watchEnabled
     ? (chokidar.watch(
         // config file dependencies and env file might be outside of root
-        [...new Set([root, ...config.configFileDependencies, config.envDir])],
+        [
+          root,
+          ...config.configFileDependencies,
+          ...getEnvFilesForMode(config.mode, config.envDir),
+        ],
         resolvedWatchOptions,
       ) as FSWatcher)
     : createNoopWatcher(resolvedWatchOptions)

commit a412c2f4837b43f9c498a42e47d7b08b285122ff

fix: hmr for env files, regression from #15365

packages/vite/src/node/server/hmr.ts
@@ -74,7 +74,7 @@ export async function handleHMRUpdate(
 
   const isEnv =
     config.inlineConfig.envFile !== false &&
-    getEnvFilesForMode(config.mode, config.envDir).includes(fileName)
+    getEnvFilesForMode(config.mode, config.envDir).includes(file)
   if (isConfig || isConfigDependency || isPackageJson || isEnv) {
     // auto restart server
     debugHmr?.(`[config change] ${colors.dim(shortFile)}`)