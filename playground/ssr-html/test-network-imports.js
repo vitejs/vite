f6446b5e6445c7e1368fabbd6082e87f738f81f8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-01-15 08:43:31 +0900

    chore: tweak test

--- playground/ssr-html/test-network-imports.js
+++ playground/ssr-html/test-network-imports.js
@@ -1,9 +1,11 @@
 import assert from 'node:assert'
+import { fileURLToPath } from 'node:url'
 import { createServer } from 'vite'
 
 async function runTest() {
   const server = await createServer({
     configFile: false,
+    root: fileURLToPath(new URL('.', import.meta.url)),
     server: {
       middlewareMode: true,
     },
