cb21728ca773ec6983477f415e53f59cb981a9c5
Arnaud Barré <arnaud.barre@carbometrix.com>
2024-01-22 10:08:01 +0100

    fix(create-vite): turn off `react/jsx-no-target-blank` ESLint rule in React JS template (#15672)

--- packages/create-vite/template-react/.eslintrc.cjs
+++ packages/create-vite/template-react/.eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
+    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
