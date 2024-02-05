24e8079907096ebf477b23b4c5c2843b47c43790
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-05 14:55:19 +0900

    chore: remove

--- playground/define-sourcemap/src/repro.ts
+++
@@ -1,9 +0,0 @@
-declare let __defineObject: object
-
-function main() {
-  // this will be replaced with `define_defineObject_default` object
-  // devtool should show "repro.ts:6"
-  console.log('[__defineObject]', __defineObject)
-}
-
-main()
