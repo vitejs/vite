309ab6b4c2e3c32bee22d29e7f710240b60fb6f4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-05 11:53:09 +0900

    chore: tweak test

--- playground/define-wip/src/repro.ts
+++ playground/define-wip/src/repro.ts
@@ -1,6 +1,6 @@
 function main() {
-  import.meta.env.VITE_NOT_EXIST
-  console.log('hello') // browser devtools should show "repro.ts:3"
+  // this will be replaced with `define_import_meta_env_default` object
+  console.log('[import.meta.env]', import.meta.env) // devtool should show "repro.ts:3"
 }
 
 main()
