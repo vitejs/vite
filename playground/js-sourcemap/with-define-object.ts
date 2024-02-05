0a7184986af2ab90ae36cd6fee211dfb5331aa86
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-02-05 14:16:25 +0900

    chore: comment

--- playground/js-sourcemap/with-define-object.ts
+++ playground/js-sourcemap/with-define-object.ts
@@ -1,6 +1,7 @@
 declare let __testDefineObject: unknown
 
-// test complicated stack since broken sourcemap might still looks correct with a simple case
+// test complicated stack since broken sourcemap
+// might still look correct with a simple case
 function f1() {
   f2()
 }
