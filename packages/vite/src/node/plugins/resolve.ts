89e430c70fbe8f9d5d294adbd49add3096b53f46
bluwy <bjornlu.dev@gmail.com>
2024-02-23 21:54:31 +0800

    feat(resolve): auto externalize node builtins for `noExternal: true` in node

--- packages/vite/src/node/plugins/resolve.ts
+++ packages/vite/src/node/plugins/resolve.ts
@@ -400,6 +400,7 @@ export function resolvePlugin(resolveOptions: InternalResolveOptions): Plugin {
         if (isBuiltin(id)) {
           if (ssr) {
             if (
+              targetWeb &&
               ssrNoExternal === true &&
               // if both noExternal and external are true, noExternal will take the higher priority and bundle it.
               // only if the id is explicitly listed in external, we will externalize it and skip this error.
