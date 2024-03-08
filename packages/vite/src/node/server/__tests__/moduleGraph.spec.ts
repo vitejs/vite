a490a97a72a09451fa2e85e7324cdd9f5f97a88d
patak <matias.capeletto@gmail.com>
2024-03-08 23:25:42 +0100

    test: basic back compat tests

--- packages/vite/src/node/server/__tests__/moduleGraph.spec.ts
+++ packages/vite/src/node/server/__tests__/moduleGraph.spec.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
-import { EnvironmentModuleGraph } from '../moduleGraph'
+import { EnvironmentModuleGraph, ModuleGraph } from '../moduleGraph'
+import type { ModuleNode } from '../moduleGraph'
 
 describe('moduleGraph', () => {
   describe('invalidateModule', () => {
@@ -34,6 +35,78 @@ describe('moduleGraph', () => {
       mod1.meta = meta
       const mod2 = await moduleGraph.ensureEntryFromUrl('/xx.js', false)
       expect(mod2.meta).to.equal(meta)
+    })
+
+    it('ensure backward compatibility', async () => {
+      const browserModuleGraph = new EnvironmentModuleGraph(
+        'browser',
+        async (url) => ({ id: url }),
+      )
+      const serverModuleGraph = new EnvironmentModuleGraph(
+        'server',
+        async (url) => ({ id: url }),
+      )
+      const moduleGraph = new ModuleGraph({
+        browser: browserModuleGraph,
+        server: serverModuleGraph,
+      })
+
+      const addBrowserModule = (url: string) =>
+        browserModuleGraph.ensureEntryFromUrl(url)
+      const getBrowserModule = (url: string) =>
+        browserModuleGraph.getModuleById(url)
+
+      const addServerModule = (url: string) =>
+        serverModuleGraph.ensureEntryFromUrl(url)
+      const getServerModule = (url: string) =>
+        serverModuleGraph.getModuleById(url)
+
+      const browserModule1 = await addBrowserModule('/1.js')
+      const serverModule1 = await addServerModule('/1.js')
+      const module1 = moduleGraph.getModuleById('/1.js')!
+      expect(module1._browserModule).toBe(browserModule1)
+      expect(module1._serverModule).toBe(serverModule1)
+
+      const module2b = await moduleGraph.ensureEntryFromUrl('/b/2.js')
+      const module2s = await moduleGraph.ensureEntryFromUrl('/s/2.js')
+      expect(module2b._browserModule).toBe(getBrowserModule('/b/2.js'))
+      expect(module2s._serverModule).toBe(getServerModule('/s/2.js'))
+
+      const importersUrls = ['/1/a.js', '/1/b.js', '/1/c.js']
+      ;(await Promise.all(importersUrls.map(addBrowserModule))).forEach((mod) =>
+        browserModule1.importers.add(mod),
+      )
+      ;(await Promise.all(importersUrls.map(addServerModule))).forEach((mod) =>
+        serverModule1.importers.add(mod),
+      )
+
+      expect(module1.importers.size).toBe(importersUrls.length)
+
+      const browserModule1importersValues = [...browserModule1.importers]
+      const serverModule1importersValues = [...serverModule1.importers]
+
+      const module1importers = module1.importers
+      const module1importersValues = [...module1importers.values()]
+      expect(module1importersValues.length).toBe(importersUrls.length)
+      expect(module1importersValues[1]._browserModule).toBe(
+        browserModule1importersValues[1],
+      )
+      expect(module1importersValues[1]._serverModule).toBe(
+        serverModule1importersValues[1],
+      )
+
+      const module1importersFromForEach: ModuleNode[] = []
+      module1.importers.forEach((imp) => {
+        moduleGraph.invalidateModule(imp)
+        module1importersFromForEach.push(imp)
+      })
+      expect(module1importersFromForEach.length).toBe(importersUrls.length)
+      expect(module1importersFromForEach[1]._browserModule).toBe(
+        browserModule1importersValues[1],
+      )
+      expect(module1importersFromForEach[1]._serverModule).toBe(
+        serverModule1importersValues[1],
+      )
     })
   })
 })
