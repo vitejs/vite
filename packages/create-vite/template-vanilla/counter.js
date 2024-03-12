ed797b1137997e65dd15203dae2c0b9b0cb5b2b5
Animesh <69345507+n-ce@users.noreply.github.com>
2024-03-12 19:26:43 +0530

    Update counter.js

--- packages/create-vite/template-vanilla/counter.js
+++ packages/create-vite/template-vanilla/counter.js
@@ -1,9 +1,9 @@
 export function setupCounter(element) {
   let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
+  function increment() {
+    element.textContent = `count is ${counter}`
+    count++
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  element.addEventListener('click', increment)
+  increment()
 }
