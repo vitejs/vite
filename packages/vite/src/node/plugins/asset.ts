75a9fc668f97bfd0fe79891e9ebc5713b2a22471
翠 / green <green@sapphi.red>
2024-03-12 22:10:53 +0900

    feat: accept assets to be specified as input (#16087)

--- packages/vite/src/node/plugins/asset.ts
+++ packages/vite/src/node/plugins/asset.ts
@@ -205,7 +205,15 @@ export function assetPlugin(config: ResolvedConfig): Plugin {
         }
       }
 
-      return `export default ${JSON.stringify(url)}`
+      return {
+        code: `export default ${JSON.stringify(url)}`,
+        // Force rollup to keep this module from being shared between other entry points if it's an entrypoint.
+        // If the resulting chunk is empty, it will be removed in generateBundle.
+        moduleSideEffects:
+          config.command === 'build' && this.getModuleInfo(id)?.isEntry
+            ? 'no-treeshake'
+            : false,
+      }
     },
 
     renderChunk(code, chunk, opts) {
@@ -224,6 +232,19 @@ export function assetPlugin(config: ResolvedConfig): Plugin {
     },
 
     generateBundle(_, bundle) {
+      // Remove empty entry point file
+      for (const file in bundle) {
+        const chunk = bundle[file]
+        if (
+          chunk.type === 'chunk' &&
+          chunk.isEntry &&
+          chunk.moduleIds.length === 1 &&
+          config.assetsInclude(chunk.moduleIds[0])
+        ) {
+          delete bundle[file]
+        }
+      }
+
       // do not emit assets for SSR build
       if (
         config.command === 'build' &&
@@ -340,7 +361,7 @@ async function fileToBuiltUrl(
   const content = await fsp.readFile(file)
 
   let url: string
-  if (shouldInline(config, file, id, content, forceInline)) {
+  if (shouldInline(config, file, id, content, pluginContext, forceInline)) {
     if (config.build.lib && isGitLfsPlaceholder(content)) {
       config.logger.warn(
         colors.yellow(`Inlined file ${id} was not downloaded via Git LFS`),
@@ -405,9 +426,11 @@ const shouldInline = (
   file: string,
   id: string,
   content: Buffer,
+  pluginContext: PluginContext,
   forceInline: boolean | undefined,
 ): boolean => {
   if (config.build.lib) return true
+  if (pluginContext.getModuleInfo(id)?.isEntry) return false
   if (forceInline !== undefined) return forceInline
   let limit: number
   if (typeof config.build.assetsInlineLimit === 'function') {
