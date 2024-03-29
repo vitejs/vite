e5c87598588d3f3ab749270e36219366eda88b35
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-29 15:41:10 +0900

    chore: remove playwright

--- playground/environment-react-ssr/__tests__/basic.playwright.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('basic', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.locator('#root')).toContainText('hydrated: true')
-  await expect(page.locator('#root')).toContainText('Count: 0')
-  await page.getByRole('button', { name: '+' }).click()
-  await expect(page.locator('#root')).toContainText('Count: 1')
-})
