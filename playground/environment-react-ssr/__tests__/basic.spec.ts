8963a27fc10d6420a8283b9fba32054adc6d32aa
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-29 15:40:03 +0900

    test: custom serve

--- playground/environment-react-ssr/__tests__/basic.spec.ts
+++ playground/environment-react-ssr/__tests__/basic.spec.ts
@@ -3,7 +3,6 @@ import { expect } from '@playwright/test'
 import { page } from '~utils'
 
 test('basic', async () => {
-  await page.pause()
   await expect(page.locator('#root')).toContainText('hydrated: true')
   await expect(page.locator('#root')).toContainText('Count: 0')
   await page.getByRole('button', { name: '+' }).click()
