6002791d497e4a6f2aa3bed66c2ee41d20c06f57
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-29 12:39:25 +0900

    fix(environment): use client environment `build.outDir` for preview

--- packages/vite/src/node/preview.ts
+++ packages/vite/src/node/preview.ts
@@ -111,7 +111,9 @@ export async function preview(
     true,
   )
 
-  const distDir = path.resolve(config.root, config.build.outDir)
+  const clientOutDir =
+    config.environments.client.build.outDir ?? config.build.outDir
+  const distDir = path.resolve(config.root, clientOutDir)
   if (
     !fs.existsSync(distDir) &&
     // error if no plugins implement `configurePreviewServer`
@@ -122,7 +124,7 @@ export async function preview(
     process.argv[2] === 'preview'
   ) {
     throw new Error(
-      `The directory "${config.build.outDir}" does not exist. Did you build your project?`,
+      `The directory "${clientOutDir}" does not exist. Did you build your project?`,
     )
   }
 
