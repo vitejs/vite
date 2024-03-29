1782094fb0d9acfe17e18ef63cbc94156d6d74e4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-03-29 19:21:37 +0900

    test: no playwright expect

--- playground/environment-react-ssr/__tests__/basic.spec.ts
+++ playground/environment-react-ssr/__tests__/basic.spec.ts
@@ -1,10 +1,9 @@
 import { test } from 'vitest'
-import { expect } from '@playwright/test'
 import { page } from '~utils'
 
 test('basic', async () => {
-  await expect(page.locator('#root')).toContainText('hydrated: true')
-  await expect(page.locator('#root')).toContainText('Count: 0')
+  await page.getByText('hydrated: true').isVisible()
+  await page.getByText('Count: 0').isVisible()
   await page.getByRole('button', { name: '+' }).click()
-  await expect(page.locator('#root')).toContainText('Count: 1')
+  await page.getByText('Count: 1').isVisible()
 })
