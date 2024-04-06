c1bffdab7686c77495f345b7c434456d65b301c2
smeng9 <38666763+smeng9@users.noreply.github.com>
2024-04-06 11:45:10 +0800

    incorporate changes in #13750

--- packages/create-vite/template-react/src/App.jsx
+++ packages/create-vite/template-react/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+        <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
+        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
