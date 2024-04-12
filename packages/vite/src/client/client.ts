0a5614abcad64d7ae6adbd9f9f21b4fab6693250
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-04-12 10:21:08 +0200

    chore: remove [vite] from hmr handler

--- packages/vite/src/client/client.ts
+++ packages/vite/src/client/client.ts
@@ -175,7 +175,7 @@ const hmrClient = new HMRClient(
 async function handleMessage(payload: HMRPayload) {
   switch (payload.type) {
     case 'connected':
-      console.debug(`[vite] connected.`)
+      console.debug(`connected.`)
       hmrClient.messenger.flush()
       // proxy(nginx, docker) hmr ws maybe caused timeout,
       // so send ping package let ws keep alive.
