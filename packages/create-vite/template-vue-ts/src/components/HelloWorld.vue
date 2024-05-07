faf023205619a0b94d1f9efd9c450651c4235ea2
翠 / green <green@sapphi.red>
2024-05-07 17:59:07 +0900

    chore: remove unneeded spaces

--- packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
+++ packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
@@ -25,7 +25,7 @@ const count = ref(0)
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank"> Vue Docs Scaling up Guide</a>.
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
