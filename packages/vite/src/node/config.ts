b726cfd7c970bdac04aca6922566a7da97b41f80
moxey.eth <jakemoxey@gmail.com>
2024-05-11 16:23:32 +1000

    fix: account for jsx imports in config bundling

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -1188,7 +1188,7 @@ async function bundleConfigFile(
       {
         name: 'inject-file-scope-variables',
         setup(build) {
-          build.onLoad({ filter: /\.[cm]?[jt]s$/ }, async (args) => {
+          build.onLoad({ filter: /\.[cm]?[jt]sx?$/ }, async (args) => {
             const contents = await fsp.readFile(args.path, 'utf-8')
             const injectValues =
               `const ${dirnameVarName} = ${JSON.stringify(
@@ -1200,7 +1200,7 @@ async function bundleConfigFile(
               )};`
 
             return {
-              loader: args.path.endsWith('ts') ? 'ts' : 'js',
+              loader: /tsx?$/.test(args.path) ? 'ts' : 'js',
               contents: injectValues + contents,
             }
           })
