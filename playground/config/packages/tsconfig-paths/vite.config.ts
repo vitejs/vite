8c947cb5ea0dee46f561dfdf6ddd058a3a7b4cd4
garydex <1895375+garydex@users.noreply.github.com>
2024-05-23 13:28:13 +0100

    fix: fixing typecheck for test config file

--- playground/config/packages/tsconfig-paths/vite.config.ts
+++ playground/config/packages/tsconfig-paths/vite.config.ts
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
 import { array } from '@test/siblings'
+// @ts-ignore
 import { foo } from '@test/wildcard/foo'
 
 export default {
