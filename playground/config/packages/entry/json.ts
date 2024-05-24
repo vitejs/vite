60fbc8e257ad49dfc9c42318d646f42077628d40
SegaraRai <SegaraRai@users.noreply.github.com>
2024-05-25 06:48:56 +0900

    fix: suppress type error

--- playground/config/packages/entry/json.ts
+++ playground/config/packages/entry/json.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// whether or not an error occurs on `import depends on the environment, so only `@ts-ignore` can be used
+
 // test if the json file is imported correctly
 
 let value: string
@@ -6,6 +9,7 @@ let value: string
 try {
   value = (
     await import('./package.json', {
+      // @ts-ignore import attributes are not supported on older Node versions
       with: { type: 'json' },
     })
   ).default.name
@@ -15,6 +19,7 @@ try {
 try {
   value = (
     await import('./package.json', {
+      // @ts-ignore import assertions are not supported on Node v22.0.0 and above
       assert: { type: 'json' },
     })
   ).default.name
