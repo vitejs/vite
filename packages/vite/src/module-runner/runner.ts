c373251123ffbfddf2f703b6a62a5d9b1a03db67
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-05-28 13:55:35 +0200

    chore: prettier

--- packages/vite/src/module-runner/runner.ts
+++ packages/vite/src/module-runner/runner.ts
@@ -242,13 +242,13 @@ export class ModuleRunner {
     const isCached = !!(typeof cachedModule === 'object' && cachedModule.meta)
 
     const fetchedModule = // fast return for established externalized pattern
-    (
-      url.startsWith('data:')
-        ? { externalize: url, type: 'builtin' }
-        : await this.transport.fetchModule(url, importer, {
-            cached: isCached,
-          })
-    ) as ResolvedResult
+      (
+        url.startsWith('data:')
+          ? { externalize: url, type: 'builtin' }
+          : await this.transport.fetchModule(url, importer, {
+              cached: isCached,
+            })
+      ) as ResolvedResult
 
     if ('cache' in fetchedModule) {
       if (!cachedModule || !cachedModule.meta) {
