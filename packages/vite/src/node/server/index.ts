80a1aadf6257a292b4024eefca9e4a6cc2438568
Vladimir <sleuths.slews0s@icloud.com>
2024-05-29 16:14:30 +0200

    feat: add an option to not start a websocket server (#16219)

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -102,6 +102,11 @@ export interface ServerOptions extends CommonServerOptions {
    */
   hmr?: HmrOptions | boolean
   /**
+   * Do not start the websocket connection.
+   * @experimental
+   */
+  ws?: false
+  /**
    * Warm-up files to transform and cache the results in advance. This improves the
    * initial page load during server starts and prevents transform waterfalls.
    */
