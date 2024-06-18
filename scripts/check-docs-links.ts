b86d9ec681315fd59e936394db863be90fa8f110
刘志伟 <2055572879@qq.com>
2024-06-18 17:03:01 +0800

    ignore x

--- scripts/check-docs-links.ts
+++ scripts/check-docs-links.ts
@@ -10,7 +10,7 @@ interface Link {
   file: string
   statusCode?: number
 }
-const ignorePatterns: RegExp[] = [/viteconf\.org/, /twitter\.com/]
+const ignorePatterns: RegExp[] = [/viteconf\.org/, /twitter\.com/, /x\.com/]
 const linkRegex = /\[(.*?)\]\((https?:\/\/.*?)\)/g
 const links: Link[] = []
 
