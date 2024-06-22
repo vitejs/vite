d71e073f60c2aaebbbf66f47b08d6ae5fbd3835d
arlo <webfansplz@gmail.com>
2024-06-22 21:55:59 +0800

    feat(create-vite): add vue-devtools in vue template

--- packages/create-vite/template-vue/vite.config.js
+++ packages/create-vite/template-vue/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
 })
