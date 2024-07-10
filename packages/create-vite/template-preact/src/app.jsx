1b7fce6d35998013d1c2083f6ffe0cec92a19108
Ryan Christian <rchristian@ryanchristian.dev>
2024-07-09 22:48:37 -0500

    chore: Formatting

--- packages/create-vite/template-preact/src/app.jsx
+++ packages/create-vite/template-preact/src/app.jsx
@@ -27,8 +27,11 @@ export function App() {
       </div>
       <p>
         Check out{' '}
-        <a href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app" target="_blank">
-            create-preact
+        <a
+          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
+          target="_blank"
+        >
+          create-preact
         </a>
         , the official Preact + Vite starter
       </p>
