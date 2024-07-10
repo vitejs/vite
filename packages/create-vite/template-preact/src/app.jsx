b4cf83fa0f790e2abaa66fdadd72d1cc71b8a720
Ryan Christian <rchristian@ryanchristian.dev>
2024-07-09 22:35:26 -0500

    chore: Add `create-preact` recommendation to Preact templates

--- packages/create-vite/template-preact/src/app.jsx
+++ packages/create-vite/template-preact/src/app.jsx
@@ -25,6 +25,12 @@ export function App() {
           Edit <code>src/app.jsx</code> and save to test HMR
         </p>
       </div>
+      <p>
+        Check out{' '}
+        <a href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app" target="_blank">
+            create-preact
+      </a>
+      , the official Preact + Vite starter
       <p class="read-the-docs">
         Click on the Vite and Preact logos to learn more
       </p>
