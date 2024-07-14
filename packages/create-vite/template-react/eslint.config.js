fcca02875e8a6bcb35f16d241c12ce6528e56662
Riva Junior <jrcavalcantejr@gmail.com>
2024-07-14 15:23:59 -0300

    chore(create-vite): use eslint flat config on template-react

---
+++ packages/create-vite/template-react/eslint.config.js
@@ -0,0 +1,54 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  js.configs.recommended,
+
+  {
+    ignores: ['**/dist'],
+  },
+
+  {
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    settings: {
+      react: {
+        version: '18.2',
+      },
+    },
+
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+    },
+  },
+]
