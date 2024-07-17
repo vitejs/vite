678e967884ea344664885871c6e9011f68b2d436
btea <2356281422@qq.com>
2024-07-17 22:05:18 +0800

    chore: update

--- scripts/extendCommitHash.ts
+++ scripts/extendCommitHash.ts
@@ -2,10 +2,10 @@ import fs from 'node:fs'
 import { execSync } from 'node:child_process'
 import colors from 'picocolors'
 
-export default function extendLogHash(path: string) {
+export default function extendCommitHash(path: string): void {
   let content = fs.readFileSync(path, 'utf-8')
   const base = 'https://github.com/vitejs/vite/commit/'
-  const matchHashReg = new RegExp(`${base}([\\d\\w]{7})\\)`, 'g')
+  const matchHashReg = new RegExp(`${base}(\\w{7})\\)`, 'g')
   console.log(colors.cyan(`\nextending commit hash in ${path}...`))
   let match
   while ((match = matchHashReg.exec(content))) {
