fba1d4d44291a02f00fd21e3ade04f1261e281d5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-07-20 18:50:40 +0900

    chore: cleanup playground

--- playground/css-sass-modern/vite.config.js
+++ playground/css-sass-modern/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   resolve: {
     alias: {
-      'alias-from': 'alias-to',
+      'alias-nested': 'nested',
     },
   },
   css: {
