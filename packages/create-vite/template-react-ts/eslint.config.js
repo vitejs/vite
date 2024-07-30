da21981f61a6e1113857103242b30370e3793b6e
Arnaud Barré <arnaud.barre@carbometrix.com>
2024-07-30 16:15:41 +0200

    use tseslint

--- packages/create-vite/template-react-ts/eslint.config.js
+++ packages/create-vite/template-react-ts/eslint.config.js
@@ -2,31 +2,28 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tsParser from '@typescript-eslint/parser'
-import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tseslint from 'typescript-eslint'
 
-export default [
-  {
-    files: ['**/*.{ts,tsx}'],
-    ignores: ['dist'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parser: tsParser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      '@typescript-eslint': tsPlugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...tsPlugin.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
+export default tseslint.config({
+  files: ['**/*.{ts,tsx}'],
+  ignores: ['dist'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
+    parser: tseslint.parser,
   },
-]
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+    '@typescript-eslint': tseslint.plugin,
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...tseslint.configs.recommended.rules,
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+})
