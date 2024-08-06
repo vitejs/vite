a2e648bed9ac96eccf858bf7c612a60b3829dbbe
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-06 09:17:15 +0900

    Revert "chore: prettier-ignore"
    This reverts commit 10fb68df064220ab72e2edffe21a99f689319405.

--- playground/worker/classic-worker.js
+++ playground/worker/classic-worker.js
@@ -1,5 +1,4 @@
-// prettier-ignore
-;(() => {})() // this is to test `importScripts` injection doesn't break the code
+(() => {})() // this is to test `importScripts` injection doesn't break the code
 
 let base = `/${self.location.pathname.split('/')[1]}`
 if (base.endsWith('.js') || base === `/worker-entries`) base = '' // for dev
