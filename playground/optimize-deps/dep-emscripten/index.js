9c85affca5c3e24f9af07f5322e51e8682e41ae9
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-09 11:08:56 +0900

    chore: remove emscripten example

--- playground/optimize-deps/dep-emscripten/index.js
+++
@@ -1,9 +0,0 @@
-export function startWorker(handler) {
-  const worker = new Worker(new URL('./worker.js', import.meta.url), {
-    type: 'module',
-  })
-  worker.addEventListener('message', (e) => {
-    handler(e)
-  })
-  worker.postMessage('ping')
-}
