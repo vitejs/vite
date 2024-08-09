62299fb3c8143458797fb8351641a08edb97f52a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-09 18:36:57 +0900

    refactor: simplify self reference worker

--- packages/vite/src/node/optimizer/importMetaUrlPlugin.ts
+++ packages/vite/src/node/optimizer/importMetaUrlPlugin.ts
@@ -2,18 +2,13 @@ import fs from 'node:fs'
 import path from 'node:path'
 import MagicString from 'magic-string'
 import * as esbuild from 'esbuild'
-import { flattenId, getHash } from '../utils'
 
 export function esbuildImportMetaUrlPlugin({
   processingCacheDir,
-  visited = new Set(),
-  recursed,
+  bundleChain = [],
 }: {
   processingCacheDir: string
-  // track worker build to prevent infinite loop on recursive worker such as
-  // https://github.com/gkjohnson/three-mesh-bvh/blob/9718501eee2619f1015fa332d7bddafaf6cf562a/src/workers/parallelMeshBVH.worker.js#L12
-  visited?: Set<string>
-  recursed?: boolean
+  bundleChain?: string[] // track recursive worker build
 }): esbuild.Plugin {
   return {
     name: esbuildImportMetaUrlPlugin.name,
@@ -40,37 +35,40 @@ export function esbuildImportMetaUrlPlugin({
               if (url[0] !== '/') {
                 const absUrl = path.resolve(path.dirname(args.path), url)
                 if (fs.existsSync(absUrl)) {
-                  const workerFilename = getWorkerFileName(absUrl)
-                  const outfile = path.resolve(
-                    processingCacheDir,
-                    '__worker',
-                    workerFilename,
-                  )
-                  // recursively bundle worker
-                  if (!fs.existsSync(outfile) && !visited.has(outfile)) {
-                    visited.add(outfile)
-                    await esbuild.build({
-                      outfile,
-                      entryPoints: [absUrl],
-                      bundle: true,
-                      // TODO: should we detect WorkerType and use esm only when `{ type: "module" }`?
-                      format: 'esm',
-                      platform: 'browser',
-                      plugins: [
-                        esbuildImportMetaUrlPlugin({
-                          processingCacheDir,
-                          visited,
-                          recursed: true,
-                        }),
-                      ],
-                    })
+                  // handle circular worker import similar to vite build
+                  if (bundleChain.at(-1) === absUrl) {
+                    output.update(urlStart, urlEnd, 'self.location.href')
+                    continue
+                  } else if (bundleChain.includes(absUrl)) {
+                    throw new Error(
+                      'Unsupported circular worker imports: ' +
+                        [...bundleChain].join(' -> '),
+                    )
                   }
-                  // To allow relocating from `deps_temp_xxx/__worker` to `deps/__worker`,
-                  // worker reference needs to be relative.
-                  const targetPath = recursed
-                    ? `./${workerFilename}`
-                    : `./__worker/${workerFilename}`
-                  output.update(urlStart, urlEnd, JSON.stringify(targetPath))
+                  const result = await esbuild.build({
+                    outdir: path.join(processingCacheDir, '__worker'),
+                    entryPoints: [absUrl],
+                    entryNames: '[name]-[hash]',
+                    bundle: true,
+                    metafile: true,
+                    // TODO: should we detect WorkerType and use esm only when `{ type: "module" }`?
+                    format: 'esm',
+                    platform: 'browser',
+                    plugins: [
+                      esbuildImportMetaUrlPlugin({
+                        processingCacheDir,
+                        bundleChain: [...bundleChain, absUrl],
+                      }),
+                    ],
+                  })
+                  const filename = path.basename(
+                    Object.keys(result.metafile.outputs)[0],
+                  )
+                  output.update(
+                    urlStart,
+                    urlEnd,
+                    JSON.stringify(`./__worker/${filename}`),
+                  )
                 }
               }
             }
@@ -108,13 +106,6 @@ export function esbuildImportMetaUrlPlugin({
       })
     },
   }
-}
-
-// make recognizable filename
-function getWorkerFileName(file: string) {
-  const id = flattenId(file.split('/node_modules/').at(-1)!)
-  const hash = getHash(file)
-  return `${id}-${hash}.js`
 }
 
 // packages/vite/src/node/plugins/assetImportMetaUrl.ts
