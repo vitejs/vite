1fe199d61751948c4016418e95eed2237e2896b1
simonlary <simonlary@gmail.com>
2024-08-14 23:33:06 -0400

    Move "ignores" config to a separate ESLint configuration.

--- packages/create-vite/template-react/eslint.config.js
+++ packages/create-vite/template-react/eslint.config.js
@@ -5,9 +5,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
