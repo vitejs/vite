e686d749d673c02ff4395971ac74340082da14e5
郁蒸十四 <55919198+yuzheng14@users.noreply.github.com>
2024-08-15 14:52:55 +0800

    fix(build): avoid re-define `__vite_import_meta_env__` (#17876)
    Co-authored-by: bluwy <bjornlu.dev@gmail.com>

--- packages/vite/src/node/plugins/define.ts
+++ packages/vite/src/node/plugins/define.ts
@@ -9,8 +9,7 @@ import { isHTMLRequest } from './html'
 const nonJsRe = /\.json(?:$|\?)/
 const isNonJsRequest = (request: string): boolean => nonJsRe.test(request)
 const importMetaEnvMarker = '__vite_import_meta_env__'
-const bareImportMetaEnvRe = new RegExp(`${importMetaEnvMarker}(?!\\.)\\b`)
-const importMetaEnvKeyRe = new RegExp(`${importMetaEnvMarker}\\..+?\\b`, 'g')
+const importMetaEnvKeyReCache = new Map<string, RegExp>()
 
 export function definePlugin(config: ResolvedConfig): Plugin {
   const isBuild = config.command === 'build'
@@ -80,7 +79,6 @@ export function definePlugin(config: ResolvedConfig): Plugin {
       SSR: ssr + '',
       ...userDefineEnv,
     })
-    const banner = `const ${importMetaEnvMarker} = ${importMetaEnvVal};\n`
 
     // Create regex pattern as a fast check before running esbuild
     const patternKeys = Object.keys(userDefine)
@@ -94,7 +92,7 @@ export function definePlugin(config: ResolvedConfig): Plugin {
       ? new RegExp(patternKeys.map(escapeRegex).join('|'))
       : null
 
-    return [define, pattern, banner] as const
+    return [define, pattern, importMetaEnvVal] as const
   }
 
   const defaultPattern = generatePattern(false)
@@ -122,28 +120,49 @@ export function definePlugin(config: ResolvedConfig): Plugin {
         return
       }
 
-      const [define, pattern, banner] = ssr ? ssrPattern : defaultPattern
+      let [define, pattern, importMetaEnvVal] = ssr
+        ? ssrPattern
+        : defaultPattern
       if (!pattern) return
 
       // Check if our code needs any replacements before running esbuild
       pattern.lastIndex = 0
       if (!pattern.test(code)) return
 
+      const hasDefineImportMetaEnv = 'import.meta.env' in define
+      let marker = importMetaEnvMarker
+
+      if (hasDefineImportMetaEnv && code.includes(marker)) {
+        // append a number to the marker until it's unique, to avoid if there is a
+        // marker already in the code
+        let i = 1
+        do {
+          marker = importMetaEnvMarker + i++
+        } while (code.includes(marker))
+
+        if (marker !== importMetaEnvMarker) {
+          define = { ...define, 'import.meta.env': marker }
+        }
+      }
+
       const result = await replaceDefine(code, id, define, config)
 
-      // Replace `import.meta.env.*` with undefined
-      result.code = result.code.replaceAll(importMetaEnvKeyRe, (m) =>
-        'undefined'.padEnd(m.length),
-      )
+      if (hasDefineImportMetaEnv) {
+        // Replace `import.meta.env.*` with undefined
+        result.code = result.code.replaceAll(
+          getImportMetaEnvKeyRe(marker),
+          (m) => 'undefined'.padEnd(m.length),
+        )
 
-      // If there's bare `import.meta.env` references, prepend the banner
-      if (bareImportMetaEnvRe.test(result.code)) {
-        result.code = banner + result.code
+        // If there's bare `import.meta.env` references, prepend the banner
+        if (result.code.includes(marker)) {
+          result.code = `const ${marker} = ${importMetaEnvVal};\n` + result.code
 
-        if (result.map) {
-          const map = JSON.parse(result.map)
-          map.mappings = ';' + map.mappings
-          result.map = map
+          if (result.map) {
+            const map = JSON.parse(result.map)
+            map.mappings = ';' + map.mappings
+            result.map = map
+          }
         }
       }
 
@@ -218,4 +237,13 @@ function handleDefineValue(value: any): string {
   if (typeof value === 'undefined') return 'undefined'
   if (typeof value === 'string') return value
   return JSON.stringify(value)
+}
+
+function getImportMetaEnvKeyRe(marker: string): RegExp {
+  let re = importMetaEnvKeyReCache.get(marker)
+  if (!re) {
+    re = new RegExp(`${marker}\\..+?\\b`, 'g')
+    importMetaEnvKeyReCache.set(marker, re)
+  }
+  return re
 }
