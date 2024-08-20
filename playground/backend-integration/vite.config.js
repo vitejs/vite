2e3e98a6e302a96d8135357c4cb4f3b624f86875
翠 / green <green@sapphi.red>
2024-08-20 16:30:05 +0900

    chore: remove TODO comment

--- playground/backend-integration/vite.config.js
+++ playground/backend-integration/vite.config.js
@@ -26,8 +26,6 @@ function BackendIntegrationExample() {
           // same port in playground/test-utils.ts
           port: 5009,
           strictPort: true,
-          // TODO: should this be automatically inferred from dev server address?
-          // (see "boolean or string" discussions in https://github.com/vitejs/vite/pull/4337)
           origin: 'http://localhost:5009',
         },
         build: {
