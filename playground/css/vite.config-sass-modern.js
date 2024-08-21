e4babedc97e469d9caddc76c98796e48e01a5549
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-21 11:59:47 +0900

    chore: lint

--- playground/css/vite.config-sass-modern.js
+++ playground/css/vite.config-sass-modern.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import baseConfig from './vite.config.js'
 import { pathToFileURL } from 'node:url'
 import path from 'node:path'
+import { defineConfig } from 'vite'
+import baseConfig from './vite.config.js'
 
 export default defineConfig({
   ...baseConfig,
