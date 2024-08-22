aaece87d8717d4672cd79e0d445de3c101989f74
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-08-22 18:23:02 +0900

    fix(css): fix sass `file://` reference (#17909)
    Co-authored-by: patak <583075+patak-dev@users.noreply.github.com>

--- playground/css/vite.config-sass-modern.js
+++ playground/css/vite.config-sass-modern.js
@@ -1,3 +1,5 @@
+import { pathToFileURL } from 'node:url'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import baseConfig from './vite.config.js'
 
@@ -20,6 +22,19 @@ export default defineConfig({
             load() {
               return {
                 contents: ``,
+                syntax: 'scss',
+              }
+            },
+          },
+          {
+            canonicalize(url) {
+              return url === 'virtual-file-absolute'
+                ? new URL('custom-importer:virtual-file-absolute')
+                : null
+            },
+            load() {
+              return {
+                contents: `@import "${pathToFileURL(path.join(import.meta.dirname, 'file-absolute.scss')).href}"`,
                 syntax: 'scss',
               }
             },
