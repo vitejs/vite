baae3438d1b9d14a3ac45209cef0196bb70fa21e
patak-dev <matias.capeletto@gmail.com>
2024-08-23 16:43:24 +0200

    chore: avoid config object in resolveChokidarOptions

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -791,10 +791,10 @@ export async function buildEnvironment(
       logger.info(colors.cyan(`\nwatching for file changes...`))
 
       const resolvedChokidarOptions = resolveChokidarOptions(
-        config,
         config.build.watch.chokidar,
         resolvedOutDirs,
         emptyOutDir,
+        config.cacheDir,
       )
 
       const { watch } = await import('rollup')
