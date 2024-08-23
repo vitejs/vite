25540e2dcb398a62920e9ff60d12650dbf1c787c
ivanzusko <ivan.zusko@gmail.com>
2024-08-24 00:41:32 +0200

    chore: lint fix

--- packages/create-vite/template-react-ts-jest/jest.config.js
+++ packages/create-vite/template-react-ts-jest/jest.config.js
@@ -1,12 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    '^.+.tsx?$': ['ts-jest', {}],
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-    "^.+\\.svg$": "jest-transformer-svg",
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '^.+\\.svg$': 'jest-transformer-svg',
   },
-};
+}
--- packages/create-vite/template-react-ts-jest/src/App.test.tsx
+++ packages/create-vite/template-react-ts-jest/src/App.test.tsx
@@ -1,35 +1,37 @@
-import { render } from '@testing-library/react';
-import { screen } from '@testing-library/dom';
+import { render } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
-import App from './App';
+import App from './App'
 
 describe('App Component', () => {
   test('renders the heading Vite + React', () => {
-    render(<App />);
-    const headingElement = screen.getByText(/Vite \+ React/i);
-    expect(headingElement).toBeInTheDocument();
-  });
+    render(<App />)
+    const headingElement = screen.getByText(/Vite \+ React/i)
+    expect(headingElement).toBeInTheDocument()
+  })
 
   test('renders the initial count value of 0', () => {
-    render(<App />);
-    const countButton = screen.getByRole('button', { name: /count is 0/i });
-    expect(countButton).toBeInTheDocument();
-  });
+    render(<App />)
+    const countButton = screen.getByRole('button', { name: /count is 0/i })
+    expect(countButton).toBeInTheDocument()
+  })
 
   test('increments the count when the button is clicked', () => {
-    render(<App />);
-    const countButton = screen.getByRole('button', { name: /count is 0/i });
-    
-    userEvent.click(countButton);
-    expect(countButton).toHaveTextContent('count is 1');
-    
-    userEvent.click(countButton);
-    expect(countButton).toHaveTextContent('count is 2');
-  });
+    render(<App />)
+    const countButton = screen.getByRole('button', { name: /count is 0/i })
+
+    userEvent.click(countButton)
+    expect(countButton).toHaveTextContent('count is 1')
+
+    userEvent.click(countButton)
+    expect(countButton).toHaveTextContent('count is 2')
+  })
 
   test('renders the "read the docs" paragraph', () => {
-    render(<App />);
-    const docsParagraph = screen.getByText(/Click on the Vite and React logos to learn more/i);
-    expect(docsParagraph).toBeInTheDocument();
-  });
-});
+    render(<App />)
+    const docsParagraph = screen.getByText(
+      /Click on the Vite and React logos to learn more/i,
+    )
+    expect(docsParagraph).toBeInTheDocument()
+  })
+})
