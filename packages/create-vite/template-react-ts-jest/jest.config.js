549671d99f4d214b1a134093c71627ee597093e8
ivanzusko <ivan.zusko@gmail.com>
2024-08-24 00:30:15 +0200

    feat: Add react-ts-jest template to create-vite package

---
+++ packages/create-vite/template-react-ts-jest/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+.tsx?$": ["ts-jest",{}],
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+  },
+};
