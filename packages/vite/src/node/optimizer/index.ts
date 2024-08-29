ffef91f5c1bc9069c33032a57af59a4da3300ff1
htmlin <10543563@qq.com>
2024-08-29 16:35:07 +0800

    fix: `esbuildOutputFromId` for symlinked root

--- packages/vite/src/node/optimizer/index.ts
+++ packages/vite/src/node/optimizer/index.ts
@@ -1053,7 +1053,7 @@ function esbuildOutputFromId(
   // If the root dir was symlinked, esbuild could return output keys as `../cwd/`
   // Normalize keys to support this case too
   for (const [key, value] of Object.entries(outputs)) {
-    if (normalizePath(path.relative(cwd, key)) === normalizedOutputPath) {
+    if (normalizePath(path.relative(cwd, key)) === normalizedOutputPath || key.split(path.sep).slice(1).join(path.sep) === normalizedOutputPath.split(path.sep).slice(1).join(path.sep)) {
       return value
     }
   }
