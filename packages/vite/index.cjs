7de22354769d7002ed45a4acc967cef71e9caccf
bluwy <bjornlu.dev@gmail.com>
2024-09-01 22:19:08 +0800

    chore: update

--- packages/vite/index.cjs
+++ packages/vite/index.cjs
@@ -26,11 +26,23 @@ asyncFunctions.forEach((name) => {
 function warnCjsUsage() {
   if (process.env.VITE_CJS_IGNORE_WARNING) return
   const yellow = (str) => `\u001b[33m${str}\u001b[39m`
-  Error.stackTraceLimit = 15
-  const log = process.env.VITE_CJS_TRACE ? console.trace : console.warn
-  log(
+  console.warn(
     yellow(
       `The CJS build of Vite's Node API is deprecated. See https://vitejs.dev/guide/troubleshooting.html#vite-cjs-node-api-deprecated for more details.`,
     ),
   )
+  if (process.env.VITE_CJS_TRACE) {
+    const e = {}
+    const stackTraceLimit = Error.stackTraceLimit
+    Error.stackTraceLimit = 100
+    Error.captureStackTrace(e)
+    Error.stackTraceLimit = stackTraceLimit
+    console.log(
+      e.stack
+        .split('\n')
+        .slice(1)
+        .filter((line) => !line.includes('(node:'))
+        .join('\n'),
+    )
+  }
 }
