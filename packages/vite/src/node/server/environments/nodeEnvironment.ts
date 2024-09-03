44d85aed1591366e06fbaafbe00d8480dad1bbff
Vladimir Sheremet <sleuths.slews0s@icloud.com>
2024-09-03 10:58:24 +0200

    fix: expose `createServerHotChannel` and validate `createNodeDevEnvironment` context

--- packages/vite/src/node/server/environments/nodeEnvironment.ts
+++ packages/vite/src/node/server/environments/nodeEnvironment.ts
@@ -8,6 +8,12 @@ export function createNodeDevEnvironment(
   config: ResolvedConfig,
   context: DevEnvironmentContext,
 ): DevEnvironment {
+  if (context.hot == null) {
+    throw new Error(
+      '`hot` is a required option. Either explicitly opt out of HMR by setting `hot: false` or provide a hot channel.',
+    )
+  }
+
   return new DevEnvironment(name, config, {
     ...context,
     runner: {
