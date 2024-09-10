abf04c3a84f4d9962a6f9697ca26cd639fa76e87
coderwei <65948611+coderwei99@users.noreply.github.com>
2024-09-10 12:09:05 +0800

    fix: ensure req.url matches moduleByEtag URL to avoid incorrect 304 (#17997)

--- packages/vite/src/node/server/middlewares/transform.ts
+++ packages/vite/src/node/server/middlewares/transform.ts
@@ -53,7 +53,10 @@ export function cachedTransformMiddleware(
     const ifNoneMatch = req.headers['if-none-match']
     if (ifNoneMatch) {
       const moduleByEtag = environment.moduleGraph.getModuleByEtag(ifNoneMatch)
-      if (moduleByEtag?.transformResult?.etag === ifNoneMatch) {
+      if (
+        moduleByEtag?.transformResult?.etag === ifNoneMatch &&
+        moduleByEtag?.url === req.url
+      ) {
         // For CSS requests, if the same CSS file is imported in a module,
         // the browser sends the request for the direct CSS request with the etag
         // from the imported CSS module. We ignore the etag in this case.
