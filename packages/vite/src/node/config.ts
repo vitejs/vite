bd92f60a62abfabc261ce9eb30c4af832b2ef3cb
socialuni <socialuni@163.com>
2024-09-20 00:33:02 +0800

    fix(config): vite.config.ts can't import untranspiled ts files from other packages in the same monorepo (#5370)

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -1641,6 +1641,10 @@ async function bundleConfigFile(
                 }
                 throw e
               }
+              // not nod_modules dependency, compile it #5370
+              if (idFsPath && !isInNodeModules(idFsPath)) {
+                return
+              }
               if (idFsPath && isImport) {
                 idFsPath = pathToFileURL(idFsPath).href
               }
