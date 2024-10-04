7fe2d3510ec1be607b9193848c979d110b461a5f
Evan You <evan@vuejs.org>
2024-10-04 16:33:59 +0800

    chore: update license copyright

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -163,8 +163,7 @@ export default defineConfig({
 
     footer: {
       message: `Released under the MIT License. (${commitRef})`,
-      copyright:
-        'Copyright © 2019-present Yuxi (Evan) You & Vite Contributors',
+      copyright: 'Copyright © 2019-present VoidZero Inc. & Vite Contributors',
     },
 
     nav: [
