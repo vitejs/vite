f22ae960f0f170d84c8a1d47fd458cdc552813cc
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-10-04 18:41:22 +0900

    fix: dont crash on ws server error

--- packages/vite/src/node/server/ws.ts
+++ packages/vite/src/node/server/ws.ts
@@ -180,6 +180,19 @@ export function createWebSocketServer(
         wss.emit('connection', ws, req)
       })
     })
+    wsHttpServer.on('error', (e: Error & { code: string }) => {
+      if (e.code === 'EADDRINUSE') {
+        config.logger.error(
+          colors.red(`WebSocket server error: Port is already in use`),
+          { error: e },
+        )
+      } else {
+        config.logger.error(
+          colors.red(`WebSocket server error:\n${e.stack || e.message}`),
+          { error: e },
+        )
+      }
+    })
   }
 
   wss.on('connection', (socket) => {
