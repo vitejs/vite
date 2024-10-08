eccba393ac99ac9baeae56ef68ec52dd7acfbf82
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-08 18:51:49 +0900

    test: test build

--- packages/vite/src/node/__tests__/environment.spec.ts
+++ packages/vite/src/node/__tests__/environment.spec.ts
@@ -1,4 +1,6 @@
+import path from 'node:path'
 import { describe, expect, test } from 'vitest'
+import type { RollupOutput } from 'rollup'
 import { createServer } from '../server'
 import { defineConfig } from '../config'
 import { createBuilder } from '../build'
@@ -10,11 +12,34 @@ describe('custom environment conditions', () => {
       root: import.meta.dirname,
       logLevel: 'error',
       environments: {
+        ssr: {
+          resolve: {
+            noExternal: true,
+          },
+          build: {
+            outDir: path.join(
+              import.meta.dirname,
+              'fixtures/test-dep-conditions/dist/ssr',
+            ),
+            rollupOptions: {
+              input: { index: '@vitejs/test-dep-conditions' },
+            },
+          },
+        },
         worker: {
           webCompatible: true,
           resolve: {
             noExternal: true,
             conditions: ['worker'],
+          },
+          build: {
+            outDir: path.join(
+              import.meta.dirname,
+              'fixtures/test-dep-conditions/dist/worker',
+            ),
+            rollupOptions: {
+              input: { index: '@vitejs/test-dep-conditions' },
+            },
           },
         },
         custom1: {
@@ -23,6 +48,15 @@ describe('custom environment conditions', () => {
             noExternal: true,
             conditions: ['custom1'],
           },
+          build: {
+            outDir: path.join(
+              import.meta.dirname,
+              'fixtures/test-dep-conditions/dist/custom1',
+            ),
+            rollupOptions: {
+              input: { index: '@vitejs/test-dep-conditions' },
+            },
+          },
         },
         custom2: {
           webCompatible: false,
@@ -30,12 +64,30 @@ describe('custom environment conditions', () => {
             noExternal: true,
             conditions: ['custom2'],
           },
+          build: {
+            outDir: path.join(
+              import.meta.dirname,
+              'fixtures/test-dep-conditions/dist/custom2',
+            ),
+            rollupOptions: {
+              input: { index: '@vitejs/test-dep-conditions' },
+            },
+          },
         },
         custom3: {
           webCompatible: false,
           resolve: {
             noExternal: true,
             conditions: ['custom3'],
+          },
+          build: {
+            outDir: path.join(
+              import.meta.dirname,
+              'fixtures/test-dep-conditions/dist/custom3',
+            ),
+            rollupOptions: {
+              input: { index: '@vitejs/test-dep-conditions' },
+            },
           },
         },
       },
@@ -67,7 +119,29 @@ describe('custom environment conditions', () => {
   })
 
   test('build', async () => {
-    const builder = createBuilder(getConfig())
-    console.log(builder)
+    const builder = await createBuilder(getConfig())
+    const results: Record<string, unknown> = {}
+    for (const key of ['ssr', 'worker', 'custom1', 'custom2', 'custom3']) {
+      const output = await builder.build(builder.environments[key])
+      const chunk = (output as RollupOutput).output[0]
+      const mod = await import(
+        path.join(
+          import.meta.dirname,
+          'fixtures/test-dep-conditions/dist',
+          key,
+          chunk.fileName,
+        )
+      )
+      results[key] = mod.default
+    }
+    expect(results).toMatchInlineSnapshot(`
+      {
+        "custom1": "index.custom1.js",
+        "custom2": "index.custom2.js",
+        "custom3": "index.custom3.js",
+        "ssr": "index.default.js",
+        "worker": "index.worker.js",
+      }
+    `)
   })
 })
