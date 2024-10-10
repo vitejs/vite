7f91bb392916e18f7d3bc40994d267ebd9b404fd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-10-11 08:37:29 +0900

    test: ssr exteranl / inline resolve test

---
+++ packages/vite/src/node/__tests__/resolve.spec.ts
@@ -0,0 +1,58 @@
+import { describe, expect, onTestFinished, test } from 'vitest'
+import { createServer } from '../server'
+import { createServerModuleRunner } from '../ssr/runtime/serverModuleRunner'
+
+describe('import and resolveId', () => {
+  async function createTestServer() {
+    const server = await createServer({
+      configFile: false,
+      root: import.meta.dirname,
+      logLevel: 'error',
+      server: {
+        middlewareMode: true,
+      },
+    })
+    onTestFinished(() => server.close())
+    return server
+  }
+
+  test('import first', async () => {
+    const server = await createTestServer()
+    const runner = createServerModuleRunner(server.environments.ssr, {
+      hmr: {
+        logger: false,
+      },
+      sourcemapInterceptor: false,
+    })
+    const mod = await runner.import(
+      '/fixtures/test-dep-conditions-app/entry-with-module',
+    )
+    const resolved = await server.environments.ssr.pluginContainer.resolveId(
+      '@vitejs/test-dep-conditions/with-module',
+    )
+    expect([mod.default, resolved?.id]).toEqual([
+      'dir/index.default.js',
+      expect.stringContaining('dir/index.module.js'),
+    ])
+  })
+
+  test('resolveId first', async () => {
+    const server = await createTestServer()
+    const runner = createServerModuleRunner(server.environments.ssr, {
+      hmr: {
+        logger: false,
+      },
+      sourcemapInterceptor: false,
+    })
+    const resolved = await server.environments.ssr.pluginContainer.resolveId(
+      '@vitejs/test-dep-conditions/with-module',
+    )
+    const mod = await runner.import(
+      '/fixtures/test-dep-conditions-app/entry-with-module',
+    )
+    expect([mod.default, resolved?.id]).toEqual([
+      'dir/index.default.js',
+      expect.stringContaining('dir/index.module.js'),
+    ])
+  })
+})
