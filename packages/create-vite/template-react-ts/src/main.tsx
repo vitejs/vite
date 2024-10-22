291830fa632c756b2a0311142d1f25ca7b56a637
翠 / green <green@sapphi.red>
2024-10-22 10:46:02 +0900

    feat(create-vite): update to svelte 5 (#18407)
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/create-vite/template-react-ts/src/main.tsx
+++ packages/create-vite/template-react-ts/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
