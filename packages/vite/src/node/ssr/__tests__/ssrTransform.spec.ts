3fd1f8b438825410e6705c0b08538b937f1342f5
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-10-24 15:58:29 +0900

    test: fix test conflict

--- packages/vite/src/node/ssr/__tests__/ssrTransform.spec.ts
+++ packages/vite/src/node/ssr/__tests__/ssrTransform.spec.ts
@@ -26,8 +26,8 @@ test('named import', async () => {
       `import { ref } from 'vue';function foo() { return ref(0) }`,
     ),
   ).toMatchInlineSnapshot(`
-    "const __vite_ssr_identity__ = v => v;
-    const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["ref"]});
+    "const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["ref"]});
+    const __vite_ssr_identity__ = v => v;
     function foo() { return __vite_ssr_identity__(__vite_ssr_import_0__.ref)(0) }"
   `)
 })
@@ -38,8 +38,8 @@ test('named import: arbitrary module namespace specifier', async () => {
       `import { "some thing" as ref } from 'vue';function foo() { return ref(0) }`,
     ),
   ).toMatchInlineSnapshot(`
-    "const __vite_ssr_identity__ = v => v;
-    const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["some thing"]});
+    "const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["some thing"]});
+    const __vite_ssr_identity__ = v => v;
     function foo() { return __vite_ssr_identity__(__vite_ssr_import_0__["some thing"])(0) }"
   `)
 })
@@ -223,8 +223,8 @@ test('do not rewrite method definition', async () => {
     `import { fn } from 'vue';class A { fn() { fn() } }`,
   )
   expect(result?.code).toMatchInlineSnapshot(`
-    "const __vite_ssr_identity__ = v => v;
-    const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["fn"]});
+    "const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["fn"]});
+    const __vite_ssr_identity__ = v => v;
     class A { fn() { __vite_ssr_identity__(__vite_ssr_import_0__.fn)() } }"
   `)
   expect(result?.deps).toEqual(['vue'])
@@ -451,24 +451,25 @@ test('sourcemap is correct for hoisted imports', async () => {
   const result = (await ssrTransform(code, null, 'input.js', code))!
 
   expect(result.code).toMatchInlineSnapshot(`
-    "const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["foo"]});
+    "const __vite_ssr_identity__ = v => v;
+    const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["foo"]});
     const __vite_ssr_import_1__ = await __vite_ssr_import__("vue2", {"importedNames":["bar"]});
 
 
 
-    console.log(__vite_ssr_import_0__.foo, __vite_ssr_import_1__.bar);
+    console.log(__vite_ssr_identity__(__vite_ssr_import_0__.foo), __vite_ssr_identity__(__vite_ssr_import_1__.bar));
 
     "
   `)
 
   const traceMap = new TraceMap(result.map as any)
-  expect(originalPositionFor(traceMap, { line: 1, column: 0 })).toStrictEqual({
+  expect(originalPositionFor(traceMap, { line: 2, column: 0 })).toStrictEqual({
     source: 'input.js',
     line: 5,
     column: 0,
     name: null,
   })
-  expect(originalPositionFor(traceMap, { line: 2, column: 0 })).toStrictEqual({
+  expect(originalPositionFor(traceMap, { line: 3, column: 0 })).toStrictEqual({
     source: 'input.js',
     line: 6,
     column: 0,
@@ -535,8 +536,8 @@ test('overwrite bindings', async () => {
         `function g() { const f = () => { const inject = true }; console.log(inject) }\n`,
     ),
   ).toMatchInlineSnapshot(`
-    "const __vite_ssr_identity__ = v => v;
-    const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["inject"]});
+    "const __vite_ssr_import_0__ = await __vite_ssr_import__("vue", {"importedNames":["inject"]});
+    const __vite_ssr_identity__ = v => v;
     const a = { inject: __vite_ssr_import_0__.inject }
     const b = { test: __vite_ssr_import_0__.inject }
     function c() { const { test: inject } = { test: true }; console.log(inject) }
@@ -861,8 +862,8 @@ test('jsx', async () => {
   const result = await transformWithEsbuild(code, id)
   expect(await ssrTransformSimpleCode(result.code, '/foo.jsx'))
     .toMatchInlineSnapshot(`
-      "const __vite_ssr_identity__ = v => v;
-      const __vite_ssr_import_0__ = await __vite_ssr_import__("react", {"importedNames":["default"]});
+      "const __vite_ssr_import_0__ = await __vite_ssr_import__("react", {"importedNames":["default"]});
+      const __vite_ssr_identity__ = v => v;
       const __vite_ssr_import_1__ = await __vite_ssr_import__("foo", {"importedNames":["Foo","Slot"]});
 
 
@@ -954,8 +955,8 @@ foo()`,
     ),
   ).toMatchInlineSnapshot(`
     "#!/usr/bin/env node
-    const __vite_ssr_identity__ = v => v;
     const __vite_ssr_import_0__ = await __vite_ssr_import__("foo", {"importedNames":["foo"]});
+    const __vite_ssr_identity__ = v => v;
 
     __vite_ssr_identity__(__vite_ssr_import_0__.foo)()"
   `)
@@ -991,8 +992,8 @@ export class Test {
 };`.trim()
 
   expect(await ssrTransformSimpleCode(code)).toMatchInlineSnapshot(`
-    "const __vite_ssr_identity__ = v => v;
-    const __vite_ssr_import_0__ = await __vite_ssr_import__("foobar", {"importedNames":["foo","bar"]});
+    "const __vite_ssr_import_0__ = await __vite_ssr_import__("foobar", {"importedNames":["foo","bar"]});
+    const __vite_ssr_identity__ = v => v;
 
     if (false) {
       const foo = 'foo'
