6f825d94d325c474d75088a43aa90d6891ca8c6d
Jonas Kuske <mail@jonaskuske.com>
2024-10-25 07:15:43 +0200

    feat: read `sec-fetch-dest` header to detect JS in transform (#9981)
    Co-authored-by: bluwy <bjornlu.dev@gmail.com>

--- packages/vite/src/node/server/middlewares/transform.ts
+++ packages/vite/src/node/server/middlewares/transform.ts
@@ -175,6 +175,7 @@ export function transformMiddleware(
       }
 
       if (
+        req.headers['sec-fetch-dest'] === 'script' ||
         isJSRequest(url) ||
         isImportRequest(url) ||
         isCSSRequest(url) ||
