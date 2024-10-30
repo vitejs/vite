472ab9aab14fa0cdce070eb502c44b83e76e114f
bluwy <bjornlu.dev@gmail.com>
2024-10-30 23:29:39 +0800

    add test

--- packages/vite/src/node/assetSource.ts
+++ packages/vite/src/node/assetSource.ts
@@ -18,60 +18,7 @@ interface HtmlAssetSourceFilterData {
   attributes: Record<string, string>
 }
 
-interface HtmlAssetAttribute {
-  type: 'src' | 'srcset' | 'remove'
-  key: string
-  value: string
-  attributes: Record<string, string>
-  location: Token.Location
-}
-
-/**
- * Given a HTML node, find all attributes that references an asset to be processed
- */
-export function getNodeAssetAttributes(
-  node: DefaultTreeAdapterMap['element'],
-): HtmlAssetAttribute[] {
-  const matched = DEFAULT_HTML_ASSET_SOURCES[node.nodeName]
-  if (!matched) return []
-
-  const attributes: Record<string, string> = {}
-  for (const attr of node.attrs) {
-    attributes[getAttrKey(attr)] = attr.value
-  }
-
-  // If the node has a `vite-ignore` attribute, remove the attribute and early out
-  // to skip processing any attributes
-  if ('vite-ignore' in attributes) {
-    return [
-      {
-        type: 'remove',
-        key: 'vite-ignore',
-        value: '',
-        attributes,
-        location: node.sourceCodeLocation!.attrs!['vite-ignore'],
-      },
-    ]
-  }
-
-  const actions: HtmlAssetAttribute[] = []
-  function handleAttributeKey(key: string, type: 'src' | 'srcset') {
-    const value = attributes[key]
-    if (!value) return
-    if (matched.filter && !matched.filter({ key, value, attributes })) return
-    const location = node.sourceCodeLocation!.attrs![key]
-    actions.push({ type, key, value, attributes, location })
-  }
-  matched.srcAttributes?.forEach((key) => handleAttributeKey(key, 'src'))
-  matched.srcsetAttributes?.forEach((key) => handleAttributeKey(key, 'srcset'))
-  return actions
-}
-
-function getAttrKey(attr: Token.Attribute): string {
-  return attr.prefix === undefined ? attr.name : `${attr.prefix}:${attr.name}`
-}
-
-const ALLOWED_REL = [
+const ALLOWED_LINK_REL = [
   'stylesheet',
   'icon',
   'shortcut icon',
@@ -84,7 +31,7 @@ const ALLOWED_REL = [
   'preload',
 ]
 
-const ALLOWED_ITEMPROP = [
+const ALLOWED_LINK_OR_META_ITEMPROP = [
   'image',
   'logo',
   'screenshot',
@@ -157,7 +104,7 @@ const DEFAULT_HTML_ASSET_SOURCES: Record<string, HtmlAssetSource> = {
     filter({ key, attributes }) {
       if (
         attributes.rel &&
-        ALLOWED_REL.includes(attributes.rel.trim().toLowerCase())
+        ALLOWED_LINK_REL.includes(attributes.rel.trim().toLowerCase())
       ) {
         return true
       }
@@ -165,7 +112,9 @@ const DEFAULT_HTML_ASSET_SOURCES: Record<string, HtmlAssetSource> = {
       if (
         key === 'href' &&
         attributes.itemprop &&
-        ALLOWED_ITEMPROP.includes(attributes.itemprop.trim().toLowerCase())
+        ALLOWED_LINK_OR_META_ITEMPROP.includes(
+          attributes.itemprop.trim().toLowerCase(),
+        )
       ) {
         return true
       }
@@ -195,7 +144,9 @@ const DEFAULT_HTML_ASSET_SOURCES: Record<string, HtmlAssetSource> = {
       if (
         key === 'content' &&
         attributes.itemprop &&
-        ALLOWED_ITEMPROP.includes(attributes.itemprop.trim().toLowerCase())
+        ALLOWED_LINK_OR_META_ITEMPROP.includes(
+          attributes.itemprop.trim().toLowerCase(),
+        )
       ) {
         return true
       }
@@ -203,4 +154,57 @@ const DEFAULT_HTML_ASSET_SOURCES: Record<string, HtmlAssetSource> = {
       return false
     },
   },
+}
+
+interface HtmlAssetAttribute {
+  type: 'src' | 'srcset' | 'remove'
+  key: string
+  value: string
+  attributes: Record<string, string>
+  location: Token.Location
+}
+
+/**
+ * Given a HTML node, find all attributes that references an asset to be processed
+ */
+export function getNodeAssetAttributes(
+  node: DefaultTreeAdapterMap['element'],
+): HtmlAssetAttribute[] {
+  const matched = DEFAULT_HTML_ASSET_SOURCES[node.nodeName]
+  if (!matched) return []
+
+  const attributes: Record<string, string> = {}
+  for (const attr of node.attrs) {
+    attributes[getAttrKey(attr)] = attr.value
+  }
+
+  // If the node has a `vite-ignore` attribute, remove the attribute and early out
+  // to skip processing any attributes
+  if ('vite-ignore' in attributes) {
+    return [
+      {
+        type: 'remove',
+        key: 'vite-ignore',
+        value: '',
+        attributes,
+        location: node.sourceCodeLocation!.attrs!['vite-ignore'],
+      },
+    ]
+  }
+
+  const actions: HtmlAssetAttribute[] = []
+  function handleAttributeKey(key: string, type: 'src' | 'srcset') {
+    const value = attributes[key]
+    if (!value) return
+    if (matched.filter && !matched.filter({ key, value, attributes })) return
+    const location = node.sourceCodeLocation!.attrs![key]
+    actions.push({ type, key, value, attributes, location })
+  }
+  matched.srcAttributes?.forEach((key) => handleAttributeKey(key, 'src'))
+  matched.srcsetAttributes?.forEach((key) => handleAttributeKey(key, 'srcset'))
+  return actions
+}
+
+function getAttrKey(attr: Token.Attribute): string {
+  return attr.prefix === undefined ? attr.name : `${attr.prefix}:${attr.name}`
 }
