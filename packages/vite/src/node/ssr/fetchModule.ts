92c26585ca39b1681966bca134204016ad72932b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-10-30 20:34:28 +0900

    fix: remove DEV_PROD_CONDITION from `fetchModule`

--- packages/vite/src/node/ssr/fetchModule.ts
+++ packages/vite/src/node/ssr/fetchModule.ts
@@ -3,7 +3,6 @@ import type { FetchResult } from 'vite/module-runner'
 import type { EnvironmentModuleNode, TransformResult } from '..'
 import { tryNodeResolve } from '../plugins/resolve'
 import { isBuiltin, isExternalUrl, isFilePathESM } from '../utils'
-import { DEV_PROD_CONDITION } from '../constants'
 import { unwrapId } from '../../shared/utils'
 import {
   MODULE_RUNNER_SOURCEMAPPING_SOURCE,
@@ -47,7 +46,7 @@ export async function fetchModule(
 
     const resolved = tryNodeResolve(url, importer, {
       mainFields: ['main'],
-      conditions: [...externalConditions, DEV_PROD_CONDITION],
+      conditions: externalConditions,
       externalConditions,
       external: [],
       noExternal: [],
