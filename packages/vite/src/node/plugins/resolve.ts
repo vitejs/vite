43ab93b54158b08b1046ec34e9e1b0df0914a882
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-06 15:00:38 +0900

    refactor(resolve): remove `environmentsOptions` parameter

--- packages/vite/src/node/plugins/resolve.ts
+++ packages/vite/src/node/plugins/resolve.ts
@@ -33,7 +33,6 @@ import {
   safeRealpathSync,
   tryStatSync,
 } from '../utils'
-import type { ResolvedEnvironmentOptions } from '../config'
 import { optimizedDepInfoFromFile, optimizedDepInfoFromId } from '../optimizer'
 import type { DepsOptimizer } from '../optimizer'
 import type { SSROptions } from '..'
@@ -181,13 +180,6 @@ export interface ResolvePluginOptionsWithOverrides
 
 export function resolvePlugin(
   resolveOptions: ResolvePluginOptionsWithOverrides,
-  /**
-   * @internal
-   * config.createResolver creates a pluginContainer before environments are created.
-   * The resolve plugin is especial as it works without environments to enable this use case.
-   * It only needs access to the resolve options.
-   */
-  environmentsOptions?: Record<string, ResolvedEnvironmentOptions>,
 ): Plugin {
   const { root, isProduction, asSrc, preferRelative = false } = resolveOptions
 
@@ -228,9 +220,8 @@ export function resolvePlugin(
       const isRequire: boolean =
         resolveOpts?.custom?.['node-resolve']?.isRequire ?? false
 
-      const environmentName = this.environment.name ?? (ssr ? 'ssr' : 'client')
-      const currentEnvironmentOptions =
-        this.environment.config || environmentsOptions?.[environmentName]
+      const environmentName = this.environment.name
+      const currentEnvironmentOptions = this.environment.config
       const environmentResolveOptions = currentEnvironmentOptions?.resolve
       if (!environmentResolveOptions) {
         throw new Error(
