ceb4a8695662e78eb7d5de8324bb617d8b87459b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-07 12:31:08 +0900

    chore(deps): update transitive deps

--- playground/external/vite.config.js
+++ playground/external/vite.config.js
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises'
 import { defineConfig } from 'vite'
 
 const npmDirectServeConfig = {
-  '/vue@3.2.0.js': 'vue32/dist/vue.runtime.esm-browser.js',
+  '/vue@3.2.47.js': 'vue32/dist/vue.runtime.esm-browser.js',
   '/slash@5.js': 'slash5/index.js',
 }
 /** @type {import('vite').Connect.NextHandleFunction} */
