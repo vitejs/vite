d7c3ac30ac7e6106578236d26e7f47e6b4edbc77
翠 / green <green@sapphi.red>
2024-11-11 20:31:34 +0900

    test: run css sourcemap playground for legacy sass (#18636)

---
+++ playground/css/vite.config-sass-legacy.js
@@ -0,0 +1,33 @@
+import path from 'node:path'
+import { pathToFileURL } from 'node:url'
+import { defineConfig } from 'vite'
+import baseConfig from './vite.config.js'
+
+export default defineConfig({
+  ...baseConfig,
+  css: {
+    ...baseConfig.css,
+    preprocessorOptions: {
+      ...baseConfig.css.preprocessorOptions,
+      scss: {
+        api: 'legacy',
+        importer: [
+          function (url) {
+            return url === 'virtual-dep' ? { contents: '' } : null
+          },
+          function (url) {
+            return url === 'virtual-file-absolute'
+              ? {
+                  contents: `@use "${pathToFileURL(path.join(import.meta.dirname, 'file-absolute.scss')).href}"`,
+                }
+              : null
+          },
+          function (url) {
+            return url.endsWith('.wxss') ? { contents: '' } : null
+          },
+        ],
+        silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
+})
