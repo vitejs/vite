670779bbdccfd6111ac56c30b47f9e7d13132e2d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2024-11-18 18:17:17 +0900

    chore: cleanup

--- playground/json/server.js
+++ playground/json/server.js
@@ -4,8 +4,6 @@ import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import express from 'express'
 
-// TODO: this can be probably unit test?
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const isTest = process.env.VITEST
 
