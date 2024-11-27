979960299f1215c944e1945cd5abe1c36cda5069
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-27 21:07:29 +0900

    docs: show old document warning

---
+++ docs/.vitepress/theme/components/OldDocument.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="old-document">
+    <p>
+      This documentation describes <b>the old</b> Vite version. For the latest
+      version, see
+      <a href="https://vite.dev/" class="new-document-link"
+        >the latest document</a
+      >.
+    </p>
+  </div>
+</template>
+
+<style>
+:root {
+  --vp-layout-top-height: 96px;
+  @media (min-width: 455px) {
+    --vp-layout-top-height: 64px;
+  }
+  @media (min-width: 960px) {
+    --vp-layout-top-height: 32px;
+  }
+}
+
+.old-document {
+  position: fixed;
+  display: flex;
+  height: var(--vp-layout-top-height);
+  width: 100%;
+  padding: 4px 32px;
+  justify-content: center;
+  align-items: center;
+  color: var(--vp-c-text-1);
+  background: var(--vp-c-danger-3);
+  z-index: var(--vp-z-index-nav);
+
+  .new-document-link {
+    text-decoration: underline;
+    color: var(--vp-c-text-1);
+    &:hover {
+      color: var(--vp-c-text-2);
+    }
+  }
+}
+</style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,12 +7,14 @@ import './styles/vars.css'
 import './styles/landing.css'
 import AsideSponsors from './components/AsideSponsors.vue'
 import SvgImage from './components/SvgImage.vue'
+import OldDocument from './components/OldDocument.vue'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'aside-ads-before': () => h(AsideSponsors),
+      'layout-top': () => h(OldDocument),
     })
   },
   enhanceApp({ app }) {
