ae6895869157e48b32088f0a1f85d2fddb2d713f
翠 / green <green@sapphi.red>
2024-11-28 22:06:18 +0900

    fix(css): referencing aliased svg asset with lightningcss enabled errored (#18819)

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -3195,7 +3195,6 @@ async function compileLightningCSS(
           css = css.replace(dep.placeholder, () => dep.url)
           break
         }
-        deps.add(dep.url)
         if (urlReplacer) {
           const replaceUrl = await urlReplacer(
             dep.url,
