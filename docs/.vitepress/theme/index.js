1bbbc831e4155c12069ac242ce0bd1079bd57b5c
翠 / green <green@sapphi.red>
2024-11-28 21:54:56 +0900

    docs: show old document warning (#18805)
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,14 @@
 import Theme from 'vitepress/theme'
 import { h } from 'vue'
 import SponsorsSidebar from './SponsorsSidebar.vue'
+import OldDocument from './components/OldDocument.vue'
 import './custom.css'
 
 export default {
   ...Theme,
   Layout() {
     return h(Theme.Layout, null, {
+      'layout-top': () => h(OldDocument),
       'sidebar-bottom': () =>
         h('div', { class: 'sponsors sidebar' }, [h(SponsorsSidebar)])
     })
