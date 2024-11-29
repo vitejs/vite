0114c871a0b3f946267f878f166fded7e430e68a
翠 / green <green@sapphi.red>
2024-11-29 13:11:44 +0900

    docs: v3 show old document warning (#18805) (#18830)
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,11 +4,13 @@ import './styles/vars.css'
 import HomeSponsors from './components/HomeSponsors.vue'
 import AsideSponsors from './components/AsideSponsors.vue'
 import SvgImage from './components/SvgImage.vue'
+import OldDocument from './components/OldDocument.vue'
 
 export default {
   ...Theme,
   Layout() {
     return h(Theme.Layout, null, {
+      'layout-top': () => h(OldDocument),
       'home-features-after': () => h(HomeSponsors),
       'aside-ads-before': () => h(AsideSponsors)
     })
