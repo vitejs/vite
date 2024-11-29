a821dfecbe9fe59f9981dcdf54d5e9689817bd6c
翠 / green <green@sapphi.red>
2024-11-29 13:35:14 +0900

    docs: v2 show old document warning (#18805) (#18831)
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

---
+++ docs/.vitepress/theme/components/OldDocument.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="old-document">
+    <p>
+      This documentation covers Vite 2 <strong>(old version)</strong>. For the
+      latest version, see
+      <a href="https://vite.dev" class="new-document-link">https://vite.dev</a>.
+    </p>
+  </div>
+</template>
+
+<style>
+.old-document {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  height: var(--vp-layout-top-height);
+  width: 100%;
+  padding: 4px 32px;
+  justify-content: center;
+  align-items: center;
+  color: var(--c-text);
+  background: var(--c-brand-lightest);
+  z-index: calc(var(--z-index-navbar) + 1);
+
+  .new-document-link {
+    text-decoration: underline;
+    color: var(--c-text);
+    &:hover {
+      color: var(--c-text-light);
+    }
+  }
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,13 @@
 import Theme from 'vitepress/theme'
 import { h } from 'vue'
 import SponsorsSidebar from './SponsorsSidebar.vue'
+import WrappedLayout from './components/WrappedLayout.vue'
 import './custom.css'
 
 export default {
   ...Theme,
   Layout() {
-    return h(Theme.Layout, null, {
+    return h(WrappedLayout, null, {
       'sidebar-bottom': () =>
         h('div', { class: 'sponsors sidebar' }, [h(SponsorsSidebar)])
     })
