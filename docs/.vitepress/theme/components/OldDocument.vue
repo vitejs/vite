d2047e83f215ec3760085ac00bb0109f018bfabc
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-11-29 13:29:48 +0900

    wip: try

--- docs/.vitepress/theme/components/OldDocument.vue
+++ docs/.vitepress/theme/components/OldDocument.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="old-document">
     <p>
-      This documentation covers Vite 5 <strong>(old version)</strong>. For the
+      This documentation covers Vite 2 <strong>(old version)</strong>. For the
       latest version, see
       <a href="https://vite.dev" class="new-document-link">https://vite.dev</a>.
     </p>
@@ -19,21 +19,16 @@
   padding: 4px 32px;
   justify-content: center;
   align-items: center;
-  color: var(--vp-c-text-1);
-  background: var(--vp-c-brand-lightest);
-  z-index: var(--vp-z-index-layout-top);
+  color: var(--c-text);
+  background: var(--c-brand-lightest);
+  z-index: calc(var(--z-index-navbar) + 1);
 
   .new-document-link {
     text-decoration: underline;
-    color: var(--vp-c-text-1);
+    color: var(--c-text);
     &:hover {
-      color: var(--vp-c-text-2);
+      color: var(--c-text-light);
     }
-  }
-}
-.dark {
-  .old-document {
-    background: var(--vp-c-brand-darker);
   }
 }
 </style>
