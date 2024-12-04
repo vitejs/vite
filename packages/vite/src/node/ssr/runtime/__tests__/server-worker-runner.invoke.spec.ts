0a1db74ff94e9eb120b0dd453647d01fb09151ec
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-12-04 12:54:45 +0900

    test: remove unused transport

--- packages/vite/src/node/ssr/runtime/__tests__/server-worker-runner.invoke.spec.ts
+++ packages/vite/src/node/ssr/runtime/__tests__/server-worker-runner.invoke.spec.ts
@@ -1,45 +1,9 @@
 import { BroadcastChannel, Worker } from 'node:worker_threads'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
-import type { HotChannel, HotChannelListener, HotPayload } from 'vite'
 import type { BirpcReturn } from 'birpc'
 import { createBirpc } from 'birpc'
 import { DevEnvironment } from '../../..'
 import { type ViteDevServer, createServer } from '../../../server'
-
-const createWorkerTransport = (w: Worker): HotChannel => {
-  const handlerToWorkerListener = new WeakMap<
-    HotChannelListener,
-    (value: HotPayload) => void
-  >()
-
-  return {
-    send: (data) => w.postMessage(data),
-    on: (event: string, handler: HotChannelListener) => {
-      if (event === 'connection') return
-
-      const listener = (value: HotPayload) => {
-        if (value.type === 'custom' && value.event === event) {
-          const client = {
-            send(payload: HotPayload) {
-              w.postMessage(payload)
-            },
-          }
-          handler(value.data, client)
-        }
-      }
-      handlerToWorkerListener.set(handler, listener)
-      w.on('message', listener)
-    },
-    off: (event, handler: HotChannelListener) => {
-      if (event === 'connection') return
-      const listener = handlerToWorkerListener.get(handler)
-      if (listener) {
-        w.off('message', listener)
-        handlerToWorkerListener.delete(handler)
-      }
-    },
-  }
-}
 
 describe('running module runner inside a worker and using the ModuleRunnerTransport#invoke API', () => {
   let worker: Worker
@@ -77,7 +41,6 @@ describe('running module runner inside a worker and using the ModuleRunnerTransp
             createEnvironment: (name, config) => {
               return new DevEnvironment(name, config, {
                 hot: false,
-                transport: createWorkerTransport(worker),
               })
             },
           },
