c09be646ce768a9ae11a08998fb930770e50e9ef
翠 / green <green@sapphi.red>
2024-12-06 13:11:29 +0900

    test: use `readDepOptimizationMetadata` (#18900)

--- playground/environment-react-ssr/__tests__/environment-react-ssr.spec.ts
+++ playground/environment-react-ssr/__tests__/environment-react-ssr.spec.ts
@@ -2,10 +2,10 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { stripVTControlCharacters } from 'node:util'
 import { describe, expect, onTestFinished, test } from 'vitest'
-import type { DepOptimizationMetadata } from 'vite'
 import {
   isBuild,
   page,
+  readDepOptimizationMetadata,
   readFile,
   serverLogs,
   testDir,
@@ -21,8 +21,7 @@ test('basic', async () => {
 
 describe.runIf(!isBuild)('pre-bundling', () => {
   test('client', async () => {
-    const meta = await readFile('node_modules/.vite/deps/_metadata.json')
-    const metaJson: DepOptimizationMetadata = JSON.parse(meta)
+    const metaJson = readDepOptimizationMetadata()
 
     expect(metaJson.optimized['react']).toBeTruthy()
     expect(metaJson.optimized['react-dom/client']).toBeTruthy()
@@ -32,8 +31,7 @@ describe.runIf(!isBuild)('pre-bundling', () => {
   })
 
   test('ssr', async () => {
-    const meta = await readFile('node_modules/.vite/deps_ssr/_metadata.json')
-    const metaJson: DepOptimizationMetadata = JSON.parse(meta)
+    const metaJson = readDepOptimizationMetadata('ssr')
 
     expect(metaJson.optimized['react']).toBeTruthy()
     expect(metaJson.optimized['react-dom/server']).toBeTruthy()
@@ -45,17 +43,12 @@ describe.runIf(!isBuild)('pre-bundling', () => {
   test('deps reload', async () => {
     const envs = ['client', 'server'] as const
 
-    const getMeta = (env: (typeof envs)[number]): DepOptimizationMetadata => {
-      const meta = readFile(
-        `node_modules/.vite/deps${env === 'client' ? '' : '_ssr'}/_metadata.json`,
-      )
-      return JSON.parse(meta)
-    }
-
-    expect(getMeta('client').optimized['react-fake-client']).toBeFalsy()
-    expect(getMeta('client').optimized['react-fake-server']).toBeFalsy()
-    expect(getMeta('server').optimized['react-fake-server']).toBeFalsy()
-    expect(getMeta('server').optimized['react-fake-client']).toBeFalsy()
+    const clientMeta = readDepOptimizationMetadata('client')
+    const ssrMeta = readDepOptimizationMetadata('ssr')
+    expect(clientMeta.optimized['react-fake-client']).toBeFalsy()
+    expect(clientMeta.optimized['react-fake-server']).toBeFalsy()
+    expect(ssrMeta.optimized['react-fake-server']).toBeFalsy()
+    expect(ssrMeta.optimized['react-fake-client']).toBeFalsy()
 
     envs.forEach((env) => {
       const filePath = path.resolve(testDir, `src/entry-${env}.tsx`)
@@ -84,9 +77,11 @@ describe.runIf(!isBuild)('pre-bundling', () => {
       'react-fake-server, react-fake-client',
     )
 
-    expect(getMeta('client').optimized['react-fake-client']).toBeTruthy()
-    expect(getMeta('client').optimized['react-fake-server']).toBeFalsy()
-    expect(getMeta('server').optimized['react-fake-server']).toBeTruthy()
-    expect(getMeta('server').optimized['react-fake-client']).toBeFalsy()
+    const clientMetaNew = readDepOptimizationMetadata('client')
+    const ssrMetaNew = readDepOptimizationMetadata('ssr')
+    expect(clientMetaNew.optimized['react-fake-client']).toBeTruthy()
+    expect(clientMetaNew.optimized['react-fake-server']).toBeFalsy()
+    expect(ssrMetaNew.optimized['react-fake-server']).toBeTruthy()
+    expect(ssrMetaNew.optimized['react-fake-client']).toBeFalsy()
   })
 })
