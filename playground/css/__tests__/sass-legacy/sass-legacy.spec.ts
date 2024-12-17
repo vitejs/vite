f8b9ea33d5d50adc9246370d131b605057ae4aa8
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-12-17 12:10:18 +0900

    chore: add sass-legacy to the "dedicated copy" list

--- playground/css/__tests__/sass-legacy/sass-legacy.spec.ts
+++ playground/css/__tests__/sass-legacy/sass-legacy.spec.ts
@@ -1,3 +1,4 @@
+// NOTE: a separate directory from `playground/css` is created by playground/vitestGlobalSetup.ts
 import { sassModuleTests, sassOtherTests, sassTest } from '../sass-tests'
 
 sassTest()
