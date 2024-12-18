392f6808b42cd412ad949a2e87914ce5b2f2f389
翠 / green <green@sapphi.red>
2024-12-18 18:47:31 +0900

    test: fix "excluded dep in included dep" test (#18999)

--- playground/nested-deps/__tests__/nested-deps.spec.ts
+++ playground/nested-deps/__tests__/nested-deps.spec.ts
@@ -10,8 +10,7 @@ test('handle nested package', async () => {
   expect(await page.textContent('.side-c')).toBe(c)
   expect(await page.textContent('.d')).toBe('D@1.0.0')
   expect(await page.textContent('.nested-d')).toBe('D-nested@1.0.0')
-  // TODO: Review if the test is correct
-  // expect(await page.textContent('.nested-e')).toBe('1')
+  expect(await page.textContent('.nested-e')).toBe('1')
 
   expect(await page.textContent('.absolute-f')).toBe('F@2.0.0')
   expect(await page.textContent('.self-referencing')).toBe('true')
