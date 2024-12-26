d64f37bf60440b910cb30eef14be36d1ca6897fe
Jamie <5964236+jamsinclair@users.noreply.github.com>
2024-12-26 11:05:17 +0000

    fix(worker): correctly convert resolved files to unix paths

--- packages/vite/src/node/plugins/workerImportMetaUrl.ts
+++ packages/vite/src/node/plugins/workerImportMetaUrl.ts
@@ -232,7 +232,7 @@ export function workerImportMetaUrlPlugin(config: ResolvedConfig): Plugin {
           let file: string | undefined
           if (url[0] === '.') {
             file = path.resolve(path.dirname(id), url)
-            file = tryFsResolve(file, fsResolveOptions) ?? file
+            file = slash(tryFsResolve(file, fsResolveOptions) ?? file)
           } else {
             workerResolver ??= createBackCompatIdResolver(config, {
               extensions: [],
