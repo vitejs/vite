fdab7ee1af88a1adf79ca87e31e138d4d3fe0135
Bjorn Lu <bjornlu.dev@gmail.com>
2025-01-14 17:38:57 +0800

    fix: use shared sigterm callback (#19203)

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -1535,20 +1535,34 @@ export function partialEncodeURIPath(uri: string): string {
   return filePath.replaceAll('%', '%25') + postfix
 }
 
+type SigtermCallback = (signal?: 'SIGTERM', exitCode?: number) => Promise<void>
+
+// Use a shared callback when attaching sigterm listeners to avoid `MaxListenersExceededWarning`
+const sigtermCallbacks = new Set<SigtermCallback>()
+const parentSigtermCallback: SigtermCallback = async (signal, exitCode) => {
+  await Promise.all([...sigtermCallbacks].map((cb) => cb(signal, exitCode)))
+}
+
 export const setupSIGTERMListener = (
   callback: (signal?: 'SIGTERM', exitCode?: number) => Promise<void>,
 ): void => {
-  process.once('SIGTERM', callback)
-  if (process.env.CI !== 'true') {
-    process.stdin.on('end', callback)
+  if (sigtermCallbacks.size === 0) {
+    process.once('SIGTERM', parentSigtermCallback)
+    if (process.env.CI !== 'true') {
+      process.stdin.on('end', parentSigtermCallback)
+    }
   }
+  sigtermCallbacks.add(callback)
 }
 
 export const teardownSIGTERMListener = (
   callback: Parameters<typeof setupSIGTERMListener>[0],
 ): void => {
-  process.off('SIGTERM', callback)
-  if (process.env.CI !== 'true') {
-    process.stdin.off('end', callback)
+  sigtermCallbacks.delete(callback)
+  if (sigtermCallbacks.size === 0) {
+    process.off('SIGTERM', parentSigtermCallback)
+    if (process.env.CI !== 'true') {
+      process.stdin.off('end', parentSigtermCallback)
+    }
   }
 }
