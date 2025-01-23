8eb182a5edcb2e17e9b0f31fe10826e04ad6bcfe
dalaoshu <laipichan@qq.com>
2025-01-23 20:50:27 +0800

    feat: support async for proxy.bypass (#18940)
    Co-authored-by: 翠 / green <green@sapphi.red>

--- playground/proxy-bypass/vite.config.js
+++ playground/proxy-bypass/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 
+const timeout = (ms) => new Promise((r) => setTimeout(r, ms))
+
 export default defineConfig({
   server: {
     port: 9606,
@@ -8,6 +10,22 @@ export default defineConfig({
         target: 'http://localhost:9607',
         bypass: () => {
           return false
+        },
+      },
+      '/asyncResponse': {
+        bypass: async (_, res) => {
+          await timeout(4)
+          res.writeHead(200, {
+            'Content-Type': 'text/plain',
+          })
+          res.end('Hello after 4 ms (async timeout)')
+          return '/asyncResponse'
+        },
+      },
+      '/asyncThrowingError': {
+        bypass: async () => {
+          await timeout(4)
+          throw new Error('bypass error')
         },
       },
     },
