5cc6279a435aab8768e6fe1ebc7386892207caf3
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-02-03 11:47:16 +0900

    test: use tailwind vite plugin in backend-integration playground

--- playground/backend-integration/postcss.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  plugins: {
-    // using postcss tailwind as aliases does not work with vite plugin one
-    // https://github.com/tailwindlabs/tailwindcss/issues/16039
-    '@tailwindcss/postcss': {},
-  },
-}
