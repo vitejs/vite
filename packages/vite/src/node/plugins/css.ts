02d1e07697e52f0672a34005fc8f0e50f465a7f7
Azat S. <to@azat.io>
2025-02-21 17:29:02 +0300

    fix(css): stabilize css module hashes with lightningcss in dev mode

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -3297,7 +3297,8 @@ async function compileLightningCSS(
   const deps = new Set<string>()
   // replace null byte as lightningcss treats that as a string terminator
   // https://github.com/parcel-bundler/lightningcss/issues/874
-  const filename = id.replace('\0', NULL_BYTE_PLACEHOLDER)
+  const normalizedId = id.includes('?') ? id.split('?')[0] : id
+  const filename = normalizedId.replace('\0', NULL_BYTE_PLACEHOLDER)
 
   let res: LightningCssTransformAttributeResult | LightningCssTransformResult
   try {
