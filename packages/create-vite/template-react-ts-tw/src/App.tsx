c998a9dce04f54b2524a873581d1ad1682493906
Hassan Rahimi <rahiminia.ai@gmail.com>
2025-02-21 15:18:04 +0330

    refactor: change styles from css to tailwind classes: - add tailwind icon - remove app.css and add styles to app.tsx directly - move #root div styles to index.css

--- packages/create-vite/template-react-ts-tw/src/App.tsx
+++ packages/create-vite/template-react-ts-tw/src/App.tsx
@@ -1,32 +1,43 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import tailwindLogo from '/tailwind.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='flex justify-between'>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="h-24 p-6 hover:drop-shadow-[0_0_2em_#646cffaa] transition-all" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="h-24 p-6 hover:drop-shadow-[0_0_2em_#61dafbaa] transition-all motion-safe:animate-[spin_20s_linear_infinite]"
+            alt="React logo"
+          />
+        </a>
+        <a href="https://tailwindcss.com/" target="_blank">
+          <img
+            src={tailwindLogo}
+            className="h-24 p-6 hover:drop-shadow-[0_0_2em_#61dafbaa] transition-all"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>Vite + React + Tailwind</h1>
+      <div className="p-8">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='pt-2'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="text-gray-500">
+        Click on the Vite, React and Tailwindcss logos to learn more
       </p>
     </>
   )
