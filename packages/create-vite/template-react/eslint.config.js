61e1ef0a8bae382e2f248791842b9ed5ca02163a
bluwy <bjornlu.dev@gmail.com>
2025-02-26 02:22:59 +0800

    chore: config no-unused-vars

--- packages/create-vite/template-react/eslint.config.js
+++ packages/create-vite/template-react/eslint.config.js
@@ -23,7 +23,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': 'off',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
