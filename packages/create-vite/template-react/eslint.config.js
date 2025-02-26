650dd9c5d413d42fba5f28ca0b9ad02b19e98567
Bjorn Lu <bjornlu.dev@gmail.com>
2025-02-26 12:11:17 +0800

    chore: ignore _
    Co-authored-by: Arnaud Barré <arnaud.barre@carbometrix.com>

--- packages/create-vite/template-react/eslint.config.js
+++ packages/create-vite/template-react/eslint.config.js
@@ -23,7 +23,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
