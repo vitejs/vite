17bd3e4e1413f305f1852d8973d527fc5d0516f9
翠 / green <green@sapphi.red>
2025-03-07 16:37:09 +0900

    fix(css): inline css correctly for double quote use strict (#19590)
    Co-authored-by: underfin <likui.underfin@gmail.com>
    Co-authored-by: patak <583075+patak-dev@users.noreply.github.com>

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -939,12 +939,19 @@ export function cssPostPlugin(config: ResolvedConfig): Plugin {
               `document.head.appendChild(${style});`
             let injectionPoint
             const wrapIdx = code.indexOf('System.register')
+            const singleQuoteUseStrict = `'use strict';`
+            const doubleQuoteUseStrict = `"use strict";`
             if (wrapIdx >= 0) {
               const executeFnStart = code.indexOf('execute:', wrapIdx)
               injectionPoint = code.indexOf('{', executeFnStart) + 1
+            } else if (code.includes(singleQuoteUseStrict)) {
+              injectionPoint =
+                code.indexOf(singleQuoteUseStrict) + singleQuoteUseStrict.length
+            } else if (code.includes(doubleQuoteUseStrict)) {
+              injectionPoint =
+                code.indexOf(doubleQuoteUseStrict) + doubleQuoteUseStrict.length
             } else {
-              const insertMark = "'use strict';"
-              injectionPoint = code.indexOf(insertMark) + insertMark.length
+              throw new Error('Injection point for inlined CSS not found')
             }
             s ||= new MagicString(code)
             s.appendRight(injectionPoint, injectCode)
