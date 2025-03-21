87a814cc60c89c90b1fc19a22e90a6f169772b3f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-03-20 23:14:35 -0400

    Update packages/create-vite/template-ember-ts/src/app.ts

--- packages/create-vite/template-ember-ts/src/app.ts
+++ packages/create-vite/template-ember-ts/src/app.ts
@@ -2,7 +2,7 @@ import Application from '@ember/application'
 import Resolver from 'ember-resolver'
 import config from '#config'
 
-import { registry } from './registry.js'
+import { registry } from './registry.ts'
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix
