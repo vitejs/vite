04d58b42ae69547f04ef8fcd574b1ee1b654dc32
翠 / green <green@sapphi.red>
2025-03-25 19:16:55 +0900

    feat: implement hook filters (#19602)

--- packages/vite/src/node/plugins/worker.ts
+++ packages/vite/src/node/plugins/worker.ts
@@ -15,7 +15,7 @@ import {
 import {
   BuildEnvironment,
   createToImportMetaURLBasedRelativeRuntime,
-  injectEnvironmentToHooks,
+  injectEnvironmentAndFilterToHooks,
   onRollupLog,
   toOutputFilePathInJS,
 } from '../build'
@@ -95,7 +95,7 @@ async function bundleWorkerEntry(
     ...rollupOptions,
     input,
     plugins: workerEnvironment.plugins.map((p) =>
-      injectEnvironmentToHooks(workerEnvironment, p),
+      injectEnvironmentAndFilterToHooks(workerEnvironment, p),
     ),
     onLog(level, log) {
       onRollupLog(level, log, workerEnvironment)
