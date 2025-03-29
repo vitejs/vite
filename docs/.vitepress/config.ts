c9b010b819a487b685e492f69a1a92848ff741b2
Okinea Dev <hi@okinea.dev>
2025-03-29 11:08:20 +0100

    update plugin and configuration

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -469,9 +469,8 @@ export default defineConfig({
       }),
       llmstxt({
         ignoreFiles: ['blog/*', 'blog.md', 'index.md', 'team.md'],
-        customTemplateVariables: {
-          description: 'The Build Tool for the Web',
-          details: `\
+        description: 'The Build Tool for the Web',
+        details: `\
 - 💡 Instant Server Start
 - ⚡️ Lightning Fast HMR
 - 🛠️ Rich Features
@@ -486,7 +485,6 @@ Vite is a new breed of frontend build tooling that significantly improves the fr
 - A [build command](https://vite.dev/guide/build.md) that bundles your code with [Rollup](https://rollupjs.org), pre-configured to output highly optimized static assets for production.
 
 In addition, Vite is highly extensible via its [Plugin API](https://vite.dev/guide/api-plugin.md) and [JavaScript API](https://vite.dev/guide/api-javascript.md) with full typing support.`
-        }
       }) as PluginOption,
     ],
     optimizeDeps: {
