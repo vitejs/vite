668ff5e9fe4fd66d30ff437440372cd9b8781128
Jimmy Leung <43258070+hkleungai@users.noreply.github.com>
2025-03-30 03:12:29 +0800

    feat: add vite-plugin-checker to template

--- packages/create-vite/template-react-ts/src/App.tsx
+++ packages/create-vite/template-react-ts/src/App.tsx
@@ -24,6 +24,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+          Uncomment <code>vite-plugin-checker</code> codes in{' '}
+          <code>vite.config.ts</code> to enable error overlay.
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
