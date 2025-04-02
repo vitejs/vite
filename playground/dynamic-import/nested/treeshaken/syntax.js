b049635719c5da3307743852d0e09f82fc97ce41
Hiroki Osame <hiroki.osame@gmail.com>
2025-04-02 19:04:54 +0900

    failing test for #19695

--- playground/dynamic-import/nested/treeshaken/syntax.js
+++ playground/dynamic-import/nested/treeshaken/syntax.js
@@ -1,6 +1,11 @@
 export const foo = () => {
   console.log('treeshaken syntax foo')
 }
+export const bar = {
+  log: () => {
+    console.log('treeshaken syntax bar')
+  },
+}
 export default () => {
   console.log('treeshaken syntax default')
 }
