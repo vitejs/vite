f0866633aa99cfbf86746e5d1d50d3913b1a9f90
翠 / green <green@sapphi.red>
2025-04-30 20:32:12 +0900

    feat(types): make exposed `Rollup` type more compatible with Rollup (#145)

--- eslint.config.js
+++ eslint.config.js
@@ -98,6 +98,7 @@ export default tseslint.config(
           allowModules: [
             'vite',
             'esbuild',
+            'rolldown',
             'less',
             'sass',
             'sass-embedded',
