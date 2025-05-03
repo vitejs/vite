847806855757cbf386f6b325691ea157ae513d7f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-05-03 15:08:42 +0900

    fix(ssr)!: don't access `Object` variable in module scope

--- packages/vite/src/module-runner/esmEvaluator.ts
+++ packages/vite/src/module-runner/esmEvaluator.ts
@@ -5,6 +5,7 @@ import {
 import {
   ssrDynamicImportKey,
   ssrExportAllKey,
+  ssrExportNameKey,
   ssrImportKey,
   ssrImportMetaKey,
   ssrModuleExportsKey,
@@ -25,6 +26,7 @@ export class ESModulesEvaluator implements ModuleEvaluator {
       ssrImportKey,
       ssrDynamicImportKey,
       ssrExportAllKey,
+      ssrExportNameKey,
       // source map should already be inlined by Vite
       '"use strict";' + code,
     )
@@ -35,6 +37,7 @@ export class ESModulesEvaluator implements ModuleEvaluator {
       context[ssrImportKey],
       context[ssrDynamicImportKey],
       context[ssrExportAllKey],
+      context[ssrExportNameKey],
     )
 
     Object.seal(context[ssrModuleExportsKey])
