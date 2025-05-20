121e2404197ec873f6cfb34d23994d306fed8295
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-05-20 18:00:40 +0900

    chore: fix import extension for node24

--- packages/create-vite/tsdown.config.ts
+++ packages/create-vite/tsdown.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 import url from 'node:url'
 import { defineConfig } from 'tsdown'
-import licensePlugin from '../vite/rollupLicensePlugin'
+import licensePlugin from '../vite/rollupLicensePlugin.ts'
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
