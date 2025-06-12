e2e1f11651c4e1ec59eaaebdcc78c6db1c02611e
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-06-12 12:24:45 +0900

    chore: align require polyfill implementation with the rolldown docs

--- playground/external/src/require-polyfill.js
+++ playground/external/src/require-polyfill.js
@@ -1,7 +1,8 @@
 import * as vue from 'vue'
 import slash3 from 'slash3'
-globalThis.require = (dep) => {
-  if (dep === 'vue') return vue
-  if (dep === 'slash3') return slash3
-  throw new Error(`Cannot require "${dep}"`)
+
+export default (id) => {
+  if (id === 'vue') return vue
+  if (id === 'slash3') return slash3
+  throw new Error(`Cannot require "${id}"`)
 }
