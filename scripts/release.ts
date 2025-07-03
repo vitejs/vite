7af3c53187898647e8c0ba0d31a438d57041f73c
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-03 16:02:56 +0900

    refactor: use function from `@vitejs/release-scripts`

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,11 +1,5 @@
-import fs from 'node:fs'
-import { release } from '@vitejs/release-scripts'
+import { generateChangelog, release } from '@vitejs/release-scripts'
 import colors from 'picocolors'
-import { ConventionalChangelog, type Preset } from 'conventional-changelog'
-import createPreset, {
-  DEFAULT_COMMIT_TYPES,
-  // @ts-expect-error no types
-} from 'conventional-changelog-conventionalcommits'
 import { logRecentCommits, updateTemplateVersions } from './releaseUtils'
 
 release({
@@ -19,55 +13,9 @@ release({
 
     console.log(colors.cyan('\nGenerating changelog...'))
 
-    const preset: Preset = await createPreset({
-      types: DEFAULT_COMMIT_TYPES.map((t: any) => ({ ...t, hidden: false })),
+    await generateChangelog({
+      getPkgDir: () => `packages/${pkgName}`,
+      tagPrefix: pkgName === 'vite' ? undefined : `${pkgName}@`,
     })
-    preset.writer ??= {}
-    preset.writer.headerPartial = `
-## {{#if isPatch~}} <small> {{~/if~}}
-{{#if @root.linkCompare~}}
-  [{{version}}](
-  {{~#if @root.repository~}}
-    {{~#if @root.host}}
-      {{~@root.host}}/
-    {{~/if}}
-    {{~#if @root.owner}}
-      {{~@root.owner}}/
-    {{~/if}}
-    {{~@root.repository}}
-  {{~else}}
-    {{~@root.repoUrl}}
-  {{~/if~}}
-  /compare/{{previousTag}}...{{currentTag}})
-{{~else}}
-  {{~version}}
-{{~/if}}
-{{~#if title}} "{{title}}"
-{{~/if}}
-{{~#if date}} ({{date}})
-{{~/if}}
-{{~#if isPatch~}} </small> {{~/if}}
-`.trim()
-    preset.writer.mainTemplate! += '\n'
-
-    const generator = new ConventionalChangelog()
-      .readPackage(`packages/${pkgName}/package.json`)
-      .config(preset)
-      .options({ releaseCount: 1 })
-      .commits({ path: `packages/${pkgName}` })
-    if (pkgName !== 'vite') {
-      generator.tags({ prefix: `${pkgName}@` })
-    }
-
-    const originalChangelog = fs.readFileSync(
-      `packages/${pkgName}/CHANGELOG.md`,
-      'utf-8',
-    )
-
-    const writeStream = fs.createWriteStream(`packages/${pkgName}/CHANGELOG.md`)
-    for await (const chunk of generator.write()) {
-      writeStream.write(chunk)
-    }
-    writeStream.write(originalChangelog)
   },
 })
