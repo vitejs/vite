9d34c4cd8d40d4428cf31f911d28eb4c3c283146
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-07-15 22:38:41 +0900

    perf: avoid buildStart hook call

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -1000,10 +1000,12 @@ export async function _createServer(
     if (initingServer) return initingServer
 
     initingServer = (async function () {
-      // For backward compatibility, we call buildStart for the client
-      // environment when initing the server. For other environments
-      // buildStart will be called when the first request is transformed
-      await environments.client.pluginContainer.buildStart()
+      if (!config.experimental.fullBundleMode) {
+        // For backward compatibility, we call buildStart for the client
+        // environment when initing the server. For other environments
+        // buildStart will be called when the first request is transformed
+        await environments.client.pluginContainer.buildStart()
+      }
 
       // ensure ws server started
       if (onListen || options.listen) {
