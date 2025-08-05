e6799fcc1c3527e60e02bc2e0bb2a0000a5d4d6b
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-05 13:21:48 +0900

    refactor: use `String().length`

--- packages/vite/src/node/utils.ts
+++ packages/vite/src/node/utils.ts
@@ -515,7 +515,7 @@ export function generateCodeFrame(
     end !== undefined
       ? numberToPos(source, end).line
       : numberToPos(source, start).line + range
-  const lineNumberWidth = Math.max(3, Math.floor(Math.log10(lastPosLine)) + 1)
+  const lineNumberWidth = Math.max(3, String(lastPosLine).length)
   const lines = source.split(splitRE)
   let count = 0
   const res: string[] = []
