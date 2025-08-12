b90eba57b78dd43b3dfb9b6f862f9faeb2a719bc
Kendell R <KTibow@users.noreply.github.com>
2025-08-11 14:59:25 -1000

    fix(client): add `[vite]` prefixes to debug logs (#20595)

--- packages/vite/src/client/client.ts
+++ packages/vite/src/client/client.ts
@@ -402,17 +402,17 @@ function pingWorkerContentMain(socketUrl: string) {
     port.addEventListener('message', (event) => {
       const { visibility } = event.data
       visibilityManager.currentState = visibility
-      console.debug('new window visibility', visibility)
+      console.debug('[vite] new window visibility', visibility)
       for (const listener of visibilityManager.listeners) {
         listener(visibility)
       }
     })
     port.start()
 
-    console.debug('connected from window')
+    console.debug('[vite] connected from window')
     waitForSuccessfulPingInternal(socketUrl, visibilityManager).then(
       () => {
-        console.debug('ping successful')
+        console.debug('[vite] ping successful')
         try {
           port.postMessage({ type: 'success' })
         } catch (error) {
@@ -420,7 +420,7 @@ function pingWorkerContentMain(socketUrl: string) {
         }
       },
       (error) => {
-        console.debug('error happened', error)
+        console.debug('[vite] error happened', error)
         try {
           port.postMessage({ type: 'error', error })
         } catch (error) {
