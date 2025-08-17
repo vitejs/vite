c0112b1e0937e61296d66ff16014f03bb40dd0ee
unknown <aliopdandan@gmail.com>
2025-08-17 13:17:24 +0100

    f

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,35 +1,17 @@
-import path from 'node:path'
-import url from 'node:url'
-import { defineConfig } from 'vitest/config'
-
-const _dirname = path.dirname(url.fileURLToPath(import.meta.url))
+import { defineConfig } from "vite";
+import path from "node:path";
 
 export default defineConfig({
-  test: {
-    include: ['**/__tests__/**/*.spec.[tj]s'],
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      './playground/**/*.*',
-      './playground-temp/**/*.*',
-    ],
-    deps: {
-      // we specify 'packages' so Vitest doesn't inline the files
-      moduleDirectories: ['node_modules', 'packages'],
-    },
-    testTimeout: 20000,
-    isolate: false,
-  },
-  esbuild: {
-    target: 'node20',
-  },
-  publicDir: false,
   resolve: {
-    alias: {
-      'vite/module-runner': path.resolve(
-        _dirname,
-        './packages/vite/src/module-runner/index.ts',
-      ),
-    },
+    alias: [
+      {
+        find: "$okay",
+        replacement: path.resolve(__dirname, "src"),
+      },
+      {
+        find: "$broke",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
   },
-})
+});
