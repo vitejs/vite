8b1008079c02ef6883c683f832ca4c10cc3f32d0
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-18 20:45:03 +0900

    feat: generate code frame for parse errors thrown by terser

--- packages/vite/src/node/plugins/terser.ts
+++ packages/vite/src/node/plugins/terser.ts
@@ -5,7 +5,7 @@ import type {
 import { WorkerWithFallback } from 'artichokie'
 import type { Plugin } from '../plugin'
 import type { ResolvedConfig } from '..'
-import { requireResolveFromRootWithFallback } from '../utils'
+import { generateCodeFrame, requireResolveFromRootWithFallback } from '../utils'
 
 export interface TerserOptions extends TerserMinifyOptions {
   /**
@@ -47,10 +47,16 @@ export function terserPlugin(config: ResolvedConfig): Plugin {
           code: string,
           options: TerserMinifyOptions,
         ) => {
-          // test fails when using `import`. maybe related: https://github.com/nodejs/node/issues/43205
-          // eslint-disable-next-line no-restricted-globals -- this function runs inside cjs
-          const terser = require(terserPath)
-          return terser.minify(code, options) as TerserMinifyOutput
+          try {
+            // test fails when using `import`. maybe related: https://github.com/nodejs/node/issues/43205
+            // eslint-disable-next-line no-restricted-globals -- this function runs inside cjs
+            const terser = require(terserPath)
+            return (await terser.minify(code, options)) as TerserMinifyOutput
+          } catch (e) {
+            // convert to a plain object as additional properties of Error instances are not
+            // sent back to the main thread
+            throw { ...e }
+          }
         },
       {
         shouldUseFake(_terserPath, _code, options) {
@@ -78,7 +84,7 @@ export function terserPlugin(config: ResolvedConfig): Plugin {
       return !!environment.config.build.minify
     },
 
-    async renderChunk(code, _chunk, outputOptions) {
+    async renderChunk(code, chunk, outputOptions) {
       // This plugin is included for any non-false value of config.build.minify,
       // so that normal chunks can use the preferred minifier, and legacy chunks
       // can use terser.
@@ -100,16 +106,26 @@ export function terserPlugin(config: ResolvedConfig): Plugin {
       worker ||= makeWorker()
 
       const terserPath = loadTerserPath(config.root)
-      const res = await worker.run(terserPath, code, {
-        safari10: true,
-        ...terserOptions,
-        sourceMap: !!outputOptions.sourcemap,
-        module: outputOptions.format.startsWith('es'),
-        toplevel: outputOptions.format === 'cjs',
-      })
-      return {
-        code: res.code!,
-        map: res.map as any,
+      try {
+        const res = await worker.run(terserPath, code, {
+          safari10: true,
+          ...terserOptions,
+          sourceMap: !!outputOptions.sourcemap,
+          module: outputOptions.format.startsWith('es'),
+          toplevel: outputOptions.format === 'cjs',
+        })
+        return {
+          code: res.code!,
+          map: res.map as any,
+        }
+      } catch (e) {
+        e.loc = {
+          file: chunk.fileName,
+          line: e.line,
+          column: e.col,
+        }
+        e.frame = generateCodeFrame(code, e.pos)
+        throw e
       }
     },
 
