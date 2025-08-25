6e672e7a0a1df07be8c63c25fd06f0260fe44c7c
unknown <jthompsonofficial0376@gmail.com>
2025-08-25 15:41:18 -0500

    This allows the create vite template , to be PWA compatible. Pulled from CRA / CLI

--- packages/create-vite/template-react/vite.config.js
+++ packages/create-vite/template-react/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-
 export default defineConfig({
   base: './', // Create a base for dist files 
   plugins: [react()],
