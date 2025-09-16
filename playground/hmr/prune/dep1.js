3e3c0c4520aba21a5823fd6008dbbeac6f0b90bf
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-16 15:25:03 +0900

    chore: comment

--- playground/hmr/prune/dep1.js
+++ playground/hmr/prune/dep1.js
@@ -1,6 +1,7 @@
 import './dep2.js'
 
-// TODO: currently we need one more `import` in this module
+// TODO: https://github.com/vitejs/vite/issues/20781
+// currently we need one more `import` in this module
 // to trigger prune for depending module `dep2.js` since
 // the prune event logic is skipped when `es-module-lexer`
 // detects `imports.length === 0`
