35e4af2ab7ed941c8c08106c21382a7259f1fcaf
翠 <green@sapphi.red>
2025-10-03 12:28:30 +0900

    test: add test that ensures syntaxes not supported are lowered (#20886)

--- playground/build-old/__tests__/build-old.spec.ts
+++ playground/build-old/__tests__/build-old.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest'
-import { page } from '~utils'
+import { findAssetFile, isBuild, page } from '~utils'
 
 describe('syntax preserve', () => {
   test('import.meta.url', async () => {
@@ -7,5 +7,16 @@ describe('syntax preserve', () => {
   })
   test('dynamic import', async () => {
     expect(await page.textContent('.dynamic-import')).toBe('success')
+  })
+})
+
+describe('syntax is lowered', () => {
+  test('private field', async () => {
+    expect(await page.textContent('.private-field')).toBe('private')
+
+    if (isBuild) {
+      const content = findAssetFile(/index-[-\w]{8}\.js/)
+      expect(content).not.toMatch(/this\.#\w+/)
+    }
   })
 })
