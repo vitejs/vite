6c77f2a477c2c33f9b8b2730afb0243af5931032
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-10-07 20:26:21 +0900

    test: use `expect.poll` in build-old playground

--- playground/build-old/__tests__/build-old.spec.ts
+++ playground/build-old/__tests__/build-old.spec.ts
@@ -3,16 +3,16 @@ import { findAssetFile, isBuild, page } from '~utils'
 
 describe('syntax preserve', () => {
   test('import.meta.url', async () => {
-    expect(await page.textContent('.import-meta-url')).toBe('string')
+    await expect.poll(() => page.textContent('.import-meta-url')).toBe('string')
   })
   test('dynamic import', async () => {
-    expect(await page.textContent('.dynamic-import')).toBe('success')
+    await expect.poll(() => page.textContent('.dynamic-import')).toBe('success')
   })
 })
 
 describe('syntax is lowered', () => {
   test('private field', async () => {
-    expect(await page.textContent('.private-field')).toBe('private')
+    await expect.poll(() => page.textContent('.private-field')).toBe('private')
 
     if (isBuild) {
       const content = findAssetFile(/index-[-\w]{8}\.js/)
