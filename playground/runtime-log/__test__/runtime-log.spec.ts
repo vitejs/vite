13958cf9e73a7f6501daacef7664dd7f8362ea69
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-10-10 00:53:12 +0900

    test: add test

---
+++ playground/runtime-log/__test__/runtime-log.spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from 'vitest'
+import { isServe, page, serverLogs } from '~utils'
+
+test.runIf(isServe)('unhandled error', async () => {
+  await page.click('#test-error')
+  await expect.poll(() => serverLogs.at(-1)).toEqual(`\
+[Unhandled error] Error: this is test error
+ > testError src/main.ts:20:8
+     18| 
+     19| function testError() {
+     20|   throw new Error('this is test error')
+       |        ^
+     21| }
+     22| 
+ > HTMLButtonElement.<anonymous> src/main.ts:6:2
+`)
+  console.log(serverLogs.at(-1))
+})
+
+test.runIf(isServe)('unhandled rejection', async () => {
+  await page.click('#test-unhandledrejection')
+  await expect.poll(() => serverLogs.at(-1)).toEqual(`\
+[Unhandled error] Error: this is test unhandledrejection
+ > testUnhandledRejection src/main.ts:24:8
+     22| 
+     23| async function testUnhandledRejection() {
+     24|   throw new Error('this is test unhandledrejection')
+       |        ^
+     25| }
+     26| 
+ > HTMLButtonElement.<anonymous> src/main.ts:12:4
+`)
+})
