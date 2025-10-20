593d6c244e246083d49410bcfb6db8afd03d0900
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-10-20 13:56:02 +0900

    fix(dev): trim trailing slash before `server.fs.deny` check

--- playground/fs-serve/__tests__/fs-serve.spec.ts
+++ playground/fs-serve/__tests__/fs-serve.spec.ts
@@ -538,6 +538,19 @@ describe.runIf(isServe)('invalid request', () => {
     expect(response).toContain('HTTP/1.1 403 Forbidden')
   })
 
+  test('should deny request to denied file when a request ends with \\', async () => {
+    const response = await sendRawRequest(viteTestUrl, '/src/.env\\')
+    expect(response).toContain('HTTP/1.1 403 Forbidden')
+  })
+
+  test('should deny request to denied file when a request ends with \\ with /@fs/', async () => {
+    const response = await sendRawRequest(
+      viteTestUrl,
+      path.posix.join('/@fs/', root, 'root/src/.env') + '\\',
+    )
+    expect(response).toContain('HTTP/1.1 403 Forbidden')
+  })
+
   test('should deny request with /@fs/ to denied file when a request has /.', async () => {
     const response = await sendRawRequest(
       viteTestUrl,
