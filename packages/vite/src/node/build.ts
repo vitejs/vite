d6362207ba817a210cbcea1253a174542a6c8c68
翠 <green@sapphi.red>
2025-10-22 17:07:59 +0900

    perf: run `dev.createEnvironment` and `build.createEnvironment` concurrently (#20699)

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -1612,6 +1612,7 @@ export async function createBuilder(
   if (useLegacyBuilder) {
     await setupEnvironment(config.build.ssr ? 'ssr' : 'client', config)
   } else {
+    const environmentConfigs: [string, ResolvedConfig][] = []
     for (const environmentName of Object.keys(config.environments)) {
       // We need to resolve the config again so we can properly merge options
       // and get a new set of plugins for each build environment. The ecosystem
@@ -1654,9 +1655,14 @@ export async function createBuilder(
           patchPlugins,
         )
       }
-
-      await setupEnvironment(environmentName, environmentConfig)
+      environmentConfigs.push([environmentName, environmentConfig])
     }
+    await Promise.all(
+      environmentConfigs.map(
+        async ([environmentName, environmentConfig]) =>
+          await setupEnvironment(environmentName, environmentConfig),
+      ),
+    )
   }
 
   return builder
