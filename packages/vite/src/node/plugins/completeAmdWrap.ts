f7d61e2fb66899f4808939be74978244ff46d893
Dominik Schmidt <dev@dominik-schmidt.de>
2025-10-24 07:03:29 +0200

    fix(build): ensure amd bundles request `require` to be injected (#20861)
    Co-authored-by: sapphi-red <49056869+sapphi-red@users.noreply.github.com>
    Co-authored-by: Bjorn Lu <bjornlu.dev@gmail.com>

---
+++ packages/vite/src/node/plugins/completeAmdWrap.ts
@@ -0,0 +1,30 @@
+import type { Plugin } from '../plugin'
+
+/**
+ * ensure amd bundles request `require` to be injected
+ */
+export function completeAmdWrapPlugin(): Plugin {
+  const AmdWrapRE =
+    /\bdefine\((?:\s*\[([^\]]*)\],)?\s*(?:\(\s*)?function\s*\(([^)]*)\)\s*\{/g
+
+  return {
+    name: 'vite:force-amd-wrap-require',
+    renderChunk(code, _chunk, opts) {
+      if (opts.format !== 'amd') return
+
+      return {
+        code: code.replace(AmdWrapRE, (_, deps, params) => {
+          if (deps?.includes(`"require"`) || deps?.includes(`'require'`)) {
+            return `define([${deps}], (function(${params}) {`
+          }
+
+          const newDeps = deps ? `"require", ${deps}` : '"require"'
+          const newParams = params.trim() ? `require, ${params}` : 'require'
+
+          return `define([${newDeps}], (function(${newParams}) {`
+        }),
+        map: null, // no need to generate sourcemap as no mapping exists for the wrapper
+      }
+    },
+  }
+}
