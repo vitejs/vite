1409c63fa1acefd679158999b79c8289a27181d5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-10-28 22:40:39 -0400

    Update packages/create-vite/src/index.ts
    Co-authored-by: 翠 <green@sapphi.red>

--- packages/create-vite/src/index.ts
+++ packages/create-vite/src/index.ts
@@ -293,7 +293,7 @@ const FRAMEWORKS: Framework[] = [
     variants: [
       {
         name: 'ember-app',
-        display: 'Production-ready App',
+        display: 'Production-ready App ↗',
         color: redBright,
         customCommand:
           'npx ember-cli@latest new TARGET_DIR --blueprint @ember/app-blueprint --pnpm --lang en',
