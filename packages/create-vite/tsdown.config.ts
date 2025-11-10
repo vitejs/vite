c92c98cc154ef9f6b2819d6ed75599797a6ba580
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-11-10 12:11:32 +0900

    chore: remove __dirname

--- packages/create-vite/tsdown.config.ts
+++ packages/create-vite/tsdown.config.ts
@@ -1,9 +1,6 @@
 import path from 'node:path'
-import url from 'node:url'
 import { defineConfig } from 'tsdown'
 import licensePlugin from '../vite/rollupLicensePlugin.ts'
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
 
 export default defineConfig(() => ({
   entry: ['src/index.ts'],
@@ -11,7 +8,7 @@ export default defineConfig(() => ({
   minify: true,
   plugins: [
     licensePlugin(
-      path.resolve(__dirname, './LICENSE'),
+      path.resolve(import.meta.dirname, './LICENSE'),
       'create-vite license',
       'create-vite',
       '# License of the files in the directories starting with "template-" in create-vite\n' +
