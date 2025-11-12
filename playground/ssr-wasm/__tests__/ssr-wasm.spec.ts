11375b18596a0ac5b288b722626df1bd137f98e0
Xidorn Quan <me@upsuper.org>
2025-11-12 13:34:16 +1100

    add build tests to verify assets

--- playground/ssr-wasm/__tests__/ssr-wasm.spec.ts
+++ playground/ssr-wasm/__tests__/ssr-wasm.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import { port } from './serve'
-import { page } from '~utils'
+import { findAssetFile, isBuild, listAssets, page } from '~utils'
 
 const url = `http://localhost:${port}`
 
@@ -13,3 +13,25 @@ test('should work when output', async () => {
   await page.goto(`${url}/static-heavy`)
   expect(await page.textContent('.static-heavy')).toMatch('24')
 })
+
+test.runIf(isBuild)('should not contain wasm file when inlined', async () => {
+  const assets = await listAssets()
+  const lightWasm = assets.find((f) => /light-.+\.wasm$/.test(f))
+  expect(lightWasm).toBeUndefined()
+
+  const staticLight = await findAssetFile(/^static-light-.+\.js$/)
+  expect(staticLight).toContain('data:application/wasm;base64,')
+})
+
+test.runIf(isBuild)(
+  'should contain and reference wasm file when output',
+  async () => {
+    const assets = await listAssets()
+    const heavyWasm = assets.find((f) => /heavy-.+\.wasm$/.test(f))
+    expect(heavyWasm).toBeDefined()
+
+    const staticHeavy = await findAssetFile(/^static-heavy-.+\.js$/)
+    expect(staticHeavy).toContain(heavyWasm)
+    expect(staticHeavy).not.toContain('data:application/wasm;base64,')
+  },
+)
