5903a1d68f2002b84ecbebf6b755ff309f2b186c
Rudransh Gupta <rudranshgupta@Rudranshs-MacBook-Pro.local>
2025-11-17 19:09:46 +0530

    fix(docs): open language switcher links in same tab (#21112)
    fix

--- docs/env.d.ts
+++ docs/env.d.ts
@@ -3,13 +3,18 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-
-  // Use Record<string, any> to avoid lint errors on '{}'
   const component: DefineComponent<
     Record<string, any>,
     Record<string, any>,
     any
   >
-
   export default component
+}
+
+declare module 'vitepress/dist/client/theme-default/composables/langs.js' {
+  export function useLangs(options?: any): any
+}
+
+declare module 'vitepress/dist/client/shared.js' {
+  export function isExternal(path: string): boolean
 }
