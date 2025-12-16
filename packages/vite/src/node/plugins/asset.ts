7a3318f31f4e7b23f1c74b7444ec61279dfc9bdb
翠 <green@sapphi.red>
2025-12-16 18:46:34 +0900

    fix: unreachable error when building with `experimental.bundledDev` is enabled (#21296)

--- packages/vite/src/node/plugins/asset.ts
+++ packages/vite/src/node/plugins/asset.ts
@@ -468,7 +468,10 @@ async function fileToBuiltUrl(
       postfix = postfix.replace(noInlineRE, '').replace(/^&/, '?')
     }
 
-    if (environment.config.experimental.bundledDev) {
+    if (
+      environment.config.command === 'serve' &&
+      environment.config.experimental.bundledDev
+    ) {
       const outputFilename = pluginContext.getFileName(referenceId)
       const outputUrl = toOutputFilePathInJS(
         environment,
