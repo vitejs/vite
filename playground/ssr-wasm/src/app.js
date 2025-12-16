80dac7494b06e83d414b2a9e3515bbd0cfeb0ed6
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-12-16 17:17:58 +0900

    chore: add index page

--- playground/ssr-wasm/src/app.js
+++ playground/ssr-wasm/src/app.js
@@ -1,5 +1,9 @@
 export async function render(url) {
   switch (url) {
+    case '/':
+      return `<ul>${['/static-light', '/static-heavy']
+        .map((name) => `<li><a href="${name}">${name}</a></li>`)
+        .join('')}</ul>`
     case '/static-light':
       return (await import('./static-light')).render()
     case '/static-heavy':
