2124258ce1df1a31ba3a88d9f932a23033f22448
ooyyh <laowan345@gmail.com>
2026-01-06 21:17:58 +0800

    fix: Creating a project using Marko doesn't work

--- packages/create-vite/src/index.ts
+++ packages/create-vite/src/index.ts
@@ -613,7 +613,8 @@ async function init() {
   }
 
   const root = path.join(cwd, targetDir)
-  fs.mkdirSync(root, { recursive: true })
+  // create target directory
+  // fs.mkdirSync(root, { recursive: true })
 
   // determine template
   let isReactSwc = false
@@ -643,7 +644,8 @@ async function init() {
     })
     process.exit(status ?? 0)
   }
-
+  // Only create directory for built-in templates, not for customCommand
+  fs.mkdirSync(root, { recursive: true })
   prompts.log.step(`Scaffolding project in ${root}...`)
 
   const templateDir = path.resolve(
