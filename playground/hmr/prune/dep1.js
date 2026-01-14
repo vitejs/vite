983c9be66ac180eb3b8ae51a924e135b044cbd05
Alex <me@almadoro.dev>
2026-01-14 09:39:45 +0100

    fix(hmr): trigger prune event when last import is removed (#20781) (#21093)

--- playground/hmr/prune/dep1.js
+++ playground/hmr/prune/dep1.js
@@ -1,8 +1,2 @@
 import './dep2.js'
-
-// TODO: https://github.com/vitejs/vite/issues/20781
-// currently we need one more `import` in this module
-// to trigger prune for depending module `dep2.js` since
-// the prune event logic is skipped when `es-module-lexer`
-// detects `imports.length === 0`
-import './dep2-other.js'
+import './dep3.js'
