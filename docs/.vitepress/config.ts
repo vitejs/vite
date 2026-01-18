603dd91ae9754f67f96814b1ade9413bf035d2f4
yuyinws <lyc657508366@gmail.com>
2026-01-18 20:08:43 +0800

    docs: update `vitepress-plugin-group-icons`

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,7 +6,6 @@ import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
 import {
   groupIconMdPlugin,
   groupIconVitePlugin,
-  localIconLoader,
 } from 'vitepress-plugin-group-icons'
 import llmstxt from 'vitepress-plugin-llms'
 import { markdownItImageSize } from 'markdown-it-image-size'
@@ -556,10 +555,6 @@ const config = defineConfig({
         customIcon: {
           firebase: 'vscode-icons:file-type-firebase',
           '.gitlab-ci.yml': 'vscode-icons:file-type-gitlab',
-          'vite.config': localIconLoader(
-            import.meta.url,
-            '../public/logo-without-border.svg',
-          ),
         },
       }),
       llmstxt({
