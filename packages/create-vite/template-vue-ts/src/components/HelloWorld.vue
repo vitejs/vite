12af93871e09be987413a3c26861a2053afbe999
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-01-26 19:51:21 +0900

    fix: path

--- packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
+++ packages/create-vite/template-vue-ts/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import viteLogo from './assets/vite.svg'
+import viteLogo from '../assets/vite.svg'
 import heroImg from '../assets/hero.png'
 import vueLogo from '../assets/vue.svg'
 
