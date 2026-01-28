ed94c4d4690cc3cf726c6460b08c0d71c6231d68
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-01-28 14:24:57 +0900

    chore: tweak

--- playground/tailwind-v3/tailwind.config.ts
+++ playground/tailwind-v3/tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'tailwindcss'
 
+// NOTE: tailwind does not seem to support `import.meta.dirname` on Node 20
+
 export default {
   content: [
     // Before editing this section, make sure no paths are matching with `/src/main.js`
     // Look https://github.com/vitejs/vite/pull/6959 for more details
-    import.meta.dirname + '/src/{components,views}/**/*.js',
-    import.meta.dirname + '/src/main.js',
+    __dirname + '/src/{components,views}/**/*.js',
+    __dirname + '/src/main.js',
   ],
   theme: {
     extend: {},
