2d3b96ca53aa926e2107e9f4d44b15ffe52272a1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-23 18:19:31 +0900

    experiment: enable by default

--- packages/vite/src/node/server/index.ts
+++ packages/vite/src/node/server/index.ts
@@ -269,6 +269,7 @@ export async function resolveForwardConsoleOptions(
   value: boolean | ForwardConsoleOptions | undefined,
 ): Promise<ResolvedForwardConsoleOptions> {
   const { isAgent } = await determineAgent()
+  value ??= true
   value ??= isAgent
 
   if (value === false) {
