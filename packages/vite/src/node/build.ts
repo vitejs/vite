eeeaf9a88683505667e4b4bd02c5fe27ad60f4e2
翠 <green@sapphi.red>
2026-03-03 16:38:54 +0900

    fix(lib): keep annotation comments for es output (#21740)

--- packages/vite/src/node/build.ts
+++ packages/vite/src/node/build.ts
@@ -741,7 +741,11 @@ export function resolveRolldownOptions(
         typeof output.comments === 'boolean'
           ? output.comments
           : {
-              annotation: !options.minify,
+              // Do not minify whitespace for ES lib output since that would remove
+              // pure annotations and break tree-shaking
+              annotation:
+                !options.minify ||
+                (libOptions && (format === 'es' || format === 'esm')),
               jsdoc: !options.minify,
               legal: !options.minify,
               ...output.comments,
