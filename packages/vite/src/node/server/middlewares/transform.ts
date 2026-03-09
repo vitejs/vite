e5278d912fac94bafda48688958ffb2eedc028ae
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-03-09 21:41:33 +0900

    fix: check `server.fs` after stripping query as well

--- packages/vite/src/node/server/middlewares/transform.ts
+++ packages/vite/src/node/server/middlewares/transform.ts
@@ -59,7 +59,10 @@ const svgRE = /\.svg\b/
 
 function isServerAccessDeniedForTransform(config: ResolvedConfig, id: string) {
   if (rawRE.test(id) || urlRE.test(id) || inlineRE.test(id) || svgRE.test(id)) {
-    return checkLoadingAccess(config, id) !== 'allowed'
+    return (
+      checkLoadingAccess(config, cleanUrl(id)) !== 'allowed' ||
+      checkLoadingAccess(config, id) !== 'allowed'
+    )
   }
   return false
 }
@@ -323,7 +326,13 @@ export function transformMiddleware(
       }
       if (e?.code === ERR_DENIED_ID) {
         const id: string = e.id
-        const servingAccessResult = checkLoadingAccess(server.config, id)
+        let servingAccessResult = checkLoadingAccess(
+          server.config,
+          cleanUrl(id),
+        )
+        if (servingAccessResult === 'allowed') {
+          servingAccessResult = checkLoadingAccess(server.config, id)
+        }
         if (servingAccessResult === 'denied') {
           respondWithAccessDenied(id, server, res)
           return true
