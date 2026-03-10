9cad7f0dede0d500d698a7a2c8112607cecb40cf
Alex Savelyev <91429106+alexdln@users.noreply.github.com>
2026-03-10 17:07:11 +0000

    feat: add live page to docs (#21782)

---
+++ docs/.vitepress/theme/live/Layout.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Hero from './Hero.vue'
+import Timer from './Timer.vue'
+import TimeoutSwitcher from './TimeoutSwitcher.vue'
+import VideoIframe from './VideoIframe.vue'
+import Events from './Events.vue'
+import Footer from '@components/oss/Footer.vue'
+</script>
+
+<template>
+  <TimeoutSwitcher at="2026-03-19T15:00:00.000Z">
+    <template #default>
+      <Hero />
+      <Timer at="2026-03-19T15:00:00.000Z" />
+    </template>
+    <template #timeout>
+      <VideoIframe />
+    </template>
+  </TimeoutSwitcher>
+  <Events />
+  <Footer
+    heading="Start building with Vite"
+    subheading="Prepare for a development environment that can finally keep pace with the speed of your mind."
+    button-text="Get Started"
+    button-link="/guide/"
+  />
+</template>
---
+++ docs/.vitepress/theme/live/VideoIframe.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper wrapper--ticks w-full border-nickel border-t md:divide-x">
+    <iframe
+      width="560"
+      height="315"
+      class="w-full h-auto max-h-[calc(100vh-5rem-var(--vp-banner-height,0px))] aspect-video"
+      src="https://www.youtube-nocookie.com/embed/bmWQqAKLgT4?si=hhgR4fwkRw9zQ0yx&amp;controls=0&amp;start=0&amp;disablekb=1&amp;rel=0&amp;autoplay=1"
+      title="YouTube video player"
+      frameborder="0"
+      allow="
+        accelerometer;
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture;
+        web-share;
+      "
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    />
+  </div>
+</template>
