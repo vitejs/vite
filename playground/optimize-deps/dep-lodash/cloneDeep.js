4ef4786d2f318082e854979e5b026116e2c5111f
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-04-08 16:26:08 +0900

    chore: fix

--- playground/optimize-deps/dep-lodash/cloneDeep.js
+++ playground/optimize-deps/dep-lodash/cloneDeep.js
@@ -1,37 +1,22 @@
-;(function () {
-  var freeExports =
-    typeof exports === 'object' && exports && !exports.nodeType && exports
+'use strict'
 
-  var freeModule =
-    freeExports &&
-    typeof module === 'object' &&
-    module &&
-    !module.nodeType &&
-    module
-
-  function cloneDeep(value) {
-    if (value == null || typeof value !== 'object') {
-      return value
-    }
-
-    if (Array.isArray(value)) {
-      return value.map(cloneDeep)
-    }
-
-    const cloned = {}
-    for (const key in value) {
-      if (Object.hasOwn(value, key)) {
-        cloned[key] = cloneDeep(value[key])
-      }
-    }
-    return cloned
+// Minimal cloneDeep implementation for testing
+function cloneDeep(value) {
+  if (value == null || typeof value !== 'object') {
+    return value
   }
 
-  const lodash = {}
-  lodash.cloneDeep = cloneDeep
-
-  if (freeModule) {
-    ;(freeModule.exports = lodash).lodash = lodash
-    freeExports.lodash = lodash
+  if (Array.isArray(value)) {
+    return value.map(cloneDeep)
   }
-}).call(this)
+
+  const cloned = {}
+  for (const key in value) {
+    if (Object.hasOwn(value, key)) {
+      cloned[key] = cloneDeep(value[key])
+    }
+  }
+  return cloned
+}
+
+module.exports = cloneDeep
--- playground/optimize-deps/dep-lodash/index.js
+++ playground/optimize-deps/dep-lodash/index.js
@@ -1,3 +1,37 @@
-module.exports = {
-  cloneDeep: require('./cloneDeep'),
-}
+;(function () {
+  var freeExports =
+    typeof exports === 'object' && exports && !exports.nodeType && exports
+
+  var freeModule =
+    freeExports &&
+    typeof module === 'object' &&
+    module &&
+    !module.nodeType &&
+    module
+
+  function cloneDeep(value) {
+    if (value == null || typeof value !== 'object') {
+      return value
+    }
+
+    if (Array.isArray(value)) {
+      return value.map(cloneDeep)
+    }
+
+    const cloned = {}
+    for (const key in value) {
+      if (Object.hasOwn(value, key)) {
+        cloned[key] = cloneDeep(value[key])
+      }
+    }
+    return cloned
+  }
+
+  const lodash = {}
+  lodash.cloneDeep = cloneDeep
+
+  if (freeModule) {
+    ;(freeModule.exports = lodash).lodash = lodash
+    freeExports.lodash = lodash
+  }
+}).call(this)
