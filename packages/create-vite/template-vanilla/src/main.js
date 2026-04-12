3a21c7c33296767c443eecf4ce0cd630f6d56ac0
nirajbajaj <bajajsniraj@gmail.com>
2026-04-12 19:41:57 +0530

    fix: add missing quotes around viteLogo src attribute

--- packages/create-vite/template-vanilla/src/main.js
+++ packages/create-vite/template-vanilla/src/main.js
@@ -9,7 +9,7 @@ document.querySelector('#app').innerHTML = `
   <div class="hero">
     <img src="${heroImg}" class="base" width="170" height="179">
     <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src=${viteLogo} class="vite" alt="Vite logo" />
+    <img src="${viteLogo}" class="vite" alt="Vite logo" />
   </div>
   <div>
     <h1>Get started</h1>
