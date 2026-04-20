2471d6227c947273e16d2d547d773298e5a0df9a
bluwy <bjornlu.dev@gmail.com>
2026-04-20 18:17:58 +0800

    refactor: enable `no-unsafe-enum-comparison`

--- eslint.config.js
+++ eslint.config.js
@@ -166,7 +166,6 @@ export default defineConfig(
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/non-nullable-type-assertion-style': 'off',
