62ef098572389e88e6a92d980a5f3712e0ca8601
dennisjclifford-gif <dennisjclifford@gmail.com>
2026-06-01 07:04:35 -0400

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,35 +1,7 @@
-import path from 'node:path'
-import url from 'node:url'
-import { defineConfig } from 'vitest/config'
-
-const _dirname = path.dirname(url.fileURLToPath(import.meta.url))
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  test: {
-    include: ['**/__tests__/**/*.spec.[tj]s'],
-    exclude: [
-      '**/node_modules/**',
-      '**/dist/**',
-      './playground/**/*.*',
-      './playground-temp/**/*.*',
-    ],
-    deps: {
-      // we specify 'packages' so Vitest doesn't inline the files
-      moduleDirectories: ['node_modules', 'packages'],
-    },
-    testTimeout: 20000,
-    isolate: false,
-  },
-  oxc: {
-    target: 'node20',
-  },
-  publicDir: false,
-  resolve: {
-    alias: {
-      'vite/module-runner': path.resolve(
-        _dirname,
-        './packages/vite/src/module-runner/index.ts',
-      ),
-    },
-  },
-})
+  plugins: [react(), tailwindcss()],
+});
