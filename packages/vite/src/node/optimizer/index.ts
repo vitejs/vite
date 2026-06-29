4f6eb9d79b18a10d101ac8188cf77c105b7a8ec6
Pieter Scheffers <pieter.scheffers@gmail.com>
2026-06-29 11:06:50 +0200

    feat: add aube-lock.yaml to lockfile formats

--- packages/vite/src/node/optimizer/index.ts
+++ packages/vite/src/node/optimizer/index.ts
@@ -1276,6 +1276,11 @@ const lockfileFormats = [
     checkPatchesDir: 'patches',
     manager: 'bun',
   },
+  {
+    path: 'aube-lock.yaml',
+    checkPatchesDir: false,
+    manager: 'aube',
+  },
 ].sort((_, { manager }) => {
   return process.env.npm_config_user_agent?.startsWith(manager) ? 1 : -1
 })
