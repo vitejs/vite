2dafd3bb7f8b265037e216f50afaa890ae2e2875
翠 <green@sapphi.red>
2026-06-30 18:43:06 +0900

    refactor(css): remove lightningcss null byte bug workaround (#22822)

--- packages/vite/src/node/plugins/css.ts
+++ packages/vite/src/node/plugins/css.ts
@@ -3221,9 +3221,7 @@ async function compileLightningCSS(
   modules?: Record<string, string>
 }> {
   const { config } = environment
-  // replace null byte as lightningcss treats that as a string terminator
-  // https://github.com/parcel-bundler/lightningcss/issues/874
-  const filename = removeDirectQuery(id).replaceAll('\0', NULL_BYTE_PLACEHOLDER)
+  const filename = removeDirectQuery(id)
 
   let res: LightningCssTransformAttributeResult | LightningCssTransformResult
   try {
