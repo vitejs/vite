c2228d14478b4551a37cec705eeeac9529acaaaf
andrew <andywangsy@gmail.com>
2026-08-03 15:29:02 +0800

    Update playground/css/__tests__/css.spec.ts

--- playground/css/__tests__/css.spec.ts
+++ playground/css/__tests__/css.spec.ts
@@ -6,8 +6,6 @@ import './tests'
 // the postcss pipeline
 test('postcss plugin that injects url() at OnceExit', async () => {
   await page.goto(viteTestUrl)
-  // under bundled dev the page can reload once right after navigation while
-  // the dev client settles, so wait for the element instead of a one-shot `$`
   const imported = await page.waitForSelector('.inject-url-once-exit')
   // url should be rebased against the injected source file
   expect(await getBg(imported)).toMatch(
