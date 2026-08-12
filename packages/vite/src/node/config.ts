888a1511b67d7a85216ffdc7bcb96d17bbfff1e4
Syed Sohail Hussain <sohailhussain.syed1992@gmail.com>
2026-08-12 19:00:53 -0400

    chore(config): add @deprecated tags to plugin hook utilities

--- packages/vite/src/node/config.ts
+++ packages/vite/src/node/config.ts
@@ -1091,9 +1091,15 @@ export function getDefaultEnvironmentOptions(
 }
 
 export interface PluginHookUtils {
+  /**
+   * @deprecated Use `config.environments` instead
+   */
   getSortedPlugins: <K extends keyof Plugin>(
     hookName: K,
   ) => PluginWithRequiredHook<K>[]
+  /**
+   * @deprecated Use `config.environments` instead
+   */
   getSortedPluginHooks: <K extends keyof Plugin>(
     hookName: K,
   ) => NonNullable<HookHandler<Plugin[K]>>[]
@@ -2178,7 +2184,6 @@ export async function resolveConfig(
   patchPlugins?.(resolvedPlugins)
   ;(resolved.plugins as Plugin[]) = resolvedPlugins
 
-  // TODO: Deprecate config.getSortedPlugins and config.getSortedPluginHooks
   Object.assign(resolved, createPluginHookUtils(resolved.plugins))
 
   // call configResolved hooks
