7fc76d8b40f68ab68980fc32c4182d12d5a262ae
samarthsinh2660 <rajuvala80@gmail.com>
2026-08-23 13:19:22 +0530

    fix(html): support .htm as html (fix #10997)

--- packages/vite/src/node/plugins/asset.ts
+++ packages/vite/src/node/plugins/asset.ts
@@ -36,6 +36,7 @@ import {
   encodeURIPath,
   getHash,
   injectQuery,
+  isHTMLRequest,
   joinUrlSegments,
   normalizePath,
   rawRE,
@@ -737,7 +738,7 @@ function shouldInline(
     if (buildPluginContext.getModuleInfo(id)?.isEntry) return false
   }
   if (forceInline !== undefined) return forceInline
-  if (file.endsWith('.html')) return false
+  if (isHTMLRequest(file)) return false
   // Don't inline SVG with fragments, as they are meant to be reused
   if (file.endsWith('.svg') && id.includes('#')) return false
   let limit: number
