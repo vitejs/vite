ee5d7e495f556016332256670c71fe49f121a120
btea <2356281422@qq.com>
2026-08-25 10:58:53 +0800

    chore: update

--- playground/ssr/src/circular-dep-init/module-b.js
+++ playground/ssr/src/circular-dep-init/module-b.js
@@ -1,7 +1,7 @@
 import { valueA } from './circular-dep-init'
 
 export const valueB = 'circ-dep-init-b'
-export const valueAB = [...valueA, ` ${valueB}`]
+export const valueAB = `${valueA} ${valueB}`
 
 export function getValueAB() {
   return valueAB
