8d6ae3dc91d0a157db212be7d656e1d9802b3085
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-09-03 20:44:54 +0900

    fix: optional type

--- packages/vite/types/internal/devtoolsOptions.d.ts
+++ packages/vite/types/internal/devtoolsOptions.d.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
 // @ts-ignore `@vitejs/devtools` may not be installed
-export type {
-  DevToolsConfig,
-  ResolvedDevToolsConfig,
-} from '@vitejs/devtools/config'
+import type * as DevToolsConfig from '@vitejs/devtools/config'
 
 /* eslint-enable @typescript-eslint/ban-ts-comment */
+
+export type DevToolsConfig = DevToolsConfig.DevToolsConfig
+export type ResolvedDevToolsConfig = DevToolsConfig.ResolvedDevToolsConfig
